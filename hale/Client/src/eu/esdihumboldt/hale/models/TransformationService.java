c6c2694e2030f0075d6538a76f988897e6179ba3
Thorsten Reitz <thorsten.reitz@igd.fraunhofer.de>
2009-05-18 11:37:22 +0000

    Initial version of interface to CST.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@1274 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/Client/src/eu/esdihumboldt/hale/models/TransformationService.java
@@ -0,0 +1,30 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                  01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+package eu.esdihumboldt.hale.models;
+
+import org.geotools.feature.FeatureCollection;
+
+import eu.esdihumboldt.modelrepository.abstractfc.mapping.Alignment;
+
+/**
+ * FIXME Add Type description.
+ * 
+ * @author Thorsten Reitz 
+ * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+ * @version $Id$ 
+ */
+public interface TransformationService 
+	extends UpdateService {
+	
+	public FeatureCollection transform(FeatureCollection fc, Alignment al);
+
+}
