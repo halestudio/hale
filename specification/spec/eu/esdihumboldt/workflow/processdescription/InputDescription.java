cf16141bb81d05e42997a6e37816f12d68cb86c0
Moses Gone <moses.gone@igd.fraunhofer.de>
2009-05-20 08:30:57 +0000

    Changed the return type of the getDatatype to String
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@1303 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- specification/spec/eu/esdihumboldt/workflow/processdescription/InputDescription.java
+++ specification/spec/eu/esdihumboldt/workflow/processdescription/InputDescription.java
@@ -7,8 +7,6 @@ import eu.esdihumboldt.workflow.transformer.domaintypes.DomainMetadata;
 
 public interface InputDescription extends Description {
 
-
-
     /**
      * Provides a metadata about this input
      * @return
