b2e1825684f871ea646ac01fd8cdc0847a0d6c6e
Thorsten Reitz <thorsten.reitz@igd.fraunhofer.de>
2009-05-20 13:13:46 +0000

    Added documentation to TransformationService.java.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@1324 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/Client/src/eu/esdihumboldt/hale/models/TransformationService.java
+++ hale/Client/src/eu/esdihumboldt/hale/models/TransformationService.java
@@ -12,11 +12,15 @@
 package eu.esdihumboldt.hale.models;
 
 import org.geotools.feature.FeatureCollection;
+import org.opengis.feature.Feature;
+import org.opengis.feature.type.FeatureType;
 
+import eu.esdihumboldt.goml.align.Cell;
 import eu.esdihumboldt.modelrepository.abstractfc.mapping.Alignment;
 
 /**
- * FIXME Add Type description.
+ * This is the interface used by HALE to access schema transformation 
+ * capabilities.
  * 
  * @author Thorsten Reitz 
  * @partner 01 / Fraunhofer Institute for Computer Graphics Research
@@ -25,6 +29,28 @@ import eu.esdihumboldt.modelrepository.abstractfc.mapping.Alignment;
 public interface TransformationService 
 	extends UpdateService {
 	
-	public FeatureCollection transform(FeatureCollection fc, Alignment al);
+	/**
+	 * This method can be employed to transform an entire data set using an
+	 * entire mapping.
+	 * @param fc a {@link FeatureCollection} with {@link Feature}s to be 
+	 * transformed, with source {@link FeatureType}s.
+	 * @param al the {@link Alignment} to be applied
+	 * @return a {@link FeatureCollection} with the transformed Features, with 
+	 * target {@link FeatureType}s.
+	 */
+	public FeatureCollection<? extends FeatureType, ? extends Feature> 
+		transform(
+				FeatureCollection<? extends FeatureType, ? extends Feature> fc, 
+				Alignment al);
+	
+	/**
+	 * Transform a single {@link Feature} using a single {@link Cell}.
+	 * @param f a single {@link Feature} to be transformed using the given 
+	 * {@link Cell}.
+	 * @param c a single cell containing mappings for the {@link FeatureType} 
+	 * of the provided {@link Feature}.
+	 * @return the transformed {@link Feature}.
+	 */
+	public Feature transform(Feature f, Cell c);
 
 }
