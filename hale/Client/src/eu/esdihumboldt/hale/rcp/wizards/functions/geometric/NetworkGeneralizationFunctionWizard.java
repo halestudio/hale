9e3409c981ec3059dc997d4de9223a7ec7d4a52c
Anna Pitaev <anna.pitaev@logica.com>
2009-05-26 12:20:39 +0000

    Added wizard-classes.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@1388 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/Client/src/eu/esdihumboldt/hale/rcp/wizards/functions/geometric/NetworkGeneralizationFunctionWizard.java
@@ -0,0 +1,23 @@
+package eu.esdihumboldt.hale.rcp.wizards.functions.geometric;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NetworkGeneralizationFunctionWizard extends Wizard 
+implements INewWizard  {
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
