974a2301c9bf99727b8f03832c847c10e671f13b
Bernd Schneiders <bernd.schneiders@logica.com>
2009-05-27 14:00:00 +0000

    A second SchemaService implementation to import the INSPIRE schemas.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@1409 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/Client/src/eu/esdihumboldt/hale/models/impl/SchemaServiceImplApache.java
@@ -0,0 +1,491 @@
+package eu.esdihumboldt.hale.models.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.ws.commons.schema.XmlSchema;
+import org.apache.ws.commons.schema.XmlSchemaCollection;
+import org.apache.ws.commons.schema.XmlSchemaComplexType;
+import org.apache.ws.commons.schema.XmlSchemaElement;
+import org.apache.ws.commons.schema.XmlSchemaObjectCollection;
+import org.apache.ws.commons.schema.XmlSchemaObjectTable;
+import org.apache.ws.commons.schema.XmlSchemaType;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.xml.SchemaFactory;
+import org.geotools.xml.XSISAXHandler;
+import org.geotools.xml.schema.ComplexType;
+import org.geotools.xml.schema.Element;
+import org.geotools.xml.schema.Schema;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
+import eu.esdihumboldt.hale.models.HaleServiceListener;
+import eu.esdihumboldt.hale.models.SchemaService;
+
+/**
+ * Implementation of {@link SchemaService}.
+ */
+public class SchemaServiceImplApache implements SchemaService {
+	
+	private static Logger _log = Logger.getLogger(SchemaServiceImpl.class);
+	
+	private static SchemaServiceImplApache instance = new SchemaServiceImplApache();
+
+	/** FeatureType collection of the source schema */
+	Collection<FeatureType> sourceSchema;
+
+	/** FeatureType collection of the target schema */
+	Collection<FeatureType> targetSchema;
+	
+	private Collection<HaleServiceListener> listeners = new HashSet<HaleServiceListener>();
+	
+	private String sourceNamespace = "";
+	private String targetNamespace = "";
+	
+	private URL sourceLocation = null;
+	private URL targetLocation = null;
+	
+	private SchemaServiceImplApache() {
+		_log.setLevel(Level.INFO);
+	}
+	
+	public static SchemaService getInstance() {
+		return SchemaServiceImplApache.instance;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.models.SchemaService#cleanSourceSchema()
+	 */
+	public boolean cleanSourceSchema() {
+		if (this.sourceSchema != null && this.sourceSchema.size() != 0) {
+			this.sourceSchema.clear();
+		}
+		this.sourceLocation = null;
+		this.sourceNamespace = "";
+		this.updateListeners();
+		return true;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.models.SchemaService#cleanTargetSchema()
+	 */
+	public boolean cleanTargetSchema() {
+		if (this.targetSchema != null && this.targetSchema.size() != 0) {
+			this.targetSchema.clear();
+		}
+		this.targetLocation = null;
+		this.targetNamespace = "";
+		this.updateListeners();
+		return true;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.models.SchemaService#getSourceSchema()
+	 */
+	public Collection<FeatureType> getSourceSchema() {
+		return this.sourceSchema;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.models.SchemaService#getTargetSchema()
+	 */
+	public Collection<FeatureType> getTargetSchema() {
+		return this.targetSchema;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.models.SchemaService#loadSourceSchema(java.net.URI)
+	 */
+	public boolean loadSourceSchema(URI file) {
+		this.cleanSourceSchema();
+		this.sourceSchema = loadSchema(file);
+		try {
+			this.sourceLocation = new URL("file://" + file.toString());
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("The source location " + file.getPath()
+					+ "could not be saved: ", e);
+		}
+		if (this.sourceSchema != null && this.sourceSchema.size() > 0) {
+			this.sourceNamespace = this.sourceSchema.iterator().next()
+					.getName().getNamespaceURI().toString();
+		}
+		if (this.sourceSchema != null) {
+			this.updateListeners();
+			return true;
+		} 
+		else {
+			return false;
+		}
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.models.SchemaService#loadTargetSchema(java.net.URI)
+	 */
+	public boolean loadTargetSchema(URI file) {
+		this.cleanTargetSchema();
+		this.targetSchema = loadSchema(file);
+		try {
+			this.targetLocation = new URL(file.getPath());
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("The target location " + file.getPath()
+					+ "could not be saved: ", e);
+		}
+		if (this.targetSchema != null && this.targetSchema.size() > 0) {
+			this.targetNamespace = this.targetSchema.iterator().next()
+					.getName().getNamespaceURI().toString();
+		}
+		if (targetSchema != null) {
+			this.updateListeners();
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/**
+	 *  Copies a file
+	 * 
+	 * @param in
+	 * @param out
+	 * @throws Exception
+	 */
+	  public static void copyFile(File in, File out) throws Exception {
+		    FileInputStream fis  = new FileInputStream(in);
+		    FileOutputStream fos = new FileOutputStream(out);
+		    try {
+		        byte[] buf = new byte[1024];
+		        int i = 0;
+		        while ((i = fis.read(buf)) != -1) {
+		            fos.write(buf, 0, i);
+		        }
+		    } 
+		    catch (Exception e) {
+		        throw e;
+		    }
+		    finally {
+		        if (fis != null) fis.close();
+		        if (fos != null) fos.close();
+		    }
+		  }
+
+	  public static String getFileContent(File in) throws Exception {
+		    FileInputStream fis  = new FileInputStream(in);
+		    StringBuffer content = new StringBuffer();
+		    try {
+		        byte[] buf = new byte[1024];
+		        int i = 0;
+		        while ((i = fis.read(buf)) != -1) {
+		            
+		        	content.append(new String(buf, 0, i));
+		        }
+		    } 
+		    catch (Exception e) {
+		        throw e;
+		    }
+		    finally {
+		        if (fis != null) fis.close();
+		    }
+		    return content.toString();
+		  }
+	  
+	  public static void writeFileContent(File out, String content) throws Exception {
+		    FileOutputStream fos  = new FileOutputStream(out);
+		    try {
+		        	fos.write(content.getBytes());
+		    } 
+		    catch (Exception e) {
+		        throw e;
+		    }
+		    finally {
+		        if (fos != null) fos.close();
+		    }
+		  }	
+	
+	
+	public boolean addListener(HaleServiceListener sl) {
+		_log.info("Adding a listener.");
+		return this.listeners.add(sl);
+	}
+	
+	/**
+	 * Inform {@link HaleServiceListener}s of an update.
+	 */
+	private void updateListeners() {
+		for (HaleServiceListener hsl : this.listeners) {
+			_log.info("Updating a listener.");
+			hsl.update();
+		}
+	}
+
+	/**
+	 * Method to load a XSD schema file and build a collection of FeatureTypes.
+	 * 
+	 * @param file
+	 *            URI which represents a file
+	 * @return Collection FeatureType collection.
+	 */
+	public Collection<FeatureType> loadSchema(URI file) {
+		// use XML Schema to load schema with all its subschema to the memory
+		InputStream is = null;
+		try {
+			String path = file.toString();
+			is = new FileInputStream(path);
+			
+		} catch (Throwable e) {
+			_log.error("-- path not resolved: " + file);
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		XmlSchema prepSchema = null;
+		try {
+			XmlSchemaCollection schemaCol = new XmlSchemaCollection();
+			// Check if the file is located on web
+			if (file.getHost() == null) {
+				schemaCol.setSchemaResolver(new HumboldtURIResolver());
+			    schemaCol.setBaseUri(findBaseUri(file));
+			}
+			prepSchema = schemaCol.read(new StreamSource(is), null);
+			is.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		_log.info("Source schema has " +
+					prepSchema.getIncludes().getCount()+ " includes");
+
+		// write schema to memory
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		out.reset();
+		prepSchema.write(out);
+		try {
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		XmlSchemaElement e = prepSchema.getElementByName("DischargePoint");
+		XmlSchemaElement e1 = prepSchema.getElementByName("HydroFacility");
+		XmlSchemaType e2 = prepSchema.getTypeByName("HydroFacilityType");
+		
+		
+		XmlSchemaElement e6 = prepSchema.getElementByName("_Feature");
+		
+		
+		Collection<FeatureType> collection = new ArrayList<org.opengis.feature.type.FeatureType>();
+		
+		try {
+
+			Schema schema = null;
+			
+//			schema = SchemaFactory.getInstance(targetNamespace, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/gml/3.2.1/basicTypes.xsd"));
+
+			
+			
+/*			schemaLocation="D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/GCM/BaseTypes.xsd" />
+			
+			<import namespace="urn:x-inspire:specification:gmlas-v31:GeographicalNames:2.0"
+				schemaLocation="D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/GN/GeographicalNames.xsd" />
+			
+			<import namespace="urn:x-inspire:specification:gmlas-v31:Network:3.1"
+				schemaLocation="D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/GCM/Network.xsd" />
+
+			<import namespace="http://www.isotc211.org/2005/gmd"
+				schemaLocation="D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/iso/19139/20060504/gmd/gmd.xsd" />*/
+			
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/gml/3.1.1/base/gml.xsd"));
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/iso/19139/20060504/gco/gco.xsd"));
+
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/GCM/Network.xsd"));
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/GN/GeographicalNames.xsd"));
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/GCM/BaseTypes.xsd"));
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/iso/19139/20060504/gmd/gmd.xsd"));
+			
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/HY/Hydrography.xsd"));
+			
+			
+			
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/iso/19139/20060504/gco/gco.xsd"));
+//			schema = SchemaFactory.getInstance(null, new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/gml/3.1.1/gml.xsd"));
+//		    schema = SchemaFactory.getInstance(schema.getTargetNamespace(), new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/gml/3.2.1/deprecatedTypes.xsd"));
+//		    schema = SchemaFactory.getInstance(schema.getTargetNamespace(), new FileInputStream("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/gml/3.2.1/gmlBase.xsd"));
+			
+			URI targetNamespace = null;
+			byte [] inputBytes = out.toByteArray();
+			System.out.println("Schema size in bytes: " + inputBytes.length);
+			ByteArrayInputStream inputS = new ByteArrayInputStream(inputBytes);
+			schema = SchemaFactory.getInstance(targetNamespace, inputS);
+//			
+			
+//			file = new URI("D:/Humboldt/workspace/HALE/resources/D2.8-I_GML-Application-Schemas_v2.0-GML3.1.1/gml/3.1.1/base/gml.xsd");
+//			XMLReader reader = XMLReaderFactory.createXMLReader();
+//			XSISAXHandler schemaHandler = new XSISAXHandler(file);
+//			reader.setContentHandler(schemaHandler);
+//			reader.parse(new InputSource(new FileInputStream(file.toString())));
+//			schema = schemaHandler.getSchema();
+//			 
+			
+			System.out.println("Number of complex types: " + schema.getComplexTypes().length);
+		
+
+		// Schema[] imports = schema.getImports();
+		// for (Schema s : imports) {
+		// _log.debug("Imported URI + Name: " + s.getURI() + " " +
+		// s.getTargetNamespace());
+		// }
+
+		Collection<SimpleFeatureType> inTypes = new HashSet<SimpleFeatureType>();
+		
+		// Build first a list of FeatureTypes
+		
+//		XmlSchemaObjectCollection o = prepSchema.getItems();
+//		List<XmlSchemaComplexType> com = new ArrayList<XmlSchemaComplexType>();
+//		for (int i = 0; i < o.getCount(); i++) {
+//			if (o.getItem(i) instanceof XmlSchemaComplexType)
+//				com.add((XmlSchemaComplexType)o.getItem(i));
+//		}
+//		
+//		for (XmlSchemaComplexType type : com ) {
+//			SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+//			// System.out.println("FeatureType: " + type.getName());
+//			builder.setName(type.getName());
+//			builder.setNamespaceURI(type.getSourceURI());
+//			builder.setAbstract(type.isAbstract());
+
+//			if (type.getParent() != null) {
+//				//if type not abstract, add its children
+//				if (type.getChildElements()!=null){
+//					for (Element element : type.getChildElements()) {
+//						if (element.getType() !=null) {
+//							builder.add(element.getName(), element.getType().getClass());
+//						}
+//					}
+//				}
+//				inTypes.add(builder.buildFeatureType());
+//			}
+//		}
+
+		
+
+		// Build first a list of FeatureTypes
+		for (ComplexType type : schema.getComplexTypes()) {
+			SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+			// System.out.println("FeatureType: " + type.getName());
+			builder.setName(type.getName());
+			builder.setNamespaceURI(type.getNamespace());
+			builder.setAbstract(type.isAbstract());
+
+			if (type.getParent() != null) {
+				//if type not abstract, add its children
+				if (type.getChildElements()!=null){
+				for (Element element : type.getChildElements()) {
+					if (element.getType() !=null) {
+						builder.add(element.getName(), element.getType()
+								.getClass());
+						 
+					}
+
+				}
+				}
+				inTypes.add(builder.buildFeatureType());
+			}
+		}
+
+		// Build collection of feature type with their parents
+		
+		for (ComplexType type : schema.getComplexTypes()) {
+			
+			// Create builder
+			SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+			builder.setName(type.getName());
+			builder.setNamespaceURI(type.getNamespace());
+			builder.setAbstract(type.isAbstract());
+			if (type.getParent() instanceof ComplexType) {
+				if (type.getParent() != null) {
+					for (Element element : type.getChildElements()) {
+						if (element.getType() !=null) {
+						/*	 System.out.println("\tsimpl0e type element: "
+							 + element.getName());*/
+							builder.add(element.getName(), element.getType()
+									.getClass());
+						}
+					}
+
+					if (type.getParent().getName()
+							.equals("AbstractFeatureType")) {
+						builder.setSuperType(null); // FIXME??
+					} else {
+						for (SimpleFeatureType featureType : inTypes) {
+							if (featureType.getName().getLocalPart().equals(
+									type.getParent().getName())) {
+								builder.setSuperType(featureType);
+							}
+						}
+					}
+				}
+			}
+			collection.add(builder.buildFeatureType());
+		}
+	} catch (Exception uhe) {
+		uhe.printStackTrace();
+		_log.error("Imported Schema only available on-line, but "
+				+ "cannot be retrieved.", uhe);
+		}
+	
+		return collection;
+	}
+	
+	private String findBaseUri(URI file) {
+		String baseUri = "";
+		baseUri = file.toString();
+		baseUri = baseUri.substring(0, baseUri.lastIndexOf("/"));
+		System.out.println("********* BASE_URI: " + baseUri + "***********");
+		return baseUri;
+	}
+
+	/**
+	 * @see SchemaService#getSourceNameSpace()
+	 */
+	public String getSourceNameSpace() {
+		return this.sourceNamespace;
+	}
+
+	/**
+	 * @see SchemaService#getSourceURL()
+	 */
+	public URL getSourceURL() {
+		return this.sourceLocation;
+	}
+
+	/**
+	 * @see SchemaService#getTargetNameSpace()
+	 */
+	public String getTargetNameSpace() {
+		return this.targetNamespace;
+	}
+
+	/**
+	 * @see SchemaService#getTargetURL()
+	 */
+	public URL getTargetURL() {
+		return this.targetLocation;
+	}
+}
+
+
+
