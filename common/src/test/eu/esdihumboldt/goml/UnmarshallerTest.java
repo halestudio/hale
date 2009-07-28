38619d110e8a16e100ee87d5835d30f5f9e89927
Anna Pitaev <anna.pitaev@logica.com>
2009-07-28 17:47:03 +0000

    added testclasses for the gml-parser.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@1673 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ common/src/test/eu/esdihumboldt/goml/UnmarshallerTest.java
@@ -0,0 +1,57 @@
+package test.eu.esdihumboldt.goml;
+
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+
+
+import java.io.File;
+
+import javax.xml.transform.stream.StreamSource;
+
+import eu.esdihumboldt.goml.generated.AlignmentType;
+
+
+
+/*
+ * HUMBOLDT: A Framework for Data Harmonistation and Service Integration.
+ * EU Integrated Project #030962                  01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this website:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to : http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2008 to 2010.
+ */
+
+/**
+ * @author Anna Pitaev, Logica
+ *
+ */
+public class UnmarshallerTest {
+public static void main(String args []){
+	//unamarchall xmltransformer to workflow
+	try{
+	JAXBContext jc = JAXBContext.newInstance( "eu.esdihumboldt.goml.generated" );
+		//System.out.println(jc.toString());
+	
+       Unmarshaller u = jc.createUnmarshaller();
+       //it will debug problems while unmarchalling
+       u.setEventHandler(new javax.xml.bind.helpers.DefaultValidationEventHandler());
+       JAXBElement<AlignmentType> root = u.unmarshal(new StreamSource(new File("res\\schema\\waterVA2inspire.xml")),AlignmentType.class);
+       AlignmentType genAlignment = root.getValue();
+       System.out.println(genAlignment.getLevel());
+	}catch (Exception e){
+		e.printStackTrace();
+	}
+	
+	
+}
+}	
+
+
+
