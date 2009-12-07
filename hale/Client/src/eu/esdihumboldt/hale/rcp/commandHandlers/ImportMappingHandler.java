eec7629c01b22ebcdc79e63e91b7b6de2a8e3f01
Simon Templer <simon.templer@igd.fraunhofer.de>
2009-12-07 14:41:11 +0000

    added mapping import wizard
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2330 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/Client/src/eu/esdihumboldt/hale/rcp/commandHandlers/ImportMappingHandler.java
+++ hale/Client/src/eu/esdihumboldt/hale/rcp/commandHandlers/ImportMappingHandler.java
@@ -15,9 +15,16 @@ package eu.esdihumboldt.hale.rcp.commandHandlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import eu.esdihumboldt.hale.rcp.wizards.io.MappingImportWizard;
 
 /**
- * 
+ * Mapping import wizard
  *
  * @author Simon Templer
  * @partner 01 / Fraunhofer Institute for Computer Graphics Research
@@ -25,12 +32,16 @@ import org.eclipse.core.commands.ExecutionException;
  */
 public class ImportMappingHandler extends AbstractHandler {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	/**
+	 * @see IHandler#execute(ExecutionEvent)
 	 */
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		IImportWizard iw = new MappingImportWizard();
+		// Instantiates the wizard container with the wizard and opens it
+		Shell shell = HandlerUtil.getActiveShell(event);
+		WizardDialog dialog = new WizardDialog(shell, iw);
+		dialog.open();
 		return null;
 	}
 
