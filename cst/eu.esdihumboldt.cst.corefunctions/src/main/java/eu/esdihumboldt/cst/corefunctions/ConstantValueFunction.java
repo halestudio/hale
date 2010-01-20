7fddfac128c09d022f226df3e700fe63ab8dce5b
Ulrich Schäffler <Ulrich.Schaeffler@bv.tu-muenchen.de>
2010-01-20 17:27:00 +0000

    Implemented Constant Value Function and first Test
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2507 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- cst/eu.esdihumboldt.cst.corefunctions/src/main/java/eu/esdihumboldt/cst/corefunctions/ConstantValueFunction.java
+++ cst/eu.esdihumboldt.cst.corefunctions/src/main/java/eu/esdihumboldt/cst/corefunctions/ConstantValueFunction.java
@@ -12,31 +12,45 @@
 
 package eu.esdihumboldt.cst.corefunctions;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 
+import org.geotools.feature.AttributeImpl;
 import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.PropertyImpl;
 import org.opengis.feature.Feature;
+import org.opengis.feature.type.AttributeDescriptor;
 import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.PropertyDescriptor;
 
 import eu.esdihumboldt.cst.align.ICell;
+import eu.esdihumboldt.cst.align.ext.IParameter;
+import eu.esdihumboldt.cst.corefunctions.inspire.IdentifierFunction;
 import eu.esdihumboldt.cst.transformer.AbstractCstFunction;
+import eu.esdihumboldt.goml.omwg.ComposedProperty;
+import eu.esdihumboldt.goml.omwg.Property;
 
 /**
  * CST Function to set default
  * attribute target values.
  *
- * @author Anna Pitaev
- * @partner 04 / Logica
+ * @author Ulrich Schaeffler, Anna Pitaev
+ * @partner Technische Universitaet Muenchen, 04 / Logica
  * @version $Id$ 
  */
 public class ConstantValueFunction extends AbstractCstFunction {
+	
+	public static final String DEFAULT_VALUE_PARAMETER_NAME = "defaultValue";
+	private Object defaultValue = null;
+	private Property targetProperty = null;
 
 	/**
 	 * @see eu.esdihumboldt.cst.transformer.AbstractCstFunction#setParametersTypes(java.util.Map)
 	 */
 	@Override
 	protected void setParametersTypes(Map<String, Class<?>> parametersTypes) {
-		// TODO Auto-generated method stub
+		parameterTypes.put(ConstantValueFunction.DEFAULT_VALUE_PARAMETER_NAME, Object.class);
 
 	}
 
@@ -45,8 +59,16 @@ public class ConstantValueFunction extends AbstractCstFunction {
 	 */
 	@Override
 	public boolean configure(ICell cell) {
-		// TODO Auto-generated method stub
-		return false;
+		for (IParameter ip : cell.getEntity2().getTransformation().getParameters()) {
+			if (ip.getName().equals(ConstantValueFunction.DEFAULT_VALUE_PARAMETER_NAME)) {
+				this.defaultValue = ip.getValue();
+			}
+		
+		}
+		
+//		this.sourceProperty = ((ComposedProperty)cell.getEntity1()).getCollection().get(0);
+		this.targetProperty = ((ComposedProperty)cell.getEntity2()).getCollection().get(0);
+		return true;
 	}
 
 	/**
@@ -64,8 +86,46 @@ public class ConstantValueFunction extends AbstractCstFunction {
 	 */
 	@Override
 	public Feature transform(Feature source, Feature target) {
-		// TODO Auto-generated method stub
-		return null;
+		PropertyDescriptor pd = target.getProperty(
+				this.targetProperty.getLocalname()).getDescriptor();
+		
+		
+		PropertyImpl p = null;
+		if (pd.getType().getBinding().isPrimitive()) {
+			
+			if (pd.getType().getBinding().equals(Integer.class)){
+				p = new AttributeImpl((Integer)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+			else if (pd.getType().getBinding().equals(Short.class)){
+				p = new AttributeImpl((Short)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+			else if (pd.getType().getBinding().equals(Double.class)){
+				p = new AttributeImpl((Double)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+			else if (pd.getType().getBinding().equals(Long.class)){
+				p = new AttributeImpl((Long)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+			else if (pd.getType().getBinding().equals(Float.class)){
+				p = new AttributeImpl((Float)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+			else if (pd.getType().getBinding().equals(Boolean.class)){
+				p = new AttributeImpl((Boolean)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+			else if (pd.getType().getBinding().equals(Byte.class)){
+				p = new AttributeImpl((Byte)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+			else {
+				p = new AttributeImpl((Character)this.defaultValue, (AttributeDescriptor) pd, null);
+			}
+
+		}
+		else if (pd.getType().getBinding().equals(String.class)){
+			p = new AttributeImpl(this.defaultValue.toString(), (AttributeDescriptor) pd, null);
+		}
+		Collection<org.opengis.feature.Property> c = new HashSet<org.opengis.feature.Property>();
+		c.add(p);
+		target.setValue(c);
+		return target;
 	}
 
 }
---
+++ cst/eu.esdihumboldt.cst.corefunctions/src/test/java/eu/esdihumboldt/cst/corefunctions/ConstantValueTest.java
@@ -0,0 +1,111 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+package eu.esdihumboldt.cst.corefunctions;
+
+import junit.framework.TestCase;
+
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.junit.Test;
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import eu.esdihumboldt.cst.corefunctions.inspire.IdentifierFunction;
+import eu.esdihumboldt.goml.align.Cell;
+import eu.esdihumboldt.goml.oml.ext.Parameter;
+import eu.esdihumboldt.goml.oml.ext.Transformation;
+import eu.esdihumboldt.goml.omwg.ComposedProperty;
+import eu.esdihumboldt.goml.omwg.Property;
+import eu.esdihumboldt.goml.rdf.About;
+import eu.esdihumboldt.goml.rdf.Resource;
+
+public class ConstantValueTest extends TestCase {
+	/**
+	 * Test for CST Function to set default
+	 * attribute target values.
+	 *
+	 * @author Ulrich Schaeffler
+	 * @partner Technische Universitaet Muenchen
+	 * @version $Id$ 
+	 */
+
+	
+	private final String targetLocalname = "FT2";
+	private final String targetLocalnamePropertyBConst = "PropertyBConst";
+	private final String targetNamespace = "http://xsdi.org";
+	
+	
+	
+
+	@Test
+	public void testTransformFeatureFeatureString() {
+
+		// set up cell to use for testing
+		Cell cell = new Cell();
+		ComposedProperty cp = new ComposedProperty(
+				new About(this.targetNamespace, this.targetLocalname));
+		cp.getCollection().add(new Property(
+				new About(this.targetNamespace, this.targetLocalname, 
+						this.targetLocalnamePropertyBConst)));
+		
+		Transformation t = new Transformation();
+		t.setService(new Resource(ConstantValueFunction.class.toString()));
+		t.getParameters().add(new Parameter("defaultValue", "Spingfield"));
+		cp.setTransformation(t);
+		cell.setEntity1(null);
+		cell.setEntity2(cp);
+//		cell.setEntity1(new Property(new About(this.sourceNamespace, this.sourceLocalname, this.sourceLocalnamePropertyAConst)));
+//		cell.setEntity2(new Property(new About(this.targetNamespace, this.targetLocalname, this.targetLocalnamePropertyBConst)));
+
+		// build source and target Features
+		
+		SimpleFeatureType targettype = this.getFeatureType(
+				this.targetNamespace, 
+				this.targetLocalname, 
+				new String[]{this.targetLocalnamePropertyBConst});
+		
+		Feature target = SimpleFeatureBuilder.build(
+				targettype, new Object[]{"City"}, "1");
+		// perform actual test
+		
+		ConstantValueFunction constFunc = new ConstantValueFunction();
+		constFunc.configure(cell);
+
+		Feature neu = constFunc.transform(null, target);
+		
+		assertTrue(neu.getProperty(
+				this.targetLocalnamePropertyBConst).getValue().toString().equals("Spingfield"));
+	
+	}
+	
+	
+	
+	
+	private SimpleFeatureType getFeatureType(String featureTypeNamespace, 
+			String featureTypeName, String[] propertyNames) {
+	
+		SimpleFeatureType ft = null;
+		try {
+			SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
+			ftbuilder.setName(featureTypeName);
+			ftbuilder.setNamespaceURI(featureTypeNamespace);
+			for (String s : propertyNames) {
+				ftbuilder.add(s, String.class);
+			}
+			ft = ftbuilder.buildFeatureType();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		return ft;
+	}
+	
+}
