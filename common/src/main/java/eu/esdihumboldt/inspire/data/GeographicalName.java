8b47ae63618a3aa0a65d468ba2486a383c618d13
Ana Belen Anton <banton.etra-id@grupoetra.com>
2010-01-27 14:16:26 +0000

    Created comparison functions in GeographicalName
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2603 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- common/src/main/java/eu/esdihumboldt/inspire/data/GeographicalName.java
+++ common/src/main/java/eu/esdihumboldt/inspire/data/GeographicalName.java
@@ -10,7 +10,7 @@
  * (c) the HUMBOLDT Consortium, 2007 to 2010.
  */
 
-package eu.esdihumboldt.inspire.data;
+package main.java.eu.esdihumboldt.inspire.data;
 
 import java.util.ArrayList;
 
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 /**
  * This is the INPSIRE GeographicalName object implementation
  * 
- * @author Jose Ignacio Gisbert
+ * @author Ana Belen Anton & Jose Ignacio Gisbert
  * @partner 02 / ETRA Research and Development
  * @version $Id$ 
  */
@@ -91,10 +91,14 @@ public class GeographicalName {
 		if (target==null)
 			return false;
 		
-		if (spelling==null && target.getSpellingCount()!=0)
+		if (spelling.size()!=target.getSpellingCount())
 			return false;
-		else if (spelling.equals(target.getSpellingList())==false)
-			return false;
+		else 
+		{
+			for (int j=0;j<spelling.size();j++)
+				if (spelling.get(j).equals(target.getSpelling(j))==false)
+					return false;
+		}
 		
 		if (pronunciation==null && target.getPronunciation()!=null)
 			return false;
@@ -131,6 +135,45 @@ public class GeographicalName {
 		else if (grammaticalNumber==null && target.getGrammaticalNumber()!=null)
 			return false;
 				
+		return true;
+	}
+	
+	public boolean equalsuniquevalues(GeographicalName target)
+	{
+		if (pronunciation==null && target.getPronunciation()!=null)
+			return false;
+		else if (pronunciation.equals(target.getPronunciation())==false)
+			return false;
+		
+		if (language!=null && language.equals(target.getLanguage())==false)
+			return false;
+		else if (language==null && target.getLanguage()!=null)
+			return false;
+		
+		if (nativeness!=null && nativeness.equals(target.getNativeness())==false)
+			return false;
+		else if (nativeness==null && target.getNativeness()!=null)
+			return false;
+		
+		if (nameStatus!=null && nameStatus.equals(target.getNameStatus())==false)
+			return false;
+		else if (nameStatus==null && target.getNameStatus()!=null)
+			return false;
+		
+		if (sourceOfName!=null && sourceOfName.equals(target.getSourceOfName())==false)
+			return false;
+		else if (sourceOfName==null && target.getSourceOfName()!=null)
+			return false;
+		
+		if (grammaticalGender!=null && grammaticalGender.equals(target.getGrammaticalGender())==false)
+			return false;
+		else if (grammaticalGender==null && target.getGrammaticalGender()!=null)
+			return false;
+		
+		if (grammaticalNumber!=null && grammaticalNumber.equals(target.getGrammaticalNumber())==false)
+			return false;
+		else if (grammaticalNumber==null && target.getGrammaticalNumber()!=null)
+			return false;
 		return true;
 	}
 }
