cfb22ecf2be48c2cd926d6f377c35d7af500f3b5
Ulrich Schäffler <Ulrich.Schaeffler@bv.tu-muenchen.de>
2010-01-27 14:42:21 +0000

    Added Wizard for Date Extraction Function
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2609 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.rcp.wizards.functions.core/src/eu/esdihumboldt/hale/rcp/wizards/functions/core/date/DateExtractionFunctionWizard.java
@@ -0,0 +1,91 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+package eu.esdihumboldt.hale.rcp.wizards.functions.core.date;
+
+import org.eclipse.jface.wizard.Wizard;
+
+import eu.esdihumboldt.cst.align.ICell;
+import eu.esdihumboldt.cst.corefunctions.DateExtractionFunction;
+import eu.esdihumboldt.goml.align.Entity;
+import eu.esdihumboldt.goml.oml.ext.Parameter;
+import eu.esdihumboldt.goml.oml.ext.Transformation;
+import eu.esdihumboldt.goml.rdf.Resource;
+import eu.esdihumboldt.hale.rcp.wizards.functions.AbstractSingleCellWizard;
+import eu.esdihumboldt.hale.rcp.wizards.functions.AbstractSingleComposedCellWizard;
+import eu.esdihumboldt.hale.rcp.wizards.functions.AlignmentInfo;
+
+
+/**
+ * Wizard for the {@link DateExtractionFunction}.
+ * 
+ * @author Ulrich Schaeffler
+ * @partner 14 / TUM
+ * @version $Id$
+ */
+public class DateExtractionFunctionWizard extends AbstractSingleComposedCellWizard {
+
+	private DateExtractionFunctionWizardPage mainPage;
+	
+	/**
+	 * @see AbstractSingleCellWizard#AbstractSingleCellWizard(AlignmentInfo)
+	 */
+	public DateExtractionFunctionWizard(AlignmentInfo selection) {
+		super(selection);
+	}
+	
+	
+	
+	/**
+	 * @see AbstractSingleCellWizard#init()
+	 */
+	@Override
+	protected void init() {
+		this.mainPage = new DateExtractionFunctionWizardPage(
+			"Configure Date/Time Exctraction"); 
+		super.setWindowTitle("Date/Time Extraction Function"); 
+		super.setNeedsProgressMonitor(true);
+	}
+	
+	
+
+	/**
+	 * @see Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		ICell cell = getResultCell();
+		Entity entity1 = (Entity) cell.getEntity1();
+		
+		Transformation transformation = new Transformation();
+		transformation.setService(new Resource(DateExtractionFunction.class.getName()));
+		transformation.getParameters().add(
+				new Parameter(
+						DateExtractionFunction.DATE_FORMAT_SOURCE, 
+						mainPage.getSourceFormat()));
+		transformation.getParameters().add(
+				new Parameter(
+						DateExtractionFunction.DATE_FORMAT_TARGET, 
+						mainPage.getTargetFormat()));
+		((Entity) cell.getEntity1()).setTransformation(transformation);
+		return true;
+	}
+	
+	/**
+	 * @see Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+		addPage(mainPage);
+	}
+
+}
---
+++ hale/eu.esdihumboldt.hale.rcp.wizards.functions.core/src/eu/esdihumboldt/hale/rcp/wizards/functions/core/date/DateExtractionFunctionWizardFactory.java
@@ -0,0 +1,77 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                  01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+package eu.esdihumboldt.hale.rcp.wizards.functions.core.date;
+
+import eu.esdihumboldt.cst.align.ICell;
+import eu.esdihumboldt.cst.corefunctions.DateExtractionFunction;
+import eu.esdihumboldt.hale.rcp.views.model.SchemaItem;
+import eu.esdihumboldt.hale.rcp.wizards.functions.AlignmentInfo;
+import eu.esdihumboldt.hale.rcp.wizards.functions.FunctionWizard;
+import eu.esdihumboldt.hale.rcp.wizards.functions.FunctionWizardFactory;
+
+/**
+ * This is the {@link FunctionWizardFactory} for the {@link DateExtractionFunction}.
+ * 
+ * @author Ulrich Schaeffler 
+ * @partner 14 / TUM
+ * @version $Id$ 
+ */
+public class DateExtractionFunctionWizardFactory implements
+		FunctionWizardFactory {
+
+	/**
+	 * @see eu.esdihumboldt.hale.rcp.wizards.functions.FunctionWizardFactory#createWizard(eu.esdihumboldt.hale.rcp.wizards.functions.AlignmentInfo)
+	 */
+	@Override
+	public FunctionWizard createWizard(AlignmentInfo selection) {
+		return new DateExtractionFunctionWizard(selection);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.rcp.wizards.functions.FunctionWizardFactory#supports(eu.esdihumboldt.hale.rcp.wizards.functions.AlignmentInfo)
+	 */
+	@Override
+	public boolean supports(AlignmentInfo selection) {
+		// must be at least one source item and exactly one target item
+		if (selection.getSourceItemCount() != 1 || selection.getTargetItemCount() != 1) {
+			return false;
+		}
+		
+		// target item must be a property
+		SchemaItem target = selection.getFirstTargetItem();
+		if (!target.isAttribute()) {
+			return false;
+		}
+		
+		// source items must be properties
+		SchemaItem source = selection.getFirstSourceItem();
+		if (!source.isAttribute()) {
+			return false;
+		}
+		
+		ICell cell = selection.getAlignment(selection.getSourceItems(),
+				selection.getTargetItems());
+		if (cell != null) {
+			// only allow editing matching transformation
+			try {
+				return cell.getEntity1().getTransformation().getService().getLocation().equals(
+						DateExtractionFunction.class.getName());
+			} catch (NullPointerException e) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+}
