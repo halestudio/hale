36721a82ce80ec80c27c776c9d8fb19e3c808cc3
Anna Pitaev <anna.pitaev@logica.com>
2010-01-29 19:13:39 +0000

    [maven-scm] copy for branch WinterOfCodeRefactor
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2718 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ humboldt-specification/branches/WinterOfCodeRefactor/src/main/java/eu/esdihumboldt/workflow/exceptions/IncompatibleMimeTypeException.java
@@ -0,0 +1,11 @@
+package eu.esdihumboldt.workflow.exceptions;
+
+public class IncompatibleMimeTypeException extends Exception {
+
+	public IncompatibleMimeTypeException() {
+		
+	}
+	public IncompatibleMimeTypeException(String s) {
+		super(s);
+	}
+}
