7e7cb62d28d15f98f2ee4e232d556d9570346e24
Anna Pitaev <anna.pitaev@logica.com>
2010-01-31 01:34:03 +0000

    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2725 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- humboldt-commons/trunk/src/main/java/eu/esdihumboldt/goml/omwg/comparator/EqualComparator.java
+++ humboldt-commons/trunk/src/main/java/eu/esdihumboldt/goml/omwg/comparator/EqualComparator.java
@@ -3,6 +3,8 @@
  */
 package eu.esdihumboldt.goml.omwg.comparator;
 
+import org.apache.log4j.Logger;
+
 import java.util.List;
 
 import org.opengis.feature.Property;
@@ -16,22 +18,30 @@ import eu.esdihumboldt.goml.omwg.Restriction;
  *
  */
 public class EqualComparator implements Comparator {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger LOG = Logger.getLogger(EqualComparator.class);
 
 	/* (non-Javadoc)
 	 * @see eu.esdihumboldt.goml.omwg.comparator.Comparator#evaluate(java.util.List, java.util.List)
 	 */
 	public boolean evaluate(Restriction sourceRestriction, Property sourceProp) {
-		List<IValueExpression> sourceValues = sourceRestriction.getValue();
-		
+		List<IValueExpression> sourceValues = sourceRestriction.getValue();		
 		Object sourcePropValue = sourceProp.getValue();
+		
+		LOG.debug("There are " + sourceValues.size() + " source values in the " + sourceRestriction);
+		LOG.debug("The source property value is: " + sourcePropValue);
 		
 		// TODO Will there be a collection of source values for equals?.
 		for(IValueExpression value : sourceValues) {
 			if(sourcePropValue.equals(value.getLiteral())) {
+				LOG.debug("Found a match between " + sourcePropValue + " and " + value.getLiteral());
 				// The value is equal so we can return true.
 				return true;
 			}
 		}
+		LOG.debug("No matches found for " + sourcePropValue);
 		return false;
 	}
 
--- humboldt-commons/trunk/src/main/java/eu/esdihumboldt/goml/omwg/comparator/OtherwiseComparator.java
+++ humboldt-commons/trunk/src/main/java/eu/esdihumboldt/goml/omwg/comparator/OtherwiseComparator.java
@@ -3,6 +3,8 @@
  */
 package eu.esdihumboldt.goml.omwg.comparator;
 
+import org.apache.log4j.Logger;
+
 import org.opengis.feature.Property;
 
 import eu.esdihumboldt.goml.omwg.Restriction;
@@ -12,11 +14,16 @@ import eu.esdihumboldt.goml.omwg.Restriction;
  *
  */
 public class OtherwiseComparator implements Comparator {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger LOG = Logger.getLogger(OtherwiseComparator.class);
 
 	/* (non-Javadoc)
 	 * @see eu.esdihumboldt.goml.omwg.comparator.Comparator#evaluate(eu.esdihumboldt.goml.omwg.Restriction, org.opengis.feature.Property)
 	 */
-	public boolean evaluate(Restriction sourceRestriction, Property sourceProp) {
+	public boolean evaluate(Restriction sourceRestriction, Property sourceProp) {		
+		LOG.debug("Return value always = " + true);
 		return true;
 	}
 
