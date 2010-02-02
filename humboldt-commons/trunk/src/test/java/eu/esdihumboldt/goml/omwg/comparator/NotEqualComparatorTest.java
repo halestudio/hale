899389a93da64d53a77b1a4c097c4d221497f39a
Anna Pitaev <anna.pitaev@logica.com>
2010-02-02 17:04:11 +0000

    Added NotEqualComparator and test
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2755 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ humboldt-commons/trunk/src/test/java/eu/esdihumboldt/goml/omwg/comparator/NotEqualComparatorTest.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package eu.esdihumboldt.goml.omwg.comparator;
+
+import org.apache.log4j.Logger;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.net.URL;
+import java.util.List;
+
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+import eu.esdihumboldt.cst.align.ICell;
+import eu.esdihumboldt.goml.align.Alignment;
+import eu.esdihumboldt.goml.oml.io.OmlRdfReader;
+import eu.esdihumboldt.goml.omwg.Property;
+import eu.esdihumboldt.goml.omwg.Restriction;
+
+/**
+ * @author Mark Doyle
+ * @partner Logica
+ *
+ */
+public class NotEqualComparatorTest {
+	
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger LOG = Logger.getLogger(NotEqualComparatorTest.class);
+	
+	/**
+	 * Test source name for the source Feature generated for the tests
+	 */
+	private final static String SOURCE_LOCAL_NAME = "OneOf comparator test feature";
+	
+	/**
+	 * Test source property name for the source Feature generated for the tests
+	 */
+	private final static String SOURCE_LOCAL_NAME_PROPERTY_A = "PropertyA";
+	
+	/**
+	 * Test source namespace for the source Feature generated for the tests
+	 */
+	private final static String SOURCE_NAMESPACE = "http://esdi-humboldt.eu";
+	
+	/**
+	 * Source restriction read from the Test oml file for all tests.
+	 */
+	private static Restriction SOURCE_RESTRICTION = null;
+	
+	
+	/**
+	 * A value for the source Feature that will evaluate as a success based upon the EqualComparatorTest.oml file.
+	 */
+	private final static String SUCCESSFUL_EVALUATION_VALUE = "65";
+	
+	/**
+	 * A value for the source Feature that will evaluate as a failure based upon the EqualComparatorTest.oml file.
+	 */
+	private final static String UNSUCCESSFUL_EVALUATION_VALUE = "1000";
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		URL url = OneOfComparatorTest.class.getResource("./EqualComparatorTest.oml");
+		Alignment alignment = new OmlRdfReader().read(url.getFile());
+		ICell cell = alignment.getMap().get(0);
+		Property propA = (Property)cell.getEntity1();
+		
+		List<Restriction> sourceRestrictions = propA.getValueCondition();
+		SOURCE_RESTRICTION  = sourceRestrictions.get(0);
+	}
+
+
+	/**
+	 * Test method for {@link eu.esdihumboldt.goml.omwg.comparator.EqualComparator#evaluate(eu.esdihumboldt.goml.omwg.Restriction, org.opengis.feature.Property)}.
+	 */
+	@Test
+	public void testEvaluateTrue() {
+		LOG.info("Testing true evaluation");
+		org.opengis.feature.Property sourceProperty = createSource(true).getProperty(SOURCE_LOCAL_NAME_PROPERTY_A);
+		
+		Comparator notEqualComp = new NotEqualComparator();
+		boolean result = notEqualComp.evaluate(SOURCE_RESTRICTION, sourceProperty);
+		
+		assertTrue("Evaluation should return true", result);
+	}
+	
+	
+	/**
+	 * Test method for {@link eu.esdihumboldt.goml.omwg.comparator.EqualComparator#evaluate(eu.esdihumboldt.goml.omwg.Restriction, org.opengis.feature.Property)}.
+	 */
+	@Test
+	public void testEvaluateFalse() {
+		LOG.info("Testing false evaluation");
+		org.opengis.feature.Property sourceProperty = createSource(false).getProperty(SOURCE_LOCAL_NAME_PROPERTY_A);
+		
+		Comparator notEqualComp = new NotEqualComparator();
+		boolean result = notEqualComp.evaluate(SOURCE_RESTRICTION, sourceProperty);
+		
+		assertFalse("Evaluation should return false", result);
+	}
+	
+	
+	/**
+	 * @return 
+	 * 
+	 */
+	private SimpleFeature createSource(boolean withSuccessfulValue) {
+		LOG.debug("Creating SimpleFeature source for " + this.getClass().getName() + " test");
+		SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
+		ftbuilder.setName(SOURCE_LOCAL_NAME);
+		ftbuilder.setNamespaceURI(SOURCE_NAMESPACE);
+		ftbuilder.add(SOURCE_LOCAL_NAME_PROPERTY_A, String.class);
+		SimpleFeatureType sourceFeatureType = ftbuilder.buildFeatureType();
+		
+		SimpleFeature sourceFeature = null;
+		if(withSuccessfulValue) {
+			LOG.debug("Creating a source feature which should map to Fluss, Bach in the target");
+			sourceFeature = SimpleFeatureBuilder.build(sourceFeatureType, new Object[]{SUCCESSFUL_EVALUATION_VALUE}, "1");
+		}
+		else {
+			LOG.debug("Creating a source feature which should not map to Fluss, Bach in the target");
+			sourceFeature = SimpleFeatureBuilder.build(sourceFeatureType, new Object[]{UNSUCCESSFUL_EVALUATION_VALUE}, "1");
+		}
+		
+		assertNotNull(sourceFeature);
+		
+		return sourceFeature;
+	}
+
+}
