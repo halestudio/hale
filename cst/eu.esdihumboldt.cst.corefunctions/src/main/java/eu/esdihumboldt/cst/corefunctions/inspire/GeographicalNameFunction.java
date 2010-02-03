e15ef2bf3feb0ea6da4fca05673f8e9d92bf6b23
Ana Belen Anton <banton.etra-id@grupoetra.com>
2010-02-03 12:49:38 +0000

    Updated multiple spelling
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2759 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- cst/eu.esdihumboldt.cst.corefunctions/src/main/java/eu/esdihumboldt/cst/corefunctions/inspire/GeographicalNameFunction.java
+++ cst/eu.esdihumboldt.cst.corefunctions/src/main/java/eu/esdihumboldt/cst/corefunctions/inspire/GeographicalNameFunction.java
@@ -16,12 +16,16 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.List;
-
 import org.geotools.feature.AttributeImpl;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.PropertyImpl;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.AttributeDescriptor;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.PropertyDescriptor;
@@ -65,79 +69,104 @@ public class GeographicalNameFunction
 	public static final String PROPERTY_GRAMMA_GENDER = "grammaticalGender";
 	public static final String PROPERTY_GRAMMA_NUMBER = "grammaticalNumber";
 	
-	private Property variable = null;
+	private ArrayList<ArrayList<Property>> variable = new ArrayList<ArrayList<Property>>();
 	private Property targetProperty = null;
 	
-	private String _script = null;
-	private String _transliteration = null;
-	private NameStatusValue _nameStatus = null;
-	private String _language = null;
-	private NativenessValue _nativeness = null;
-	private String _sourceOfName = null;
-	private String _pronunciationIPA = null;
-	private URI _pronunciationSoundLink = null;
-	private GrammaticalGenderValue _grammaticalGender = null;
-	private GrammaticalNumberValue _grammaticalNumber = null;
+	private ArrayList<ArrayList<String>> _script = new ArrayList<ArrayList<String>>();
+	private ArrayList<ArrayList<String>> _transliteration = new ArrayList<ArrayList<String>>();
+	private ArrayList<NameStatusValue> _nameStatus = new ArrayList<NameStatusValue>();
+	private ArrayList<String> _language = new ArrayList<String>();
+	private ArrayList<NativenessValue> _nativeness = new ArrayList<NativenessValue>();
+	private ArrayList<String> _sourceOfName = new ArrayList<String>();
+	private ArrayList<String> _pronunciationIPA = new ArrayList<String>();
+	private ArrayList<URI> _pronunciationSoundLink = new ArrayList<URI>();
+	private ArrayList<GrammaticalGenderValue> _grammaticalGender = new ArrayList<GrammaticalGenderValue>();
+	private ArrayList<GrammaticalNumberValue> _grammaticalNumber = new ArrayList<GrammaticalNumberValue>();
+	private int cellcount=0;
+	private int spellcount=0;
 	
 	/* (non-Javadoc)
 	 * @see eu.esdihumboldt.cst.transformer.CstFunction#configure(eu.esdihumboldt.cst.align.ICell)
 	 */
 	public boolean configure(ICell cell) {
-		for (IParameter ip : cell.getEntity1().getTransformation().getParameters()) {
-			if(ip.getName().equals(GeographicalNameFunction.PROPERTY_SCRIPT))
+		for (Property p : ((ComposedProperty)cell.getEntity1()).getCollection()){
+			spellcount=0;
+			this._script.add(cellcount,new ArrayList<String>());
+			this._transliteration.add(cellcount,new ArrayList<String>());
+			this.variable.add(cellcount,new ArrayList<Property>());
+			for (Property prop : ((ComposedProperty)p).getCollection())
 			{
-				this._script=ip.getValue();
+				this.variable.get(cellcount).add(spellcount,prop);
+				for (IParameter ip : prop.getTransformation().getParameters()) {
+					if(ip.getName().equals(GeographicalNameFunction.PROPERTY_SCRIPT))
+					{
+						this._script.get(cellcount).add(spellcount,ip.getValue());
+					}
+					else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_TRANSLITERATION))
+					{
+						this._transliteration.get(cellcount).add(spellcount,ip.getValue());
+					}
+					if (this._script.get(cellcount).size()<spellcount+1) this._script.get(cellcount).add(spellcount,null);
+					if (this._transliteration.get(cellcount).size()<spellcount+1) this._transliteration.get(cellcount).add(spellcount,null);
+				}
+				spellcount++;
 			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_TRANSLITERATION))
-			{
-				this._transliteration=ip.getValue();
+			for (IParameter ip : p.getTransformation().getParameters()) {
+				if(ip.getName().equals(GeographicalNameFunction.PROPERTY_NAMESTATUS))
+				{
+					this._nameStatus.add(cellcount,NameStatusValue.valueOf(ip.getValue()));
+				}
+				else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_LANGUAGE))
+				{
+					this._language.add(cellcount,ip.getValue());
+				}
+				else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_NATIVENESS))
+				{
+					this._nativeness.add(cellcount,NativenessValue.valueOf(ip.getValue()));
+				}
+				else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_SOURCEOFNAME))
+				{
+					this._sourceOfName.add(cellcount,ip.getValue());
+				}
+				else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_PRONUNCIATIONIPA))
+				{
+					this._pronunciationIPA.add(cellcount,ip.getValue());
+				}
+				else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_PRONUNCIATIONSOUNDLINK))
+				{
+					this._pronunciationSoundLink.add(cellcount,URI.create(ip.getValue()));
+				}
+				else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_GRAMMA_GENDER))
+				{
+					this._grammaticalGender.add(cellcount,GrammaticalGenderValue.valueOf(ip.getValue()));
+				}
+				else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_GRAMMA_NUMBER))
+				{
+					this._grammaticalNumber.add(cellcount,GrammaticalNumberValue.valueOf(ip.getValue()));
+				}
 			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_NAMESTATUS))
-			{
-				this._nameStatus=NameStatusValue.valueOf(ip.getValue());
-			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_LANGUAGE))
-			{
-				this._language=ip.getValue();
-			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_NATIVENESS))
-			{
-				this._nativeness=NativenessValue.valueOf(ip.getValue());
-			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_SOURCEOFNAME))
-			{
-				this._sourceOfName=ip.getValue();
-			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_PRONUNCIATIONIPA))
-			{
-				this._pronunciationIPA=ip.getValue();
-			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_PRONUNCIATIONSOUNDLINK))
-			{
-				this._pronunciationSoundLink=URI.create(ip.getValue());
-			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_GRAMMA_GENDER))
-			{
-				this._grammaticalGender=GrammaticalGenderValue.valueOf(ip.getValue());
-			}
-			else if(ip.getName().equals(GeographicalNameFunction.PROPERTY_GRAMMA_NUMBER))
-			{
-				this._grammaticalNumber=GrammaticalNumberValue.valueOf(ip.getValue());
-			}
-			
-		}
-		for (Property p : ((ComposedProperty)cell.getEntity1()).getCollection()) {
-			this.variable=p;
+			// Force to complete index for all arrayLists
+			//if (this._script.size()<cellcount+1) this._script.add(cellcount,null);
+			//if (this._transliteration.size()<cellcount+1) this._transliteration.add(cellcount,null);
+			if (this._nameStatus.size()<cellcount+1) this._nameStatus.add(cellcount,null);
+			if (this._language.size()<cellcount+1) this._language.add(cellcount,null);
+			if (this._nativeness.size()<cellcount+1) this._nativeness.add(cellcount,null);
+			if (this._sourceOfName.size()<cellcount+1) this._sourceOfName.add(cellcount,null);
+			if (this._pronunciationIPA.size()<cellcount+1) this._pronunciationIPA.add(cellcount,null);
+			if (this._pronunciationSoundLink.size()<cellcount+1) this._pronunciationSoundLink.add(cellcount,null);
+			if (this._grammaticalGender.size()<cellcount+1) this._grammaticalGender.add(cellcount,null);
+			if (this._grammaticalNumber.size()<cellcount+1) this._grammaticalNumber.add(cellcount,null);
+			//this.variable.add(cellcount,p);
+			cellcount++;
 		}
 		this.targetProperty = (Property) cell.getEntity2();
 		
-		if (this.variable == null) {
+		if (this.variable.size() == 0) {
 			throw new RuntimeException("The Source property must be defined.");
 		}
 		if (this.targetProperty == null) {
 			throw new RuntimeException("The Target property must be defined.");
 		}
-		
 		return true;
 	}
 
@@ -158,49 +187,84 @@ public class GeographicalNameFunction
 		
 		//check if the input features have the expected property name
 		if (target.getProperties(targetProperty.getLocalname()).size()==0) return null;
-		if (source.getProperties(variable.getLocalname()).size()==0) return null;
 		
-		try {
-			Object result = source.getProperty(this.variable.getLocalname()).getValue();
-
-			// inject result into target object
-			PropertyDescriptor pd = target.getProperty(
-					this.targetProperty.getLocalname()).getDescriptor();
-			PropertyImpl p = null;
-			if (pd.getType().getBinding().equals(GeographicalName.class)) {
-				GeographicalName gn=new GeographicalName();
-				gn.setGrammaticalGender(_grammaticalGender);
-				gn.setGrammaticalNumber(_grammaticalNumber);
-				gn.setLanguage(_language);
-				gn.setNameStatus(_nameStatus);
-				gn.setNativeness(_nativeness);
-				gn.setSourceOfName(_sourceOfName);
-				
-				PronunciationOfName pn = new PronunciationOfName();
-				pn.setPronunciationIPA(_pronunciationIPA);
-				pn.setPronunciationSoundLink(_pronunciationSoundLink);
-				gn.setPronunciation(pn);
-				
+		for (int i=0;i<variable.size();i++)
+			for (int j=0;j<variable.get(i).size();j++)
+				if (source.getProperties(variable.get(i).get(j).getLocalname()).size()==0) return null;
+		
+		// Creates an Array of GeographicalNames with Configuration values
+		ArrayList<GeographicalName> gns = new ArrayList<GeographicalName>();
+		for (int i=0;i<cellcount;i++)
+		{
+			GeographicalName gn = new GeographicalName();
+			for (int j=0;j<_script.get(i).size();j++)
+			{
 				SpellingOfName sp = new SpellingOfName();
-				sp.setScript(_script);
+				Object result = source.getProperty(this.variable.get(i).get(j).getLocalname()).getValue();
 				sp.setText(result.toString());
-				sp.setTransliterationScheme(_transliteration);
-				gn.setSpelling(sp);
-				
-				p = new AttributeImpl(gn, (AttributeDescriptor) pd, null);
+				sp.setScript(_script.get(i).get(j));
+				sp.setTransliterationScheme(_transliteration.get(i).get(j));
+				gn.addSpelling(sp);
 			}
-			else {
-				return null;
-			}
-			
-			Collection<org.opengis.feature.Property> c = new HashSet<org.opengis.feature.Property>();
-			c.add(p);
-			target.setValue(c);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+			gn.setLanguage(_language.get(i));
+			gn.setNativeness(_nativeness.get(i));
+			gn.setNameStatus(_nameStatus.get(i));
+			gn.setSourceOfName(_sourceOfName.get(i));
+			PronunciationOfName pron = new PronunciationOfName();
+			pron.setPronunciationIPA(_pronunciationIPA.get(i));
+			pron.setPronunciationSoundLink(_pronunciationSoundLink.get(i));
+			gn.setPronunciation(pron);
+			gn.setGrammaticalGender(_grammaticalGender.get(i));
+			gn.setGrammaticalNumber(_grammaticalNumber.get(i));
+			gns.add(gn);
 		}
-
 		
+		/*
+		// Combine those GeographicalName objects which are equal except of Spelling
+		for (int i=0;i<gns.size();i++)
+		{
+			if (gns.get(i)==null) continue;
+			for (int j=i+1;j<gns.size();j++)
+			{
+				if (gns.get(j)==null) continue;
+				/** TODO Uncomment when library common is updated!!
+				if (gns.get(i).equalsuniquevalues(gns.get(j)))
+				{
+					gns.addSpelling(gns.get(j).getSpelling());
+					gns.set(j,null);
+				}
+				
+			}
+		}*/
+		
+		
+		SimpleFeatureType targetType2 = this.getFeatureType(
+				GeographicalNameFunctionTest.targetNamespace,
+			    GeographicalNameFunctionTest.targetLocalName, 
+			    GeographicalName.class);
+		Feature target2 = SimpleFeatureBuilder.build(
+				targetType2, new Object[]{}, "2");		
+		//Add collection of GeographicNames to target feature
+		//PropertyDescriptor pd = target.getProperty(this.targetProperty.getLocalname()).getDescriptor();
+		
+		Cell cell = new Cell();
+		String targetLocalName2 = "FT2";
+		String targetLocalNameProperty2 = "GeographicalName";
+		String targetNamespace2 = "urn:x-inspire:specification:gmlas-v31:Hydrography:2.0";
+		
+		cell.setEntity2(new Property ( 
+				new About (targetNamespace2, targetLocalName2, 
+						targetLocalNameProperty2)));
+		
+		Property targetProperty2 = (Property) cell.getEntity2();
+		PropertyDescriptor pd2 = target2.getProperty(targetProperty2.getLocalname()).getDescriptor();
+		Collection<org.opengis.feature.Property> c = new HashSet<org.opengis.feature.Property>();
+		for (int i=0;i<gns.size();i++)
+		{
+			PropertyImpl p = new AttributeImpl(gns.get(i), (AttributeDescriptor) pd2, null);			
+			c.add(p);
+		}
+		((SimpleFeature)target).setAttribute(this.targetProperty.getLocalname(), c);
 		return target;
 	}
 	
@@ -223,6 +287,21 @@ public class GeographicalNameFunction
 		parameterCell.setEntity1(entity1);
 		parameterCell.setEntity2(entity2);
 		return parameterCell;
+	}
+	
+	private SimpleFeatureType getFeatureType(String featureTypeNamespace, String featureTypeName, Class <? extends Object> name) {
+		
+		SimpleFeatureType ft = null;
+		try {
+			SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
+			ftbuilder.setName(featureTypeName);
+			ftbuilder.setNamespaceURI(featureTypeNamespace);
+			ftbuilder.add("GeographicalName", name);
+			ft = ftbuilder.buildFeatureType();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		return ft;
 	}
 
 }
