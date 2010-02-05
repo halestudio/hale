7592d531ed0e48ebfbf6aa58bfc264cf35d13204
Simon Templer <simon.templer@igd.fraunhofer.de>
2010-02-05 11:20:00 +0000

    changed resolved task natural order
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@2791 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/eu.esdihumboldt.hale/src/eu/esdihumboldt/hale/task/ResolvedTask.java
+++ hale/eu.esdihumboldt.hale/src/eu/esdihumboldt/hale/task/ResolvedTask.java
@@ -177,11 +177,18 @@ public class ResolvedTask implements Task {
 	@Override
 	public int compareTo(Task other) {
 		if (other instanceof ResolvedTask) {
-			if (getValue() > ((ResolvedTask) other).getValue()) {
-				return -1;
+			int result = getSeverityLevel().compareTo(((ResolvedTask) other).getSeverityLevel());
+			
+			if (result == 0) {
+				if (getValue() > ((ResolvedTask) other).getValue()) {
+					return -1;
+				}
+				else if (getValue() < ((ResolvedTask) other).getValue()) {
+					return 1;
+				}
 			}
-			else if (getValue() < ((ResolvedTask) other).getValue()) {
-				return 1;
+			else {
+				return result;
 			}
 		}
 		
