a35668df6560e0ff61c6a0f9689f170d6f0acff6
Jan Jezek <h.jezek@centrum.cz>
2010-02-24 16:13:16 +0000

    Demo Servlet edited.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3020 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- cst/cst-wps/trunk/src/main/java/eu/esdihumboldt/wps/Servlet.java
+++ cst/cst-wps/trunk/src/main/java/eu/esdihumboldt/wps/Servlet.java
@@ -2,6 +2,7 @@ package eu.esdihumboldt.wps;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,22 +14,34 @@ import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.NoSuchAuthorityCodeException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
+import eu.esdihumboldt.cst.transformer.capabilities.FunctionDescription;
+import eu.esdihumboldt.cst.transformer.capabilities.impl.CstServiceCapabilitiesImpl;
+import eu.esdihumboldt.cst.transformer.service.CstFunctionFactory;
+
 public class Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
 
-		try {
-			CoordinateReferenceSystem crs = CRS.decode("EPSG:2065");
-			PrintWriter out = response.getWriter();
-			out.println(crs.toWKT());
-			out.flush();
-			out.close();
-		} catch (NoSuchAuthorityCodeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FactoryException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		CstFunctionFactory.getInstance().registerCstPackage(
+				"eu.esdihumboldt.cst.corefunctions");
+
+		CstServiceCapabilitiesImpl service = new CstServiceCapabilitiesImpl();
+		for (Iterator<FunctionDescription> i = service
+				.getFunctionDescriptions().iterator(); i.hasNext();) {
+
+			FunctionDescription fd = i.next();
+			out.println(fd.getFunctionId());
+
+			for (Iterator<String> j = fd.getParameterConfiguration().keySet()
+					.iterator(); j.hasNext();) {
+				String name = j.next();
+
+				fd.getParameterConfiguration().get(name);
+				System.out.println(name + " --- "
+						+ fd.getParameterConfiguration().get(name));
+			}
+
 		}
 	}
 }
