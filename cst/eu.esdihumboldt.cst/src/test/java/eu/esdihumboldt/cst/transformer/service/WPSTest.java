70738dd81a80a845f5ff9c16ebc93bffaf82abbe
Jan Jezek <h.jezek@centrum.cz>
2010-03-29 09:33:14 +0000

    Small corrections of WPSTest.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3275 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- cst/eu.esdihumboldt.cst/src/test/java/eu/esdihumboldt/cst/transformer/service/WPSTest.java
+++ cst/eu.esdihumboldt.cst/src/test/java/eu/esdihumboldt/cst/transformer/service/WPSTest.java
@@ -1,41 +1,69 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                  01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+
 package eu.esdihumboldt.cst.transformer.service;
 
-import java.net.URL;
+import static org.junit.Assert.assertNotNull;
+
 import java.util.Iterator;
 
-import junit.framework.TestCase;
-import eu.esdihumboldt.cst.CstFunction;
+import org.junit.Before;
+import org.junit.Test;
+
 import eu.esdihumboldt.cst.transformer.capabilities.FunctionDescription;
 import eu.esdihumboldt.cst.transformer.capabilities.impl.CstServiceCapabilitiesImpl;
 
-public class WPSTest extends TestCase {
 
-	
-  protected void setUp() throws Exception {		
-		super.setUp();						
-	/**
-	 * We should add corefunctions jar to classpath before trying to register it.
-	 */
+/**
+ * Test of CstServiceCapabilitiesImpl and functions parameter configuration. 
+ * @author jezekjan
+ * @partner Help Service - Remote Sensing
+ * @version $Id$ 
+ */
+public class WPSTest {
+
+	@Before
+	public void addFunctions() {		
+		/**
+		 * We should add corefunctions jar to classpath before trying to
+		 * register it.
+		 */
 		AddFunctionsToPathUtility.getInstance().add();
-    }
-    
-	public void testWPSInfo(){
-		CstFunctionFactory.getInstance().registerCstPackage("eu.esdihumboldt.cst.corefunctions");			
-		
+	}
+
+	/**
+	 * Tests if at least some parameters are returned. It does not test if the parameters are correct.
+	 */
+	@Test
+	public void testWPSInfo() {
+		CstFunctionFactory.getInstance().registerCstPackage(
+				"eu.esdihumboldt.cst.corefunctions");
+
 		CstServiceCapabilitiesImpl service = new CstServiceCapabilitiesImpl();
-		for (Iterator<FunctionDescription> i = service.getFunctionDescriptions().iterator(); i.hasNext();){
-			
+		for (Iterator<FunctionDescription> i = service
+				.getFunctionDescriptions().iterator(); i.hasNext();) {
+
 			FunctionDescription fd = i.next();
-			System.out.println(fd.getFunctionId());
-			
-			
-			for (Iterator<String> j = fd.getParameterConfiguration().keySet().iterator();j.hasNext();){
+			assertNotNull(fd.getFunctionId());
+
+			for (Iterator<String> j = fd.getParameterConfiguration().keySet()
+					.iterator(); j.hasNext();) {
 				String name = j.next();
-				
+
 				fd.getParameterConfiguration().get(name);
-				System.out.println(name+" --- " +fd.getParameterConfiguration().get(name));
+				assertNotNull(fd.getParameterConfiguration().get(name));
 			}
-			
+
 		}
 	}
 }
