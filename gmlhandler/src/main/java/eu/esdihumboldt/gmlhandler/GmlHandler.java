1eb98b0d19a50fa92f450c69d98756d4d89e62fb
Anna Pitaev <anna.pitaev@logica.com>
2010-04-22 12:12:56 +0000

    GMLHandler initial commit.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3449 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ gmlhandler/src/main/java/eu/esdihumboldt/gmlhandler/GmlHandler.java
@@ -0,0 +1,37 @@
+package eu.esdihumboldt.gmlhandler;
+
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+/**
+ * <p>
+ * This class implements methods for handling of the gml data.
+ * </p>
+ * 
+ * <p>
+ * Supported GML versions:
+ * <ul>
+ * <li>gml_2</li>
+ * <li>gml_3_0</li>
+ * <li>gml_3_1</li>
+ * <li>gml_3_2</li> </li>
+ * </p>
+ * 
+ * 
+ * 
+ * @author Anna Pitaev
+ * @partner 04 / Logica
+ * @version $Id$
+ */
+public class GmlHandler {
+
+}
