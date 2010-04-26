afde6da60bcb1c58ff393049f11d53d7143e819f
Anna Pitaev <anna.pitaev@logica.com>
2010-04-26 10:21:15 +0000

    initial commit for the junit-tests.
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3490 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ gmlhandler/src/test/java/eu/esdihumboldt/gmlhandler/GmlHandlerTest.java
@@ -0,0 +1,128 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+package eu.esdihumboldt.gmlhandler;
+
+import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+import org.deegree.feature.types.ApplicationSchema;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.vividsolutions.jts.io.gml2.GMLHandler;
+
+/**
+ * 
+ * Test-class for the GMLHandler functionality.
+ * @author Anna Pitaev
+ * @partner 04 / Logica
+ * @version $Id$ 
+ */
+public class GmlHandlerTest {
+	
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger LOG = Logger.getLogger(GmlHandlerTest.class);
+	
+	/** application schema location */
+	private static final String SCHEMA_LOCATION_GML32 = "urn:x-inspire:specification:gmlas:HydroPhysicalWaters:3.0 http://svn.esdi-humboldt.eu/repo/humboldt2/trunk/cst/eu.esdihumboldt.cst.corefunctions/src/test/resource/inspire_v3.0_xsd/"+
+	"HydroPhysicalWaters.xsd";
+
+	/** http-based URL for the  application schema */
+	private static final String SCHEMA_URL = "http://svn.esdi-humboldt.eu/repo/humboldt2/trunk/cst/eu.esdihumboldt.cst.corefunctions/src/test/resource/inspire_v3.0_xsd/"+
+	"HydroPhysicalWaters.xsd";
+	/** source gml location */
+	private static final String GML32_INSTANCE_LOCATION = "http://svn.esdi-humboldt.eu/repo/humboldt2/branches/humboldt-deegree3/resource/sourceData/va_target_v3.gml";
+	
+	/** handler to proceed gmldata */
+	private static GmlHandler gmlHandler;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		//pre-define namespaces 
+		HashMap<String,String> namespaces = new HashMap<String, String>();
+		namespaces.put("gco","http://www.isotc211.org/2005/gco");
+		namespaces.put("gmd","http://www.isotc211.org/2005/gmd");
+		namespaces.put("gn","urn:x-inspire:specification:gmlas:GeographicalNames:3.0");
+		namespaces.put("hy-p","urn:x-inspire:specification:gmlas:HydroPhysicalWaters:3.0");
+		namespaces.put("hy","urn:x-inspire:specification:gmlas:HydroBase:3.0");
+		namespaces.put("base","urn:x-inspire:specification:gmlas:BaseTypes:3.2");
+		namespaces.put("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+		//set up GMLHandler with the test configuration
+		gmlHandler = new GmlHandler(GMLVersions.gml3_2_1, SCHEMA_URL, namespaces);
+		//set target gml destination
+		//gmlHandler.setTargetGmlUrl(targetGmlUrl);
+		
+		
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link eu.esdihumboldt.gmlhandler.GmlHandler#readSchema()}.
+	 */
+	@Test
+	public final void testReadSchema() {
+		//read application schema
+		try {
+			ApplicationSchema schema = gmlHandler.readSchema();
+			//compair the count of the FeatureTypes
+			assertEquals(23, schema.getFeatureTypes().length);
+		} catch (MalformedURLException e) {
+			
+			LOG.error(e.getStackTrace(),e);
+		} catch (ClassCastException e) {
+			
+			LOG.error(e.getStackTrace(),e);
+		} catch (ClassNotFoundException e) {
+			
+			LOG.error(e.getStackTrace(),e);
+		} catch (InstantiationException e) {
+			
+			LOG.error(e.getStackTrace(),e);
+		} catch (IllegalAccessException e) {
+			
+			LOG.error(e.getStackTrace(),e);
+		}
+		
+	}
+
+	/**
+	 * Test method for {@link eu.esdihumboldt.gmlhandler.GmlHandler#readFC()}.
+	 */
+	@Test
+	public final void testReadFC() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link eu.esdihumboldt.gmlhandler.GmlHandler#writeFC(org.deegree.feature.FeatureCollection)}.
+	 */
+	@Test
+	public final void testWriteFC() {
+		fail("Not yet implemented");
+	}
+
+}
