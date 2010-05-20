acb0f8a6f9c10b194ac8e38751813f902f8cabfc
Thorsten Reitz <thorsten.reitz@igd.fraunhofer.de>
2010-05-20 08:37:28 +0000

    skeleton of the HtmlMappingExporter project
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3635 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.htmlexporter/src/eu/esdihumboldt/hale/htmlexporter/HtmlMappingExportFactory.java
@@ -0,0 +1,36 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+package eu.esdihumboldt.hale.htmlexporter;
+
+import eu.esdihumboldt.goml.align.Alignment;
+import eu.esdihumboldt.hale.rcp.wizards.io.mappingexport.MappingExportException;
+import eu.esdihumboldt.hale.rcp.wizards.io.mappingexport.MappingExportProvider;
+
+/**
+ * Export a Mapping to HTML for documentation purposes.
+ * 
+ * @author Thorsten Reitz
+ * @version $Id$
+ */
+public class HtmlMappingExportFactory 
+	implements MappingExportProvider {
+
+
+	/* (non-Javadoc)
+	 * @see eu.esdihumboldt.hale.rcp.wizards.io.mappingexport.MappingExportProvider#export(eu.esdihumboldt.hale.rcp.wizards.io.mappingexport.Alignment, java.lang.String)
+	 */
+	public void export(Alignment al, String path) throws MappingExportException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
