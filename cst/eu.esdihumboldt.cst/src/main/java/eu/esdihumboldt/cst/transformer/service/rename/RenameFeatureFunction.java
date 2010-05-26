c12e94d61c2fcd8eb17a487ce93bd07c55e3761e
Simon Templer <simon.templer@igd.fraunhofer.de>
2010-05-26 12:25:53 +0000

    don't create the full feature structure in RenameFeatureFunction (use FeatureInspector to set/get nested properties)
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3710 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- cst/eu.esdihumboldt.cst/src/main/java/eu/esdihumboldt/cst/transformer/service/rename/RenameFeatureFunction.java
+++ cst/eu.esdihumboldt.cst/src/main/java/eu/esdihumboldt/cst/transformer/service/rename/RenameFeatureFunction.java
@@ -68,7 +68,7 @@ public class RenameFeatureFunction
 	 */
 	public Feature transform(Feature source, Feature target) {
 		SimpleFeatureType targetType = this.getTargetType(this.newName);
-		target = FeatureBuilder.buildFeature(targetType, source, true);
+		target = FeatureBuilder.buildFeature(targetType, source, false);
 		
 		// copy geometry by default if possible
 		this.copyGeometry(source, target);
