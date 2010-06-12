89995a6427066ae7181c70f240ba0a0592fc4e82
Ulrich Schäffler <Ulrich.Schaeffler@bv.tu-muenchen.de>
2010-06-12 19:52:41 +0000

    Changed Joiner and Test
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3826 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- cst/eu.esdihumboldt.cst/src/main/java/eu/esdihumboldt/cst/transformer/service/rename/FeatureSpatialJoiner.java
+++ cst/eu.esdihumboldt.cst/src/main/java/eu/esdihumboldt/cst/transformer/service/rename/FeatureSpatialJoiner.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 import org.geotools.data.DataUtilities;
 import org.geotools.data.DefaultQuery;
@@ -24,14 +25,21 @@ import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureCollections;
 import org.geotools.feature.FeatureIterator;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.cql2.CQLException;
 import org.opengis.feature.Feature;
+import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.PropertyDescriptor;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
 
 /**
 * @author Ulrich Schaeffler 
@@ -53,50 +61,55 @@ public class FeatureSpatialJoiner {
 	private String UOM = null;
 	
 	
-	public FeatureSpatialJoiner(String onAttributeName0, String onAttributename1, boolean spatial, String joinRule) {
-		this.onAttributeName0 = onAttributeName0;
-		this.onAttributeName1 = onAttributeName1;
+	public FeatureSpatialJoiner(String onAttributename0, String onAttributename1, boolean spatial, String joinRule) {
+		this.onAttributeName0 = onAttributename0;
+		this.onAttributeName1 = onAttributename1;
 		this.spatial = spatial;
 		this.joinRule = joinRule;
 		
-		
-		String[] jRule = joinRule.split(":");
-		if (jRule[0].equals("join")) {
-			if (jRule[1].startsWith("intersects")) {
-				this.joinRule = FeatureSpatialJoiner.INTERSECTS;
-			}
-			else if (jRule[1].startsWith("dWithin")) {
-				this.joinRule = FeatureSpatialJoiner.DWITHIN;
-				this.value = Double.valueOf(jRule[1].substring(
-						jRule[1].indexOf("(") + 1, 
-						jRule[1].indexOf(";")));
-				this.UOM = jRule[1].substring(
-						jRule[1].indexOf(";") + 1, 
-						jRule[1].indexOf(")"));
-			}
-			else if (jRule[1].startsWith("beyond")) {
-				this.joinRule = FeatureSpatialJoiner.BEYOND;
-				this.value = Double.valueOf(jRule[1].substring(
-						jRule[1].indexOf("(") + 1, 
-						jRule[1].indexOf(";")));
-				this.UOM = jRule[1].substring(
-						jRule[1].indexOf(";") + 1, 
-						jRule[1].indexOf(")"));
-			}
-			else if (jRule[1].startsWith("not")) {
-				this.joinRule = FeatureSpatialJoiner.NOT;
-				this.value = Double.valueOf(jRule[1].substring(
-						jRule[1].indexOf("(") + 1, 
-						jRule[1].indexOf(";")));
-				this.UOM = jRule[1].substring(
-						jRule[1].indexOf(";") + 1, 
-						jRule[1].indexOf(")"));
+		//TODO:Repalca join with enum
+		if (joinRule.startsWith("join")){
+			if (joinRule.contains(":")){
+				String[] jRule = joinRule.split(":");
+				if (jRule[1].startsWith("intersects")) {
+					this.joinRule = FeatureSpatialJoiner.INTERSECTS;
+				}
+				else if (jRule[1].startsWith("dWithin")) {
+					this.joinRule = FeatureSpatialJoiner.DWITHIN;
+					this.value = Double.valueOf(jRule[1].substring(
+							jRule[1].indexOf("(") + 1, 
+							jRule[1].indexOf(";")));
+					this.UOM = jRule[1].substring(
+							jRule[1].indexOf(";") + 1, 
+							jRule[1].indexOf(")"));
+				}
+				else if (jRule[1].startsWith("beyond")) {
+					this.joinRule = FeatureSpatialJoiner.BEYOND;
+					this.value = Double.valueOf(jRule[1].substring(
+							jRule[1].indexOf("(") + 1, 
+							jRule[1].indexOf(";")));
+					this.UOM = jRule[1].substring(
+							jRule[1].indexOf(";") + 1, 
+							jRule[1].indexOf(")"));
+				}
+				else if (jRule[1].startsWith("not")) {
+					this.joinRule = FeatureSpatialJoiner.NOT;
+					this.value = Double.valueOf(jRule[1].substring(
+							jRule[1].indexOf("(") + 1, 
+							jRule[1].indexOf(";")));
+					this.UOM = jRule[1].substring(
+							jRule[1].indexOf(";") + 1, 
+							jRule[1].indexOf(")"));
+				}
+				else{
+					throw new RuntimeException (jRule[1] + " is not a valid join rule.");
+				}
 			}
 			else{
-				throw new RuntimeException (jRule[1] + " is not a valid join rule.");
+				this.joinRule = "alpha";
 			}
-
 		}
+
 		else {
 			throw new RuntimeException("You can only create a " +
 				"Feature Joiner from a valid join rule.");
@@ -107,7 +120,6 @@ public class FeatureSpatialJoiner {
 	
 	public List<Feature> join (List<Collection<Feature>> sources, FeatureType targetType){
 		if (spatial){
-			System.out.println("spatial");
 			return spatialJoin (sources, targetType);
 		}
 		if (!spatial) return alphaJoin (sources, targetType);
@@ -115,132 +127,154 @@ public class FeatureSpatialJoiner {
 	}
 	
 	private List<Feature> alphaJoin (List<Collection<Feature>> sources, FeatureType targetType){
+
 		List<Feature> result = new ArrayList<Feature>();
 		
 		FeatureCollection sourceFeatures0 = FeatureCollections.newCollection(); 
 		for (Feature f : sources.get(0)){
 			sourceFeatures0.add(f);
 		}
-		FeatureSource source0 = DataUtilities.source( sourceFeatures0 );
-	 	FeatureType schema0 = sourceFeatures0.getSchema();
-        String typeName0 = schema0.getName().getLocalPart();
+		
 		FeatureCollection sourceFeatures1 = FeatureCollections.newCollection(); 
 		for (Feature f : sources.get(1)){
 			sourceFeatures1.add(f);
 		}
-		FeatureSource source1 = DataUtilities.source( sourceFeatures1 );
-	 	FeatureType schema1 = sourceFeatures1.getSchema();
+
+		FeatureType schema0 = sourceFeatures0.getSchema();
+        String typeName0 = schema0.getName().getLocalPart();
+		FeatureType schema1 = sourceFeatures1.getSchema();
         String typeName1 = schema1.getName().getLocalPart();
         
-        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);        
-        DefaultQuery outerSelection = new DefaultQuery( typeName0, Filter.INCLUDE, new String[]{ this.onAttributeName0 } );        
-        FeatureCollection<SimpleFeatureType, SimpleFeature> outerFeatures = null;
-        try {
-        	outerFeatures = source0.getFeatures( outerSelection );
-        } catch (IOException e) {
-        	// TODO Auto-generated catch block
-        	e.printStackTrace();
-        }
-        FeatureIterator<SimpleFeature> iterator = outerFeatures.features();
-        try {
+        //Build synthetic FT from source0 and source 1
+        SimpleFeatureType joinedFt = null;
+		SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
+		ftbuilder.setName("Joined_" + typeName0+ "_"+typeName1);
+		//TODO: Which namespace for synthetic FT?
+		ftbuilder.setNamespaceURI(schema0.getName().getNamespaceURI());
+		for (PropertyDescriptor p :schema0.getDescriptors()){
+			ftbuilder.add(p.getName().getLocalPart(), p.getType().getBinding());
+		}
+		for (PropertyDescriptor p :schema1.getDescriptors()){
+			ftbuilder.add(p.getName().getLocalPart(), p.getType().getBinding());
+		}
+		joinedFt = ftbuilder.buildFeatureType();
+		
+		FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null); 
+		SimpleFeatureBuilder builder = new SimpleFeatureBuilder(joinedFt);
+		
+        FeatureIterator iterator = sourceFeatures0.features();
+        try{
         	while( iterator.hasNext() ){
-        		SimpleFeature feature = (SimpleFeature) iterator.next();
-        		System.out.println("erstes outer "+feature) ;
-        		try {
+        		Feature feature = (Feature) iterator.next();
+        		Filter	innerFilter = ff.equals( ff.property(this.onAttributeName1), ff.literal( feature.getProperty(this.onAttributeName0).getValue() ));
 
-        			Filter innerFilter=  ff.equal( ff.property(this.onAttributeName1), ff.literal( feature.getAttribute(this.onAttributeName0) ));
-        			
-        			DefaultQuery innerQuery = new DefaultQuery( typeName1, innerFilter, new String[]{ this.onAttributeName1 } );
-        			FeatureCollection join = source1.getFeatures( innerQuery );
-        			System.out.println("Join Size " +join.size());
-        			for ( Iterator<Feature> i = join.iterator(); i.hasNext(); )
-        			{
-        				System.out.println("Add result "+ i.next());
-        			  result.add(i.next());
-        			}
-
-        		}
-        		catch( Exception skipBadData ){
+        		FeatureCollection join = sourceFeatures1.subCollection(innerFilter);
+        		if (join.size()>0){
+        			FeatureIterator iteratorJoin = join.features();
+        			try{
+        		        	while( iteratorJoin.hasNext() ){
+        		        		Feature featureJoin = (Feature) iteratorJoin.next();
+        		        		for (Property p :feature.getProperties()){
+        		        			builder.add(p.getValue());
+        		        		}
+        		        		for (Property p :featureJoin.getProperties()){
+        		        			builder.add(p.getValue());
+        		        		}
+        		        		
+        		        		Feature resultJoinedFeature = builder.buildFeature(UUID.randomUUID().toString());
+        		        		result.add(resultJoinedFeature);
+        		        	}
+        		        }
+        		        finally {
+        		        	iteratorJoin.close();
+        		        }
         		}
         	}
         }
         finally {
-        	iterator.close();            
-        }
-
+            iterator.close();
+       }
     return result;
         
 	}
 	
 	
-	
-	private List<Feature> spatialJoin(List<Collection<Feature>> sources, FeatureType targetType) {
+	private List<Feature> spatialJoin (List<Collection<Feature>> sources, FeatureType targetType){
 		
-			List<Feature> result = new ArrayList<Feature>();
-			
-			FeatureCollection sourceFeatures0 = FeatureCollections.newCollection(); 
-			for (Feature f : sources.get(0)){
-				sourceFeatures0.add(f);
-			}
-			FeatureSource source0 = DataUtilities.source( sourceFeatures0 );
-		 	FeatureType schema0 = sourceFeatures0.getSchema();
-	        String typeName0 = schema0.getName().getLocalPart();
-	        String geomName0 = schema0.getGeometryDescriptor().getLocalName();
-	        System.out.println("GeomName0 " + geomName0);
-	
-	        
-			FeatureCollection sourceFeatures1 = FeatureCollections.newCollection(); 
-			for (Feature f : sources.get(1)){
-				sourceFeatures1.add(f);
-			}
-			FeatureSource source1 = DataUtilities.source( sourceFeatures1 );
-		 	FeatureType schema1 = sourceFeatures1.getSchema();
-	        String typeName1 = schema1.getName().getLocalPart();
-	        String geomName1 = schema1.getGeometryDescriptor().getLocalName();   
+		List<Feature> result = new ArrayList<Feature>();
 		
-	        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);        
-	        DefaultQuery outerGeometry = new DefaultQuery( typeName0, Filter.INCLUDE, new String[]{ geomName0 } );        
-	        FeatureCollection<SimpleFeatureType, SimpleFeature> outerFeatures = null;
-	        try {
-	        	outerFeatures = source0.getFeatures( outerGeometry );
-	        } catch (IOException e) {
-	        	// TODO Auto-generated catch block
-	        	e.printStackTrace();
-	        }
-	        FeatureIterator<SimpleFeature> iterator = outerFeatures.features();
-	        try {
-	        	while( iterator.hasNext() ){
-	        		SimpleFeature feature = (SimpleFeature) iterator.next();
-	        		System.out.println("erstes outer "+feature) ;
-	        		try {
-	        			Geometry geometry = (Geometry) feature.getDefaultGeometry();
-	    				System.out.println("DefGeom " + geometry);
-	        			Filter innerFilter= null;
-	        			if (this.joinRule.equals(FeatureSpatialJoiner.INTERSECTS))innerFilter= ff.intersects( ff.property(geomName1), ff.literal( geometry ));
-	        			else if (this.joinRule.equals(FeatureSpatialJoiner.DWITHIN))innerFilter= ff.dwithin(ff.property(geomName1), ff.literal( geometry ),1.0,"km");
-	        			else if (this.joinRule.equals(FeatureSpatialJoiner.BEYOND))innerFilter=  ff.beyond(ff.property(geomName1), ff.literal( geometry ),1.0,"km");
-	        			else if (this.joinRule.equals(FeatureSpatialJoiner.NOT))innerFilter=  ff.not( ff.disjoint(ff.property(geomName1), ff.literal( geometry )) );
-                    
-	        			DefaultQuery innerQuery = new DefaultQuery( typeName1, innerFilter, new String[]{ geomName1 } );
-	        			FeatureCollection join = source1.getFeatures( innerQuery );
-	        			System.out.println("Join Size " +join.size());
-	        			for ( Iterator<Feature> i = join.iterator(); i.hasNext(); )
-	        			{
-	        				System.out.println("Add result "+ i.next());
-	        			  result.add(i.next());
-	        			}
+		FeatureCollection sourceFeatures0 = FeatureCollections.newCollection(); 
+		for (Feature f : sources.get(0)){
+			sourceFeatures0.add(f);
+		}
+		
+		FeatureCollection sourceFeatures1 = FeatureCollections.newCollection(); 
+		for (Feature f : sources.get(1)){
+			sourceFeatures1.add(f);
+		}
 
-	        		}
-	        		catch( Exception skipBadData ){
-	        		}
-	        	}
-	        }
-	        finally {
-	        	iterator.close();            
-	        }
-
-        return result;
+		FeatureType schema0 = sourceFeatures0.getSchema();
+        String typeName0 = schema0.getName().getLocalPart();
+		FeatureType schema1 = sourceFeatures1.getSchema();
+        String typeName1 = schema1.getName().getLocalPart();
+        
+        //Build synthetic FT from source0 and source 1
+        SimpleFeatureType joinedFt = null;
+		SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
+		ftbuilder.setName("Joined_" + typeName0+ "_"+typeName1);
+		//TODO: Which namespace for synthetic FT?
+		ftbuilder.setNamespaceURI(schema0.getName().getNamespaceURI());
+		for (PropertyDescriptor p :schema0.getDescriptors()){
+			ftbuilder.add(p.getName().getLocalPart(), p.getType().getBinding());
+		}
+		for (PropertyDescriptor p :schema1.getDescriptors()){
+			ftbuilder.add(p.getName().getLocalPart(), p.getType().getBinding());
+		}
+		joinedFt = ftbuilder.buildFeatureType();
+        
+		
+		FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null); 
+		SimpleFeatureBuilder builder = new SimpleFeatureBuilder(joinedFt);
+		
+        FeatureIterator iterator = sourceFeatures0.features();
+        try{
+        	while( iterator.hasNext() ){
+        		Feature feature = (Feature) iterator.next();
+        		Filter	innerFilter = ff.equals( ff.property(this.onAttributeName1), ff.literal( feature.getProperty(this.onAttributeName0).getValue() ));
+        		if (this.joinRule.equals(FeatureSpatialJoiner.INTERSECTS))innerFilter= ff.intersects( ff.property(this.onAttributeName1), ff.literal( (Geometry)feature.getProperty(this.onAttributeName0).getValue() ));
+    			else if (this.joinRule.equals(FeatureSpatialJoiner.DWITHIN))innerFilter= ff.dwithin(ff.property(this.onAttributeName1), ff.literal( (Geometry)feature.getProperty(this.onAttributeName0).getValue() ),1.0,"km");
+    			else if (this.joinRule.equals(FeatureSpatialJoiner.BEYOND))innerFilter=  ff.beyond(ff.property(this.onAttributeName1), ff.literal( (Geometry)feature.getProperty(this.onAttributeName0).getValue() ),1.0,"km");
+    			else if (this.joinRule.equals(FeatureSpatialJoiner.NOT))innerFilter=  ff.not( ff.disjoint(ff.property(this.onAttributeName1), ff.literal( (Geometry)feature.getProperty(this.onAttributeName0).getValue() )) );
+        		FeatureCollection join = sourceFeatures1.subCollection(innerFilter);
+        		if (join.size()>0){
+        			FeatureIterator iteratorJoin = join.features();
+        			try{
+        		        	while( iteratorJoin.hasNext() ){
+        		        		Feature featureJoin = (Feature) iteratorJoin.next();
+        		        		for (Property p :feature.getProperties()){
+        		        			builder.add(p.getValue());
+        		        		}
+        		        		for (Property p :featureJoin.getProperties()){
+        		        			builder.add(p.getValue());
+        		        		}
+        		        		
+        		        		Feature resultJoinedFeature = builder.buildFeature(UUID.randomUUID().toString());
+        		        		result.add(resultJoinedFeature);
+        		        	}
+        		        }
+        		        finally {
+        		        	iteratorJoin.close();
+        		        }
+        		}
+        	}
+        }
+        finally {
+            iterator.close();
+       }
+    return result;
+        
 	}
+	
 
 }
 
--- cst/eu.esdihumboldt.cst/src/test/java/eu/esdihumboldt/cst/service/rename/JoinTest.java
+++ cst/eu.esdihumboldt.cst/src/test/java/eu/esdihumboldt/cst/service/rename/JoinTest.java
@@ -2,12 +2,17 @@ package eu.esdihumboldt.cst.service.rename;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.geotools.data.DataStoreFactorySpi;
 import org.geotools.data.DataStoreFinder;
 import org.geotools.data.DataUtilities;
@@ -15,10 +20,12 @@ import org.geotools.data.FeatureSource;
 import org.geotools.data.store.ContentFeatureStore;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.PropertyDescriptor;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -27,83 +34,169 @@ import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 
 import eu.esdihumboldt.cst.NameHelper;
+import eu.esdihumboldt.cst.service.impl.SchemaTranslationControllerAggregateTest;
 import eu.esdihumboldt.cst.transformer.service.rename.FeatureAggregator;
 import eu.esdihumboldt.cst.transformer.service.rename.FeatureSpatialJoiner;
 
 public class JoinTest {
+	private static Logger _log = Logger.getLogger(JoinTest.class);
 	
-	
+	@BeforeClass
+	public static void testSchemaTranslationController() throws URISyntaxException {
+		// set up log4j logger manually if necessary
+		if (!Logger.getRootLogger().getAllAppenders().hasMoreElements()) {
+			Appender appender = new ConsoleAppender(
+					new PatternLayout("%d{ISO8601} %5p %C{1}:%L %m%n"), 
+					 ConsoleAppender.SYSTEM_OUT );
+			appender.setName("A1");
+			Logger.getRootLogger().addAppender(appender);
+		}
+	}
 	
 	@Test
-	public void testSpatialJoin(){
-	
+	public void testAplhaJoin(){
 		GeometryFactory geomFactory = new GeometryFactory();
-//		SimpleFeatureType ft = null;
-//		SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
-//		ftbuilder.setName(NameHelper.sourceLocalname);
-//		ftbuilder.setNamespaceURI(NameHelper.sourceNamespace);
-//		ftbuilder.add("SomePoint", Point.class);
-//		ftbuilder.add("SomeValues", Integer.class);
-//		ft = ftbuilder.buildFeatureType();
-//		SimpleFeatureBuilder builder = new SimpleFeatureBuilder(ft);
-//		Feature source1 = SimpleFeatureBuilder.build(
-//				ft, new Object[]{geomFactory.createPoint(
-//						new Coordinate(1.0, 1.0)), new Integer (2)}, UUID.randomUUID().toString());
-//		Feature source2 = SimpleFeatureBuilder.build(
-//				ft, new Object[]{geomFactory.createPoint(
-//						new Coordinate(1.0,1.0)), new Integer (3)}, UUID.randomUUID().toString());
-//		
-		
 		SimpleFeatureType ft = null;
 		SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
 		ftbuilder.setName("FirstType");
 		ftbuilder.setNamespaceURI("http://first.de");
-		ftbuilder.add("1_Polygon", Polygon.class);
-		ftbuilder.add("SomeValues", Integer.class);
+		ftbuilder.add("1_P", Point.class);
+		ftbuilder.add("SomeValues0", Integer.class);
 		ft = ftbuilder.buildFeatureType();
 		SimpleFeatureBuilder builder = new SimpleFeatureBuilder(ft);
 		Feature sourceFt1_1 = builder.build(
-				ft, new Object[]{geomFactory.createPolygon(
-						geomFactory.createLinearRing(new Coordinate[] {new Coordinate (0.0, 0,0), new Coordinate(2.0, 0.0),new Coordinate(0.0, 2.0), new Coordinate(2.0, 2.0), new Coordinate(0.0, 0.0)}), null), new Integer (2)}, UUID.randomUUID().toString());
-						
-		
+				ft, new Object[]{geomFactory.createPoint(
+						new Coordinate(20.0,20.0)), new Integer (2)}, "first");
+		Feature sourceFt1_2 = builder.build(
+				ft, new Object[]{geomFactory.createPoint(
+						new Coordinate(100.0,200.0)), new Integer (3)}, "second");
+
 		SimpleFeatureType ft2 = null;
 		SimpleFeatureTypeBuilder ftbuilder2 = new SimpleFeatureTypeBuilder();
 		ftbuilder2.setName("SecondType");
 		ftbuilder2.setNamespaceURI("http://second.de");
-		ftbuilder2.add("SomePolygon", Polygon.class);
-		ftbuilder2.add("SomeValues", Integer.class);
+		ftbuilder2.add("SomeP", Point.class);
+		ftbuilder2.add("SomeValues1", Integer.class);
 		ft2 = ftbuilder2.buildFeatureType();
 		SimpleFeatureBuilder builder2 = new SimpleFeatureBuilder(ft2);
 		Feature sourceFt2_1 = builder2.build(
-				ft, new Object[]{geomFactory.createPolygon(
-						geomFactory.createLinearRing(new Coordinate[] {new Coordinate (0.0, 0,0), new Coordinate(1.0, 0.0), new Coordinate(1.0, 1.0), new Coordinate(0.0, 1.0), new Coordinate(0.0, 0.0)}), null), new Integer (2)}, UUID.randomUUID().toString());
+				ft2, new Object[]{geomFactory.createPoint(
+						new Coordinate(1.0,2.0)),new Integer (2)}, UUID.randomUUID().toString());
+		Feature sourceFt2_2 = builder2.build(
+				ft2, new Object[]{geomFactory.createPoint(
+						new Coordinate(1.0,2.0)), new Integer (10)}, UUID.randomUUID().toString());
+		Feature sourceFt2_3 = builder2.build(
+				ft2, new Object[]{geomFactory.createPoint(
+						new Coordinate(6.0,6.0)),new Integer (2)}, UUID.randomUUID().toString());
 
-				
-						
-
-		
-		
 		Collection<Feature> features1 = new ArrayList<Feature>();
 		features1.add(sourceFt1_1);
+		features1.add(sourceFt1_2);
 
 		Collection<Feature> features2 = new ArrayList<Feature>();
 		features2.add(sourceFt2_1);
+		features2.add(sourceFt2_2);
+		features2.add(sourceFt2_3);
 		
 		List<Collection<Feature>> all = new ArrayList<Collection<Feature>>();
 		all.add(features1);
 		all.add(features2);
+	
 
-		FeatureSpatialJoiner fj = new FeatureSpatialJoiner("SomeValues",null, true, "join:intersects");
+		FeatureSpatialJoiner fj = new FeatureSpatialJoiner("SomeValues0","SomeValues1", false, "join");
 		List<Feature>results = fj.join(all, ft2);
-		System.out.println("TESTSIZE " +results.size());
+		_log.info("JOINSIZE " +results.size());
 
+		assertTrue(results.size()==2);
+		SimpleFeatureType joinedFt = null;
+		SimpleFeatureTypeBuilder ftbuilder3 = new SimpleFeatureTypeBuilder();
+		ftbuilder3.setName("Joined_" + "FirstType"+ "_"+"SecondType");
+		ftbuilder3.setNamespaceURI("http://first.de");
+		ftbuilder3.add("1_P", Point.class);
+		ftbuilder3.add("SomeValues0", Integer.class);
+		ftbuilder3.add("SomeP", Point.class);
+		ftbuilder3.add("SomeValues1", Integer.class);
+		joinedFt = ftbuilder3.buildFeatureType();
 		for (Feature f : results){
-			System.out.println("TEST");
-			System.out.println(f.getType().getDescriptors());
+			_log.info("RESULT__________________________________");
+			_log.info("RESULT " + f);
+			assertTrue(f.getType().equals(joinedFt));
 		}
 
-//		assertTrue(results.get(0).getProperty("SomePoint").getType().getBinding().equals(MultiPoint.class));
+	}
+	
+
+	
+	@Test
+	public void testSpatialJoin(){
+		GeometryFactory geomFactory = new GeometryFactory();
+		SimpleFeatureType ft = null;
+		SimpleFeatureTypeBuilder ftbuilder = new SimpleFeatureTypeBuilder();
+		ftbuilder.setName("FirstType");
+		ftbuilder.setNamespaceURI("http://first.de");
+		ftbuilder.add("1_P", Polygon.class);
+		ftbuilder.add("SomeValues0", Integer.class);
+		ft = ftbuilder.buildFeatureType();
+		SimpleFeatureBuilder builder = new SimpleFeatureBuilder(ft);
+		Feature sourceFt1_1 = builder.build(
+				ft, new Object[]{geomFactory.createPolygon(
+						geomFactory.createLinearRing(new Coordinate[] {new Coordinate (0.0, 0.0), new Coordinate(1.0, 0.0),new Coordinate(0.0, 1.0), new Coordinate(1.0, 1.0), new Coordinate(0.0, 0.0)}), null), new Integer (2)}, "first");
+		Feature sourceFt1_2 = builder.build(
+				ft, new Object[]{geomFactory.createPolygon(
+						geomFactory.createLinearRing(new Coordinate[] {new Coordinate (100.0, 100.0), new Coordinate(200.0, 100.0),new Coordinate(100.0, 200.0), new Coordinate(200.0, 200.0), new Coordinate(100.0, 100.0)}), null), new Integer (3)}, "second");
+
+		SimpleFeatureType ft2 = null;
+		SimpleFeatureTypeBuilder ftbuilder2 = new SimpleFeatureTypeBuilder();
+		ftbuilder2.setName("SecondType");
+		ftbuilder2.setNamespaceURI("http://second.de");
+		ftbuilder2.add("SomeP", Polygon.class);
+		ftbuilder2.add("SomeValues1", Integer.class);
+		ft2 = ftbuilder2.buildFeatureType();
+		SimpleFeatureBuilder builder2 = new SimpleFeatureBuilder(ft2);
+		Feature sourceFt2_1 = builder2.build(
+				ft2, new Object[]{geomFactory.createPolygon(
+						geomFactory.createLinearRing(new Coordinate[] {new Coordinate (0.0, 0.0), new Coordinate(2.0, 0.0),new Coordinate(0.0, 2.0), new Coordinate(2.0, 2.0), new Coordinate(0.0, 0.0)}), null),new Integer (2)}, UUID.randomUUID().toString());
+		Feature sourceFt2_2 = builder2.build(
+				ft2, new Object[]{geomFactory.createPolygon(
+						geomFactory.createLinearRing(new Coordinate[] {new Coordinate (0.0, 0.0), new Coordinate(2.0, 0.0),new Coordinate(0.0, 2.0), new Coordinate(2.0, 2.0), new Coordinate(0.0, 0.0)}), null), new Integer (10)}, UUID.randomUUID().toString());
+		Feature sourceFt2_3 = builder2.build(
+				ft2, new Object[]{geomFactory.createPolygon(
+						geomFactory.createLinearRing(new Coordinate[] {new Coordinate (300.0, 300.0), new Coordinate(400.0, 400.0),new Coordinate(400.0, 402.0), new Coordinate(402.0, 402.0), new Coordinate(300.0, 300.0)}), null),new Integer (2)}, UUID.randomUUID().toString());
+
+		Collection<Feature> features1 = new ArrayList<Feature>();
+		features1.add(sourceFt1_1);
+		features1.add(sourceFt1_2);
+
+		Collection<Feature> features2 = new ArrayList<Feature>();
+		features2.add(sourceFt2_1);
+		features2.add(sourceFt2_2);
+		features2.add(sourceFt2_3);
+		
+		List<Collection<Feature>> all = new ArrayList<Collection<Feature>>();
+		all.add(features1);
+		all.add(features2);
+	
+
+		FeatureSpatialJoiner fj = new FeatureSpatialJoiner("1_P","SomeP", true, "join:intersects");
+		List<Feature>results = fj.join(all, ft2);
+		_log.info("JoinSIZE " +results.size());
+
+		assertTrue(results.size()==2);
+		SimpleFeatureType joinedFt = null;
+		SimpleFeatureTypeBuilder ftbuilder3 = new SimpleFeatureTypeBuilder();
+		ftbuilder3.setName("Joined_" + "FirstType"+ "_"+"SecondType");
+		ftbuilder3.setNamespaceURI("http://first.de");
+		ftbuilder3.add("1_P", Polygon.class);
+		ftbuilder3.add("SomeValues0", Integer.class);
+		ftbuilder3.add("SomeP", Polygon.class);
+		ftbuilder3.add("SomeValues1", Integer.class);
+		joinedFt = ftbuilder3.buildFeatureType();
+		for (Feature f : results){
+			_log.info("RESULT__________________________________");
+			_log.info("RESULT " + f);
+			assertTrue(f.getType().equals(joinedFt));
+		}
+
 	}
 
 }
