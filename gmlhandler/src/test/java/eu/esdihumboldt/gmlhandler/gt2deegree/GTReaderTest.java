5fb3edd3f204beae6b6b195f4ed5e069e5692b7b
Anna Pitaev <anna.pitaev@logica.com>
2010-06-21 08:31:03 +0000

    added  a testclass  for Geotools-Deegree Mapping
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3902 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ gmlhandler/src/test/java/eu/esdihumboldt/gmlhandler/gt2deegree/GTReaderTest.java
@@ -0,0 +1,44 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+package eu.esdihumboldt.gmlhandler.gt2deegree;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ * It's a TestCase for Geotools-Deegree Translation
+ *
+ * @author Anna Pitaev
+ * @partner 04 / Logica
+ * @version $Id$ 
+ */
+public class GTReaderTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		//Read XmlAttributes from the Geotools UserData
+		//f.getUserData().get("XmlAttributes");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+}
