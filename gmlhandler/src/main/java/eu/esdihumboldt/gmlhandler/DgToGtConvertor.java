b607cb333736420e6aef652eaf6ff7241236ff0e
Anna Pitaev <anna.pitaev@logica.com>
2010-06-24 09:43:13 +0000

    added initial structure for GT-->Deegree Mapping
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3935 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- gmlhandler/src/main/java/eu/esdihumboldt/gmlhandler/DgToGtConvertor.java
+++ gmlhandler/src/main/java/eu/esdihumboldt/gmlhandler/DgToGtConvertor.java
@@ -1,5 +1,12 @@
 package eu.esdihumboldt.gmlhandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.deegree.feature.types.GenericFeatureType;
+import org.deegree.feature.types.property.SimplePropertyType;
 import org.geotools.data.DataUtilities;
 import org.geotools.factory.FactoryRegistryException;
 import org.geotools.feature.FeatureCollection;
@@ -11,6 +18,10 @@ import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.Name;
+import org.opengis.feature.type.PropertyDescriptor;
+import org.opengis.feature.type.PropertyType;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -22,10 +33,34 @@ public class DgToGtConvertor {
 			org.geotools.feature.FeatureCollection fc) {
 		
 		
-		
+		org.deegree.feature.Feature dgFeature = null;
+		GenericFeatureType dfFT = null;
+		List<org.deegree.feature.property.Property> dgProperties = null;
+		List<org.deegree.feature.types.property.PropertyType> dgPropertyTypes = null;
 		for (FeatureIterator i = fc.features(); i.hasNext() ;){
-			  Feature feature = i.next();	
+			dgPropertyTypes = new ArrayList<org.deegree.feature.types.property.PropertyType> (); 
+			Feature feature = i.next();
 			  System.out.println(feature.getDefaultGeometryProperty().getType());
+			  FeatureType gtFT = feature.getType();
+			  //convert gtFT to gtFT
+			  //1. GenericFeatureType
+			  //1.0 QName
+			  Name gtFTName = gtFT.getName();
+			  QName ftName = new QName(gtFTName.getNamespaceURI(), gtFTName.getLocalPart());
+			  //1.1 List<PropertyType>
+			  for (PropertyDescriptor gtPD :gtFT.getDescriptors()){
+				 PropertyType gtPT = gtPD.getType();
+				 Name gtPTName = gtPT.getName();
+				 
+				 // create deegree PropertyType
+				 org.deegree.feature.types.property.PropertyType dfPT = new SimplePropertyType(ftName, 1, 1, null, false, dgPropertyTypes);
+			  }
+			  //1.2 boolean isAbstract
+			  boolean isAbstract = gtFT.isAbstract();
+			  //2. Feature id
+			  //3. List<Property>
+			  //4. GMLVersion 
+			
 			
 		}
 		return null;
