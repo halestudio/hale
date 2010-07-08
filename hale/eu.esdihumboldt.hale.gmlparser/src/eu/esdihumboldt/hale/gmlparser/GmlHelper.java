546840bfc3c5eb86ff50265972381484033cf632
Simon Templer <simon.templer@igd.fraunhofer.de>
2010-07-08 08:36:29 +0000

    added GmlHelper class to gml parser project
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@3980 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.gmlparser/src/eu/esdihumboldt/hale/gmlparser/GmlHelper.java
@@ -0,0 +1,90 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+package eu.esdihumboldt.hale.gmlparser;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.geotools.feature.FeatureCollection;
+import org.geotools.gml3.GMLConfiguration;
+import org.geotools.xml.Configuration;
+import org.opengis.feature.Feature;
+import org.opengis.feature.type.FeatureType;
+
+/**
+ * 
+ *
+ * @author Simon Templer
+ * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+ * @version $Id$ 
+ */
+public class GmlHelper {
+	
+	/**
+	 * Configuration types
+	 */
+	public enum ConfigurationType {
+		/** GML 2.x */
+		GML2,
+		/** GML 3.x */
+		GML3,
+		/** GML 3.2 */
+		GML3_2
+	}
+
+	/**
+	 * Load GML from an input stream
+	 * 
+	 * @param xml the XML input stream
+	 * @param type the configuration type, defaults to GML3
+	 * 
+	 * @return the loaded feature collection or <code>null</code>
+	 */
+	@SuppressWarnings("unchecked")
+	public static FeatureCollection<FeatureType, Feature> loadGml(InputStream xml, 
+			ConfigurationType type) {
+		try {
+			Configuration conf;
+			switch (type) {
+			case GML2:
+				conf = new org.geotools.gml2.GMLConfiguration();
+				break;
+			case GML3_2:
+				conf = new org.geotools.gml3.v3_2.GMLConfiguration();
+				break;
+			case GML3: // fall through
+			default: // default to GML3
+				conf = new GMLConfiguration();
+			}
+			HaleGMLParser parser = new HaleGMLParser(conf);
+			Object result = parser.parse(xml);
+			
+			if (result instanceof FeatureCollection<?, ?>) {
+				return (FeatureCollection<FeatureType, Feature>) result;
+			}
+			else if (result instanceof Map<?, ?>) {
+				// extract features from Map
+				List<Feature> features = (List<Feature>) ((Map<?, ?>)result).get("featureMember");
+				CstFeatureCollection fc = new CstFeatureCollection();
+				fc.addAll(features);
+				return fc;
+			}
+			
+			return null;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} 
+	}
+
+}
