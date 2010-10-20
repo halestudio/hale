a54ea1fc1810779dabbc67bd008ce2909bca0a8b
Simon Templer <simon.templer@igd.fraunhofer.de>
2010-10-20 14:14:29 +0000

    reload target schema and data when a default geometry in the target schema is changed
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@4245 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/eu.esdihumboldt.hale/src/eu/esdihumboldt/hale/rcp/views/model/SetAsDefaultGeometryAction.java
+++ hale/eu.esdihumboldt.hale/src/eu/esdihumboldt/hale/rcp/views/model/SetAsDefaultGeometryAction.java
@@ -23,6 +23,9 @@ import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
+import org.geotools.feature.FeatureCollection;
+import org.opengis.feature.Feature;
+import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.Name;
 
 import de.cs3d.util.logging.ALogger;
@@ -119,6 +122,67 @@ public class SetAsDefaultGeometryAction extends Action {
 		}
 
 	}
+	
+	/**
+	 * Reload target schema and transformed instances
+	 */
+	public static class ReloadTargetRunner implements IRunnableWithProgress {
+
+		/**
+		 * @see org.eclipse.jface.operation.IRunnableWithProgress#run(org.eclipse.core.runtime.IProgressMonitor)
+		 */
+		@Override
+		public void run(final IProgressMonitor monitor)
+				throws InvocationTargetException, InterruptedException {
+			monitor.beginTask("Reloading target schema and data", IProgressMonitor.UNKNOWN);
+			ATransaction trans = log.begin("Reloading target schema and data");
+			try {
+				// reload source schema and instance data
+				InstanceService instanceService = (InstanceService) PlatformUI.getWorkbench().getService(InstanceService.class);
+				ProjectService projectService = (ProjectService) PlatformUI.getWorkbench().getService(ProjectService.class);
+				SchemaService schemaService = (SchemaService) PlatformUI.getWorkbench().getService(SchemaService.class);
+
+				// remember instance data
+				FeatureCollection<FeatureType, Feature> instances = instanceService.getFeatures(DatasetType.reference);
+				
+				// clean transformed data
+				instanceService.cleanInstances();
+	
+				// remember target schema info
+				String schemaLoc = projectService.getTargetSchemaPath();
+				
+				// clean target schema
+				schemaService.cleanTargetSchema();
+				
+				// reload schema
+				try {
+					schemaService.loadSchema(new URI(schemaLoc), (String)null, SchemaType.TARGET, new ProgressIndicator() {
+						
+						@Override
+						public void setProgress(int percent) {
+							// do nothing
+						}
+						
+						@Override
+						public void setCurrentTask(String taskName) {
+							monitor.subTask(taskName);
+						}
+					});
+				} catch (Exception e) {
+					log.userError("Error reloading target schema", e);
+				}
+				
+				// trigger retransformation
+				monitor.subTask("Transforming source data");
+				// use a trick to trigger the transformation - add empty feature collection
+				instanceService.addInstances(DatasetType.reference, instances);
+			} finally {
+				trans.end();
+				monitor.done();
+			}
+		}
+
+	}
 
 	private final SchemaItem item;
 	
@@ -145,9 +209,9 @@ public class SetAsDefaultGeometryAction extends Action {
 		IRunnableWithProgress runner = null;
 		Display display = Display.getCurrent();
 		
-		// reload schema and instances
 		switch (item.getSchemaType()) {
 		case SOURCE:
+			// reload schema and instances
 			if (!MessageDialog.openQuestion(display.getActiveShell(), "Change default geometry", 
 					"Reloading source schema and data is needed when changing" +
 					" the default geometry. Do you want to proceed?")) {
@@ -157,6 +221,12 @@ public class SetAsDefaultGeometryAction extends Action {
 			break;
 		case TARGET:
 			// reload target schema and transformations
+			if (!MessageDialog.openQuestion(display.getActiveShell(), "Change default geometry", 
+					"Reloading target schema and data is needed when changing" +
+					" the default geometry. Do you want to proceed?")) {
+				return;
+			}
+			runner = new ReloadTargetRunner();
 			break;
 		}
 		
