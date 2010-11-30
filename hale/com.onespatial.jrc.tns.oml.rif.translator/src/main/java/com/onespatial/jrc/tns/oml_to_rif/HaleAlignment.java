a5b7c2785746a3cc28e2ac5716e9baff69624e8d
Simon Templer <simon.templer@igd.fraunhofer.de>
2010-11-30 10:39:12 +0000

    harmonized file comments
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@4277 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/com.onespatial.jrc.tns.oml.rif.translator/src/main/java/com/onespatial/jrc/tns/oml_to_rif/HaleAlignment.java
+++ hale/com.onespatial.jrc.tns.oml.rif.translator/src/main/java/com/onespatial/jrc/tns/oml_to_rif/HaleAlignment.java
@@ -9,7 +9,6 @@
  * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
  * (c) the HUMBOLDT Consortium, 2007 to 2010.
  */
-
 package com.onespatial.jrc.tns.oml_to_rif;
 
 import java.util.Collection;
--- hale/com.onespatial.jrc.tns.oml.rif.translator/src/main/java/com/onespatial/jrc/tns/oml_to_rif/model/rif/filter/nonterminal/comparison/LessThanNode.java
+++ hale/com.onespatial.jrc.tns.oml.rif.translator/src/main/java/com/onespatial/jrc/tns/oml_to_rif/model/rif/filter/nonterminal/comparison/LessThanNode.java
@@ -1,5 +1,13 @@
 /*
- * Copyright (c) 1Spatial Group Ltd.
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
  */
 package com.onespatial.jrc.tns.oml_to_rif.model.rif.filter.nonterminal.comparison;
 
