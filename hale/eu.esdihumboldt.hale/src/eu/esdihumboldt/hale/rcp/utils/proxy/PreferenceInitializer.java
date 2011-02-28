d4df3a7c05651c41dfec826c301ac830b035d23a
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-02-28 11:00:25 +0000

    fixed file comments, removed obsolete messages
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/trunk@4591 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/eu.esdihumboldt.hale/src/eu/esdihumboldt/hale/rcp/utils/proxy/PreferenceInitializer.java
+++ hale/eu.esdihumboldt.hale/src/eu/esdihumboldt/hale/rcp/utils/proxy/PreferenceInitializer.java
@@ -1,11 +1,14 @@
-// Fraunhofer Institute for Computer Graphics Research (IGD)
-// Department Graphical Information Systems (GIS)
-//
-// Copyright (c) 2004-2010 Fraunhofer IGD. All rights reserved.
-//
-// This source code is property of the Fraunhofer IGD and underlies
-// copyright restrictions. It may only be used with explicit
-// permission from the respective owner.
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
 
 package eu.esdihumboldt.hale.rcp.utils.proxy;
 
