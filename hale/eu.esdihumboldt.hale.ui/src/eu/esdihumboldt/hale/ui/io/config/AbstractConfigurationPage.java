d314fc8f99f32b4f48525fd0b76cbe4280a4673c
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-04-20 15:56:57 +0000

    added extension point for configuration pages
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/gssoil@4892 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/config/AbstractConfigurationPage.java
@@ -0,0 +1,50 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.io.config;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+
+import eu.esdihumboldt.hale.core.io.IOProvider;
+import eu.esdihumboldt.hale.core.io.IOProviderFactory;
+import eu.esdihumboldt.hale.ui.io.IOWizard;
+import eu.esdihumboldt.hale.ui.io.IOWizardPage;
+
+/**
+ * Base type for I/O configuration wizard pages
+ * @param <W> the concrete I/O wizard type
+ * @param <P> the {@link IOProvider} type used in the wizard
+ * @param <T> the {@link IOProviderFactory} type used in the wizard
+ *
+ * @author Simon Templer
+ * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+ * @since 2.2
+ */
+public abstract class AbstractConfigurationPage<P extends IOProvider, T extends IOProviderFactory<P>, 
+	W extends IOWizard<P, T>> extends IOWizardPage<P, T, W> {
+	
+	/**
+	 * @see IOWizardPage#IOWizardPage(String, String, ImageDescriptor)
+	 */
+	protected AbstractConfigurationPage(String pageName, String title,
+			ImageDescriptor titleImage) {
+		super(pageName, title, titleImage);
+	}
+
+	/**
+	 * @see IOWizardPage#IOWizardPage(String)
+	 */
+	protected AbstractConfigurationPage(String pageName) {
+		super(pageName);
+	}
+
+}
