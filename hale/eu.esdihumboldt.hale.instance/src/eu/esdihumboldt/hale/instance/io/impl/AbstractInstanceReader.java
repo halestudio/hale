e34a5e4ee8416c6452fc6459be6893534f1e4610
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-05-19 14:52:00 +0000

    added initial version of InstanceReader
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/instance-exp@5010 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.instance/src/eu/esdihumboldt/hale/instance/io/impl/AbstractInstanceReader.java
@@ -0,0 +1,60 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.instance.io.impl;
+
+import eu.esdihumboldt.hale.core.io.IOProviderConfigurationException;
+import eu.esdihumboldt.hale.core.io.impl.AbstractImportProvider;
+import eu.esdihumboldt.hale.instance.io.InstanceReader;
+import eu.esdihumboldt.hale.schemaprovider.Schema;
+
+/**
+ * Abstract {@link InstanceReader} base implementation
+ *
+ * @author Simon Templer
+ * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+ */
+public abstract class AbstractInstanceReader extends AbstractImportProvider implements
+		InstanceReader {
+	
+	private Schema sourceSchema;
+
+	/**
+	 * @see InstanceReader#setSourceSchema(Schema)
+	 */
+	@Override
+	public void setSourceSchema(Schema sourceSchema) {
+		this.sourceSchema = sourceSchema;
+	}
+
+	/**
+	 * Get the source schema
+	 * 
+	 * @return the source schema
+	 */
+	public Schema getSourceSchema() {
+		return sourceSchema;
+	}
+
+	/**
+	 * @see AbstractImportProvider#validate()
+	 */
+	@Override
+	public void validate() throws IOProviderConfigurationException {
+		super.validate();
+		
+		if (sourceSchema == null) {
+			fail("No source schema given for import");
+		}
+	}
+
+}
