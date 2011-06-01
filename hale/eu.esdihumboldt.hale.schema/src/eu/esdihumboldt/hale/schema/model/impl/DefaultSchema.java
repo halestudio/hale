6455a37be55f2e2350c91aec6e4e20cb3552c1b0
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-06-01 16:37:51 +0000

    added TypeIndex, Schema and SchemaSpace concepts
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5090 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.schema/src/eu/esdihumboldt/hale/schema/model/impl/DefaultSchema.java
@@ -0,0 +1,59 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.schema.model.impl;
+
+import java.net.URI;
+
+
+import eu.esdihumboldt.hale.core.io.supplier.Locatable;
+import eu.esdihumboldt.hale.schema.model.Schema;
+
+/**
+ * Default {@link Schema} implemenation
+ * @author Simon Templer
+ */
+public class DefaultSchema extends DefaultTypeIndex implements Schema {
+	
+	private final String namespace;
+	
+	private final URI location;
+
+	/**
+	 * Create a schema
+	 * 
+	 * @param namespace the schema namespace
+	 * @param location the schema location 
+	 */
+	public DefaultSchema(String namespace, URI location) {
+		super();
+		this.namespace = namespace;
+		this.location = location;
+	}
+	
+	/**
+	 * @see Locatable#getLocation()
+	 */
+	@Override
+	public URI getLocation() {
+		return location;
+	}
+
+	/**
+	 * @see Schema#getNamespace()
+	 */
+	@Override
+	public String getNamespace() {
+		return namespace;
+	}
+
+}
