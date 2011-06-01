4e64f87337edab0a0dcbc6228eae9a35b4182f77
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-06-01 16:39:52 +0000

    added xsd io bundle (not compiling yet)
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5100 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/XMLSchemaIO.java
@@ -0,0 +1,28 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.xsd;
+
+import eu.esdihumboldt.hale.core.io.ContentType;
+
+/**
+ * Utilities and constants for XML schemas
+ * @author Simon Templer
+ */
+public abstract class XMLSchemaIO {
+	
+	/**
+	 * The Shapefile content type
+	 */
+	public static final ContentType XSD_CT = ContentType.getContentType("XSD");
+
+}
---
+++ hale/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/internal/Messages.java
@@ -0,0 +1,39 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                  01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to this website:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to : http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+package eu.esdihumboldt.hale.io.xsd.internal;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Translated messages
+ */
+@SuppressWarnings("all")
+public class Messages {
+	private static final String BUNDLE_NAME = "eu.esdihumboldt.hale.io.xsd.internal.messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
