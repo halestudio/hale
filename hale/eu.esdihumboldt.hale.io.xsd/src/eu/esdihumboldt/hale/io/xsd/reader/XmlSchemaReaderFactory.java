4261b6bce4957d3bf49aedc2f5b163fc80aa5ece
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-06-06 16:26:06 +0000

    added service declaration for XmlSchemaReaderFactory
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5167 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/reader/XmlSchemaReaderFactory.java
@@ -0,0 +1,54 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.xsd.reader;
+
+import eu.esdihumboldt.hale.core.io.HaleIO;
+import eu.esdihumboldt.hale.core.io.IOProviderFactory;
+import eu.esdihumboldt.hale.core.io.impl.AbstractIOProviderFactory;
+import eu.esdihumboldt.hale.io.xsd.XmlSchemaIO;
+import eu.esdihumboldt.hale.schema.io.SchemaReader;
+import eu.esdihumboldt.hale.schema.io.SchemaReaderFactory;
+
+/**
+ * Factory for Shapefile {@link SchemaReader}s
+ * @author Simon Templer
+ */
+public class XmlSchemaReaderFactory extends
+		AbstractIOProviderFactory<SchemaReader> implements SchemaReaderFactory {
+	
+	private static final String PROVIDER_ID = "eu.esdihumboldt.hale.io.xsd.reader";
+
+	/**
+	 * Default constructor
+	 */
+	public XmlSchemaReaderFactory() {
+		super(PROVIDER_ID);
+	}
+
+	/**
+	 * @see IOProviderFactory#getDisplayName()
+	 */
+	@Override
+	public String getDisplayName() {
+		return HaleIO.getDisplayName(XmlSchemaIO.XSD_CT);
+	}
+
+	/**
+	 * @see IOProviderFactory#createProvider()
+	 */
+	@Override
+	public SchemaReader createProvider() {
+		return new XmlSchemaReader();
+	}
+
+}
