237a898444062c2c9ff2747a095e8eaea4426147
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-06-20 07:52:36 +0000

    added preferences for mdl projects
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/gssoil@5281 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/projectpreferences/updateprefs.bat
+++ hale/projectpreferences/updateprefs.bat
@@ -1,4 +1,5 @@
-java -jar proprefapp.jar common ..
+java -jar proprefapp.jar common .. -i eu.xsdi.mdl eu.xsdi.mdlui
 java -jar proprefapp.jar common ../../cst -i cst-wps
 java -jar proprefapp.jar common ../../humboldt-commons trunk
 java -jar proprefapp.jar common ../../humboldt-specification trunk
+java -jar proprefapp.jar mdl .. eu.xsdi.mdl eu.xsdi.mdlui
