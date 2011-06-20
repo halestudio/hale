4b22ccaa33914b1696f9251f307117b372c20c01
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-06-20 09:13:17 +0000

    added decorators for property and group property definitions that allow overriding constraints
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5295 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.schema/src/eu/esdihumboldt/hale/schema/model/impl/internal/ConstraintOverrideGroupProperty.java
@@ -0,0 +1,67 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.schema.model.impl.internal;
+
+import net.jcip.annotations.Immutable;
+import eu.esdihumboldt.hale.schema.model.GroupPropertyConstraint;
+import eu.esdihumboldt.hale.schema.model.GroupPropertyDefinition;
+import eu.esdihumboldt.hale.schema.model.impl.AbstractDefinition;
+import eu.esdihumboldt.hale.schema.model.impl.AbstractGroupPropertyDecorator;
+import eu.esdihumboldt.hale.schema.model.impl.AbstractPropertyDecorator;
+
+/**
+ * Decorator for {@link GroupPropertyDefinition}s that overrides given 
+ * constraints.
+ * @author Simon Templer
+ */
+@Immutable
+public class ConstraintOverrideGroupProperty extends AbstractGroupPropertyDecorator {
+	
+	private final AbstractDefinition<GroupPropertyConstraint> constraints;
+
+	/**
+	 * Create a decorator for the given group property where the given 
+	 * constraints override the constraints of the group.
+	 * 
+	 * @param property the property to decorate
+	 * @param constraints the overriding constraints
+	 */
+	public ConstraintOverrideGroupProperty(GroupPropertyDefinition property, 
+			GroupPropertyConstraint... constraints) {
+		super(property);
+		
+		this.constraints = new AbstractDefinition<GroupPropertyConstraint>(property.getName()) {
+			// empty implementation
+		};
+		
+		// add constraints
+		for (GroupPropertyConstraint constraint : constraints) {
+			this.constraints.setConstraint(constraint);
+		}
+	}
+
+	/**
+	 * @see AbstractPropertyDecorator#getConstraint(Class)
+	 */
+	@Override
+	public <T extends GroupPropertyConstraint> T getConstraint(
+			Class<T> constraintType) {
+		// return overriding constraint if present
+		if (constraints.hasConstraint(constraintType)) {
+			return constraints.getConstraint(constraintType);
+		}
+		
+		return super.getConstraint(constraintType);
+	}
+
+}
