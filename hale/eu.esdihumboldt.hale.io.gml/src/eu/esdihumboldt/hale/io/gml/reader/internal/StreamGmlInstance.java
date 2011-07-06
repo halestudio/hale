1f4b900a83d8c3abb050b45fb64784d70de5ca6c
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-07-06 15:08:06 +0000

    StreamGmlInstance: split parseInstance in parseInstance and parseProperties
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5388 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/eu.esdihumboldt.hale.io.gml/src/eu/esdihumboldt/hale/io/gml/reader/internal/StreamGmlInstance.java
+++ hale/eu.esdihumboldt.hale.io.gml/src/eu/esdihumboldt/hale/io/gml/reader/internal/StreamGmlInstance.java
@@ -23,10 +23,12 @@ import de.cs3d.util.logging.ALogger;
 import de.cs3d.util.logging.ALoggerFactory;
 
 import eu.esdihumboldt.hale.instance.model.Instance;
+import eu.esdihumboldt.hale.instance.model.MutableGroup;
 import eu.esdihumboldt.hale.instance.model.MutableInstance;
 import eu.esdihumboldt.hale.instance.model.impl.OInstance;
 import eu.esdihumboldt.hale.io.xsd.constraint.XmlAttributeFlag;
 import eu.esdihumboldt.hale.schema.model.ChildDefinition;
+import eu.esdihumboldt.hale.schema.model.DefinitionGroup;
 import eu.esdihumboldt.hale.schema.model.PropertyDefinition;
 import eu.esdihumboldt.hale.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.schema.model.constraint.type.HasValueFlag;
@@ -44,7 +46,7 @@ public abstract class StreamGmlInstance {
 	private static final ALogger log = ALoggerFactory.getLogger(StreamGmlInstance.class);
 
 	/**
-	 * Parses an instance with the given type from the given XML stream reader
+	 * Parses an instance with the given type from the given XML stream reader.
 	 * @param reader the XML stream reader, the current event must be the start
 	 *   element of the instance
 	 * @param type the definition of the instance type
@@ -58,85 +60,14 @@ public abstract class StreamGmlInstance {
 		
 		MutableInstance instance = new OInstance(type);
 		
-		//TODO fill instance with values
-		
-		// attributes
-		for (int i = 0; i < reader.getAttributeCount(); i++) {
-			QName propertyName = reader.getAttributeName(i);
-			//FIXME may (currently) also be inside a group!
-			ChildDefinition<?> child = type.getChild(propertyName);
-			if (child != null && child.asProperty() != null) {
-				// add property value
-				addSimpleProperty(instance, child.asProperty(), reader.getAttributeValue(i));
-			}
-			else {
-				log.warn(MessageFormat.format(
-						"No property ''{0}'' found in type ''{1}'', value is ignored", 
-						propertyName, type.getDisplayName()));
-			}
-		}
-		
-		//FIXME check for xsi:nil!
-		//XXX or use reader.hasText()?
-		
-		if (hasElements(type)) {
-			// elements
-			int open = 1;
-			while (open > 0 && reader.hasNext()) {
-				int event = reader.next();
-				switch (event) {
-				case XMLStreamConstants.START_ELEMENT:
-					// get child
-					ChildDefinition<?> child = type.getChild(reader.getName());
-					if (child != null) {
-						PropertyDefinition property = child.asProperty();
-						checkNotNull(property);
-						
-						//TODO check also namespace?
-						if (hasElements(property.getPropertyType())) {
-							// use an instance as value
-							instance.addProperty(property.getName(), 
-									parseInstance(reader, property.getPropertyType()));
-						}
-						else {
-							if (hasAttributes(property.getPropertyType())) {
-								// no elements but attributes
-								// use an instance as value, it will be assigned an instance value if possible
-								instance.addProperty(property.getName(), 
-										parseInstance(reader, property.getPropertyType()));
-							}
-							else {
-								// no elements and no attributes
-								// use simple value
-								String value = reader.getElementText();
-								if (value != null) {
-									addSimpleProperty(instance, property, value);
-								}
-							}
-						}
-					}
-					else {
-						//TODO search in groups for property
-						//XXX remember a last group (hierarchy?) and keep it open for following elements?
-					}
-//					else {
-//						log.warn(MessageFormat.format(
-//								"No property ''{0}'' found in type ''{1}'', value is ignored", 
-//								reader.getLocalName(), type.getDisplayName()));
-//					}
-					
-					if (reader.getEventType() != XMLStreamConstants.END_ELEMENT) {
-						// only increase open if the current event is not already the end element (because we used getElementText)
-						open++;
-					}
-					break;
-				case XMLStreamConstants.END_ELEMENT:
-					open--;
-					break;
-				}
-			}
-		}
-		else {
+		// instance properties
+		parseProperties(reader, instance, type);
+
+		// instance value
+		if (type.getConstraint(HasValueFlag.class).isEnabled()) {
+			//FIXME check for xsi:nil!
+			//XXX or use reader.hasText()?
+			
 			// try to get text value
 			String value = reader.getElementText();
 			if (value != null) {
@@ -144,7 +75,96 @@ public abstract class StreamGmlInstance {
 			}
 		}
 		
+		//TODO add geometry as a GeometryProperty value where applicable
+		
 		return instance;
+	}
+
+	/**
+	 * Populates an instance or group with its properties based on the given
+	 * XML stream reader.
+	 * @param reader the XML stream reader
+	 * @param group the group to populate with properties
+	 * @param definition the associated definition
+	 * @throws XMLStreamException if parsing the properties failed
+	 */
+	private static void parseProperties(XMLStreamReader reader,
+			MutableGroup group, DefinitionGroup definition) throws XMLStreamException {
+		// attributes (usually only present in Instances)
+		for (int i = 0; i < reader.getAttributeCount(); i++) {
+			QName propertyName = reader.getAttributeName(i);
+			//XXX might also be inside a group? currently every attribute group should be flattened
+			// for group support there would have to be some other kind of handling than for elements, cause order doesn't matter for attributes  
+			ChildDefinition<?> child = definition.getChild(propertyName);
+			if (child != null && child.asProperty() != null) {
+				// add property value
+				addSimpleProperty(group, child.asProperty(), reader.getAttributeValue(i));
+			}
+			else {
+				log.warn(MessageFormat.format(
+						"No property ''{0}'' found in type ''{1}'', value is ignored", 
+						propertyName, definition.getIdentifier()));
+			}
+		}
+				
+		// elements
+		int open = 1;
+		while (open > 0 && reader.hasNext()) {
+			int event = reader.next();
+			switch (event) {
+			case XMLStreamConstants.START_ELEMENT:
+				// get child
+				ChildDefinition<?> child = definition.getChild(reader.getName());
+				
+				//XXX
+				
+				if (child != null) {
+					PropertyDefinition property = child.asProperty();
+					checkNotNull(property);
+					
+					//TODO check also namespace?
+					if (hasElements(property.getPropertyType())) {
+						// use an instance as value
+						group.addProperty(property.getName(), 
+								parseInstance(reader, property.getPropertyType()));
+					}
+					else {
+						if (hasAttributes(property.getPropertyType())) {
+							// no elements but attributes
+							// use an instance as value, it will be assigned an instance value if possible
+							group.addProperty(property.getName(), 
+									parseInstance(reader, property.getPropertyType()));
+						}
+						else {
+							// no elements and no attributes
+							// use simple value
+							String value = reader.getElementText();
+							if (value != null) {
+								addSimpleProperty(group, property, value);
+							}
+						}
+					}
+				}
+				else {
+					//TODO search in groups for property
+					//XXX remember a last group (hierarchy?) and keep it open for following elements?
+				}
+//							else {
+//								log.warn(MessageFormat.format(
+//										"No property ''{0}'' found in type ''{1}'', value is ignored", 
+//										reader.getLocalName(), type.getDisplayName()));
+//							}
+				
+				if (reader.getEventType() != XMLStreamConstants.END_ELEMENT) {
+					// only increase open if the current event is not already the end element (because we used getElementText)
+					open++;
+				}
+				break;
+			case XMLStreamConstants.END_ELEMENT:
+				open--;
+				break;
+			}
+		}
 	}
 
 	/**
@@ -155,7 +175,18 @@ public abstract class StreamGmlInstance {
 	 * @return if the type is a complex type
 	 */
 	private static boolean hasElements(TypeDefinition type) {
-		return !type.getConstraint(HasValueFlag.class).isEnabled();
+		for (ChildDefinition<?> child : type.getChildren()) {
+			if (child.asProperty() != null) {
+				if (!child.asProperty().getConstraint(XmlAttributeFlag.class).isEnabled()) {
+					return true;
+				}
+			}
+			else if (child.asGroup() != null) {
+				//FIXME what about groups?!
+			}
+		}
+		
+		return false;
 	}
 	
 	/**
@@ -185,14 +216,14 @@ public abstract class StreamGmlInstance {
 	 * Adds a property value to the given instance. The property value will
 	 * be converted appropriately.
 	 * 
-	 * @param instance the instance
+	 * @param group the instance
 	 * @param property the property
 	 * @param value the property value as specified in the XML
 	 */
-	private static void addSimpleProperty(MutableInstance instance,
+	private static void addSimpleProperty(MutableGroup group,
 			PropertyDefinition property, String value) {
 		Object val = convertSimple(property.getPropertyType(), value);
-		instance.addProperty(property.getName(), val);
+		group.addProperty(property.getName(), val);
 	}
 
 	/**
