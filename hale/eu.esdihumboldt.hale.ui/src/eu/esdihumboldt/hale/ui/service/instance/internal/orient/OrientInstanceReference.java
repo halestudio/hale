a24f5cd11c23d77285ce1659fd84a0e1c4e771d5
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-07-14 15:25:15 +0000

    added equals method to orient db instance reference
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5473 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/instance/internal/orient/OrientInstanceReference.java
+++ hale/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/instance/internal/orient/OrientInstanceReference.java
@@ -64,4 +64,38 @@ public class OrientInstanceReference implements InstanceReference {
 		return typeDefinition;
 	}
 
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataSet == null) ? 0 : dataSet.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrientInstanceReference other = (OrientInstanceReference) obj;
+		if (dataSet != other.dataSet)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
