e092270a3c7462558bca16d36d055819c3822d34
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-07-27 16:07:19 +0000

    added condition interfaces
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5504 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.align/src/eu/esdihumboldt/hale/align/model/condition/EntityCondition.java
@@ -0,0 +1,33 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.align.model.condition;
+
+import eu.esdihumboldt.hale.align.model.Entity;
+
+/**
+ * Condition an {@link Entity} may fulfill
+ * @param <T> the entity type
+ * 
+ * @author Simon Templer
+ * @since 2.5 
+ */
+public interface EntityCondition<T extends Entity> {
+
+	/**
+	 * Determines if the given entity matches the constraint
+	 * @param entity the entity
+	 * @return if the entity matches the constraint
+	 */
+	public boolean accept(T entity);
+	
+}
---
+++ hale/eu.esdihumboldt.hale.align/src/eu/esdihumboldt/hale/align/model/condition/impl/BindingCondition.java
@@ -0,0 +1,33 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.align.model.condition.impl;
+
+import eu.esdihumboldt.hale.align.model.Type;
+import eu.esdihumboldt.hale.align.model.condition.TypeCondition;
+
+/**
+ * TODO Type description
+ * @author sitemple
+ */
+public class BindingCondition implements TypeCondition {
+
+	/**
+	 * @see eu.esdihumboldt.hale.align.model.condition.EntityCondition#accept(eu.esdihumboldt.hale.align.model.Entity)
+	 */
+	@Override
+	public boolean accept(Type entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
