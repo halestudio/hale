8fc8e195ff85abd33c1eee6dd5cc6b15f8aa581e
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-08-01 15:43:25 +0000

    added InstanceFactory interface and implementation based on OInstance
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5543 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

---
+++ hale/eu.esdihumboldt.hale.instance/src/eu/esdihumboldt/hale/instance/model/InstanceFactory.java
@@ -0,0 +1,31 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.instance.model;
+
+import eu.esdihumboldt.hale.schema.model.TypeDefinition;
+
+/**
+ * Factory for creating new instances
+ * @author Simon Templer
+ * @since 2.5
+ */
+public interface InstanceFactory {
+	
+	/**
+	 * Create an empty instance of the given type
+	 * @param type the type of the instance to create
+	 * @return the created instance
+	 */
+	public MutableInstance createInstance(TypeDefinition type);
+
+}
---
+++ hale/eu.esdihumboldt.hale.instance/src/eu/esdihumboldt/hale/instance/model/impl/OInstanceFactory.java
@@ -0,0 +1,33 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.instance.model.impl;
+
+import eu.esdihumboldt.hale.instance.model.InstanceFactory;
+import eu.esdihumboldt.hale.instance.model.MutableInstance;
+import eu.esdihumboldt.hale.schema.model.TypeDefinition;
+
+/**
+ * Instance factory based on {@link OInstance}
+ * @author Simon Templer
+ */
+public class OInstanceFactory implements InstanceFactory {
+
+	/**
+	 * @see InstanceFactory#createInstance(TypeDefinition)
+	 */
+	@Override
+	public MutableInstance createInstance(TypeDefinition type) {
+		return new OInstance(type);
+	}
+
+}
