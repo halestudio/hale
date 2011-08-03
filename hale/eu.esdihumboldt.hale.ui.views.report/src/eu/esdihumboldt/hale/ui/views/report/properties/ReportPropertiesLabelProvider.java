5e3fbde188ed96a62ff747bae1535c078a978bfe
Andreas Burchert <andreas.burchert@igd.fraunhofer.de>
2011-08-03 10:11:00 +0000

    Implemented getText() and getImage().
    git-svn-id: http://svn.esdi-humboldt.eu/repo/humboldt2/branches/schema-exp@5554 5d385326-3dfc-11dd-8f4f-eb0017f4c70c

--- hale/eu.esdihumboldt.hale.ui.views.report/src/eu/esdihumboldt/hale/ui/views/report/properties/ReportPropertiesLabelProvider.java
+++ hale/eu.esdihumboldt.hale.ui.views.report/src/eu/esdihumboldt/hale/ui/views/report/properties/ReportPropertiesLabelProvider.java
@@ -12,7 +12,16 @@
 
 package eu.esdihumboldt.hale.ui.views.report.properties;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import eu.esdihumboldt.hale.core.report.Report;
 
 /**
  * @author Andreas Burchert
@@ -20,12 +29,62 @@ import org.eclipse.jface.viewers.LabelProvider;
  */
 public class ReportPropertiesLabelProvider extends LabelProvider {
 
+	private Map<ImageDescriptor, Image> imageCache = new HashMap<ImageDescriptor, Image>();
+	/**
+	 * @see org.eclipse.jface.viewers.ILabelProvider#getImage(java.lang.Object)
+	 */
+	@Override
+	public Image getImage(Object element) {
+		if (element instanceof IStructuredSelection) {
+			// overwrite element with first element from selection
+			element = ((IStructuredSelection) element).getFirstElement();
+		}
+		
+		if (element instanceof Report) {
+			// get the right image
+			Report report = (Report) element;
+			
+			String img = "icons/signed_yes.gif";
+			if (report.getWarnings().size() > 0 && report.getErrors().size() > 0) {
+				img = "icons/errorwarning_tab.gif";
+			} else if (report.getErrors().size() > 0) {
+				img = "icons/error.gif";
+			} else if (report.getWarnings().size() > 0) {
+				img = "icons/warning.gif";
+			}
+			
+			ImageDescriptor descriptor = null;
+			
+			// TODO Platform.getBundle(ReportList.ID) does not work so here is a static plugin path!
+			descriptor = AbstractUIPlugin.imageDescriptorFromPlugin("eu.esdihumboldt.hale.ui.views.report", img);
+			if (descriptor == null) {
+				return null;
+			}
+			
+			Image image = imageCache.get(descriptor);
+			if (image == null) {
+				image = descriptor.createImage();
+				imageCache.put(descriptor, image);
+			}
+			return image;
+		}
+		return null;
+	}
+	
 	/**
 	 * @see LabelProvider#getText(Object)
 	 */
 	@Override
 	public String getText(Object element) {
-		System.err.println("ReportPropertiesLabelProvider.getText()");
-		return element.toString();
+		if (element instanceof IStructuredSelection) {
+			// overwrite element with first element from selection
+			element = ((IStructuredSelection) element).getFirstElement();
+			
+			if (element instanceof Report) {
+				return ((Report) element).getTaskName();
+			}
+		}
+		
+		return "";
 	}
 }
