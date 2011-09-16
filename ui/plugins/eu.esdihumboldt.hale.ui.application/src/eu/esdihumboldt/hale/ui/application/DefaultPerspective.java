77e305187170712c91e9ec854bb28ec7bb32561a
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-09-16 12:03:48 +0200

    added some views to the default perspecitve

--- ui/plugins/eu.esdihumboldt.hale.ui.application/src/eu/esdihumboldt/hale/ui/application/DefaultPerspective.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.application/src/eu/esdihumboldt/hale/ui/application/DefaultPerspective.java
@@ -15,7 +15,12 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import eu.esdihumboldt.hale.ui.views.data.SourceDataView;
+import eu.esdihumboldt.hale.ui.views.data.TransformedDataView;
+import eu.esdihumboldt.hale.ui.views.functions.FunctionsView;
+import eu.esdihumboldt.hale.ui.views.report.ReportList;
 import eu.esdihumboldt.hale.ui.views.schemas.SchemasView;
+import eu.esdihumboldt.hale.ui.views.typehierarchy.TypeHierarchyView;
 
 /**
  * The Perspective, i.e. the top layout element for the client. The Perspective 
@@ -36,7 +41,15 @@ public class DefaultPerspective implements IPerspectiveFactory {
 		// bottom
 		IFolderLayout bottom = _layout.createFolder(
 				"bottom", IPageLayout.BOTTOM, 0.7f, editorArea); //$NON-NLS-1$
-//		bottom.addView(TaskTreeView.ID);
+		bottom.addView("org.eclipse.pde.runtime.LogView");
+		bottom.addView("org.eclipse.ui.views.PropertySheet");
+		
+		// bottom right
+		IFolderLayout bottomRight = _layout.createFolder("bottomRight", 
+				IPageLayout.RIGHT, 0.7f, "bottom");
+		bottomRight.addView(TypeHierarchyView.ID);
+		bottomRight.addView(FunctionsView.ID);
+		bottomRight.addView(ReportList.ID);
 		
 		// top left
 		IFolderLayout topLeft = _layout.createFolder("topLeft", IPageLayout.LEFT, 0.6f, editorArea); //$NON-NLS-1$
@@ -44,18 +57,21 @@ public class DefaultPerspective implements IPerspectiveFactory {
 		
 		// top right
 		IFolderLayout topRight = _layout.createFolder("topRight", IPageLayout.RIGHT, 0.4f, editorArea); //$NON-NLS-1$
-//		topRight.addView(MapView.ID);
+		topRight.addView(SourceDataView.ID);
 		
 		// lesser top right
-		IFolderLayout lesserTopRight = _layout.createFolder("lesserTopRight", IPageLayout.BOTTOM, 0.6f, "topRight"); //$NON-NLS-1$ //$NON-NLS-2$
-//		lesserTopRight.addView(MappingView.ID);
+		IFolderLayout lesserTopRight = _layout.createFolder("lesserTopRight", IPageLayout.BOTTOM, 0.5f, "topRight"); //$NON-NLS-1$ //$NON-NLS-2$
+		lesserTopRight.addView(TransformedDataView.ID);
 		
 		_layout.addShowViewShortcut(SchemasView.ID);
 //		_layout.addShowViewShortcut(MapView.ID);
 //		_layout.addShowViewShortcut(MappingView.ID);
-//		_layout.addShowViewShortcut(SourceDataView.ID);
-//		_layout.addShowViewShortcut(TransformedDataView.ID);
+		_layout.addShowViewShortcut(SourceDataView.ID);
+		_layout.addShowViewShortcut(TransformedDataView.ID);
 //		_layout.addShowViewShortcut(TaskTreeView.ID);
+		_layout.addShowViewShortcut(ReportList.ID);
+		_layout.addShowViewShortcut(TypeHierarchyView.ID);
+		_layout.addShowViewShortcut(FunctionsView.ID);
 		
 		_layout.setEditorAreaVisible(false);
 	}
