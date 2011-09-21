24480ac3dfc622e7aa934004cf2ecb1752ababc6
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2011-09-21 16:53:47 +0200

    added next version of schema reader page, edited ReadConfigurationPage
    refs #308
    
    Change-Id: Ifd5b9f0f2291a7a70415977b00e1f7837aae8dff

--- io/plugins/eu.esdihumboldt.hale.io.csv.ui/src/eu/esdihumboldt/hale/io/csv/ui/ReadConfigurationPage.java
+++ io/plugins/eu.esdihumboldt.hale.io.csv.ui/src/eu/esdihumboldt/hale/io/csv/ui/ReadConfigurationPage.java
@@ -12,6 +12,8 @@
 
 package eu.esdihumboldt.hale.io.csv.ui;
 
+import java.util.HashMap;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -25,36 +27,43 @@ import eu.esdihumboldt.hale.io.csv.reader.internal.CSVSchemaReader;
 import eu.esdihumboldt.hale.ui.io.schema.SchemaReaderConfigurationPage;
 
 /**
- * TODO Type description
+ * Advanced configuration for the SchemaReader
+ * 
  * @author Kevin Mais
  */
 @SuppressWarnings("restriction")
-public class ReadConfigurationPage extends SchemaReaderConfigurationPage implements ModifyListener{
+public class ReadConfigurationPage extends SchemaReaderConfigurationPage
+		implements ModifyListener {
 
 	private Label separator;
 	private Label quote;
 	private Label escape;
-	
+
 	private Combo combo;
 	private Combo combo2;
 	private Combo combo3;
-	
+
+	private HashMap<String, String> map = new HashMap<String, String>();
+
 	/**
 	 * default constructor
-	 * 
-	 * @param pageName Name of the Page
 	 */
 	public ReadConfigurationPage() {
 		super("CSVRead");
+
+		setTitle("Reader Settings");
+		setDescription("Set the Separating character, Quote character and Escape character");
 	}
 
 	/**
-	 * @see org.eclipse.swt.events.ModifyListener#modifyLabel(org.eclipse.swt.events.ModifyEvent)
+	 * sets the PageComplete boolean to true, if the text is modified
+	 * 
+	 * @see org.eclipse.swt.events.ModifyListener
 	 */
 	@Override
 	public void modifyText(ModifyEvent e) {
 		setPageComplete(true);
-		
+
 	}
 
 	/**
@@ -63,7 +72,7 @@ public class ReadConfigurationPage extends SchemaReaderConfigurationPage impleme
 	@Override
 	public void enable() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
@@ -72,20 +81,36 @@ public class ReadConfigurationPage extends SchemaReaderConfigurationPage impleme
 	@Override
 	public void disable() {
 		// TODO Auto-generated method stub
-		
+
 	}
-		
+
 	/**
 	 * @see eu.esdihumboldt.hale.ui.io.IOWizardPage#updateConfiguration(eu.esdihumboldt.hale.common.core.io.IOProvider)
 	 */
 	@Override
 	public boolean updateConfiguration(SchemaReader provider) {
-		provider.setParameter(CSVSchemaReader.PARAM_SEPARATOR,
-				combo.getText());
-		provider.setParameter(CSVSchemaReader.PARAM_QUOTE,
-				combo2.getText());
-		provider.setParameter(CSVSchemaReader.PARAM_ESCAPE,
-				combo3.getText());
+
+		setErrorMessage("You have not entered valid characters");
+
+		map.put("TAB", "\t");
+
+//		if (map.containsKey(combo.getText())
+//				&& map.get(combo.getText()).length() <= 2) {
+//			provider.setParameter(CSVSchemaReader.PARAM_SEPARATOR,
+//					map.get(combo.getText()));
+//		} else if (combo.getText().length() == 1
+//				&& !(map.containsKey(combo.getText()))) {
+//			provider.setParameter(CSVSchemaReader.PARAM_SEPARATOR,
+//					combo.getText());
+//		}
+//		getErrorMessage();
+//		System.out.println(map.get(combo.getText()));
+//		setPageComplete(false);
+
+		provider.setParameter(CSVSchemaReader.PARAM_SEPARATOR, map.get(combo.getText()));
+		provider.setParameter(CSVSchemaReader.PARAM_QUOTE, combo2.getText());
+		provider.setParameter(CSVSchemaReader.PARAM_ESCAPE, combo3.getText());
+		
 		return true;
 	}
 
@@ -95,37 +120,36 @@ public class ReadConfigurationPage extends SchemaReaderConfigurationPage impleme
 	@Override
 	protected void createContent(Composite page) {
 		page.setLayout(new GridLayout(2, false));
-		
 		// column 1, row 1
-		separator = new Label (page, SWT.SINGLE | SWT.BORDER);
-		separator.setText ("Select Separating Sign");
+		separator = new Label(page, SWT.NONE);
+		separator.setText("Select Separating Sign");
 		// column 2, row 1
 		combo = new Combo(page, SWT.NONE);
-		combo.setItems (new String [] {"\t", "," , "."});
+		combo.setItems(new String[] { "TAB", ",", "." });
 		combo.addModifyListener(this);
-		
+
 		// column 1, row 2
-		quote = new Label (page, SWT.SINGLE | SWT.BORDER);
-		quote.setText ("Select Quote Sign");
-		
+		quote = new Label(page, SWT.NONE);
+		quote.setText("Select Quote Sign");
+
 		// column 2, row 2
 		combo2 = new Combo(page, SWT.NONE);
-		combo2.setItems (new String [] {"\" ", "\'", ",", "-"});
+		combo2.setItems(new String[] { "\" ", "\'", ",", "-" });
 		combo2.addModifyListener(this);
-		
+
 		// column 1, row 3
-		escape = new Label (page, SWT.SINGLE | SWT.BORDER);
-		escape.setText ("Select Escape Sign");
-		
+		escape = new Label(page, SWT.NONE);
+		escape.setText("Select Escape Sign");
+
 		// column 2, row 3
 		combo3 = new Combo(page, SWT.NONE);
-		combo3.setItems (new String [] {"\\", " ", "dot"});
+		combo3.setItems(new String[] { "\\", "." });
 		combo3.addModifyListener(this);
-		
-		page.pack ();
-		
+
+		page.pack();
+
 		setPageComplete(false);
-		
+
 	}
 
 }
