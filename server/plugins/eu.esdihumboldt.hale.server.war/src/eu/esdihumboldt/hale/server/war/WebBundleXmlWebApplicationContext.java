5a98eb8852c17477d94e72179f2a87fe8ca61b26
Andreas Burchert <andreas.burchert@igd.fraunhofer.de>
2011-09-22 13:35:02 +0200

    Jetty now runs as an OSGi framework. CstWps needs adjustments and will follow soon, refs #337
    Change-Id: I97ef27c64e945eb86d31a7f60b29c56946584e25

---
+++ server/plugins/eu.esdihumboldt.hale.server.war/src/eu/esdihumboldt/hale/server/war/WebBundleXmlWebApplicationContext.java
@@ -0,0 +1,9 @@
+package eu.esdihumboldt.hale.server.war;
+
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+
+
+public class WebBundleXmlWebApplicationContext extends XmlWebApplicationContext {
+
+}
