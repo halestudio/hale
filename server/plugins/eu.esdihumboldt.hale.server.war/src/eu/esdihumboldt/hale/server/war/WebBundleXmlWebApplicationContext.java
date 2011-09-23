7bbab01785120d70cda5a09c7420bd20e203b1a2
Andreas Burchert <andreas.burchert@igd.fraunhofer.de>
2011-09-23 11:23:41 +0200

    Set DirectoryListing to false. Removed unneeded file, refs #337
    Change-Id: Ife617e904a4f9ac45572457f6c8e5db90e4c31dc
    Reviewed-on: http://gerrit.esdi-community.eu/52
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>
    Tested-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- server/plugins/eu.esdihumboldt.hale.server.war/src/eu/esdihumboldt/hale/server/war/WebBundleXmlWebApplicationContext.java
+++
@@ -1,9 +0,0 @@
-package eu.esdihumboldt.hale.server.war;
-
-import org.springframework.web.context.support.XmlWebApplicationContext;
-
-
-
-public class WebBundleXmlWebApplicationContext extends XmlWebApplicationContext {
-
-}
