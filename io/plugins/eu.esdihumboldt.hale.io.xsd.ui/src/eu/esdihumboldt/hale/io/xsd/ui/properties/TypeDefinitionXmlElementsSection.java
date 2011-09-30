fa2807d6d39d9fede2e545dd7dff6121d343ec15
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-09-30 10:12:44 +0200

    let XML schema related properties sections extend DefaultDefinitionSection
    refs #320
    
    Change-Id: Ib45e4933f1403151379d379b44f9fd10362e4939
    Reviewed-on: http://gerrit.esdi-community.eu/77
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>
    Tested-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- io/plugins/eu.esdihumboldt.hale.io.xsd.ui/src/eu/esdihumboldt/hale/io/xsd/ui/properties/TypeDefinitionXmlElementsSection.java
+++ io/plugins/eu.esdihumboldt.hale.io.xsd.ui/src/eu/esdihumboldt/hale/io/xsd/ui/properties/TypeDefinitionXmlElementsSection.java
@@ -14,58 +14,38 @@ package eu.esdihumboldt.hale.io.xsd.ui.properties;
 
 import java.util.Collection;
 
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
-import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.io.xsd.constraint.XmlElements;
 import eu.esdihumboldt.hale.io.xsd.model.XmlElement;
+import eu.esdihumboldt.hale.ui.views.properties.DefaultDefinitionSection;
 
 /**
  * Properties Section with XmlElements
  * @author Patrick Lieb
  */
-public class TypeDefinitionXmlElementsSection extends AbstractPropertySection{
-	
-	private TypeDefinition typedefinition;
+public class TypeDefinitionXmlElementsSection extends DefaultDefinitionSection<TypeDefinition> {
 	
 	private Text[] textarray;
 	
 	private Text text;
 
-
-	/**
-	 * @see AbstractPropertySection#setInput(IWorkbenchPart, ISelection)
-	 */
-	@Override
-	public void setInput(IWorkbenchPart part, ISelection selection) {
-		super.setInput(part, selection);
-		Assert.isTrue(selection instanceof IStructuredSelection);
-		Object input = ((IStructuredSelection) selection).getFirstElement();
-		if (input instanceof EntityDefinition) {
-			typedefinition = (TypeDefinition) ((EntityDefinition) input);
-		}
-	}
-	
 	/**
 	 * @see AbstractPropertySection#createControls(Composite, TabbedPropertySheetPage)
 	 */
 	@Override
 	public void createControls(Composite parent,
 			TabbedPropertySheetPage aTabbedPropertySheetPage) {
-		Collection<? extends XmlElement> elements = typedefinition.getConstraint(XmlElements.class).getElements();
+		Collection<? extends XmlElement> elements = getDefinition().getConstraint(XmlElements.class).getElements();
 		int length = elements.size();
 		for(int pos = 0; pos < length; pos++){
 			super.createControls(parent, aTabbedPropertySheetPage);
@@ -112,7 +92,7 @@ public class TypeDefinitionXmlElementsSection extends AbstractPropertySection{
 	 */
 	@Override
 	public void refresh() {
-		Collection<? extends XmlElement> elements = typedefinition.getConstraint(XmlElements.class).getElements();
+		Collection<? extends XmlElement> elements = getDefinition().getConstraint(XmlElements.class).getElements();
 		for(XmlElement element : elements){
 			textarray[0].setText(element.getName().getNamespaceURI().toString());
 		}
