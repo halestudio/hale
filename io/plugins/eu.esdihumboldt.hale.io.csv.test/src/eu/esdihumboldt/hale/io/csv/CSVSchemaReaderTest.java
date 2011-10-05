d0c21ea69fa59dad486b2a7a2728435e02f8f201
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2011-10-05 15:54:04 +0200

    added some Tests for CSVSchemaReader
    refs #308
    
    Change-Id: I91e296ef63588914ade914061d4e4a7a132fdb86

--- io/plugins/eu.esdihumboldt.hale.io.csv.test/src/eu/esdihumboldt/hale/io/csv/CSVSchemaReaderTest.java
+++ io/plugins/eu.esdihumboldt.hale.io.csv.test/src/eu/esdihumboldt/hale/io/csv/CSVSchemaReaderTest.java
@@ -12,39 +12,142 @@
 
 package eu.esdihumboldt.hale.io.csv;
 
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-
+import eu.esdihumboldt.hale.common.core.io.impl.LogProgressIndicator;
 import eu.esdihumboldt.hale.common.core.io.report.IOReport;
 import eu.esdihumboldt.hale.common.core.io.supplier.DefaultInputSupplier;
+import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
 import eu.esdihumboldt.hale.common.schema.model.Schema;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.io.csv.reader.internal.CSVSchemaReader;
 
 /**
- * TODO Type description
- * @author sitemple
+ * Test Class for CSVSchemaReader
+ * 
+ * @author Kevin Mais
  */
 public class CSVSchemaReaderTest {
-	
+
 	/**
-	 * TODO
+	 * Reader Test for given property names and property types
+	 * 
+	 * @throws Exception
+	 *             the Exception thrown if the test fails
 	 */
 	@Test
 	public void testRead() throws Exception {
-		//TODO
-		
+
+		String props = "muh,kuh,bla,blub";
+
 		CSVSchemaReader schemaReader = new CSVSchemaReader();
-		schemaReader.setSource(new DefaultInputSupplier(getClass().getResource("/data/test1.csv").toURI()));
-		//TODO configure type name
+		schemaReader.setSource(new DefaultInputSupplier(getClass().getResource(
+				"/data/test1.csv").toURI()));
 		schemaReader.setParameter(CSVSchemaReader.PARAM_TYPENAME, "TestTyp");
-		
-		IOReport report = schemaReader.execute(null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_PROPERTY, props);
+		schemaReader
+				.setParameter(CSVSchemaReader.PARAM_PROPERTYTYPE,
+						"java.lang.String,java.lang.String,java.lang.String,java.lang.String");
+		schemaReader.setParameter(CSVSchemaReader.PARAM_SEPARATOR, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_QUOTE, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_ESCAPE, null);
+
+		IOReport report = schemaReader.execute(new LogProgressIndicator());
 		assertTrue(report.isSuccess());
-		
+
 		Schema schema = schemaReader.getSchema();
-		
-		//TODO
+		assertEquals(1, schema.getMappableTypes().size());
+		TypeDefinition type = schema.getMappableTypes().iterator().next();
+		assertTrue(type.getName().getLocalPart().equals("TestTyp"));
+		Iterator<? extends ChildDefinition<?>> it = type.getChildren()
+				.iterator();
+
+		while (it.hasNext()) {
+			assertTrue(props.contains(it.next().getName().getLocalPart()));
+		}
 	}
 
+	/**
+	 * Reader Test for no given property names and property types (using default settings)
+	 * 
+	 * @throws Exception
+	 *             the Exception thrown if the test fails
+	 */
+	@Test
+	public void testRead2() throws Exception {
+
+		String prop = "Name,Xcoord,Ycoord,id";
+
+		CSVSchemaReader schemaReader = new CSVSchemaReader();
+		schemaReader.setSource(new DefaultInputSupplier(getClass().getResource(
+				"/data/test1.csv").toURI()));
+		schemaReader.setParameter(CSVSchemaReader.PARAM_TYPENAME, "TestTyp");
+		schemaReader.setParameter(CSVSchemaReader.PARAM_PROPERTY, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_PROPERTYTYPE, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_SEPARATOR, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_QUOTE, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_ESCAPE, null);
+
+		IOReport report = schemaReader.execute(new LogProgressIndicator());
+		assertTrue(report.isSuccess());
+
+		Schema schema = schemaReader.getSchema();
+		assertEquals(1, schema.getMappableTypes().size());
+		TypeDefinition type = schema.getMappableTypes().iterator().next();
+		assertTrue(type.getName().getLocalPart().equals("TestTyp"));
+		Iterator<? extends ChildDefinition<?>> it = type.getChildren()
+				.iterator();
+
+		while (it.hasNext()) {
+			assertTrue(prop.contains(it.next().getName().getLocalPart()));
+		}
+	}
+	
+	/**
+	 * Reader Test for no given property names and property types (using default settings)
+	 * 
+	 * @throws Exception
+	 *             the Exception thrown if the test fails
+	 */
+	@Test(expected=RuntimeException.class)
+	public void failTest() throws Exception {
+
+		CSVSchemaReader schemaReader = new CSVSchemaReader();
+		schemaReader.setSource(new DefaultInputSupplier(getClass().getResource(
+				"/data/test1.csv").toURI()));
+		schemaReader.setParameter(CSVSchemaReader.PARAM_TYPENAME, "TestTyp");
+		schemaReader.setParameter(CSVSchemaReader.PARAM_PROPERTY, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_PROPERTYTYPE, "java.lang.String,java.lang.String");
+		schemaReader.setParameter(CSVSchemaReader.PARAM_SEPARATOR, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_QUOTE, null);
+		schemaReader.setParameter(CSVSchemaReader.PARAM_ESCAPE, null);
+
+		// unused because we expect a runtimeexception
+		@SuppressWarnings("unused")
+		IOReport report = schemaReader.execute(new LogProgressIndicator());
+
+	}
+
+	/**
+	 * Reader Test
+	 * 
+	 * @throws Exception
+	 *             the Exception thrown if the test fails
+	 */
+	@Test
+	public void failTest2() throws Exception {
+
+		CSVSchemaReader schemaReader2 = new CSVSchemaReader();
+		schemaReader2.setSource(new DefaultInputSupplier(getClass()
+				.getResource("/data/test1.csv").toURI()));
+		schemaReader2.setParameter(CSVSchemaReader.PARAM_TYPENAME, null);
+
+		IOReport report = schemaReader2.execute(new LogProgressIndicator());
+
+		assertFalse(report.isSuccess());
+	}
 }
