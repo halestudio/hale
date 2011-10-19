9374416d78549a875edb9a6da313f3285fc3fbad
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-10-19 10:32:45 +0200

    fixed class name for request tests, added documentation
    Change-Id: Ie41e1b893a2ec5f704b35ae076c3b0b4f3b6d6bd
    Reviewed-on: http://gerrit.esdi-community.eu/126
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- common/plugins/eu.esdihumboldt.hale.common.cache.test/src/eu/esdihumboldt/hale/common/cache/RequestTest.java
+++ common/plugins/eu.esdihumboldt.hale.common.cache.test/src/eu/esdihumboldt/hale/common/cache/RequestTest.java
@@ -1,3 +1,15 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
 package eu.esdihumboldt.hale.common.cache;
 
 import static org.junit.Assert.*;
@@ -9,10 +21,18 @@ import java.net.URISyntaxException;
 import org.apache.http.client.ClientProtocolException;
 import org.junit.Test;
 
-public class TestRequestCache {
+/**
+ * Tests for cached requests
+ * @author Andreas Burchert
+ */
+public class RequestTest {
 
+	/**
+	 * Test retrieving online resources using {@link Request}
+	 */
 	@Test
 	public void testRequest() {
+		//FIXME activate cache?
 		try {
 			Request.getInstance().get(new URI("http://schemas.opengis.net/gml/3.1.1/base/gml.xsd"));
 			Request.getInstance().get(new URI("http://schemas.opengis.net/gml/3.1.1/base/gml.xsd"));
@@ -37,6 +57,7 @@ public class TestRequestCache {
 			assertTrue(false);
 		}
 		assertTrue(true);
+		//FIXME any better way to check the test results?
 		
 		CacheManager.getInstance().shutdown();
 	}
