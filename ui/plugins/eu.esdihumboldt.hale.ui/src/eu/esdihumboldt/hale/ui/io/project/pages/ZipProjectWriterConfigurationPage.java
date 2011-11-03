7331d8de28a91ee2471b971cedc7ce09f59c417a
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-11-03 10:01:03 +0100

    added configuration page for ZipProjectWriter
    It allows specifying that internal project files should be placed
    outside the project archive.
    
    refs #301
    
    Change-Id: Ia6b90fb62e47f5d72a64619ff005d395e32c7817
    Reviewed-on: http://gerrit.esdi-community.eu/199
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/project/pages/ZipProjectWriterConfigurationPage.java
@@ -0,0 +1,85 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.io.project.pages;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import eu.esdihumboldt.hale.common.core.io.IOProvider;
+import eu.esdihumboldt.hale.common.core.io.project.impl.ZipProjectWriter;
+import eu.esdihumboldt.hale.ui.HaleWizardPage;
+import eu.esdihumboldt.hale.ui.io.IOWizard;
+import eu.esdihumboldt.hale.ui.io.IOWizardPage;
+import eu.esdihumboldt.hale.ui.io.config.AbstractConfigurationPage;
+
+/**
+ * Configuration page for the {@link ZipProjectWriter}
+ * @author Simon Templer
+ */
+public class ZipProjectWriterConfigurationPage extends
+		AbstractConfigurationPage<ZipProjectWriter, IOWizard<ZipProjectWriter>> {
+
+	private Button checkSeparateFiles;
+	
+	/**
+	 * Default constructor
+	 */
+	public ZipProjectWriterConfigurationPage() {
+		super("zipWriter");
+		
+		setTitle("Project file");
+		setDescription("Project file settings");
+	}
+
+	/**
+	 * @see AbstractConfigurationPage#enable()
+	 */
+	@Override
+	public void enable() {
+		// do nothing
+	}
+
+	/**
+	 * @see AbstractConfigurationPage#disable()
+	 */
+	@Override
+	public void disable() {
+		// do nothing
+	}
+
+	/**
+	 * @see IOWizardPage#updateConfiguration(IOProvider)
+	 */
+	@Override
+	public boolean updateConfiguration(ZipProjectWriter provider) {
+		provider.setUseSeparateFiles(checkSeparateFiles.getSelection());
+		return true;
+	}
+
+	/**
+	 * @see HaleWizardPage#createContent(Composite)
+	 */
+	@Override
+	protected void createContent(Composite page) {
+		page.setLayout(new GridLayout(1, false));
+		
+		checkSeparateFiles = new Button(page, SWT.CHECK);
+		checkSeparateFiles.setSelection(false); // default
+		checkSeparateFiles.setText("Place alignment next to the project file instead of inside the project archive");
+		
+		setPageComplete(true);
+	}
+
+}
