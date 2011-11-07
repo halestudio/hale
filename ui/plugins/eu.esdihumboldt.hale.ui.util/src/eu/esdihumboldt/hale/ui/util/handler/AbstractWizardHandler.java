62e64feaa7d4147d8fde4a58994ed29c61e9d9d5
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-11-07 17:31:50 +0100

    added abstract handler for showing a wizard
    Change-Id: Ifc3512d02a369c7ce21533d4c2b2dfaa63c25ea1
    Reviewed-on: http://gerrit.esdi-community.eu/216
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.util/src/eu/esdihumboldt/hale/ui/util/handler/AbstractWizardHandler.java
@@ -0,0 +1,49 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.util.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Shows a wizard
+ * @author Simon Templer
+ */
+public abstract class AbstractWizardHandler extends AbstractHandler {
+
+	/**
+	 * @see IHandler#execute(ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWizard wizard = createWizard();
+		Shell shell = Display.getCurrent().getActiveShell();
+		WizardDialog dialog = new WizardDialog(shell, wizard);
+		dialog.open();
+		
+		return null;
+	}
+
+	/**
+	 * Create the wizard
+	 * @return the wizard instance
+	 */
+	protected abstract IWizard createWizard();
+
+}
