28c2fc3760532c501d12289b0e90cb87f3bc1e4d
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-11-08 10:49:33 +0100

    AlignmentUtil: added methods for to analyze an alignment for type relations
    Change-Id: I8a491c60aa8fe0eb2229451ad99c2d89e19bb1b1

--- common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/AlignmentUtil.java
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/AlignmentUtil.java
@@ -12,6 +12,10 @@
 
 package eu.esdihumboldt.hale.common.align.model;
 
+import java.util.Collection;
+
+import eu.esdihumboldt.hale.common.align.model.impl.TypeEntityDefinition;
+
 /**
  * Alignment model utility methods.
  * @author Simon Templer
@@ -26,6 +30,56 @@ public abstract class AlignmentUtil {
 	public static boolean isTypeCell(Cell cell) {
 		// check if cell is a type cell
 		return cell.getTarget().values().iterator().next() instanceof Type;
+	}
+	
+	/**
+	 * Determines if the given alignment has any type relations.
+	 * @param alignment the alignment
+	 * @return if any type cells are present in the alignment
+	 */
+	public static boolean hasTypeRelation(Alignment alignment) {
+		for (Cell cell : alignment.getCells()) {
+			if (isTypeCell(cell)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Determines if the given alignment contains a relation between the
+	 * given types.
+	 * @param alignment the alignment
+	 * @param sourceType the source type, may be <code>null</code> for any 
+	 *   source type
+	 * @param targetType the target type, may be <code>null</code> for any
+	 *   target type 
+	 * @return if a relation between the given types exists in the alignment
+	 */
+	public static boolean hasTypeRelation(Alignment alignment, 
+			TypeEntityDefinition sourceType, TypeEntityDefinition targetType) {
+		if (sourceType == null && targetType == null) {
+			// accept any type relation
+			return hasTypeRelation(alignment);
+		}
+		else if (sourceType == null) {
+			// accept any relation to the given target type
+			Collection<? extends Cell> cells = alignment.getCells(targetType); //FIXME only target!
+			return !cells.isEmpty();
+		}
+		else if (targetType == null) {
+			// accept any relation to the given source type
+			Collection<? extends Cell> cells = alignment.getCells(sourceType); //FIXME only source!
+			return !cells.isEmpty();
+		}
+		else {
+			// accept relations only if they combine both types
+			Collection<? extends Cell> targetCells = alignment.getCells(targetType); //FIXME only target!
+			Collection<? extends Cell> sourceCells = alignment.getCells(sourceType); //FIXME only source!
+			targetCells.retainAll(sourceCells);
+			return !targetCells.isEmpty();
+		}
 	}
 	
 	/**
