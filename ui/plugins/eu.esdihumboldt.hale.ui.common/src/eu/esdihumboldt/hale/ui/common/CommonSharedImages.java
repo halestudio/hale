d6b294227a981ca9abe6325da9a4f65330ffadbf
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-11-11 16:02:55 +0100

    use shared images for definition images
    Change-Id: I17f2ad81d4fd430bf2ee935b19eca057ba72593a

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.common/src/eu/esdihumboldt/hale/ui/common/CommonSharedImages.java
@@ -0,0 +1,33 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.common;
+
+import org.eclipse.jface.resource.ImageRegistry;
+
+import eu.esdihumboldt.hale.ui.common.internal.CommonUIPlugin;
+
+/**
+ * Provides access to HALE common shared images
+ * @author Simon Templer
+ */
+public abstract class CommonSharedImages implements CommonSharedImagesConstants {
+
+	/**
+	 * Get the image registry
+	 * @return the image registry
+	 */
+	public static ImageRegistry getImageRegistry() {
+		return CommonUIPlugin.getDefault().getImageRegistry();
+	}
+	
+}
---
+++ ui/plugins/eu.esdihumboldt.hale.ui.common/src/eu/esdihumboldt/hale/ui/common/CommonSharedImagesConstants.java
@@ -0,0 +1,67 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.common;
+
+/**
+ * Constants defining identifiers for shared images. 
+ * @author Simon Templer
+ * @since 2.5
+ */
+public interface CommonSharedImagesConstants {
+	
+	/**
+	 * Key for abstract feature type image 
+	 */
+	public static final String IMG_DEFINITION_ABSTRACT_FT = "IMG_DEFINITION_ABSTRACT_FT";
+
+	/**
+	 * Key for concrete feature type image
+	 */
+	public static final String IMG_DEFINITION_CONCRETE_FT = "IMG_DEFINITION_CONCRETE_FT";
+	
+	/**
+	 * Key for string property image
+	 */
+	public static final String IMG_DEFINITION_STRING_PROPERTY = "IMG_DEFINITION_STRING_PROPERTY";
+	
+	/**
+	 * Key for numeric property image
+	 */
+	public static final String IMG_DEFINITION_NUMERIC_PROPERTY = "IMG_DEFINITION_NUMERIC_PROPERTY";
+
+	/**
+	 * Key for geometric property image
+	 */
+	public static final String IMG_DEFINITION_GEOMETRIC_PROPERTY = "IMG_DEFINITION_GEOMETRIC_PROPERTY";
+	
+	/**
+	 * Key for group image
+	 */
+	public static final String IMG_DEFINITION_GROUP = "IMG_DEFINITION_GROUP";
+	
+	/**
+	 * Key for choice image
+	 */
+	public static final String IMG_DEFINITION_CHOICE = "IMG_DEFINITION_CHOICE";
+	
+	/**
+	 * Key for concrete type image
+	 */
+	public static final String IMG_DEFINITION_CONCRETE_TYPE = "IMG_DEFINITION_CONCRETE_TYPE";
+	
+	/**
+	 * Key for abstract type image
+	 */
+	public static final String IMG_DEFINITION_ABSTRACT_TYPE = "IMG_DEFINITION_ABSTRACT_TYPE";
+
+}
