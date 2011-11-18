bb33dff7306142e4f2a8f1a15a0eb6f3380fe39a
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-11-18 12:10:17 +0100

    added LayoutAction to AbstractMappingView
    Also use protected methods to create layout, label and content providers.
    
    refs #345
    
    Change-Id: I76c6d750054c7e5cc3298c22a614df6d7cbad5e1
    Reviewed-on: http://gerrit.esdi-community.eu/291
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.mapping/src/eu/esdihumboldt/hale/ui/views/mapping/LayoutAction.java
@@ -0,0 +1,48 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.mapping;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.zest.core.viewers.GraphViewer;
+
+import eu.esdihumboldt.hale.ui.views.mapping.internal.MappingViewPlugin;
+
+/**
+ * Action that triggers relayouting a {@link GraphViewer}
+ * @author Simon Templer
+ */
+public class LayoutAction extends Action {
+
+	private final GraphViewer viewer;
+	
+	/**
+	 * Constructor
+	 * @param viewer the graph viewer to layout
+	 */
+	public LayoutAction(GraphViewer viewer) {
+		super();
+		
+		this.viewer = viewer;
+		
+		setImageDescriptor(MappingViewPlugin.getImageDescriptor("icons/layout.gif"));
+	}
+
+	/**
+	 * @see Action#run()
+	 */
+	@Override
+	public void run() {
+		viewer.applyLayout();
+	}
+
+}
