4b949a831a8fce0af99d0563a5e0d1fe99aa01a0
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2011-11-22 18:20:17 +0100

    changed PropertyResolver and its Tests for proper working with geotools filtering system
    Change-Id: I6d67ce33fae6ca3e829d2dad9ef3262d80ad8ebe

--- common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/helper/QueueDefinitionItem.java
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/helper/QueueDefinitionItem.java
@@ -19,9 +19,11 @@ import javax.xml.namespace.QName;
 import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
 
 /**
- * TODO Type description
+ * Objects of these Class are used by the PropertyResolver in a Queue for traversing the instance-definition-tree	
  * @author Sebastian Reinhardt
  */
+
+
 public class QueueDefinitionItem {
 
 	ChildDefinition<?> def;
@@ -36,33 +38,38 @@ public class QueueDefinitionItem {
 	}
 
 	/**
-	 * @return the propDef
+	 * @return the propDef returns the instance definition in this item
 	 */
 	public ChildDefinition<?> getDefinition() {
 		return def;
 	}
 
 	/**
-	 * @param propDef the propDef to set
+	 * @param propDef sets the instance definition in this item
 	 */
 	public void setDef(ChildDefinition<?> propDef) {
 		this.def = def;
 	}
 
 	/**
-	 * @return the qnames
+	 * @return the qnames from the path of the definition inside the instance-definition-tree
 	 */
 	public ArrayList<QName> getQnames() {
 		return qnames;
 	}
 
 	/**
-	 * @param qname the qname to add
+	 * Adds a single QName to the path
+	 * @param qname the QName to be add
 	 */
 	public void addQname(QName qname) {
 		this.qnames.add(0, qname);
 	}
 	
+	/**
+	 * Adds multiple QNames to the path
+	 * @param qnames the QName sto be add
+	 */
 	public void addQnames(ArrayList<QName> qnames){
 		
 		int i = 0;
@@ -73,15 +80,29 @@ public class QueueDefinitionItem {
 		
 	}
 	
+	
+	/**
+	 * adds known loop paths wich appear in the 
+	 * path of the instance-definition-tree on the way to the definition of this item
+	 * @param loopQNames the loop paths to add
+	 */
 	public void addLoopQNames(ArrayList<QName> loopQNames) {
 		loops.add(loopQNames);
 	}
 	
+	/**
+	 * returns the known loop paths wich appear in the 
+	 * path of the instance-definition-tree on the way to the definition of this item
+	 * @return the known loop-paths
+	 */
 	public ArrayList<ArrayList<QName>> getLoopQNames(){
 		return loops;
 	}
 	
-	
+	/**
+	 * returns the path of the definition of this item in the instance-definition-tree as a String
+	 * @return the stringrespresentation of the path
+	 */
 	public String qNamesToString(){
 		String result = "";
 		for(int i = 0; i < qnames.size(); i++){
@@ -89,7 +110,7 @@ public class QueueDefinitionItem {
 				result = qnames.get(i).toString();
 			}
 			else{ 
-				result = result.concat("." + qnames.get(i));
+				result = result.concat("/" + qnames.get(i));
 			}
 		}
 		
