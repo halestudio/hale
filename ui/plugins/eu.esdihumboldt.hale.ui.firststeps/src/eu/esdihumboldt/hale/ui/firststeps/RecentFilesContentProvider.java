354cf2977b94f649112c08f015fb6995667941b7
Kai Schwierczek <kai@schwierczek.de>
2011-11-23 15:08:43 +0100

    small changes to the recent files list
    added example schemas, cheatsheet that builds up on it is in work
    
    refs #335
    
    Change-Id: I3d4d03e5aa7e1b16abbac7f74d07cbc12cf1269b
    Reviewed-on: http://gerrit.esdi-community.eu/314
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ui/plugins/eu.esdihumboldt.hale.ui.firststeps/src/eu/esdihumboldt/hale/ui/firststeps/RecentFilesContentProvider.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.firststeps/src/eu/esdihumboldt/hale/ui/firststeps/RecentFilesContentProvider.java
@@ -37,9 +37,13 @@ import eu.esdihumboldt.hale.ui.service.project.RecentFilesService;
  * 
  * @author Kai Schwierczek
  */
-public class RecentFilesContentProvider implements IIntroContentProvider,
-		IIntroAction {
+public class RecentFilesContentProvider implements IIntroContentProvider, IIntroAction {
 	private boolean disposed = false;
+
+	/**
+	 * Max length of the project file path shown.
+	 */
+	public static final int MAX_LENGTH = 60;
 
 	/**
 	 * @see org.eclipse.ui.intro.config.IIntroContentProvider#init(org.eclipse.ui.intro.config.IIntroContentProviderSite)
@@ -48,8 +52,7 @@ public class RecentFilesContentProvider implements IIntroContentProvider,
 	public void init(IIntroContentProviderSite site) {
 		// site isn't needed or directly react to changes in recent entries?
 		// -> somehow add listener to recent entries, reflow after changes,
-		// remove
-		// listener in dispose ?
+		//    remove listener in dispose ?
 	}
 
 	/**
@@ -65,22 +68,22 @@ public class RecentFilesContentProvider implements IIntroContentProvider,
 		RecentFilesService.Entry[] entries = getRecentFiles();
 		if (entries.length > 0) {
 			out.print("<ul id=\"recent-files-list\">"); //$NON-NLS-1$
-			for (RecentFilesService.Entry entry : entries) {
+			for (int i = entries.length - 1; i >= 0; i--) {
+				RecentFilesService.Entry entry = entries[i];
 				out.print("<li>"); //$NON-NLS-1$
+				if (entry.getProjectName() != null) {
+					out.print(entry.getProjectName());
+					out.print("<br>"); //$NON-NLS-1$
+				}
 				out.print("<a class=\"recentFile\" href=\""); //$NON-NLS-1$
 				out.print("http://org.eclipse.ui.intro/runAction?"); //$NON-NLS-1$
 				out.print("pluginId=eu.esdihumboldt.hale.ui.firststeps&"); //$NON-NLS-1$
-				out.print("class=" + this.getClass().getName() + "&"); //$NON-NLS-1$
+				out.print("class=" + this.getClass().getName() + "&"); //$NON-NLS-1$ //$NON-NLS-2$
 				out.print("file=" + entry.getFile()); //$NON-NLS-1$
 				out.print("\">"); //$NON-NLS-1$
-				out.print(RecentFilesMenu.shorten(entry.getFile(),
-						RecentFilesMenu.MAX_LENGTH, new File(entry.getFile())
-								.getName().length()));
+				out.print(RecentFilesMenu.shorten(entry.getFile(), MAX_LENGTH, new File(entry.getFile()).getName()
+						.length()));
 				out.print("</a>"); //$NON-NLS-1$
-				if (entry.getProjectName() != null) {
-					out.print("<br />Project name: "); //$NON-NLS-1$
-					out.print(entry.getProjectName());
-				}
 				out.print("</li>"); //$NON-NLS-1$
 			}
 			out.print("</ul>"); //$NON-NLS-1$
@@ -118,19 +121,19 @@ public class RecentFilesContentProvider implements IIntroContentProvider,
 
 		RecentFilesService.Entry[] entries = getRecentFiles();
 		if (entries.length > 0) {
-			for (RecentFilesService.Entry entry : entries) {
+			for (int i = entries.length - 1; i >= 0; i--) {
+				RecentFilesService.Entry entry = entries[i];
 				buffer.append("<li style=\"none\">"); //$NON-NLS-1$
+				if (entry.getProjectName() != null) {
+					buffer.append(entry.getProjectName());
+					buffer.append("<br>"); //$NON-NLS-1$
+				}
 				buffer.append("<a href=\""); //$NON-NLS-1$
 				buffer.append(entry.getFile());
 				buffer.append("\">"); //$NON-NLS-1$
-				buffer.append(RecentFilesMenu.shorten(entry.getFile(),
-						RecentFilesMenu.MAX_LENGTH, new File(entry.getFile())
-								.getName().length()));
+				buffer.append(RecentFilesMenu.shorten(entry.getFile(), MAX_LENGTH, new File(entry.getFile()).getName()
+						.length()));
 				buffer.append("</a>"); //$NON-NLS-1$
-				if (entry.getProjectName() != null) {
-					buffer.append("<br>Project name: "); //$NON-NLS-1$
-					buffer.append(entry.getProjectName());
-				}
 				buffer.append("</li>"); //$NON-NLS-1$
 			}
 		} else {
@@ -147,8 +150,7 @@ public class RecentFilesContentProvider implements IIntroContentProvider,
 	}
 
 	private RecentFilesService.Entry[] getRecentFiles() {
-		RecentFilesService rfs = (RecentFilesService) PlatformUI.getWorkbench()
-				.getService(RecentFilesService.class);
+		RecentFilesService rfs = (RecentFilesService) PlatformUI.getWorkbench().getService(RecentFilesService.class);
 		RecentFilesService.Entry[] entries = rfs.getRecentFiles();
 		if (entries == null)
 			entries = new RecentFilesService.Entry[0];
@@ -171,7 +173,12 @@ public class RecentFilesContentProvider implements IIntroContentProvider,
 	public void run(IIntroSite site, Properties params) {
 		open(params.getProperty("file")); //$NON-NLS-1$
 	}
-	
+
+	/**
+	 * Opens the specified project file and closes the intro part.
+	 * 
+	 * @param file the project file
+	 */
 	private void open(String file) {
 		ProjectService ps = (ProjectService) PlatformUI.getWorkbench().getService(ProjectService.class);
 		ps.load(new File(file));
