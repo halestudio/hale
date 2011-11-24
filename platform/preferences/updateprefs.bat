262729bd2f247f5188738d3393744125c9cc62dc
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-11-24 08:42:21 +0100

    updated preferences script to include doc projects
    Change-Id: I0736eeb98f74e20f27a0d6156e5702eddf2b1de3

--- platform/preferences/updateprefs.bat
+++ platform/preferences/updateprefs.bat
@@ -2,6 +2,7 @@ java -jar proprefapp.jar common ../../common
 java -jar proprefapp.jar common ../../cst
 java -jar proprefapp.jar common ../../io
 java -jar proprefapp.jar common ../../server
+java -jar proprefapp.jar common ../../doc
 java -jar proprefapp.jar common ../../ui
 java -jar proprefapp.jar common ../../util
 java -jar proprefapp.jar mdl ../../ext/mdl eu.xsdi.mdl eu.xsdi.mdlui
