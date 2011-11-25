af67fbfa90681bbb5e0c8e649d2aa362e17a06ec
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2011-11-25 13:18:55 +0100

    added first version of function in properties view
    refs #346
    
    Change-Id: I58d1303c6af99638dc5d77b83868b4e842f4639e

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.properties/src/eu/esdihumboldt/hale/ui/views/properties/function/AbstractFunctionSection.java
@@ -0,0 +1,43 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.properties.function;
+
+import eu.esdihumboldt.hale.common.align.extension.function.Function;
+import eu.esdihumboldt.hale.ui.views.properties.AbstractSection;
+
+/**
+ * Abstract section for function properties
+ * @author Patrick Lieb
+ * @param <F> the function
+ */
+public abstract class AbstractFunctionSection<F extends Function> extends AbstractSection{
+
+	/**
+	 * the general Function for this package
+	 */
+	private F function;
+	
+	/**
+	 * @param fun the Function
+	 */
+	protected void setFunction(F fun){
+		function = fun;
+	}
+
+	/**
+	 * @return the Function
+	 */
+	public F getFunction() {
+		return function;
+	}
+}
---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.properties/src/eu/esdihumboldt/hale/ui/views/properties/function/abstractfunction/AbstractFunctionNameSection.java
@@ -0,0 +1,48 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.properties.function.abstractfunction;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+
+import eu.esdihumboldt.hale.common.align.extension.function.AbstractFunction;
+import eu.esdihumboldt.hale.ui.views.properties.function.DefaultFunctionSection;
+
+/**
+ * Abstract function section with function name
+ * @author Patrick Lieb
+ */
+public class AbstractFunctionNameSection extends DefaultFunctionSection<AbstractFunction<?>>{
+
+	private Text name;
+
+	/**
+	 * @see AbstractPropertySection#createControls(Composite, TabbedPropertySheetPage)
+	 */
+	@Override
+	public void createControls(Composite parent,
+			TabbedPropertySheetPage aTabbedPropertySheetPage) {
+		abstractCreateControls(parent, aTabbedPropertySheetPage, "Name:", null);
+		name = getText();
+	}
+	
+	/**
+	 * @see AbstractPropertySection#refresh()
+	 */
+	@Override
+	public void refresh() {
+		name.setText(getFunction().getDisplayName());
+	}
+}
