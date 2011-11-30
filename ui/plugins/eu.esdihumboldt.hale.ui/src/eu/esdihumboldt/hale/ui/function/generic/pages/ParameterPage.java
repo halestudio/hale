861bbe1936a4481bb89628baf93bab6bc237db5b
Kai Schwierczek <kai@schwierczek.de>
2011-11-30 16:59:14 +0100

    added minimal input for parameter conf page
    refs #348
    
    Change-Id: Ifbca2db962acda3dbbe33ffed374bb5d4ba5c177
    Reviewed-on: http://gerrit.esdi-community.eu/361
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/function/generic/pages/ParameterPage.java
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/function/generic/pages/ParameterPage.java
@@ -12,8 +12,23 @@
 
 package eu.esdihumboldt.hale.ui.function.generic.pages;
 
-import org.eclipse.swt.widgets.Composite;
+import java.util.Map;
 
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+
+import eu.esdihumboldt.hale.common.align.extension.function.AbstractParameter;
+import eu.esdihumboldt.hale.common.align.extension.function.FunctionParameter;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.MutableCell;
 import eu.esdihumboldt.hale.ui.HaleWizardPage;
@@ -21,25 +36,28 @@ import eu.esdihumboldt.hale.ui.function.generic.AbstractGenericFunctionWizard;
 
 /**
  * Page for configuring function parameters.
+ * 
  * @author Simon Templer
+ * @author Kai Schwierczek
  */
-public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<?, ?>> 
-		implements FunctionWizardPage {
-
+public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<?, ?>> implements FunctionWizardPage {
 	private final Cell initialCell;
+	private final ListMultimap<FunctionParameter, Text> inputFields;
 
 	/**
 	 * Default constructor
+	 * 
 	 * @param initialCell the initial cell, may be <code>null</code>
 	 */
 	public ParameterPage(Cell initialCell) {
 		super("parameters");
-		
+
 		setTitle("Function parameters");
 		setDescription("Specify the parameters for the relation");
-		
+
 		this.initialCell = initialCell;
-		
+		inputFields = ArrayListMultimap.create();
+
 		setPageComplete(false);
 	}
 
@@ -55,7 +73,13 @@ public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<
 	 * Update the page state
 	 */
 	private void updateState() {
-		//TODO
+		// XXX how to validate fields? -> bindings!?
+		// for testing: check whether all fields aren't empty.
+		for (Map.Entry<FunctionParameter, Text> entry : inputFields.entries())
+			if (entry.getValue().getText().isEmpty()) {
+				setPageComplete(false);
+				return;
+			}
 		setPageComplete(true);
 	}
 
@@ -64,8 +88,18 @@ public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<
 	 */
 	@Override
 	public void configureCell(MutableCell cell) {
-		// TODO Auto-generated method stub
-		
+		ListMultimap<String, String> map = cell.getTransformationParameters();
+		if (map == null) {
+			map = ArrayListMultimap.create();
+			cell.setTransformationParameters(map);
+		}
+		// XXX update instead of reset?
+		//     how determine which key-value pairs replace which?
+		//     or remove all key-value pairs where key is used by this function?
+		//        can there even be some?
+		map.clear();
+		for (Map.Entry<FunctionParameter, Text> entry : inputFields.entries())
+			map.put(entry.getKey().getName(), entry.getValue().getText());
 	}
 
 	/**
@@ -73,9 +107,33 @@ public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<
 	 */
 	@Override
 	protected void createContent(Composite page) {
-		// TODO Auto-generated method stub
-		
-		//TODO create controls
+		page.setLayout(GridLayoutFactory.swtDefaults().create());
+
+		for (FunctionParameter fp : this.getWizard().getFunction().getDefinedParameters()) {
+			Group group = new Group(page, SWT.NONE);
+			group.setText(fp.getDisplayName());
+			group.setLayout(GridLayoutFactory.swtDefaults().create());
+			if (fp.getDescription() != null) {
+				Label description = new Label(group, SWT.NONE);
+				description.setText(fp.getDescription());
+			}
+
+			for (int i = 0; i < fp.getMinOccurrence(); i++) {
+				Text text = new Text(group, SWT.SINGLE);
+				text.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+				text.addModifyListener(new ModifyListener() {
+					@Override
+					public void modifyText(ModifyEvent e) {
+						updateState();
+					}
+				});
+				inputFields.put(fp, text);
+			}
+			if (fp.getMaxOccurrence() == AbstractParameter.UNBOUNDED || fp.getMaxOccurrence() > fp.getMinOccurrence()) {
+				// TODO plus button to add more entries
+			}
+			group.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+		}
 		//TODO configure with initial cell if present
 	}
 
@@ -85,5 +143,4 @@ public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<
 	public Cell getInitialCell() {
 		return initialCell;
 	}
-
 }
