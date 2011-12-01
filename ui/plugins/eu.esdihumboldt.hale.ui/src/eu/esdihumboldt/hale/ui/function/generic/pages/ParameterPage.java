d0428d5ff6382cc34190a25d7645a93233dcfcbe
Kai Schwierczek <kai@schwierczek.de>
2011-12-01 16:08:03 +0100

    enhanced parameter page
    refs #348
    
    Change-Id: Idb57231698b6764a53cccb9bb664efb2548ad030

--- ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/function/generic/pages/ParameterPage.java
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/function/generic/pages/ParameterPage.java
@@ -12,13 +12,19 @@
 
 package eu.esdihumboldt.hale.ui.function.generic.pages;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
@@ -93,11 +99,11 @@ public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<
 			map = ArrayListMultimap.create();
 			cell.setTransformationParameters(map);
 		}
-		// XXX update instead of reset?
-		//     how determine which key-value pairs replace which?
-		//     or remove all key-value pairs where key is used by this function?
-		//        can there even be some?
-		map.clear();
+		// XXX What if two functions use the same parameter name?
+		// remove all parameters that get set by this wizards function
+		for (FunctionParameter fp : getWizard().getFunction().getDefinedParameters())
+			map.removeAll(fp.getName());
+		// and add those now
 		for (Map.Entry<FunctionParameter, Text> entry : inputFields.entries())
 			map.put(entry.getKey().getName(), entry.getValue().getText());
 	}
@@ -109,7 +115,8 @@ public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<
 	protected void createContent(Composite page) {
 		page.setLayout(GridLayoutFactory.swtDefaults().create());
 
-		for (FunctionParameter fp : this.getWizard().getFunction().getDefinedParameters()) {
+		// create section for each function parameter
+		for (final FunctionParameter fp : getWizard().getFunction().getDefinedParameters()) {
 			Group group = new Group(page, SWT.NONE);
 			group.setText(fp.getDisplayName());
 			group.setLayout(GridLayoutFactory.swtDefaults().create());
@@ -118,23 +125,119 @@ public class ParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<
 				description.setText(fp.getDescription());
 			}
 
-			for (int i = 0; i < fp.getMinOccurrence(); i++) {
-				Text text = new Text(group, SWT.SINGLE);
-				text.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
-				text.addModifyListener(new ModifyListener() {
-					@Override
-					public void modifyText(ModifyEvent e) {
-						updateState();
-					}
-				});
-				inputFields.put(fp, text);
+			// walk over data of initial cell while creating input fields
+			List<String> initialData = new ArrayList<String>(0);
+			if (initialCell != null && initialCell.getTransformationParameters() != null)
+				initialData = initialCell.getTransformationParameters().get(fp.getName());
+			Iterator<String> initialDataIter = initialData.iterator();
+
+			// create a minimum number of input fields
+			int i;
+			for (i = 0; i < fp.getMinOccurrence(); i++)
+				if (initialDataIter.hasNext())
+					createField(group, fp, initialDataIter.next());
+				else
+					createField(group, fp, null);
+
+			// create further fields if initial cell has more
+			// XXX check for max occurrence necessary? Is initialCell always correct?
+			for (; initialDataIter.hasNext()
+					&& (fp.getMaxOccurrence() == AbstractParameter.UNBOUNDED || i < fp.getMaxOccurrence()); i++)
+				createField(group, fp, initialDataIter.next());
+
+			// create control buttons if max occurrence != min occurrence
+			if (fp.getMaxOccurrence() != fp.getMinOccurrence()) {
+				final Composite buttons = new Composite(group, SWT.NONE);
+				buttons.setLayout(GridLayoutFactory.swtDefaults().numColumns(2).create());
+				buttons.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+				createButtons(buttons, fp, i < fp.getMaxOccurrence()
+						|| fp.getMaxOccurrence() == AbstractParameter.UNBOUNDED, i > fp.getMinOccurrence());
 			}
-			if (fp.getMaxOccurrence() == AbstractParameter.UNBOUNDED || fp.getMaxOccurrence() > fp.getMinOccurrence()) {
-				// TODO plus button to add more entries
-			}
+
 			group.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
 		}
-		//TODO configure with initial cell if present
+	}
+
+	/**
+	 * Create add and remove buttons for the given function parameter in the
+	 * given composite with the given initial visibility.
+	 * 
+	 * @param parent the composite
+	 * @param fp the function parameter
+	 * @param addVisible whether the add button is visible in the beginning
+	 * @param removeVisible whether the remove button is visible in the
+	 *            beginning
+	 */
+	private void createButtons(final Composite parent, final FunctionParameter fp, boolean addVisible,
+			boolean removeVisible) {
+		// create add button -> left
+		final Button addButton = new Button(parent, SWT.PUSH);
+		addButton.setLayoutData(new GridData(SWT.BEGINNING, SWT.BEGINNING, true, false));
+		addButton.setText("Add parameter value");
+		addButton.setVisible(addVisible);
+
+		// create remove button -> right
+		final Button removeButton = new Button(parent, SWT.PUSH);
+		removeButton.setLayoutData(new GridData(SWT.END, SWT.BEGINNING, true, false));
+		removeButton.setText("Remove parameter value");
+		removeButton.setVisible(removeVisible);
+
+		// create selection listeners
+		removeButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// remove last text field
+				List<Text> texts = inputFields.get(fp);
+				Text removed = texts.remove(texts.size() - 1);
+				updateState();
+				removed.dispose();
+				addButton.setVisible(true);
+				if (texts.size() == fp.getMinOccurrence())
+					removeButton.setVisible(false);
+				((Composite) getControl()).layout();
+				getWizard().getShell().pack();
+			}
+		});
+
+		addButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// add text field
+				List<Text> texts = inputFields.get(fp);
+				Text added = createField(parent.getParent(), fp, null);
+				updateState();
+				added.moveAbove(parent);
+				if (texts.size() == fp.getMaxOccurrence())
+					addButton.setVisible(false);
+				removeButton.setVisible(true);
+				((Composite) getControl()).layout();
+				getWizard().getShell().pack();
+			}
+		});
+	}
+
+	/**
+	 * Creates a text field for the given function parameter and given initial
+	 * value.
+	 * 
+	 * @param parent the composite in which to place the text field
+	 * @param fp the function parameter
+	 * @param initialData initial value or null
+	 * @return the created text field
+	 */
+	private Text createField(Composite parent, FunctionParameter fp, String initialData) {
+		Text text = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+		if (initialData != null)
+			text.setText(initialData);
+		text.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				updateState();
+			}
+		});
+		inputFields.put(fp, text);
+		return text;
 	}
 
 	/**
