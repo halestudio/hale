bfdc5c80290cdc1ad0facd43a1e634b21678746e
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-12-08 12:15:33 +0100

    fixed problem with function duplicates in function view
    Two AbstractFunctions now are equal if their ID is equal.
    
    Change-Id: Ic1384350d925034db0fd2c0a605c580e794de558

--- common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/extension/function/AbstractFunction.java
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/extension/function/AbstractFunction.java
@@ -155,4 +155,35 @@ public abstract class AbstractFunction<P extends AbstractParameter> implements F
 		return null;
 	}
 
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+		return result;
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractFunction<?> other = (AbstractFunction<?>) obj;
+		if (getId() == null) {
+			if (other.getId() != null)
+				return false;
+		} else if (!getId().equals(other.getId()))
+			return false;
+		return true;
+	}
+
 }
