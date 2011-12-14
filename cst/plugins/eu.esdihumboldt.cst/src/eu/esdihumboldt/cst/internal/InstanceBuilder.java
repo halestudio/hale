071654b887b0a2e6906f8d2231b7ee8e64a11bf2
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-12-14 15:11:25 +0100

    added first version of InstanceBuilder
    It populates a target instance from a transformation tree.
    
    refs #321
    
    Change-Id: Id11dca42a5ee22be3f6cbec5e7b19bb58db94af7

---
+++ cst/plugins/eu.esdihumboldt.cst/src/eu/esdihumboldt/cst/internal/InstanceBuilder.java
@@ -0,0 +1,116 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.internal;
+
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.GroupNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TargetNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationTree;
+import eu.esdihumboldt.hale.common.instance.model.MutableGroup;
+import eu.esdihumboldt.hale.common.instance.model.MutableInstance;
+import eu.esdihumboldt.hale.common.instance.model.impl.OGroup;
+import eu.esdihumboldt.hale.common.instance.model.impl.OInstance;
+
+/**
+ * Populates an instance from a transformation tree.
+ * @author Simon Templer
+ */
+public class InstanceBuilder {
+	
+	/**
+	 * Represents no object.
+	 */
+	private static enum NoObject {
+		NONE;
+	}
+
+	/**
+	 * Populate the given instance from a transformation tree.
+	 * @param target the target instance
+	 * @param tree the transformation tree
+	 */
+	public void populate(MutableInstance target, TransformationTree tree) {
+		populateGroup(target, tree);
+	}
+	
+	/**
+	 * Get the value for a target node.
+	 * @param node the target node
+	 * @return the value or {@link NoObject#NONE} representing no value
+	 */
+	private Object getValue(TargetNode node) {
+		if (node.getChildren().isEmpty()) {
+			// simple leaf
+			if (node.isDefined()) {
+				//XXX case where an Instance should be returned? XXX according to the definition?!
+				return node.getResult();
+			}
+			else {
+				return NoObject.NONE;
+			}
+		}
+		
+		// group or complex property
+		boolean empty = true;
+		MutableGroup group;
+		if (node.getDefinition().asGroup() != null) {
+			group = new OGroup(node.getDefinition().asGroup());
+		}
+		else if (node.getDefinition().asProperty() != null) {
+			group = new OInstance(node.getDefinition().asProperty().getPropertyType());
+		}
+		else {
+			throw new IllegalStateException("Illegal child definition");
+		}
+		
+		// populate with children
+		empty = !populateGroup(group, node);
+		
+		// populate with instance value (if applicable)
+		if (group instanceof MutableInstance) {
+			if (node.isDefined()) {
+				MutableInstance instance = (MutableInstance) group;
+				instance.setValue(node.getResult());
+				empty = false;
+			}
+		}
+		
+		if (empty) {
+			return NoObject.NONE;
+		}
+		else {
+			return group;
+		}
+	}
+
+	/**
+	 * Populates a group with values from its children.
+	 * @param group the group
+	 * @param node the node associated with the group
+	 * @return if any values were added to the group
+	 */
+	private boolean populateGroup(MutableGroup group, GroupNode node) {
+		boolean anyValue = false;
+		for (TargetNode child : node.getChildren()) {
+			Object value = getValue(child);
+			if (value != NoObject.NONE) {
+				// add value to group
+				group.addProperty(child.getDefinition().getName(), value);
+				
+				// valid value
+				anyValue = true;
+			}
+		}
+		return anyValue;
+	}
+
+}
