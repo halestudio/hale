13749e570d5ba3433aa3f017328a718f1ff6753b
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-12-14 09:25:06 +0100

    added ConversionServiceNotAvailableException
    Change-Id: I1cfb56fac7470e852e191d5382e517642a1c343e

---
+++ common/plugins/eu.esdihumboldt.hale.common.convert/src/eu/esdihumboldt/hale/common/convert/ConversionServiceNotAvailableException.java
@@ -0,0 +1,47 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.convert;
+
+import org.springframework.core.convert.ConversionException;
+
+/**
+ * Exception that should be thrown when the conversion service is not available.
+ * @author Simon Templer
+ */
+public class ConversionServiceNotAvailableException extends ConversionException {
+
+	private static final long serialVersionUID = -6073334239505337796L;
+
+	/**
+	 * Constructor using a default message.
+	 */
+	public ConversionServiceNotAvailableException() {
+		this("Conversion service not available");
+	}
+	
+	/**
+	 * @see ConversionException#ConversionException(String, Throwable)
+	 */
+	public ConversionServiceNotAvailableException(String message,
+			Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * @see ConversionException#ConversionException(String)
+	 */
+	public ConversionServiceNotAvailableException(String message) {
+		super(message);
+	}
+
+}
