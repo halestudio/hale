69de0ab5a537500bd2ea5653216a39d418e98659
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2011-12-14 12:42:43 +0100

    changed color and figure of nodes
    refs #346
    
    Change-Id: Ibf3b7a8d9c00edc10135e06730c371007deb8ab0

--- ui/plugins/eu.esdihumboldt.hale.ui.common.graph/src/eu/esdihumboldt/hale/ui/common/graph/labels/FunctionGraphLabelProvider.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.common.graph/src/eu/esdihumboldt/hale/ui/common/graph/labels/FunctionGraphLabelProvider.java
@@ -12,27 +12,49 @@
 
 package eu.esdihumboldt.hale.ui.common.graph.labels;
 
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.zest.core.viewers.EntityConnectionData;
+import org.eclipse.zest.core.widgets.custom.shapes.FingerPost;
 
 import eu.esdihumboldt.hale.common.align.extension.function.AbstractParameter;
+import eu.esdihumboldt.hale.common.align.extension.function.Function;
 import eu.esdihumboldt.util.Pair;
 
 /**
- * Label provider for graphs based on function(s).
+ * Label provider for graphs based on {@link Function}(s).
+ * 
  * @author Patrick Lieb
  */
 public class FunctionGraphLabelProvider extends GraphLabelProvider {
+	
+	private final Color targetbackgroundcolor;
+	
+	private final Color sourcebackgroundcolor;
 
 	/**
 	 * @see eu.esdihumboldt.hale.ui.common.graph.labels.GraphLabelProvider#getImage(java.lang.Object)
 	 */
+	
+	public FunctionGraphLabelProvider(){
+		
+		final Display display = PlatformUI.getWorkbench().getDisplay();
+		
+		targetbackgroundcolor = new Color(display, 255, 160, 122);
+		sourcebackgroundcolor = new Color(display, 255, 236, 139);	
+	}
+	
 	@Override
 	public Image getImage(Object element) {
-		if(element instanceof Pair<?,?>){
-			return super.getImage(((Pair<?,?>) element).getFirst());
-		}
 		
+		if (element instanceof Pair<?, ?>) {
+			return super.getImage(((Pair<?, ?>) element).getFirst());
+		}
+
 		return super.getImage(element);
 	}
 
@@ -41,24 +63,100 @@ public class FunctionGraphLabelProvider extends GraphLabelProvider {
 	 */
 	@Override
 	public String getText(Object element) {
-		if(element instanceof Pair<?,?>){
-			element = ((Pair<?,?>) element).getFirst();
-		}
 		
+		if (element instanceof Pair<?, ?>) {
+			element = ((Pair<?, ?>) element).getFirst();
+		}
+
 		if (element instanceof EntityConnectionData) {
 			return "";
 		}
-		
-		if(element instanceof AbstractParameter){
+
+		if (element instanceof AbstractParameter) {
 			String result = ((AbstractParameter) element).getDisplayName();
-			if(!result.equals(""))
+			if (!result.equals(""))
 				return result;
 			// XXX only for developing use
 			return "(not set)";
 		}
-		
+
 		return super.getText(element);
 	}
 
+	/**
+	 * @see eu.esdihumboldt.hale.ui.common.graph.labels.GraphLabelProvider#getFigure(java.lang.Object)
+	 */
+	@Override
+	public IFigure getFigure(Object element) {
+
+		if (element instanceof AbstractParameter) {
+			return new ParameterFigure(
+					new FingerPost(10, SWT.LEFT), getOccurenceString((AbstractParameter) element));
+		}
+		
+		if (element instanceof Pair<?, ?>) {
+			element = ((Pair<?, ?>) element).getFirst();
+		}
+		
+		if (element instanceof AbstractParameter) {
+			return new ParameterFigure(
+					new FingerPost(10, SWT.RIGHT), getOccurenceString((AbstractParameter) element));
+		}
+		
+		return super.getFigure(element);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.common.graph.labels.GraphLabelProvider#getBackgroundColour(java.lang.Object)
+	 */
+	@Override
+	public Color getBackgroundColour(Object entity) {
+		
+		if (entity instanceof AbstractParameter) {
+			return targetbackgroundcolor;
+		}
+		
+		if (entity instanceof Pair<?, ?>) {
+			entity = ((Pair<?, ?>) entity).getFirst();
+		}
+
+		if (entity instanceof AbstractParameter) {
+			return sourcebackgroundcolor;
+		}
+		
+		return super.getBackgroundColour(entity);
+	}
 	
+	private String getOccurenceString(AbstractParameter parameter){
+		
+		String max;
+		String min;
+		
+		if(parameter.getMinOccurrence() == -1){
+			min = "n";
+		} else {
+			min = String.valueOf(parameter.getMinOccurrence());
+		}
+		
+		if(parameter.getMaxOccurrence() == -1){
+			max = "n";
+		} else {
+			max = String.valueOf(parameter.getMaxOccurrence());
+		}
+		
+		String text = min + "..." + max;
+		
+		return text;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.common.graph.labels.GraphLabelProvider#dispose()
+	 */
+	@Override
+	public void dispose() {
+		targetbackgroundcolor.dispose();
+		sourcebackgroundcolor.dispose();
+		super.dispose();
+	}
+
 }
