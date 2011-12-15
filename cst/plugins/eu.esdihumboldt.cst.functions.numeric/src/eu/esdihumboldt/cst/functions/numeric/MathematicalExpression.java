6d5a5fa558109fe98d65873f9e3498833b1c262b
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-12-15 10:14:14 +0100

    implemented MathematicalExpression function
    refs #321
    
    Change-Id: I9f5dfe57c17900fdc44685451981a9c76fe335ae

--- cst/plugins/eu.esdihumboldt.cst.functions.numeric/src/eu/esdihumboldt/cst/functions/numeric/MathematicalExpression.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.numeric/src/eu/esdihumboldt/cst/functions/numeric/MathematicalExpression.java
@@ -12,17 +12,26 @@
 
 package eu.esdihumboldt.cst.functions.numeric;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import net.jcip.annotations.Immutable;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.ListMultimap;
+import com.iabcinc.jmep.Environment;
+import com.iabcinc.jmep.Expression;
+import com.iabcinc.jmep.XExpression;
+import com.iabcinc.jmep.hooks.Constant;
 
+import eu.esdihumboldt.hale.common.align.model.ChildContext;
 import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
 import eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine;
 import eu.esdihumboldt.hale.common.align.transformation.function.PropertyValue;
 import eu.esdihumboldt.hale.common.align.transformation.function.TransformationException;
-import eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractPropertyTransformation;
+import eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation;
 import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
 
 /**
@@ -30,17 +39,81 @@ import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog
  * @author Simon Templer
  */
 @Immutable
-public class MathematicalExpression extends AbstractPropertyTransformation<TransformationEngine> {
+public class MathematicalExpression extends AbstractSingleTargetPropertyTransformation<TransformationEngine> {
 
+	/**
+	 * Name of the parameter specifying the mathematical expression.
+	 */
+	public static final String PARAMETER_EXPRESSION = "expression";
+	
+	/**
+	 * Entity name for variables.
+	 */
+	public static final String ENTITY_VARIABLE = "var";
+
+	/**
+	 * @see AbstractSingleTargetPropertyTransformation#evaluate(String, TransformationEngine, ListMultimap, String, PropertyEntityDefinition, Map, TransformationLog)
+	 */
 	@Override
-	protected ListMultimap<String, Object> evaluate(
-			String transformationIdentifier, TransformationEngine engine,
-			ListMultimap<String, PropertyValue> variables,
-			ListMultimap<String, PropertyEntityDefinition> resultNames,
+	protected Object evaluate(String transformationIdentifier,
+			TransformationEngine engine,
+			ListMultimap<String, PropertyValue> variables, String resultName,
+			PropertyEntityDefinition resultProperty,
 			Map<String, String> executionParameters, TransformationLog log)
 			throws TransformationException {
-		// TODO Auto-generated method stub
-		return null;
+		if (getParameters() == null
+				|| getParameters().get(PARAMETER_EXPRESSION) == null
+				|| getParameters().get(PARAMETER_EXPRESSION).isEmpty()) {
+			throw new TransformationException(MessageFormat.format(
+					"Mandatory parameter {0} not defined", PARAMETER_EXPRESSION));
+		}
+		
+		// get the mathematical expression
+		String expression = getParameters().get(PARAMETER_EXPRESSION).get(0);
+		
+		Environment env = new Environment();
+		List<PropertyValue> vars = variables.get(ENTITY_VARIABLE);
+		for (PropertyValue var : vars) {
+			// add the variable to the environment
+			
+			// determine the variable value
+			Object value = var.getValue();
+			Number number;
+			if (value instanceof Number) {
+				number = (Number) value;
+			}
+			else {
+				// try conversion to Double as default
+				number = var.getValueAs(Double.class);
+			}
+			
+			// determine the variable name
+			String name = var.getProperty().getDefinition().getName().getLocalPart();
+			Constant varValue = new Constant(number);
+			
+			// add with short name, but ensure no variable with only a short name is overridden
+			if (env.getVariable(name) == null
+					|| var.getProperty().getPropertyPath().size() == 1) {
+				env.addVariable(name, varValue);
+			}
+			
+			// add with long name if applicable
+			if (var.getProperty().getPropertyPath().size() > 1) {
+				List<String> names = new ArrayList<String>();
+				for (ChildContext context : var.getProperty().getPropertyPath()) {
+					names.add(context.getChild().getName().getLocalPart());
+				}
+				String longName = Joiner.on('.').join(names);
+				env.addVariable(longName, varValue);
+			}
+		}
+
+		try {
+			Expression ex = new Expression(expression, env);
+			return ex.evaluate();
+		} catch (XExpression e) {
+			throw new TransformationException("Error evaluating the cell expression", e);
+		}
 	}
 
 }
