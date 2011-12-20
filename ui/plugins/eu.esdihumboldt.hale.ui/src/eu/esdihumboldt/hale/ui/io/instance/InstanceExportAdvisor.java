947437631969962ca10ee884b04d728c3d1276d7
Simon Templer <simon.templer@igd.fraunhofer.de>
2011-12-20 15:55:07 +0100

    added action, advisor and action-ui for writing transformed instances
    The XML/GML export currently is offered, but is not working.
    
    refs #301
    
    Change-Id: Ie924aebaf88580d801f7069e6b224a6feb877459

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/instance/InstanceExportAdvisor.java
@@ -0,0 +1,46 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.io.instance;
+
+import org.eclipse.ui.PlatformUI;
+
+import eu.esdihumboldt.hale.common.core.io.IOAdvisor;
+import eu.esdihumboldt.hale.common.core.io.IOProvider;
+import eu.esdihumboldt.hale.common.core.io.impl.AbstractIOAdvisor;
+import eu.esdihumboldt.hale.common.instance.io.InstanceWriter;
+import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
+import eu.esdihumboldt.hale.ui.service.instance.DataSet;
+import eu.esdihumboldt.hale.ui.service.instance.InstanceService;
+import eu.esdihumboldt.hale.ui.service.schema.SchemaService;
+
+/**
+ * Advisor for exporting source instances.
+ * @author Simon Templer
+ */
+public class InstanceExportAdvisor extends AbstractIOAdvisor<InstanceWriter> {
+
+	/**
+	 * @see IOAdvisor#prepareProvider(IOProvider)
+	 */
+	@Override
+	public void prepareProvider(InstanceWriter provider) {
+		// set target schema
+		SchemaService ss = (SchemaService) PlatformUI.getWorkbench().getService(SchemaService.class);
+		provider.setTargetSchema(ss.getSchemas(SchemaSpaceID.TARGET));
+		
+		// set instances to export
+		InstanceService is = (InstanceService) PlatformUI.getWorkbench().getService(InstanceService.class);
+		provider.setInstances(is.getInstances(DataSet.TRANSFORMED));
+	}
+
+}
