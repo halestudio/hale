5b29258a59043d70957840252f963d1a94099422
Andreas Burchert <andreas.burchert@igd.fraunhofer.de>
2011-12-21 13:23:40 +0100

    Added ExtensionPoint for Message and Report Factory, refs #310
    Change-Id: Icb89dc1c3464c451c330b55685bd790ab16ecf10
    Signed-off-by: Andreas Burchert <andreas.burchert@igd.fraunhofer.de>

--- common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/report/MessageFactory.java
+++ common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/report/MessageFactory.java
@@ -15,7 +15,13 @@ package eu.esdihumboldt.hale.common.core.report;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.fhg.igd.osgi.util.OsgiUtils;
+import org.eclipse.core.runtime.IConfigurationElement;
+
+import de.cs3d.util.eclipse.extension.AbstractConfigurationFactory;
+import de.cs3d.util.eclipse.extension.AbstractExtension;
+import de.cs3d.util.eclipse.extension.ExtensionObjectFactory;
+import de.cs3d.util.logging.ALogger;
+import de.cs3d.util.logging.ALoggerFactory;
 import eu.esdihumboldt.util.definition.AbstractObjectFactory;
 
 /**
@@ -28,14 +34,115 @@ import eu.esdihumboldt.util.definition.AbstractObjectFactory;
 public class MessageFactory extends AbstractObjectFactory<Message, MessageDefinition<?>> {
 
 	/**
+	 * Logger
+	 */
+	private static final ALogger _log = ALoggerFactory.getLogger(MessageFactory.class);
+	
+	private final MessageDefinitions definitionExtension = new MessageDefinitions();
+	
+	/**
+	 * Contains all {@link MessageDefinition} for {@link MessageFactory#getDefinitions()}.
+	 */
+	private ArrayList<MessageDefinition<?>> messageDefinitions = new ArrayList<MessageDefinition<?>>();
+	
+	/**
+	 * Instance
+	 */
+	private static MessageFactory _instance;
+	
+	/**
+	 * Constructor
+	 */
+	private MessageFactory() {
+		/* nothing */
+	}
+	
+	/**
+	 * Get the instance of this factory.
+	 * 
+	 * @return the instance
+	 */
+	public static MessageFactory getInstance() {
+		if (_instance == null) {
+			_instance = new MessageFactory();
+		}
+		
+		return _instance;
+	}
+	
+	/**
+	 * 
+	 * @author Andreas Burchert
+	 * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+	 */
+	public static class MessageDefinitions extends AbstractExtension<MessageDefinition<?>, ExtensionObjectFactory<MessageDefinition<?>>> {
+
+		/**
+		 * Default constructor
+		 */
+		public MessageDefinitions() {
+			super(ReportFactory.EXTENSION_ID);
+		}
+
+		/**
+		 * @see AbstractExtension#createFactory(IConfigurationElement)
+		 */
+		@Override
+		protected ExtensionObjectFactory<MessageDefinition<?>> createFactory(
+				IConfigurationElement conf) throws Exception {
+			if (conf.getName().equals("messageDefinition")) {
+				return new AbstractConfigurationFactory<MessageDefinition<?>>(conf, "class") {
+
+					@Override
+					public void dispose(MessageDefinition<?> md) {
+						// do nothing
+					}
+
+					@Override
+					public String getDisplayName() {
+						return getIdentifier();
+					}
+
+					@Override
+					public String getIdentifier() {
+						return conf.getAttribute("id");
+					}
+					
+				};
+			}
+			
+			return null;
+		}
+
+	}
+	
+	/**
 	 * @see AbstractObjectFactory#getDefinitions()
 	 */
 	@Override
 	protected Iterable<MessageDefinition<?>> getDefinitions() {
-		List<MessageDefinition<?>> result = new ArrayList<MessageDefinition<?>>();
-		for (MessageDefinition<?> def : OsgiUtils.getServices(MessageDefinition.class)) {
-			result.add(def);
+		// check if definitions are available
+		if (this.messageDefinitions.size() > 0) {
+			return this.messageDefinitions;
 		}
+		
+		// get all factories
+		List<ExtensionObjectFactory<MessageDefinition<?>>> factories = definitionExtension.getFactories();
+		
+		// create arraylist
+		List<MessageDefinition<?>> result = new ArrayList<MessageDefinition<?>>();
+		
+		// iterate through factories and create MessageDefinition
+		for (ExtensionObjectFactory<MessageDefinition<?>> m : factories) {
+				try {
+					MessageDefinition<?> md = m.createExtensionObject();
+					result.add(md);
+				} catch (Exception e) {
+					_log.error("Error during object creation", e);
+				}
+		}
+		
+		this.messageDefinitions.addAll(result);
 		return result;
 	}
 
--- common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/report/ReportFactory.java
+++ common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/report/ReportFactory.java
@@ -15,7 +15,13 @@ package eu.esdihumboldt.hale.common.core.report;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.fhg.igd.osgi.util.OsgiUtils;
+import org.eclipse.core.runtime.IConfigurationElement;
+
+import de.cs3d.util.eclipse.extension.AbstractConfigurationFactory;
+import de.cs3d.util.eclipse.extension.AbstractExtension;
+import de.cs3d.util.eclipse.extension.ExtensionObjectFactory;
+import de.cs3d.util.logging.ALogger;
+import de.cs3d.util.logging.ALoggerFactory;
 import eu.esdihumboldt.util.definition.AbstractObjectFactory;
 
 /**
@@ -23,18 +29,123 @@ import eu.esdihumboldt.util.definition.AbstractObjectFactory;
  * @author Andreas Burchert
  * @partner 01 / Fraunhofer Institute for Computer Graphics Research
  */
-public class ReportFactory extends AbstractObjectFactory<Report<?>, ReportDefinition<?>>{
+public class ReportFactory extends AbstractObjectFactory<Report<?>, ReportDefinition<?>> {
+	
+	/**
+	 * Extension point ID
+	 */
+	public static final String EXTENSION_ID = "eu.esdihumboldt.hale.report";
+	
+	/**
+	 * Logger
+	 */
+	private static final ALogger _log = ALoggerFactory.getLogger(ReportFactory.class);
+	
+	private final ReportDefintions reportExtension = new ReportDefintions();
+	
+	/**
+	 * Contains all {@link ReportDefinition} for {@link MessageFactory#getDefinitions()}.
+	 */
+	private ArrayList<ReportDefinition<?>> reportDefinitions = new ArrayList<ReportDefinition<?>>();
+	
+	/**
+	 * Instance
+	 */
+	private static ReportFactory _instance;
+	
+	/**
+	 * Constructor
+	 */
+	private ReportFactory() {
+		/* nothing */
+	}
+	
+	/**
+	 * Get the instance of this factory.
+	 * 
+	 * @return the instance
+	 */
+	public static ReportFactory getInstance() {
+		if (_instance == null) {
+			_instance = new ReportFactory();
+		}
+		
+		return _instance;
+	}
+	
+	/**
+	 * 
+	 * @author Andreas Burchert
+	 * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+	 */
+	public static class ReportDefintions extends AbstractExtension<ReportDefinition<?>, ExtensionObjectFactory<ReportDefinition<?>>> {
+		
+		/**
+		 * Default constructor
+		 */
+		public ReportDefintions() {
+			super(ReportFactory.EXTENSION_ID);
+		}
+
+		/**
+		 * @see de.cs3d.util.eclipse.extension.AbstractExtension#createFactory(org.eclipse.core.runtime.IConfigurationElement)
+		 */
+		@Override
+		protected ExtensionObjectFactory<ReportDefinition<?>> createFactory(
+				IConfigurationElement conf) throws Exception {
+			if (conf.getName().equals("reportDefinition")) {
+				return new AbstractConfigurationFactory<ReportDefinition<?>>(conf, "class") {
+
+					@Override
+					public void dispose(ReportDefinition<?> rd) {
+						// do nothing
+					}
+
+					@Override
+					public String getDisplayName() {
+						return getIdentifier();
+					}
+
+					@Override
+					public String getIdentifier() {
+						return conf.getAttribute("id");
+					}
+					
+				};
+			}
+			
+			return null;
+		}
+	}
+
 
 	/**
 	 * @see eu.esdihumboldt.util.definition.AbstractObjectFactory#getDefinitions()
 	 */
 	@Override
-	protected Iterable<ReportDefinition<?>> getDefinitions() {
-		List<ReportDefinition<?>> result = new ArrayList<ReportDefinition<?>>();
-		for (ReportDefinition<?> def : OsgiUtils.getServices(ReportDefinition.class)) {
-			result.add(def);
+	public Iterable<ReportDefinition<?>> getDefinitions() {
+		// check if definitions are available
+		if (this.reportDefinitions.size() > 0) {
+			return this.reportDefinitions;
 		}
+		
+		// get all factories
+		List<ExtensionObjectFactory<ReportDefinition<?>>> factories = reportExtension.getFactories();
+		
+		//create arrayliet
+		List<ReportDefinition<?>> result = new ArrayList<ReportDefinition<?>>();
+		
+		// iterate through factories and create ReportDefinition
+		for (ExtensionObjectFactory<ReportDefinition<?>> r : factories) {
+			try {
+				ReportDefinition<?> rd = r.createExtensionObject();
+				result.add(rd);
+			} catch (Exception e) {
+				_log.error("Error during object creation", e);
+			}
+		}
+		
+		this.reportDefinitions.addAll(result);
 		return result;
 	}
-
 }
