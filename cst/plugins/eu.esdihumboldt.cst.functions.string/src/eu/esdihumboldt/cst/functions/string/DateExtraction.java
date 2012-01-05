2136e9e3c2302f36c5a691045ebdf8fe55dfbd8b
Kai Schwierczek <kai@schwierczek.de>
2012-01-05 15:15:09 +0100

    date extraction function
    refs #358
    
    Change-Id: I1e6e233b586f73ce47847cc619bf001401d4d2d5

---
+++ cst/plugins/eu.esdihumboldt.cst.functions.string/src/eu/esdihumboldt/cst/functions/string/DateExtraction.java
@@ -0,0 +1,65 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.string;
+
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+import com.google.common.collect.ListMultimap;
+
+import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
+import eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine;
+import eu.esdihumboldt.hale.common.align.transformation.function.PropertyValue;
+import eu.esdihumboldt.hale.common.align.transformation.function.TransformationException;
+import eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation;
+import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
+
+/**
+ * Property date extraction function.
+ * 
+ * @author Kai Schwierczek
+ */
+public class DateExtraction extends AbstractSingleTargetPropertyTransformation<TransformationEngine> {
+	/**
+	 * Name of the parameter specifying the date format of the source entity.<br>
+	 * See the function definition on <code>eu.esdihumboldt.hale.common.align</code>.
+	 */
+	public static final String PARAMETER_DATE_FORMAT = "dateFormat";
+	
+	/**
+	 * @see eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation#evaluate(java.lang.String, eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine, com.google.common.collect.ListMultimap, java.lang.String, eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition, java.util.Map, eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog)
+	 */
+	@Override
+	protected Object evaluate(String transformationIdentifier, TransformationEngine engine,
+			ListMultimap<String, PropertyValue> variables, String resultName, PropertyEntityDefinition resultProperty,
+			Map<String, String> executionParameters, TransformationLog log) throws TransformationException {
+		if (getParameters() == null
+				|| getParameters().get(PARAMETER_DATE_FORMAT) == null
+				|| getParameters().get(PARAMETER_DATE_FORMAT).isEmpty()) {
+			throw new TransformationException(MessageFormat.format(
+					"Mandatory parameter {0} not defined", PARAMETER_DATE_FORMAT));
+		}
+		
+		String dateFormat = getParameters().get(PARAMETER_DATE_FORMAT).get(0);
+		String sourceString = variables.values().iterator().next().getValueAs(String.class);
+		SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+		
+		try {
+			return sdf.parse(sourceString);
+		} catch (ParseException pe) {
+			throw new TransformationException("Error parsing the source string", pe);
+		}
+	}
+}
---
+++ cst/plugins/eu.esdihumboldt.cst.functions.string/src/eu/esdihumboldt/cst/functions/string/DateExtractionFormatValidator.java
@@ -0,0 +1,47 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.string;
+
+import java.text.SimpleDateFormat;
+
+import com.google.common.collect.ListMultimap;
+
+import eu.esdihumboldt.hale.common.align.extension.function.Validator;
+
+/**
+ * Validator for date format strings.
+ * 
+ * @author Kai Schwierczek
+ */
+public class DateExtractionFormatValidator implements Validator {
+	/**
+	 * @see eu.esdihumboldt.hale.common.align.extension.function.Validator#validate(java.lang.String)
+	 */
+	@Override
+	public String validate(String value) {
+		try {
+			new SimpleDateFormat(value);
+		} catch (Exception e) {
+			return e.getLocalizedMessage();
+		}
+		return null;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.align.extension.function.Validator#setParameters(com.google.common.collect.ListMultimap)
+	 */
+	@Override
+	public void setParameters(ListMultimap<String, String> parameters) {
+		// no parameters
+	}
+}
