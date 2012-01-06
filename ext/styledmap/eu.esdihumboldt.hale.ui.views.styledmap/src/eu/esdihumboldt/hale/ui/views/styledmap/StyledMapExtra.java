1c8b9771edd22e2bc48457e97a145ca10bbb818b
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-01-06 16:37:23 +0100

    added initial styled map view bundle
    Contains a map perspective including the view.
    Some of the dependencies needed for the map view are not yet included.
    
    refs #362
    
    Change-Id: Ib635abc3b1355e37136c0bb577e18d57f6bb7668
    Reviewed-on: http://gerrit.esdi-community.eu/549
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/StyledMapExtra.java
@@ -0,0 +1,33 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.styledmap;
+
+import de.fhg.igd.mapviewer.view.MapView;
+import de.fhg.igd.mapviewer.view.MapViewExtension;
+
+/**
+ * Map view extension for the styled map.
+ * @author Simon Templer
+ */
+public class StyledMapExtra implements MapViewExtension {
+
+	/**
+	 * @see MapViewExtension#setMapView(MapView)
+	 */
+	@Override
+	public void setMapView(MapView mapView) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/StyledMapPerspective.java
@@ -0,0 +1,54 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.styledmap;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+import de.fhg.igd.mapviewer.view.MapView;
+
+import eu.esdihumboldt.hale.ui.views.data.SourceDataView;
+import eu.esdihumboldt.hale.ui.views.data.TransformedDataView;
+
+/**
+ * Styled map perspective.
+ * @author Simon Templer
+ */
+public class StyledMapPerspective implements IPerspectiveFactory {
+
+	/**
+	 * @see IPerspectiveFactory#createInitialLayout(IPageLayout)
+	 */
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		// top
+		IFolderLayout top = layout.createFolder(
+				"top", IPageLayout.TOP, 0.7f, editorArea);
+		top.addView(MapView.ID);
+
+		// bottom left
+		IFolderLayout bottomLeft = layout.createFolder("bottomLeft",
+				IPageLayout.BOTTOM, 0.7f, editorArea);
+		bottomLeft.addView(SourceDataView.ID);
+
+		// bottom right
+		IFolderLayout bottomRight = layout.createFolder(
+				"bottomRight", IPageLayout.RIGHT, 0.5f, "bottomLeft");
+		bottomRight.addView(TransformedDataView.ID);
+		
+		layout.setEditorAreaVisible(false);
+	}
+
+}
