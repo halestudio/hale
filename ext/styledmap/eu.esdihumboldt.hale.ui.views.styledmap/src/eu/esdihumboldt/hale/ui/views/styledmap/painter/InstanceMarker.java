12361a4a7f317e078a9f7d92de54e55dc9e952fb
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-01-10 13:31:31 +0100

    default line string rendering for instance marker
    Yet untested because of lack of corresponding test data
    as Shapefile.
    
    refs #362
    
    Change-Id: Ia1547f17c0eba8868904878fff26d347c2125e4f

--- ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/painter/InstanceMarker.java
+++ ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/painter/InstanceMarker.java
@@ -16,6 +16,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.jdesktop.swingx.mapviewer.GeoPosition;
@@ -25,12 +26,14 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 
 import de.cs3d.common.metamodel.Point3D;
+import de.cs3d.common.metamodel.shape.Line2D;
+import de.cs3d.common.metamodel.shape.Surface;
 import de.cs3d.util.logging.ALogger;
 import de.cs3d.util.logging.ALoggerFactory;
 import de.fhg.igd.mapviewer.marker.BoundingBoxMarker;
@@ -56,6 +59,8 @@ public class InstanceMarker extends BoundingBoxMarker<InstanceWaypoint> {
 	
 	private static final ALogger log = ALoggerFactory.getLogger(InstanceMarker.class);
 	
+	private final GeometryFactory geometryFactory = new GeometryFactory();
+	
 	private final int defaultPointSize = 7;
 	
 	/**
@@ -80,7 +85,10 @@ public class InstanceMarker extends BoundingBoxMarker<InstanceWaypoint> {
 			}
 		}
 		
-		if (!areas.isEmpty()) {
+		if (areas.size() == 1) {
+			return areas.get(0);
+		}
+		else if (!areas.isEmpty()) {
 			return new MultiArea(areas);
 		}
 		
@@ -102,8 +110,6 @@ public class InstanceMarker extends BoundingBoxMarker<InstanceWaypoint> {
 	protected Area paintGeometry(Graphics2D g, CRSDefinition crsDefinition, 
 			Geometry geometry, InstanceWaypoint context, 
 			PixelConverter converter, int zoom) {
-		//TODO any special cases?
-		
 		if (geometry instanceof GeometryCollection) {
 			// paint each geometry in a geometry collection
 			List<Area> areas = new ArrayList<Area>();
@@ -134,14 +140,13 @@ public class InstanceMarker extends BoundingBoxMarker<InstanceWaypoint> {
 					converter, zoom);
 		}
 		
-		if (geometry instanceof LinearRing) {
-			//TODO
-			//XXX buffer for line as area?
-		}
+//		if (geometry instanceof LinearRing) {
+//			//TODO any special handling needed?
+//		}
 		
 		if (geometry instanceof LineString) {
-			//TODO
-			//XXX buffer for line as area?
+			return paintLine((LineString) geometry, g, crsDefinition, context, 
+					converter, zoom);
 		}
 		
 		return null;
@@ -255,6 +260,107 @@ public class InstanceMarker extends BoundingBoxMarker<InstanceWaypoint> {
 			log.error("Error painting instance polygon geometry", e);
 			return null;
 		}
+	}
+	
+	/**
+	 * Paint a line string geometry.
+	 * @param geometry the line string
+	 * @param g the graphics object to paint on
+	 * @param crsDefinition the CRS definition associated to the geometry
+	 * @param context the context
+	 * @param converter the pixel converter
+	 * @param zoom the zoom level
+	 * @return the polygon area or <code>null</code> if painting failed
+	 */
+	protected Area paintLine(LineString geometry, Graphics2D g,
+			CRSDefinition crsDefinition, InstanceWaypoint context,
+			PixelConverter converter, int zoom) {
+		Coordinate[] coordinates = geometry.getCoordinates();
+		if (coordinates.length <= 0) {
+			return null;
+		}
+		if (coordinates.length == 1) {
+			// fall back to point drawing
+			Point point = geometryFactory.createPoint(coordinates[0]);
+			return paintPoint(point, g, crsDefinition, context, converter, zoom);
+		}
+		
+		try {
+			// map CRS
+			CoordinateReferenceSystem mapCRS = CRSDecode.getCRS(converter.getMapEpsg());
+			
+			// get CRS converter
+			CRSConverter conv = CRSConverter.getConverter(crsDefinition.getCRS(), mapCRS);
+			
+			List<Point2D> mapPoints = new ArrayList<Point2D>(coordinates.length);
+			for (Coordinate coord : coordinates) {
+				// manually convert to map CRS
+				Point3D mapPoint = conv.convert(coord.x, coord.y, 0);
+				
+				GeoPosition pos = new GeoPosition(mapPoint.getX(), 
+						mapPoint.getY(), converter.getMapEpsg());
+				Point2D point = converter.geoToPixel(pos , zoom);
+				
+				mapPoints.add(point);
+			}
+			
+			if (applyStroke(g, context)) {
+				for (int i = 0; i < mapPoints.size() - 1; i++) {
+					// draw each connecting line
+					Point2D p1 = mapPoints.get(i);
+					Point2D p2 = mapPoints.get(i + 1);
+					g.drawLine(
+							(int) p1.getX(), 
+							(int) p1.getY(), 
+							(int) p2.getX(), 
+							(int) p2.getY());
+				}
+			}
+			else {
+				log.warn("Stroke disabled in style, LineString is not rednered");
+			}
+			
+			// use a buffer around the line as area
+			java.awt.Polygon[] buffer = createBufferPolygon(mapPoints, 3); //XXX buffer size is in pixels, which value is ok?
+			if (buffer.length == 0) {
+				return null;
+			}
+			else if (buffer.length == 1) {
+				return new PolygonArea(buffer[0]);
+			}
+			else {
+				Collection<Area> areas = new ArrayList<Area>();
+				for (java.awt.Polygon bufferPoly : buffer) {
+					areas.add(new PolygonArea(bufferPoly));
+				}
+				return new MultiArea(areas);
+			}
+		} catch (Exception e) {
+			log.error("Error painting instance polygon geometry", e);
+			return null;
+		}
+	}
+	
+	/**
+	 * Create a buffered polygon for the given line and a distance.
+	 * @param linePoints the points defining the line 
+	 * @param distance the buffer size
+	 * @return the buffer polygon(s)
+	 */
+	private static java.awt.Polygon[] createBufferPolygon(List<Point2D> linePoints, double distance) {
+		// create metamodel line
+		de.cs3d.common.metamodel.Point2D[] convertedLinePoints = new de.cs3d.common.metamodel.Point2D[linePoints.size()];
+		
+		int index = 0;
+		for (Point2D point : linePoints) {
+			convertedLinePoints[index] = new de.cs3d.common.metamodel.Point2D(point.getX(), point.getY());
+			index++;
+		}
+		
+		Line2D line = new Line2D(convertedLinePoints);
+		
+		Surface buffer = line.computeBuffer(distance);
+		return buffer.toAWTPolygons(1, 1, new de.cs3d.common.metamodel.Point2D(0, 0));
 	}
 
 	/**
