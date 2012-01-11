f4aefb346f2783ee78f3978cb3e66af685457ff6
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-01-11 12:04:42 +0100

    added command to zoom to the instances display in the map
    Added the command to the map view toolbar and a new map menu
    (which is bound to the map perspective and/or view).
    
    refs #362
    
    Change-Id: If2f785ec4d6ed190117992865932b7a279936a1b
    Reviewed-on: http://gerrit.esdi-community.eu/565
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/handler/ZoomToAllHandler.java
@@ -0,0 +1,45 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.styledmap.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import de.fhg.igd.mapviewer.view.MapView;
+import eu.esdihumboldt.hale.ui.views.styledmap.StyledMapUtil;
+
+/**
+ * Zooms to all instances present in the map.
+ * @author Simon Templer
+ */
+public class ZoomToAllHandler extends AbstractHandler {
+
+	/**
+	 * @see IHandler#execute(ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IViewPart viewPart = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().findView(MapView.ID);
+		
+		if (viewPart instanceof MapView) {
+			StyledMapUtil.zoomToAll(((MapView) viewPart).getMapKit());
+		}
+		
+		return null;
+	}
+
+}
