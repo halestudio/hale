545143e71eded6b5822961819f3c03343086c2ec
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-01-13 16:40:42 +0100

    added additional painter layouts
    Diagonal layouts and layouts where only one painter is shown.
    
    refs #362
    
    Change-Id: I02f036dd54a88a2a7bf2855485cf3636e5bd9733

---
+++ ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/clip/layout/impl/DiagonalUpLayout.java
@@ -0,0 +1,61 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.styledmap.clip.layout.impl;
+
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdesktop.swingx.mapviewer.JXMapViewer;
+
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.Clip;
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.impl.BottomRightClip;
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.impl.TopLeftClip;
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.layout.LayoutAugmentation;
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.layout.PainterLayout;
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.layout.extension.PainterProxy;
+
+/**
+ * Diagonal layout that splits the view-port in top-left and bottom-right.
+ * Supports only two painters, for additional painters no clip is provided.
+ * @author Simon Templer
+ */
+public class DiagonalUpLayout implements PainterLayout {
+
+	/**
+	 * @see PainterLayout#createClips(int)
+	 */
+	@Override
+	public List<Clip> createClips(int count) {
+		List<Clip> result = new ArrayList<Clip>();
+		result.add(new TopLeftClip());
+		result.add(new BottomRightClip());
+		return result;
+	}
+
+	/**
+	 * @see PainterLayout#getAugmentation(int)
+	 */
+	@Override
+	public LayoutAugmentation getAugmentation(int count) {
+		return new AbstractDefaultAugmentation() {
+			
+			@Override
+			protected void doPaint(Graphics2D g, JXMapViewer map,
+					List<PainterProxy> painters, int width, int height) {
+				drawSplitLine(g, 0, height, width, 0);
+			}
+		};
+	}
+
+}
---
+++ ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/clip/layout/impl/OnlyFirstLayout.java
@@ -0,0 +1,44 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.styledmap.clip.layout.impl;
+
+import java.util.Collections;
+import java.util.List;
+
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.Clip;
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.layout.LayoutAugmentation;
+import eu.esdihumboldt.hale.ui.views.styledmap.clip.layout.PainterLayout;
+
+/**
+ * Layout that displays only the first painter.
+ * @author Simon Templer
+ */
+public class OnlyFirstLayout implements PainterLayout {
+
+	/**
+	 * @see PainterLayout#createClips(int)
+	 */
+	@Override
+	public List<Clip> createClips(int count) {
+		return Collections.singletonList(null);
+	}
+
+	/**
+	 * @see PainterLayout#getAugmentation(int)
+	 */
+	@Override
+	public LayoutAugmentation getAugmentation(int count) {
+		return null;
+	}
+
+}
