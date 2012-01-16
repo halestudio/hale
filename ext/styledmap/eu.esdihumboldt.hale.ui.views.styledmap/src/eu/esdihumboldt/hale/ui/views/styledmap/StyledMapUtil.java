7e4d7c3fb769fc29d170f06b3f12e79f3fe451a9
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-01-16 11:33:54 +0100

    added command for zooming to instance selection
    refs #362
    
    Change-Id: I23bf1ad17fd9d6f83899dd1468ea1ba93c2b76e2

--- ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/StyledMapUtil.java
+++ ext/styledmap/eu.esdihumboldt.hale.ui.views.styledmap/src/eu/esdihumboldt/hale/ui/views/styledmap/StyledMapUtil.java
@@ -15,12 +15,18 @@ package eu.esdihumboldt.hale.ui.views.styledmap;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.PlatformUI;
 import org.jdesktop.swingx.mapviewer.GeoPosition;
 
 import de.cs3d.common.metamodel.helperGeometry.BoundingBox;
 import de.fhg.igd.mapviewer.BasicMapKit;
 import de.fhg.igd.mapviewer.waypoints.SelectableWaypoint;
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+import eu.esdihumboldt.hale.ui.service.instance.InstanceReference;
+import eu.esdihumboldt.hale.ui.service.instance.InstanceService;
 import eu.esdihumboldt.hale.ui.views.styledmap.painter.AbstractInstancePainter;
+import eu.esdihumboldt.hale.ui.views.styledmap.painter.InstanceWaypoint;
 
 /**
  * Utility methods regarding the map view.
@@ -57,6 +63,58 @@ public abstract class StyledMapUtil {
 					SelectableWaypoint.COMMON_EPSG));
 			mapKit.zoomToPositions(positions);
 		}
+	}
+	
+	/**
+	 * Zoom to the selected instances. Does nothing if the selection is empty
+	 * or contains no {@link Instance}s or {@link InstanceReference}s.
+	 * @param mapKit the map kit
+	 * @param selection the selection
+	 */
+	public static void zoomToSelection(BasicMapKit mapKit, 
+			IStructuredSelection selection) {
+		BoundingBox bb = null;
+		
+		// determine bounding box for each reference and accumulate it
+		for (AbstractInstancePainter painter : mapKit.getTilePainters(AbstractInstancePainter.class)) {
+			for (Object element : selection.toList()) {
+				InstanceReference ref = getReference(element);
+				if (ref != null) {
+					InstanceWaypoint wp = painter.findWaypoint(ref);
+					if (wp != null) {
+						BoundingBox wpBB = wp.getBoundingBox();
+						if (wpBB.checkIntegrity() && !wpBB.isEmpty()) {
+							if (bb == null) {
+								bb = new BoundingBox(wpBB);
+							}
+							else {
+								bb.add(wpBB);
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		if (bb != null) {
+			Set<GeoPosition> positions = new HashSet<GeoPosition>();
+			positions.add(new GeoPosition(bb.getMinX(), bb.getMinY(), 
+					SelectableWaypoint.COMMON_EPSG));
+			positions.add(new GeoPosition(bb.getMaxX(), bb.getMaxY(), 
+					SelectableWaypoint.COMMON_EPSG));
+			mapKit.zoomToPositions(positions);
+		}
+	}
+
+	private static InstanceReference getReference(Object element) {
+		if (element instanceof InstanceReference) {
+			return (InstanceReference) element;
+		}
+		if (element instanceof Instance) {
+			InstanceService is = (InstanceService) PlatformUI.getWorkbench().getService(InstanceService.class);
+			is.getReference((Instance) element);
+		}
+		return null;
 	}
 	
 }
