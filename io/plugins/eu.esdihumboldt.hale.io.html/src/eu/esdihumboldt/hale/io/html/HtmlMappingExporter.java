4ce2aa302c61ac86b7135b26cd983d2b8f32a942
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-01-25 14:26:22 +0100

    updated export function; working at saving images
    refs #370
    
    Change-Id: Ia0c65e56a0653e12c25fc0e6394c7b60a5e97f2d

--- io/plugins/eu.esdihumboldt.hale.io.html/src/eu/esdihumboldt/hale/io/html/HtmlMappingExporter.java
+++ io/plugins/eu.esdihumboldt.hale.io.html/src/eu/esdihumboldt/hale/io/html/HtmlMappingExporter.java
@@ -6,41 +6,48 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
-import org.osgi.framework.Version;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.zest.core.viewers.GraphViewer;
+import org.eclipse.zest.layouts.LayoutAlgorithm;
+import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
 
 import com.google.common.io.Files;
 
-import eu.esdihumboldt.commons.goml.align.Alignment;
 import eu.esdihumboldt.hale.common.align.io.impl.AbstractAlignmentWriter;
+import eu.esdihumboldt.hale.common.align.model.Alignment;
+import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.core.io.IOProviderConfigurationException;
 import eu.esdihumboldt.hale.common.core.io.ProgressIndicator;
 import eu.esdihumboldt.hale.common.core.io.project.ProjectInfo;
 import eu.esdihumboldt.hale.common.core.io.project.ProjectInfoAware;
 import eu.esdihumboldt.hale.common.core.io.report.IOReport;
 import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
-import eu.esdihumboldt.specification.cst.align.ICell;
+import eu.esdihumboldt.hale.ui.common.graph.content.CellGraphContentProvider;
+import eu.esdihumboldt.hale.ui.common.graph.labels.GraphLabelProvider;
+import eu.esdihumboldt.hale.ui.util.DisplayThread;
+import eu.esdihumboldt.hale.ui.util.graph.OffscreenGraph;
 
 /**
  * Export a Mapping to HTML for documentation purposes.
  * 
  * @author Kevin Mais
  */
-@SuppressWarnings("unused")
-// TODO: remove
 public class HtmlMappingExporter extends AbstractAlignmentWriter implements
 		ProjectInfoAware {
 
@@ -50,19 +57,22 @@ public class HtmlMappingExporter extends AbstractAlignmentWriter implements
 	private File file_template;
 	private File tempDir;
 	private Alignment alignment = null;
-	private List<ICell> retypes = new ArrayList<ICell>();
-	private List<ICell> transformations = new ArrayList<ICell>();
-	private List<ICell> augmentations = new ArrayList<ICell>();
 
 	@Override
 	public boolean isCancelable() {
-		// TODO Auto-generated method stub
 		return false;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.project.ProjectInfoAware#setProjectInfo(eu.esdihumboldt.hale.common.core.io.project.ProjectInfo)
+	 */
+	@Override
+	public void setProjectInfo(ProjectInfo projectInfo) {
+		pi = projectInfo;
 	}
 
 	@Override
 	protected String getDefaultTypeName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -70,39 +80,128 @@ public class HtmlMappingExporter extends AbstractAlignmentWriter implements
 	protected IOReport execute(ProgressIndicator progress, IOReporter reporter)
 			throws IOProviderConfigurationException, IOException {
 
+		alignment = getAlignment();
+
 		Template template = null;
-		
-		System.out.println("Path to save the file to: " + getTarget().getLocation().getPath());
+		URL headlinePath = this.getClass().getResource("bg-headline.png"); //$NON-NLS-1$
+		URL cssPath = this.getClass().getResource("style.css"); //$NON-NLS-1$
+		URL linkPath = this.getClass().getResource("int_link.png"); //$NON-NLS-1$
+
+		final String filesSubDir = FilenameUtils.removeExtension(FilenameUtils
+				.getName(getTarget().getLocation().getPath())) + "_files"; //$NON-NLS-1$
+		final File filesDir = new File(FilenameUtils.getFullPath(getTarget()
+				.getLocation().getPath()), filesSubDir); //$NON-NLS-1$
+		filesDir.mkdirs();
+
+		System.out.println("Path to save the file to: "
+				+ getTarget().getLocation().getPath());
 
 		try {
 			init();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+
+		// generates a byteArray out of the style sheet
+		byte[] cssByteArray = null;
+		try {
+			cssByteArray = this.urlToByteArray(cssPath);
+		} catch (UnsupportedEncodingException e2) {
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+
+		// Create CSS export file
+		File cssOutputFile = new File(filesDir, "style.css"); //$NON-NLS-1$
+		try {
+			this.byteArrayToFile(cssOutputFile, cssByteArray);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// generates a byteArray out of the headline picture
+		byte[] headlineByteArray = null;
+		try {
+			headlineByteArray = this.urlToByteArray(headlinePath);
+		} catch (UnsupportedEncodingException e2) {
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+
+		// Create headline picture
+
+		File headlineOutputFile = new File(filesDir, "bg-headline.png"); //$NON-NLS-1$
+		try {
+			byteArrayToFile(headlineOutputFile, headlineByteArray);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// generates a byteArray out of the link picture
+		byte[] linkByteArray = null;
+		try {
+			linkByteArray = this.urlToByteArray(linkPath);
+		} catch (UnsupportedEncodingException e2) {
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+
+		// Create link picture
+
+		File linkOutputFile = new File(filesDir, "int_link.png"); //$NON-NLS-1$
+		try {
+			this.byteArrayToFile(linkOutputFile, linkByteArray);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (alignment != null) {
+			createImages();
 		}
 
 		File htmlExportFile = null;
 		if (pi != null) {
 			Date date = new Date();
 			SimpleDateFormat dfm = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss"); //$NON-NLS-1$
+			if (getTarget().getLocation() == null) {
+				return null;
+			}
 			htmlExportFile = new File(getTarget().getLocation().getPath());
 
-			String projectName = "Project Name: " + pi.getName();
-			String author = "Project Author: " + pi.getAuthor();
-			String haleVers = "Hale Version: " + pi.getHaleVersion().toString();
-			String exportDate = "Export Date: " + dfm.format(date);
-			String description = "Description: " + pi.getDescription();
-			String created = "Created Date: " + dfm.format(pi.getCreated());
+			String projectName = "Project Name : " + pi.getName();
+			String author = "Project Author : " + pi.getAuthor();
+			String haleVers = "Hale Version : "
+					+ pi.getHaleVersion().toString();
+			String exportDate = "Export Date : " + dfm.format(date);
+			String description = "Description : " + pi.getDescription();
+			String created = "Created Date : " + dfm.format(pi.getCreated());
 
 			context = new VelocityContext();
-			
+
 			// associate variables with information datas
 			context.put("author", author);
 			context.put("project", projectName);
 			context.put("haleVers", haleVers);
 			context.put("exportDate", exportDate);
 			context.put("createdDate", created);
+			context.put("pi", pi);
 			context.put("description", description);
+			context.put("filesDir", filesSubDir);
 		} else {
 			// do nothing
 		}
@@ -110,21 +209,21 @@ public class HtmlMappingExporter extends AbstractAlignmentWriter implements
 		try {
 			template = ve.getTemplate(file_template.getName(), "UTF-8");
 		} catch (ResourceNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ParseErrorException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		if (template != null && htmlExportFile != null) {
 			FileWriter fw = new FileWriter(htmlExportFile);
 			template.merge(context, fw);
 			fw.close();
 		}
+
+		// delete tempDir for cleanup
+		tempDir.deleteOnExit();
 
 		return null;
 	}
@@ -161,39 +260,40 @@ public class HtmlMappingExporter extends AbstractAlignmentWriter implements
 		}
 	}
 
-	private void sortAlignment() {
-		for (Iterator<ICell> iterator = this.alignment.getMap().iterator(); iterator
-				.hasNext();) {
-			ICell cell = iterator.next();
-
-			// Retype
-			String cellName;
-			if (cell.getEntity1().getTransformation() == null) {
-				cellName = cell.getEntity2().getTransformation().getService()
-						.getLocation();
-			} else {
-				cellName = cell.getEntity1().getTransformation().getService()
-						.getLocation();
-			}
-			String[] tempSplit = cellName.split("\\."); //$NON-NLS-1$
-			String graphConnectionNodeName = tempSplit[tempSplit.length - 1];
-			if (graphConnectionNodeName.equals("RenameFeatureFunction")) { //$NON-NLS-1$
-				this.retypes.add(cell);
-			}
-
-			// Augmentation
-			if (cell.getEntity1().getTransformation() == null
-					|| cell.getEntity1().getAbout().getAbout()
-							.equals("entity/null")) { //$NON-NLS-1$
-				this.augmentations.add(cell);
-			}
-
-			// Transformation
-			if (cell.getEntity1().getTransformation() != null) {
-				this.transformations.add(cell);
-			}
-		}
-	}
+	// private void sortAlignment() {
+	// for (Iterator<? extends Cell> iterator =
+	// alignment.getTypeCells().iterator(); iterator
+	// .hasNext();) {
+	// Cell cell = iterator.next();
+	//
+	// // Retype
+	// String cellName;
+	// if (cell.getEntity1().getTransformation() == null) {
+	// cellName = cell.getEntity2().getTransformation().getService()
+	// .getLocation();
+	// } else {
+	// cellName = cell.getEntity1().getTransformation().getService()
+	// .getLocation();
+	// }
+	//			String[] tempSplit = cellName.split("\\."); //$NON-NLS-1$
+	// String graphConnectionNodeName = tempSplit[tempSplit.length - 1];
+	//			if (graphConnectionNodeName.equals("RenameFeatureFunction")) { //$NON-NLS-1$
+	// this.retypes.add(cell);
+	// }
+	//
+	// // Augmentation
+	// if (cell.getEntity1().getTransformation() == null
+	// || cell.getEntity1().getAbout().getAbout()
+	//							.equals("entity/null")) { //$NON-NLS-1$
+	// this.augmentations.add(cell);
+	// }
+	//
+	// // Transformation
+	// if (cell.getEntity1().getTransformation() != null) {
+	// this.transformations.add(cell);
+	// }
+	// }
+	// }
 
 	private void byteArrayToFile(File file, byte[] byteArray)
 			throws FileNotFoundException, IOException {
@@ -204,21 +304,75 @@ public class HtmlMappingExporter extends AbstractAlignmentWriter implements
 		}
 	}
 
-	private void stringWriterToFile(File file, StringWriter writer)
-			throws FileNotFoundException, IOException {
-		if (writer != null && file != null) {
-			FileOutputStream fileOutputStream = new FileOutputStream(file);
-			fileOutputStream.write(writer.toString().getBytes());
-			fileOutputStream.close();
+	private byte[] urlToByteArray(URL url) throws Exception, IOException,
+			UnsupportedEncodingException {
+		URLConnection connection = url.openConnection();
+		int contentLength = connection.getContentLength();
+		InputStream inputStream = url.openStream();
+		byte[] data = new byte[contentLength];
+		inputStream.read(data);
+		inputStream.close();
+		return data;
+	}
+
+	private void createImages() {
+
+		Display display;
+		if (Display.getCurrent() != null) {
+			// use the current display if available
+			display = Display.getCurrent();
+		} else {
+			try {
+				// use workbench display if available
+				display = PlatformUI.getWorkbench().getDisplay();
+			} catch (Throwable e) {
+				// use a dedicated display thread if no workbench is
+				// available
+				display = DisplayThread.getInstance().getDisplay();
+			}
+		}
+
+		final String filesSubDir = FilenameUtils.removeExtension(FilenameUtils
+				.getName(getTarget().getLocation().getPath())) + "_files"; //$NON-NLS-1$
+		final File filesDir = new File(FilenameUtils.getFullPath(getTarget()
+				.getLocation().getPath()), filesSubDir);
+
+		Collection<? extends Cell> cells = alignment.getTypeCells();
+		Iterator<? extends Cell> it = cells.iterator();
+		for (int i = 0; i < cells.size(); i++) {
+			final Cell cell = it.next();
+			display.syncExec(new Runnable() {
+				
+				@Override
+				public void run() {
+					OffscreenGraph off_graph = new OffscreenGraph(300, 100) {
+
+						@Override
+						protected void configureViewer(GraphViewer viewer) {
+							LayoutAlgorithm algo = new TreeLayoutAlgorithm(
+									TreeLayoutAlgorithm.LEFT_RIGHT);
+
+							CellGraphContentProvider cgcp = new CellGraphContentProvider();
+							GraphLabelProvider glp = new GraphLabelProvider();
+							viewer.setContentProvider(cgcp);
+							viewer.setLabelProvider(glp);
+							viewer.setInput(cell);
+							viewer.setLayoutAlgorithm(algo);
+						}
+					};
+
+					try {// TODO: 
+						off_graph.saveImage(new FileOutputStream(new File(
+								filesDir, "img_" + "IDENTIFIER")), null);
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+			});
+			i++;
 		}
 	}
-
-	/**
-	 * @see eu.esdihumboldt.hale.common.core.io.project.ProjectInfoAware#setProjectInfo(eu.esdihumboldt.hale.common.core.io.project.ProjectInfo)
-	 */
-	@Override
-	public void setProjectInfo(ProjectInfo projectInfo) {
-		pi = projectInfo;
-	}
-
 }
