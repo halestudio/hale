fc1554b58042d1c3da572b46a7f301e6674a1fcf
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-01-31 17:11:01 +0100

    added first version of MergeHandler interface
    refs #321
    refs #371
    
    Change-Id: Ifb97f80e4d0fd445a4d2094859f3593b5234f93d

---
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/transformation/function/MergeHandler.java
@@ -0,0 +1,51 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.align.transformation.function;
+
+import java.util.Map;
+
+import com.google.common.collect.ListMultimap;
+
+import eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine;
+import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
+import eu.esdihumboldt.hale.common.instance.model.InstanceCollection;
+
+/**
+ * Merges multiple instances of the same type.
+ * @param <E> the transformation engine type
+ * @author Simon Templer
+ */
+public interface MergeHandler<E extends TransformationEngine> {
+
+	/**
+	 * Merge the given instances.
+	 * @param instances the instances
+	 * @param transformationIdentifier the transformation function identifier
+	 * @param engine the transformation engine that may be used for the
+	 *   function execution
+	 * @param transformationParameters the transformation parameters,
+	 *   may be <code>null</code> 
+	 * @param executionParameters additional parameters for the execution, 
+	 *   may be <code>null</code>
+	 * @param log the transformation log to report any information about the
+	 *   execution of the transformation to
+	 * @return the collection of merged instances
+	 * @throws TransformationException if an unrecoverable error occurs during
+	 *   transformation
+	 */
+	public InstanceCollection mergeInstances(InstanceCollection instances,
+			String transformationIdentifier, E engine,
+			ListMultimap<String, String> transformationParameters, 
+			Map<String, String> executionParameters, TransformationLog log) throws TransformationException;
+	
+}
