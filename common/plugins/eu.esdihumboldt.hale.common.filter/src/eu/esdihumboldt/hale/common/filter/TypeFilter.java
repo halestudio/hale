2fa9f95d4ae1bcb7ca21a8bfa7e474452ba787f4
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-01-31 11:46:53 +0100

    added TypeFilter
    refs #312
    
    Change-Id: I24359aea759aeec618a7e08e125d7cc30336ee6e
    Reviewed-on: http://gerrit.esdi-community.eu/670
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ common/plugins/eu.esdihumboldt.hale.common.filter/src/eu/esdihumboldt/hale/common/filter/TypeFilter.java
@@ -0,0 +1,49 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.filter;
+
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+
+/**
+ * Filter that matches instances with a certain associated type.
+ * @author Simon Templer
+ */
+public class TypeFilter implements Filter {
+	
+	private final TypeDefinition type;
+
+	/**
+	 * Create a filter matching instances associated with the given type.
+	 * @param type the type definition to match, if <code>null</code> any type
+	 *   associated with an instance will be a match
+	 */
+	public TypeFilter(TypeDefinition type) {
+		super();
+		this.type = type;
+	}
+
+	/**
+	 * @see Filter#match(Instance)
+	 */
+	@Override
+	public boolean match(Instance instance) {
+		if (type == null) {
+			return true;
+		}
+		else {
+			return type.equals(instance.getDefinition());
+		}
+	}
+
+}
