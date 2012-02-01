a09ea322298a3c1020bb7471b4657768810d22f1
Kai Schwierczek <kai@schwierczek.de>
2012-02-01 18:01:25 +0100

    implemented validation constraint
    Type restrictions find their way into the ValidationRestriction.
    min/max Inclusive/Exclusive facets for date/time types and whiteSpace facet
    aren't yet supported.
    For checking base type restrictions the XML Schema schema gets loaded, too.
    
    refs #340
    
    Change-Id: I1927a1c568fba37cc05793c43bc361c2d16b1b6b

---
+++ io/plugins/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/reader/internal/constraint/UnionValidationConstraint.java
@@ -0,0 +1,56 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.xsd.reader.internal.constraint;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.common.schema.model.constraint.type.ValidationConstraint;
+import eu.esdihumboldt.util.validator.OrValidator;
+import eu.esdihumboldt.util.validator.Validator;
+
+/**
+ * Validation constraint for type unions.
+ * 
+ * @author Kai Schwierczek
+ */
+public class UnionValidationConstraint extends ValidationConstraint {
+	private Collection<? extends TypeDefinition> unionTypes;
+	private boolean initialized = false;
+	private Validator validator;
+
+	/**
+	 * Create a type union validation constraint.
+	 * 
+	 * @param unionTypes the definitions of the types contained in the union
+	 */
+	public UnionValidationConstraint(Collection<? extends TypeDefinition> unionTypes) {
+		this.unionTypes = unionTypes;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.schema.model.constraint.type.ValidationConstraint#getValidator()
+	 */
+	@Override
+	public Validator getValidator() {
+		if (!initialized) {
+			ArrayList<Validator> validators = new ArrayList<Validator>(unionTypes.size());
+			for (TypeDefinition type : unionTypes)
+				validators.add(type.getConstraint(ValidationConstraint.class).getValidator());
+			validator = new OrValidator(validators);
+		}
+
+		return validator;
+	}
+}
---
+++ util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/validator/EnumerationValidator.java
@@ -0,0 +1,70 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.util.validator;
+
+import java.util.Collection;
+
+/**
+ * Validator for a specific set of allowed inputs.
+ * 
+ * @author Kai Schwierczek
+ */
+public class EnumerationValidator extends AbstractValidator {
+	//XXX rewrite to parameterized version?
+
+	private final Collection<String> values;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param values the allowed values
+	 */
+	public EnumerationValidator(Collection<String> values) {
+		this.values = values;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.util.validator.Validator#validate(java.lang.Object)
+	 */
+	@Override
+	public String validate(Object value) {
+		String stringValue = getObjectAs(value, String.class);
+		if (values.contains(stringValue))
+			return null;
+		else
+			return "Input " + stringValue + " is not one of the allowed values (" + allowedValues() + ").";
+	}
+
+	/**
+	 * Returns a string containing all allowed values.
+	 * 
+	 * @return a string containing all allowed values
+	 */
+	private String allowedValues() {
+		StringBuilder buf = new StringBuilder();
+		for (String s : values) {
+			if (buf.length() > 0)
+				buf.append(", ");
+			buf.append(s);
+		}
+		return buf.toString();
+	}
+
+	/**
+	 * @see eu.esdihumboldt.util.validator.Validator#getDescription()
+	 */
+	@Override
+	public String getDescription() {
+		return "Input must be one of: " + allowedValues();
+	}
+}
