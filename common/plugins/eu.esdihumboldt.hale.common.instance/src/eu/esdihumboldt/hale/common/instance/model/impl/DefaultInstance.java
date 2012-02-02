6b6bce109453d3275d390c994e6ac8da8e0b9a36
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-02-02 15:49:03 +0100

    added Instance/Group implementation backed by a ListMultimap
    refs #303
    
    Change-Id: Idf4c9863dc99828809edb4a9e47bc80101685759
    Reviewed-on: http://gerrit.esdi-community.eu/700
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/model/impl/DefaultInstance.java
@@ -0,0 +1,82 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.instance.model.impl;
+
+import com.google.common.collect.ListMultimap;
+
+import eu.esdihumboldt.hale.common.instance.model.DataSet;
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+import eu.esdihumboldt.hale.common.instance.model.MutableInstance;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+
+/**
+ * Default instance implementation backed by a {@link ListMultimap}.
+ * @author Simon Templer
+ */
+public class DefaultInstance extends DefaultGroup implements MutableInstance {
+
+	private DataSet dataSet;
+	
+	private Object value;
+
+	/**
+	 * Create an empty instance.
+	 * @param definition the associated type definition
+	 * @param dataSet the data set the instance is associated to
+	 */
+	public DefaultInstance(TypeDefinition definition, DataSet dataSet) {
+		super(definition);
+		this.dataSet = dataSet;
+	}
+
+	/**
+	 * @see Instance#getValue()
+	 */
+	@Override
+	public Object getValue() {
+		return value;
+	}
+
+	/**
+	 * @see Instance#getDataSet()
+	 */
+	@Override
+	public DataSet getDataSet() {
+		return dataSet;
+	}
+
+	/**
+	 * @see MutableInstance#setValue(Object)
+	 */
+	@Override
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+	/**
+	 * @see MutableInstance#setDataSet(DataSet)
+	 */
+	@Override
+	public void setDataSet(DataSet dataSet) {
+		this.dataSet = dataSet;
+	}
+
+	/**
+	 * @see DefaultGroup#getDefinition()
+	 */
+	@Override
+	public TypeDefinition getDefinition() {
+		return (TypeDefinition) super.getDefinition();
+	}
+
+}
