6a96ab8b9caeee3e83bb060364de046ef082f827
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-02-02 16:05:14 +0100

    added copy constructor to DefaultGroup/DefaultInstance
    refs #303
    
    Change-Id: I6835621e8c7fc476c0f605a81118a9ebe09b88bb
    Reviewed-on: http://gerrit.esdi-community.eu/701
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/model/impl/DefaultGroup.java
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/model/impl/DefaultGroup.java
@@ -16,10 +16,13 @@ import java.util.Arrays;
 
 import javax.xml.namespace.QName;
 
+import com.google.common.base.Function;
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.ListMultimap;
 
 import eu.esdihumboldt.hale.common.instance.model.Group;
+import eu.esdihumboldt.hale.common.instance.model.Instance;
 import eu.esdihumboldt.hale.common.instance.model.MutableGroup;
 import eu.esdihumboldt.hale.common.schema.model.DefinitionGroup;
 
@@ -42,6 +45,20 @@ public class DefaultGroup implements MutableGroup {
 		this.definition = definition;
 	}
 
+	/**
+	 * Copy constructor.
+	 * Creates a group based on the properties and values of the given 
+	 * group.
+	 * @param org the instance to copy
+	 */
+	public DefaultGroup(Group org) {
+		this(org.getDefinition());
+		
+		for (QName property : org.getPropertyNames()) {
+			setPropertyCopy(property, org.getProperty(property));
+		}
+	}
+	
 	/**
 	 * @see Group#getProperty(QName)
 	 */
@@ -80,6 +97,29 @@ public class DefaultGroup implements MutableGroup {
 	@Override
 	public void setProperty(QName propertyName, Object... values) {
 		properties.replaceValues(propertyName, Arrays.asList(values));
+	}
+		
+	private void setPropertyCopy(QName propertyName, Object... values) {
+		properties.replaceValues(propertyName, 
+				Collections2.transform(Arrays.asList(values), new Function<Object, Object>() {
+
+					@Override
+					public Object apply(Object input) {
+						/*
+						 * If the input is a Group/Instance. This is to prevent
+						 * OInstances/OGroups to live on in a copy.
+						 */
+						if (input instanceof Instance) {
+							return new DefaultInstance((Instance) input);
+						}
+						if (input instanceof Group) {
+							return new DefaultGroup((DefinitionGroup) input);
+						}
+						
+						return input;
+					}
+				})
+		);
 	}
 
 }
