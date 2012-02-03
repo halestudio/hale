ea9178b2c0619611f91af46b8b2033da4b91cfe6
Kai Schwierczek <kai@schwierczek.de>
2012-02-03 15:30:39 +0100

    parameter page for formatted string function
    refs #270
    
    Change-Id: Ifd2239eead21e126985d2f9ab0ce23d997821968

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.core/src/eu/esdihumboldt/hale/ui/functions/core/SourceListParameterPage.java
@@ -0,0 +1,209 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.functions.core;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+
+import eu.esdihumboldt.hale.common.align.extension.function.FunctionParameter;
+import eu.esdihumboldt.hale.common.align.model.Cell;
+import eu.esdihumboldt.hale.common.align.model.Entity;
+import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
+import eu.esdihumboldt.hale.ui.HaleWizardPage;
+import eu.esdihumboldt.hale.ui.common.definition.viewer.DefinitionLabelProvider;
+import eu.esdihumboldt.hale.ui.function.generic.AbstractGenericFunctionWizard;
+import eu.esdihumboldt.hale.ui.function.generic.pages.ParameterPage;
+
+/**
+ * Base parameter page for parameter pages that contain a listing of source
+ * types which can be put together to a target value.
+ * 
+ * @author Kai Schwierczek
+ */
+public abstract class SourceListParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<?, ?>> implements
+		ParameterPage {
+	private String initialValue = "";
+	private Text textField;
+	private TableViewer varTable;
+	private EntityDefinition[] variables = new EntityDefinition[0];
+
+	/**
+	 * @see HaleWizardPage#HaleWizardPage(String, String, ImageDescriptor)
+	 */
+	protected SourceListParameterPage(String pageName, String title, ImageDescriptor titleImage) {
+		super(pageName, title, titleImage);
+	}
+
+	/**
+	 * @see HaleWizardPage#HaleWizardPage(String)
+	 */
+	protected SourceListParameterPage(String pageName) {
+		super(pageName);
+	}
+
+	/**
+	 * Should return the parameter which should be configured using all source properties.
+	 * 
+	 * @return the parameter name
+	 */
+	protected abstract String getParameterName() ;
+
+	/**
+	 * Should return the name of the source property which should be used.
+	 * 
+	 * @return the source property name
+	 */
+	protected abstract String getSourcePropertyName() ;
+
+	/**
+	 * Subclasses can configure the text field to for example add some validation mechanism.
+	 * 
+	 * @param textField the text field to configure
+	 */
+	protected void configure(Text textField) {
+		// default: do nothing
+	}
+
+	/**
+	 * This gets called when the user chose a source property to insert.<br>
+	 * Subclasses can change the inserted string here.
+	 * 
+	 * @param value the value that should get inserted
+	 * @return the modified value which gets inserted
+	 */
+	protected String insert(String value) {
+		// give EntityDefinition instead? More freedom for subclasses...
+		return value;
+	}
+
+	/**
+	 * This gets called, when the user chose other source properties.
+	 * 
+	 * @param variables the new source properties
+	 */
+	protected void sourcePropertiesChanged(EntityDefinition[] variables) {
+		// do nothing by default
+	}
+
+	/**
+	 * @see ParameterPage#setParameter(Set, ListMultimap)
+	 */
+	@Override
+	public void setParameter(Set<FunctionParameter> params, ListMultimap<String, String> initialValues) {
+		if (initialValues != null) {
+			List<String> initialData = initialValues.get(getParameterName());
+			if (initialData.size() > 0)
+				initialValue = initialData.get(0);
+		}
+	}
+
+	/**
+	 * @see ParameterPage#getConfiguration()
+	 */
+	@Override
+	public ListMultimap<String, String> getConfiguration() {
+		ListMultimap<String, String> params = ArrayListMultimap.create();
+		params.put(getParameterName(), textField.getText());
+		return params;
+	}
+
+	/**
+	 * @see HaleWizardPage#onShowPage(boolean)
+	 */
+	@Override
+	protected void onShowPage(boolean firstShow) {
+		Cell cell = getWizard().getUnfinishedCell();
+
+		// update variables as they could have changed
+		List<? extends Entity> sourceEntities = cell.getSource().get(getSourcePropertyName());
+		variables = new EntityDefinition[sourceEntities.size()];
+		Iterator<? extends Entity> iter = sourceEntities.iterator();
+		for (int i = 0; i < variables.length; i++)
+			variables[i] = iter.next().getDefinition();
+
+		// inform subclasses
+		sourcePropertiesChanged(variables);
+
+		varTable.setInput(variables);
+
+		((Composite) getControl()).layout();
+	}
+
+	/**
+	 * @see HaleWizardPage#createContent(Composite)
+	 */
+	@Override
+	protected void createContent(Composite page) {
+		page.setLayout(GridLayoutFactory.swtDefaults().create());
+
+		// input field
+		textField = new Text(page, SWT.SINGLE | SWT.BORDER);
+		textField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+		// let subclasses for example add validation
+		configure(textField);
+
+		textField.setText(initialValue);
+
+		// variables
+		Label label = new Label(page, SWT.NONE);
+		label.setText("Available variables (double click to insert)");
+		label.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+
+		// variables table
+		Composite tableComposite = new Composite(page, SWT.NONE);
+		tableComposite.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+		TableColumnLayout columnLayout = new TableColumnLayout();
+		tableComposite.setLayout(columnLayout);
+		varTable = new TableViewer(tableComposite, SWT.BORDER | SWT.SINGLE | SWT.FULL_SELECTION);
+		TableViewerColumn column = new TableViewerColumn(varTable, SWT.NONE);
+		columnLayout.setColumnData(column.getColumn(), new ColumnWeightData(1, false));
+		varTable.setContentProvider(ArrayContentProvider.getInstance());
+		varTable.setLabelProvider(new DefinitionLabelProvider(true, true));
+		varTable.getTable().addMouseListener(new MouseAdapter() {
+			/**
+			 * @see MouseAdapter#mouseDoubleClick(MouseEvent)
+			 */
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				int index = varTable.getTable().getSelectionIndex();
+				if (index >= 0) {
+					String var = varTable.getTable().getItem(index).getText();
+					// let subclass modify variable
+					textField.insert(insert(var));
+					textField.setFocus();
+				}
+			}
+		});
+	}
+}
--- ui/plugins/eu.esdihumboldt.hale.ui.functions.numeric/src/eu/esdihumboldt/hale/ui/functions/numeric/MathExpressionParameterPage.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.numeric/src/eu/esdihumboldt/hale/ui/functions/numeric/MathExpressionParameterPage.java
@@ -1,52 +1,23 @@
 package eu.esdihumboldt.hale.ui.functions.numeric;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
-import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.layout.TableColumnLayout;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ListMultimap;
 import com.iabcinc.jmep.Environment;
 import com.iabcinc.jmep.Expression;
 import com.iabcinc.jmep.hooks.Constant;
 
-import eu.esdihumboldt.hale.common.align.extension.function.FunctionParameter;
-import eu.esdihumboldt.hale.common.align.model.Cell;
-import eu.esdihumboldt.hale.common.align.model.Entity;
 import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
-import eu.esdihumboldt.hale.ui.HaleWizardPage;
 import eu.esdihumboldt.hale.ui.common.definition.viewer.DefinitionLabelProvider;
-import eu.esdihumboldt.hale.ui.function.generic.AbstractGenericFunctionWizard;
-import eu.esdihumboldt.hale.ui.function.generic.pages.ParameterPage;
+import eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage;
 
 /**
  * Parameter page for mathematical expression function.
  * 
  * @author Kai Schwierczek
  */
-public class MathExpressionParameterPage extends HaleWizardPage<AbstractGenericFunctionWizard<?, ?>> implements
-		ParameterPage {
-	private String initialExpression = "";
-	private Text expressionField;
-	private TableViewer varTable;
-	private EntityDefinition[] variables = new EntityDefinition[0];
+public class MathExpressionParameterPage extends SourceListParameterPage {
 	private Environment environment = new Environment();
 
 	/**
@@ -62,70 +33,32 @@ public class MathExpressionParameterPage extends HaleWizardPage<AbstractGenericF
 	}
 
 	/**
-	 * @see ParameterPage#setParameter(Set, ListMultimap)
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#getParameterName()
 	 */
 	@Override
-	public void setParameter(Set<FunctionParameter> params, ListMultimap<String, String> initialValues) {
-		// this page is only for parameter expression, ignore params
-		// XXX don't ignore params? more generic approach necessary? how?
-		if (initialValues != null) {
-			List<String> initialData = initialValues.get("expression");
-			if (initialData.size() > 0)
-				initialExpression = initialData.get(0);
-		}
+	protected String getParameterName() {
+		return "expression";
 	}
 
 	/**
-	 * @see ParameterPage#getConfiguration()
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#getSourcePropertyName()
 	 */
 	@Override
-	public ListMultimap<String, String> getConfiguration() {
-		ListMultimap<String, String> params = ArrayListMultimap.create();
-		params.put("expression", expressionField.getText());
-		return params;
+	protected String getSourcePropertyName() {
+		return "var";
 	}
 
 	/**
-	 * @see HaleWizardPage#onShowPage(boolean)
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#configure(org.eclipse.swt.widgets.Text)
 	 */
 	@Override
-	protected void onShowPage(boolean firstShow) {
-		Cell cell = getWizard().getUnfinishedCell();
-
-		// update variables as they could have changed
-		List<? extends Entity> sourceEntities = cell.getSource().get("var");
-		variables = new EntityDefinition[sourceEntities.size()];
-		Iterator<? extends Entity> iter = sourceEntities.iterator();
-		DefinitionLabelProvider dlp = new DefinitionLabelProvider(true, true);
-		environment = new Environment(); // update environment, too
-		for (int i = 0; i < variables.length; i++) {
-			variables[i] = iter.next().getDefinition();
-			// add dummy variables to environment
-			environment.addVariable(dlp.getText(variables[i]), new Constant(new Double(1)));
-		}
-		varTable.setInput(variables);
-
-		// update check whether current value is valid
-		expressionField.setText(expressionField.getText());
-
-		((Composite) getControl()).layout();
-	}
-
-	/**
-	 * @see HaleWizardPage#createContent(Composite)
-	 */
-	@Override
-	protected void createContent(Composite page) {
-		page.setLayout(GridLayoutFactory.swtDefaults().create());
-
-		// input field
-		expressionField = new Text(page, SWT.SINGLE | SWT.BORDER);
-		expressionField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		expressionField.addModifyListener(new ModifyListener() {
+	protected void configure(final Text textField) {
+		super.configure(textField);
+		textField.addModifyListener(new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				try {
-					Expression ex = new Expression(expressionField.getText(), environment);
+					Expression ex = new Expression(textField.getText(), environment);
 					ex.evaluate();
 					setErrorMessage(null);
 					setPageComplete(true);
@@ -139,36 +72,19 @@ public class MathExpressionParameterPage extends HaleWizardPage<AbstractGenericF
 				}
 			}
 		});
-		expressionField.setText(initialExpression);
+	}
 
-		// variables
-		Label label = new Label(page, SWT.NONE);
-		label.setText("Available variables (double click to insert)");
-		label.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+	/**
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#sourcePropertiesChanged(eu.esdihumboldt.hale.common.align.model.EntityDefinition[])
+	 */
+	@Override
+	protected void sourcePropertiesChanged(EntityDefinition[] variables) {
+		super.sourcePropertiesChanged(variables);
 
-		// variables table
-		Composite tableComposite = new Composite(page, SWT.NONE);
-		tableComposite.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
-		TableColumnLayout columnLayout = new TableColumnLayout();
-		tableComposite.setLayout(columnLayout);
-		varTable = new TableViewer(tableComposite, SWT.BORDER | SWT.SINGLE | SWT.FULL_SELECTION);
-		TableViewerColumn column = new TableViewerColumn(varTable, SWT.NONE);
-		columnLayout.setColumnData(column.getColumn(), new ColumnWeightData(1, false));
-		varTable.setContentProvider(ArrayContentProvider.getInstance());
-		varTable.setLabelProvider(new DefinitionLabelProvider(true, true));
-		varTable.getTable().addMouseListener(new MouseAdapter() {
-			/**
-			 * @see MouseAdapter#mouseDoubleClick(MouseEvent)
-			 */
-			@Override
-			public void mouseDoubleClick(MouseEvent e) {
-				int index = varTable.getTable().getSelectionIndex();
-				if (index >= 0) {
-					String var = varTable.getTable().getItem(index).getText();
-					expressionField.insert(var);
-					expressionField.setFocus();
-				}
-			}
-		});
+		// update environment
+		environment = new Environment();
+		DefinitionLabelProvider dlp = new DefinitionLabelProvider(true, true);
+		for (EntityDefinition variable : variables)
+			environment.addVariable(dlp.getText(variable), new Constant(new Double(1)));
 	}
 }
