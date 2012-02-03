74362e32d977c670fb803e21283b5e5f0ed27a0e
Kai Schwierczek <kai@schwierczek.de>
2012-02-03 15:30:39 +0100

    parameter page for formatted string function
    refs #270
    
    Change-Id: Ifd2239eead21e126985d2f9ab0ce23d997821968
    Reviewed-on: http://gerrit.esdi-community.eu/714
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.core/src/eu/esdihumboldt/hale/ui/functions/core/FormattedStringParameterPage.java
@@ -0,0 +1,65 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.functions.core;
+
+/**
+ * Parameter page for formatted string function.
+ *
+ * @author Kai Schwierczek
+ */
+public class FormattedStringParameterPage extends SourceListParameterPage {
+	/**
+	 * Default constructor.
+	 */
+	public FormattedStringParameterPage() {
+		super("pattern");
+
+		setTitle("Function parameters");
+		setDescription("Enter a pattern");
+
+		setPageComplete(false);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#onShowPage(boolean)
+	 */
+	@Override
+	protected void onShowPage(boolean firstShow) {
+		super.onShowPage(firstShow);
+		setPageComplete(true);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#getParameterName()
+	 */
+	@Override
+	protected String getParameterName() {
+		return "pattern";
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#getSourcePropertyName()
+	 */
+	@Override
+	protected String getSourcePropertyName() {
+		return "var";
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.functions.core.SourceListParameterPage#insert(java.lang.String)
+	 */
+	@Override
+	protected String insert(String value) {
+		return String.format("{%s}", super.insert(value));
+	}
+}
