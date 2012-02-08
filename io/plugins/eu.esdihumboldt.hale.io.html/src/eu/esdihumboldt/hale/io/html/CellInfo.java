62a69744b9b81ebfba304195e229a7c2f8546d93
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-02-08 16:01:50 +0100

    added new interface, basic cell class and type cell class
    refs #370
    
    Change-Id: Ia1e42dc80b52a79dc2ba04d32288791b5f36e22d

---
+++ io/plugins/eu.esdihumboldt.hale.io.html/src/eu/esdihumboldt/hale/io/html/CellInfo.java
@@ -0,0 +1,81 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.html;
+
+import eu.esdihumboldt.hale.common.align.model.Cell;
+import eu.esdihumboldt.util.Identifiers;
+
+/**
+ * Basic class for all cell infos
+ * 
+ * @author Kevin Mais
+ */
+public class CellInfo implements ICellInfo {
+
+	private final Cell cell;
+	/**
+	 * the sub directory where the files will be created
+	 */
+	protected final String subDir;
+	/**
+	 * the cell identifier
+	 */
+	protected final Identifiers<Cell> cellIds;
+
+	/**
+	 * Constructor for a cell info
+	 * 
+	 * @param cell
+	 *            a cell the created cell info is associated with
+	 * @param cellIds
+	 *            the cell identifier
+	 * @param subDir
+	 *            the sub directory where files will be created
+	 */
+	public CellInfo(Cell cell, Identifiers<Cell> cellIds, String subDir) {
+		this.cell = cell;
+		this.cellIds = cellIds;
+		this.subDir = subDir;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.io.html.ICellInfo#getImageLocation()
+	 */
+	@Override
+	public String getImageLocation() {
+		return subDir + "/" + "img_" + getId() + ".png";
+	}
+
+	/**
+	 * Returns the unique id for a cell
+	 * 
+	 * @return the unique id for the cell
+	 */
+	public String getId() {
+		if (cell != null) {
+			String id = cellIds.getId(cell);
+			return id;
+		}
+		return null;
+	}
+
+	/**
+	 * Getter for the cell
+	 * 
+	 * @return the cell
+	 */
+	public Cell getCell() {
+		return cell;
+	}
+
+}
