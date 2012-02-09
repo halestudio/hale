d1fd1aab8d9929c2ef8ab3b5e8059ab8745a6989
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-02-09 09:07:48 +0100

    added IdentityWrapper utility class
    It wraps a value and implements equals and hashCode
    based on its identity.
    
    Change-Id: Ieca442ac758ddf75d4a4bdca4813af1c0a72874d
    Reviewed-on: http://gerrit.esdi-community.eu/737
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/IdentityWrapper.java
@@ -0,0 +1,58 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.util;
+
+import net.jcip.annotations.Immutable;
+
+/**
+ * Class that defines its {@link #equals(Object)} and {@link #hashCode()}
+ * based on the contained values identity.
+ * @param <T> the value type
+ * @author Simon Templer
+ */
+@Immutable
+public class IdentityWrapper<T> {
+
+	private final T value;
+
+	/**
+	 * Create a wrapper for the given value.
+	 * @param value the value to wrap
+	 */
+	public IdentityWrapper(T value) {
+		super();
+		this.value = value;
+	}
+
+	/**
+	 * Get the contained value.
+	 * @return the value
+	 */
+	public T getValue() {
+		return value;
+	}
+
+	@Override
+	public int hashCode() {
+		return System.identityHashCode(value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof IdentityWrapper<?>) {
+			return value == ((IdentityWrapper<?>) obj).value;
+		}
+		return false;
+	}
+	
+}
