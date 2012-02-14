9f9ecea072da3e4f14023beadaf39e66943852bc
Kai Schwierczek <kai@schwierczek.de>
2012-02-14 12:50:17 +0100

    improved tree viewer of mappable type selection
    - namespaces got a folder icon
    - items get sorted
    - namespace checkboxes represent the actual state of their children and are
      selectable
    
    refs #351
    
    Change-Id: Iec1864724c6ff71db50edcff0f538cd1ff12d640
    Reviewed-on: http://gerrit.esdi-community.eu/772
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/schema/internal/EditMappableTypesPage.java
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/schema/internal/EditMappableTypesPage.java
@@ -24,8 +24,11 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.FilteredTree;
 import org.eclipse.ui.dialogs.PatternFilter;
 
@@ -33,6 +36,7 @@ import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.common.schema.model.TypeIndex;
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.HasValueFlag;
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.MappableFlag;
+import eu.esdihumboldt.hale.ui.common.definition.viewer.DefinitionComparator;
 import eu.esdihumboldt.hale.ui.common.definition.viewer.DefinitionLabelProvider;
 import eu.esdihumboldt.hale.ui.service.schema.util.NSTypeTreeContentProvider;
 
@@ -78,36 +82,84 @@ public class EditMappableTypesPage extends WizardPage {
 
 		// configure viewer
 		final CheckboxTreeViewer viewer = (CheckboxTreeViewer) tree.getViewer();
-		viewer.setContentProvider(new NSTypeTreeContentProvider());
-		viewer.setLabelProvider(new DefinitionLabelProvider());
+		final NSTypeTreeContentProvider contentProvider = new NSTypeTreeContentProvider();
+		viewer.setContentProvider(contentProvider);
+		viewer.setComparator(new DefinitionComparator());
+		viewer.setLabelProvider(new DefinitionLabelProvider() {
+			/**
+			 * @see eu.esdihumboldt.hale.ui.common.definition.viewer.DefinitionLabelProvider#getImage(java.lang.Object)
+			 */
+			@Override
+			public Image getImage(Object element) {
+				if (element instanceof String)
+					return PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_FOLDER);
+				return super.getImage(element);
+			}
+		});
 		// because elements filtered by FilteredTree lose their checked state:
-		viewer.setCheckStateProvider(new ICheckStateProvider() {
+		final ICheckStateProvider checkStateProvider = new ICheckStateProvider() {
 			@Override
 			public boolean isGrayed(Object element) {
-				return element instanceof String;
+				if (element instanceof String) {
+					Object[] children = contentProvider.getChildren(element);
+					boolean containsChecked = false;
+					boolean containsUnchecked = false;
+					for (Object child : children) {
+						if (isChecked(child))
+							containsChecked = true;
+						else
+							containsUnchecked = true;
+						if (containsChecked && containsUnchecked)
+							return true;
+					}
+				}
+				return false;
 			}
 
 			@Override
 			public boolean isChecked(Object element) {
-				return element instanceof String || ((TypeDefinition) element).getConstraint(MappableFlag.class).isEnabled() != changedTypes
+				if (element instanceof String) {
+					for (Object child : contentProvider.getChildren(element))
+						if (isChecked(child))
+							return true;
+					return false;
+				}				
+				return ((TypeDefinition) element).getConstraint(MappableFlag.class).isEnabled() != changedTypes
 								.contains(element);
 			}
-		});
+		};
+		viewer.setCheckStateProvider(checkStateProvider);
 		viewer.addCheckStateListener(new ICheckStateListener() {
 			@Override
 			public void checkStateChanged(CheckStateChangedEvent event) {
 				if (event.getElement() instanceof String) {
-					viewer.setChecked(event.getElement(), true);
-					viewer.setGrayed(event.getElement(), true);
+					// update children
+					viewer.setGrayed(event.getElement(), false);
+					for (Object child : contentProvider.getChildren(event.getElement()))
+						if (checkStateProvider.isChecked(child) != event.getChecked()) {
+							viewer.setChecked(child, event.getChecked());
+							updateType((TypeDefinition) child, event.getChecked());
+						}
+					// only two levels, no need to update any parents or children's children
 				} else {
-					TypeDefinition type = (TypeDefinition) event.getElement();
-					if (event.getChecked() == type.getConstraint(MappableFlag.class).isEnabled())
-						changedTypes.remove(type);
-					else
-						changedTypes.add(type);
+					updateType((TypeDefinition) event.getElement(), event.getChecked());
+					// update parent
+					Object parent = contentProvider.getParent(event.getElement());
+					viewer.setGrayed(parent, checkStateProvider.isGrayed(parent));
+					viewer.setChecked(parent, checkStateProvider.isChecked(parent));
+					// only two levels, no need to update any parent's parents or children
 				}
 			}
+
+			private void updateType(TypeDefinition type, boolean checked) {
+				if (checked == type.getConstraint(MappableFlag.class).isEnabled())
+					changedTypes.remove(type);
+				else
+					changedTypes.add(type);
+			}
 		});
+
+		// set input to all types in the given index
 		viewer.setInput(typeIndex.getTypes());
 
 		// filter to exclude simple types
