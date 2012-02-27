98149b60014b485b11cb438c49511f4b82200591
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-02-27 14:36:14 +0100

    extracted core functionality of SchemaExplorerLabelProvider
    to StyledDefinitionLabelProvider in common ui bundle.
    
    Change-Id: Ib6432bc060153ee52ee4f269a461fd9245cdf95d

--- ui/plugins/eu.esdihumboldt.hale.ui.views.schemas/src/eu/esdihumboldt/hale/ui/views/schemas/explorer/SchemaExplorerLabelProvider.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.schemas/src/eu/esdihumboldt/hale/ui/views/schemas/explorer/SchemaExplorerLabelProvider.java
@@ -12,20 +12,13 @@
 
 package eu.esdihumboldt.hale.ui.views.schemas.explorer;
 
-import java.text.MessageFormat;
 import java.util.Collection;
 
 import org.eclipse.jface.viewers.IColorProvider;
-import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.StyledCellLabelProvider;
-import org.eclipse.jface.viewers.StyledString;
-import org.eclipse.jface.viewers.ViewerCell;
-import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.dialogs.PatternFilter;
 
 import com.google.common.collect.ListMultimap;
 
@@ -34,10 +27,8 @@ import eu.esdihumboldt.hale.common.align.model.AlignmentUtil;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.Entity;
 import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
-import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
 import eu.esdihumboldt.hale.common.schema.model.Definition;
-import eu.esdihumboldt.hale.common.schema.model.constraint.property.Cardinality;
-import eu.esdihumboldt.hale.ui.common.definition.viewer.DefinitionLabelProvider;
+import eu.esdihumboldt.hale.ui.common.definition.viewer.StyledDefinitionLabelProvider;
 import eu.esdihumboldt.hale.ui.service.align.AlignmentService;
 import eu.esdihumboldt.hale.ui.util.viewer.TipProvider;
 
@@ -45,10 +36,8 @@ import eu.esdihumboldt.hale.ui.util.viewer.TipProvider;
  * Extended label provider for definitions
  * @author Simon Templer
  */
-public class SchemaExplorerLabelProvider extends StyledCellLabelProvider
-		implements TipProvider, ILabelProvider, IColorProvider {
-	
-	private final DefinitionLabelProvider defaultLabels = new DefinitionLabelProvider();
+public class SchemaExplorerLabelProvider extends StyledDefinitionLabelProvider
+		implements TipProvider {
 	
 	private final Color typeCellColor;
 	private final Color propertyCellColor;
@@ -67,63 +56,6 @@ public class SchemaExplorerLabelProvider extends StyledCellLabelProvider
 		propertyCellColor = new Color(display, 190, 220, 170);
 		augmentedColor = new Color(display, 184, 181, 220);
 		indirectMappingColor = new Color(display, 245, 245, 145);
-	}
-
-	/**
-	 * @see StyledCellLabelProvider#update(ViewerCell)
-	 */
-	@Override
-	public void update(ViewerCell cell) {
-		Object element = cell.getElement();
-		
-		StyledString text = new StyledString(defaultLabels.getText(element));
-		
-		cell.setImage(defaultLabels.getImage(element));
-		
-//		boolean defContext = true;
-		if (element instanceof EntityDefinition) {
-			EntityDefinition entityDef = (EntityDefinition) element;
-			element = entityDef.getDefinition();
-//			
-//			List<ChildContext> path = entityDef.getPropertyPath();
-//			if (path != null && !path.isEmpty()) {
-//				ChildContext lastContext = path.get(path.size() - 1);
-//				defContext = lastContext.getContextName() == null;
-//			}
-		}
-		//TODO different styling for non-default context?
-		
-		// append cardinality
-		if (element instanceof ChildDefinition<?>) {
-			Cardinality cardinality = null;
-			if (((ChildDefinition<?>) element).asGroup() != null) {
-				cardinality = ((ChildDefinition<?>) element).asGroup().getConstraint(Cardinality.class);
-			}
-			else if (((ChildDefinition<?>) element).asProperty() != null) {
-				cardinality = ((ChildDefinition<?>) element).asProperty().getConstraint(Cardinality.class);
-			}
-			
-			if (cardinality != null) {
-				// only append cardinality if it isn't 1/1
-				if (cardinality.getMinOccurs() != 1 || cardinality.getMaxOccurs() != 1) {
-					String card = " " + MessageFormat.format("({0}..{1})", 
-							new Object[]{Long.valueOf(cardinality.getMinOccurs()), 
-							(cardinality.getMaxOccurs() == Cardinality.UNBOUNDED)?("n"):(Long.valueOf(cardinality.getMaxOccurs()))});
-					text.append(card, StyledString.COUNTER_STYLER);
-				}
-			}
-		}
-		
-		cell.setText(text.toString());
-		cell.setStyleRanges(text.getStyleRanges());
-		
-		Color foreground = getForeground(cell.getElement());
-		cell.setForeground(foreground);
-		
-		Color background = getBackground(cell.getElement());
-		cell.setBackground(background);
-		
-		super.update(cell);
 	}
 
 	/**
@@ -146,42 +78,10 @@ public class SchemaExplorerLabelProvider extends StyledCellLabelProvider
 	}
 
 	/**
-	 * Only implemented because of use with {@link PatternFilter} and
-	 * {@link ViewerComparator}
-	 * 
-	 * @see ILabelProvider#getImage(Object)
-	 */
-	@Override
-	public Image getImage(Object element) {
-		return null;
-	}
-
-	/**
-	 * Only implemented for use with {@link PatternFilter} and 
-	 * {@link ViewerComparator}
-	 * 
-	 * @see ILabelProvider#getText(Object)
-	 */
-	@Override
-	public String getText(Object element) {
-		if (element instanceof EntityDefinition) {
-			element = ((EntityDefinition) element).getDefinition();
-		}
-		
-		if (element instanceof Definition<?>) {
-			return ((Definition<?>) element).getDisplayName();
-		}
-		
-		return null;
-	}
-
-	/**
 	 * @see StyledCellLabelProvider#dispose()
 	 */
 	@Override
 	public void dispose() {
-		defaultLabels.dispose();
-		
 		typeCellColor.dispose();
 		propertyCellColor.dispose();
 		augmentedColor.dispose();
