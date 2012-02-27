c3e6f6f9add5d93a4999c7b7ba141c1dd3c04bec
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-02-27 14:36:14 +0100

    extracted core functionality of SchemaExplorerLabelProvider
    to StyledDefinitionLabelProvider in common ui bundle.
    
    Change-Id: Ib6432bc060153ee52ee4f269a461fd9245cdf95d
    Reviewed-on: http://gerrit.esdi-community.eu/837
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.common/src/eu/esdihumboldt/hale/ui/common/definition/viewer/StyledDefinitionLabelProvider.java
@@ -0,0 +1,175 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.common.definition.viewer;
+
+import java.text.MessageFormat;
+
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.StyledCellLabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.jface.viewers.ViewerComparator;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.dialogs.PatternFilter;
+
+import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
+import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
+import eu.esdihumboldt.hale.common.schema.model.Definition;
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.Cardinality;
+
+/**
+ * Extended label provider for definitions.
+ * @author Simon Templer
+ */
+public class StyledDefinitionLabelProvider extends StyledCellLabelProvider
+		implements ILabelProvider, IColorProvider {
+	
+	private final ILabelProvider defaultLabels;
+	
+	/**
+	 * Default constructor 
+	 */
+	public StyledDefinitionLabelProvider() {
+		this(new DefinitionLabelProvider());
+	}
+
+	/**
+	 * Create a styled label provider based on the given plain label provider
+	 * for definitions.
+	 * @param definitionLabelProvider the definition label provider
+	 */
+	public StyledDefinitionLabelProvider(
+			ILabelProvider definitionLabelProvider) {
+		super();
+		
+		this.defaultLabels = definitionLabelProvider;
+	}
+
+	/**
+	 * @see StyledCellLabelProvider#update(ViewerCell)
+	 */
+	@Override
+	public void update(ViewerCell cell) {
+		Object element = cell.getElement();
+		
+		StyledString text = new StyledString(defaultLabels.getText(element));
+		
+		cell.setImage(defaultLabels.getImage(element));
+		
+//		boolean defContext = true;
+		if (element instanceof EntityDefinition) {
+			EntityDefinition entityDef = (EntityDefinition) element;
+			element = entityDef.getDefinition();
+//			
+//			List<ChildContext> path = entityDef.getPropertyPath();
+//			if (path != null && !path.isEmpty()) {
+//				ChildContext lastContext = path.get(path.size() - 1);
+//				defContext = lastContext.getContextName() == null;
+//			}
+		}
+		//TODO different styling for non-default context?
+		
+		// append cardinality
+		if (element instanceof ChildDefinition<?>) {
+			Cardinality cardinality = null;
+			if (((ChildDefinition<?>) element).asGroup() != null) {
+				cardinality = ((ChildDefinition<?>) element).asGroup().getConstraint(Cardinality.class);
+			}
+			else if (((ChildDefinition<?>) element).asProperty() != null) {
+				cardinality = ((ChildDefinition<?>) element).asProperty().getConstraint(Cardinality.class);
+			}
+			
+			if (cardinality != null) {
+				// only append cardinality if it isn't 1/1
+				if (cardinality.getMinOccurs() != 1 || cardinality.getMaxOccurs() != 1) {
+					String card = " " + MessageFormat.format("({0}..{1})", 
+							new Object[]{Long.valueOf(cardinality.getMinOccurs()), 
+							(cardinality.getMaxOccurs() == Cardinality.UNBOUNDED)?("n"):(Long.valueOf(cardinality.getMaxOccurs()))});
+					text.append(card, StyledString.COUNTER_STYLER);
+				}
+			}
+		}
+		
+		cell.setText(text.toString());
+		cell.setStyleRanges(text.getStyleRanges());
+		
+		Color foreground = getForeground(cell.getElement());
+		cell.setForeground(foreground);
+		
+		Color background = getBackground(cell.getElement());
+		cell.setBackground(background);
+		
+		super.update(cell);
+	}
+
+	/**
+	 * Only implemented because of use with {@link PatternFilter} and
+	 * {@link ViewerComparator}
+	 * 
+	 * @see ILabelProvider#getImage(Object)
+	 */
+	@Override
+	public Image getImage(Object element) {
+		return null;
+	}
+
+	/**
+	 * Only implemented for use with {@link PatternFilter} and 
+	 * {@link ViewerComparator}
+	 * 
+	 * @see ILabelProvider#getText(Object)
+	 */
+	@Override
+	public String getText(Object element) {
+		if (element instanceof EntityDefinition) {
+			element = ((EntityDefinition) element).getDefinition();
+		}
+		
+		if (element instanceof Definition<?>) {
+			return ((Definition<?>) element).getDisplayName();
+		}
+		
+		return null;
+	}
+
+	/**
+	 * @see StyledCellLabelProvider#dispose()
+	 */
+	@Override
+	public void dispose() {
+		defaultLabels.dispose();
+		
+		super.dispose();
+	}
+
+	/**
+	 * @see IColorProvider#getForeground(Object)
+	 */
+	@Override
+	public Color getForeground(Object element) {
+		// default foreground
+		return null;
+	}
+
+	/**
+	 * @see IColorProvider#getBackground(Object)
+	 */
+	@Override
+	public Color getBackground(Object element) {
+		// default background
+		return null;
+	}
+
+}
