5e0a034a06b2f00bd4a9b1a8006fd5e60a355a22
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-02-27 16:06:38 +0100

    added WrappedText figure
    ...which wraps all text exceeding a maximum width.
    
    Change-Id: I5a2d0b6bb90b61f4af89fb3d8785d5249e6872cb
    Reviewed-on: http://gerrit.esdi-community.eu/839
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.util.graph/src/eu/esdihumboldt/hale/ui/util/graph/WrappedText.java
@@ -0,0 +1,62 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.util.graph;
+
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.StackLayout;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.text.FlowPage;
+import org.eclipse.draw2d.text.TextFlow;
+
+/**
+ * Figure that displays text and word-wraps it if the figure would exceed a
+ * maximum width.
+ * @author Simon Templer
+ */
+public class WrappedText extends Figure {
+
+	private final int maxWidth;
+	
+	private final FlowPage page;
+
+	/**
+	 * Create a wrapped text figure. 
+	 * @param text the text to display
+	 * @param maxWidth the maximum width
+	 */
+	public WrappedText(String text, int maxWidth) {
+		super();
+		this.maxWidth = maxWidth;
+		
+		setLayoutManager(new StackLayout());
+		
+		page = new FlowPage();
+		add(page);
+		
+		TextFlow flow = new TextFlow(text);
+		page.add(flow);
+	}
+
+	/**
+	 * @see Figure#getPreferredSize(int, int)
+	 */
+	@Override
+	public Dimension getPreferredSize(int wHint, int hHint) {
+		Dimension size = page.getPreferredSize();
+		if (size.width > maxWidth) {
+			return page.getPreferredSize(maxWidth, -1);
+		}
+		return size;
+	}
+	
+}
