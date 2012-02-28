56a93e8cb13876fe509c1dc2556c80607427ca85
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-02-28 11:26:55 +0100

    added filter definition extension point
    refs #312
    refs #382
    
    Change-Id: If2b3d20defdb03dd0065f789969a0236d3db966f

---
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/extension/FilterDefinition.java
@@ -0,0 +1,26 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.instance.extension;
+
+import eu.esdihumboldt.hale.common.instance.model.Filter;
+import eu.esdihumboldt.util.definition.ObjectDefinition;
+
+/**
+ * Object definition for {@link Filter}s.
+ * @author Simon Templer
+ */
+public interface FilterDefinition extends ObjectDefinition<Filter> {
+
+	// concrete typed interface
+	
+}
---
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/extension/FilterDefinitionFactory.java
@@ -0,0 +1,26 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.instance.extension;
+
+import de.cs3d.util.eclipse.extension.ExtensionObjectFactory;
+
+/**
+ * Factory interface for {@link FilterDefinition}s.
+ * @author Simon Templer
+ */
+public interface FilterDefinitionFactory extends
+		ExtensionObjectFactory<FilterDefinition> {
+
+	// concrete typed interface
+	
+}
