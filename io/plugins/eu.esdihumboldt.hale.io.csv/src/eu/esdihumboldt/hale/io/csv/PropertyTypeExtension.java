65af2030cff373e0218dee729762122c1b2b6042
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-02-29 13:54:22 +0100

    modified complete structure to extend functionality
    refs #308
    
    Change-Id: I43fd9c6497f4ff503112b39ef18a363889c5a4b2

---
+++ io/plugins/eu.esdihumboldt.hale.io.csv/src/eu/esdihumboldt/hale/io/csv/PropertyTypeExtension.java
@@ -0,0 +1,107 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.csv;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+
+import de.cs3d.util.eclipse.extension.AbstractConfigurationFactory;
+import de.cs3d.util.eclipse.extension.AbstractExtension;
+
+/**
+ * Class for the property type extension
+ * 
+ * @author Kevin Mais
+ */
+public class PropertyTypeExtension extends
+		AbstractExtension<PropertyType, PropertyTypeFactory> {
+	
+	/**
+	 * Default implementation class
+	 * @author Kevin Mais
+	 */
+	private static class DefaultFactory extends AbstractConfigurationFactory<PropertyType>
+			implements PropertyTypeFactory {
+
+		/**
+		 * Default constructor
+		 * 
+		 * @param conf the configuration element
+		 */
+		protected DefaultFactory(IConfigurationElement conf) {
+			super(conf, "class");
+		}
+
+		/**
+		 * @see de.cs3d.util.eclipse.extension.ExtensionObjectFactory#dispose(java.lang.Object)
+		 */
+		@Override
+		public void dispose(PropertyType arg0) {
+			// ignore
+		}
+
+		/**
+		 * @see de.cs3d.util.eclipse.extension.ExtensionObjectDefinition#getDisplayName()
+		 */
+		@Override
+		public String getDisplayName() {
+			return conf.getAttribute("name");
+		}
+
+		/**
+		 * @see de.cs3d.util.eclipse.extension.ExtensionObjectDefinition#getIdentifier()
+		 */
+		@Override
+		public String getIdentifier() {
+			return conf.getAttribute("id");
+		}
+
+	}
+	
+	private static volatile PropertyTypeExtension instance;
+	
+	/**
+	 * Getter for the PropertyTypeExtension
+	 * 
+	 * @return an Object of PropertyTypeExtension
+	 */
+	public static PropertyTypeExtension getInstance() {
+		if (instance == null) {
+			instance = new PropertyTypeExtension();
+		}
+		return instance;
+
+	}
+	
+
+	/**
+	 * the property type identifier
+	 */
+	public static final String ID = "eu.esdihumboldt.hale.io.csv.propertyType";
+
+	/**
+	 * Default constructor
+	 */
+	public PropertyTypeExtension() {
+		super(ID);
+	}
+
+	/**
+	 * @see de.cs3d.util.eclipse.extension.AbstractExtension#createFactory(org.eclipse.core.runtime.IConfigurationElement)
+	 */
+	@Override
+	protected PropertyTypeFactory createFactory(IConfigurationElement conf)
+			throws Exception {
+		return new DefaultFactory(conf);
+	}
+
+}
