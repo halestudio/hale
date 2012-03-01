bcaafc16b8aa597215fc0cc459074abba9b4ce27
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-03-01 17:46:47 +0100

    cleaned up filter ui bundle
    Change-Id: I2fe3d2a06f3a40cec9d6d4f4cafe8962389d2161
    Reviewed-on: http://gerrit.esdi-community.eu/857
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.filter/src/eu/esdihumboldt/hale/ui/filter/internal/Messages.java
@@ -0,0 +1,46 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+package eu.esdihumboldt.hale.ui.filter.internal;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Provides means to access externalized strings.
+ */
+@SuppressWarnings("all")
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "eu.esdihumboldt.hale.ui.filter.internal.messages"; //$NON-NLS-1$
+
+	public static String FeatureFilterField_0;
+	public static String FeatureFilterField_3;
+	public static String FeatureFilterField_6;
+	public static String FeatureFilterField_7;
+	public static String FeatureFilterField_8;
+	public static String FeatureFilterField_9;
+
+	public static String FeatureFilterFormDialog_0;
+	public static String FeatureFilterFormDialog_1;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	/**
+	 * Hidden constructor
+	 */
+	private Messages() {
+		// nothing to do here
+	}
+
+}
