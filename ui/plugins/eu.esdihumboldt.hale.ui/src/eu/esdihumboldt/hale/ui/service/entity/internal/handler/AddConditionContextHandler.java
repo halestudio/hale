c3a02b1631b7934e69e6b62e4b297c4c6dae2dc1
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-03-05 14:42:35 +0100

    added support for specifying source property filters
    As of now those conditions are not supported in the
    transformation.
    
    refs #382
    
    Change-Id: I93dbbce6bab513c6991dd94a3d19a6465b2d0400
    Reviewed-on: http://gerrit.esdi-community.eu/876
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/entity/internal/handler/AddConditionContextHandler.java
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/entity/internal/handler/AddConditionContextHandler.java
@@ -12,6 +12,8 @@
 
 package eu.esdihumboldt.hale.ui.service.entity.internal.handler;
 
+import javax.xml.namespace.QName;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -23,6 +25,11 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
 import eu.esdihumboldt.hale.common.instance.model.Filter;
+import eu.esdihumboldt.hale.common.schema.model.Definition;
+import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.common.schema.model.impl.DefaultPropertyDefinition;
+import eu.esdihumboldt.hale.common.schema.model.impl.DefaultTypeDefinition;
 import eu.esdihumboldt.hale.ui.filter.TypeFilterDialog;
 import eu.esdihumboldt.hale.ui.service.entity.EntityDefinitionService;
 
@@ -52,13 +59,37 @@ public class AddConditionContextHandler extends AbstractHandler {
 							HandlerUtil.getActiveShell(event), 
 							entityDef.getType(),
 							"Type condition",
-							"Define the filter for the new context");
+							"Define the condition for the new context");
 					if (tfd.open() == TypeFilterDialog.OK) {
 						filter = tfd.getFilter();
 					}
 				}
 				else {
-					//TODO value filter
+					// value filter
+					Definition<?> def = entityDef.getDefinition();
+					if (def instanceof PropertyDefinition) {
+						TypeDefinition propertyType = ((PropertyDefinition) def).getPropertyType();
+						// create a dummy type for the filter
+						TypeDefinition dummyType = new DefaultTypeDefinition(
+								new QName("ValueFilterDummy"));
+						// with the property type being contained as value property
+						new DefaultPropertyDefinition(new QName("value"),
+								dummyType, propertyType);
+						// and the parent type as parent property
+						new DefaultPropertyDefinition(new QName("parent"),
+								dummyType,
+								((PropertyDefinition) def).getParentType());
+						
+						// open the filter dialog
+						TypeFilterDialog tfd = new TypeFilterDialog(
+								HandlerUtil.getActiveShell(event), 
+								dummyType,
+								"Property condition",
+								"Define the condition for the new context");
+						if (tfd.open() == TypeFilterDialog.OK) {
+							filter = tfd.getFilter();
+						}
+					}
 				}
 				if (filter != null) {
 					eds.addConditionContext((EntityDefinition) element, filter);
