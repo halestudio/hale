6c1285889b47d707505468255f11c184caa91563
Andreas Burchert <andreas.burchert@igd.fraunhofer.de>
2012-03-05 21:09:51 +0100

    Added FilteredTree to detailpage.
    refs #310
    
    Change-Id: I8f61a2dc4da4cbf1ac9ef4fcd71d5febf25a4cc8
    Signed-off-by: Andreas Burchert <andreas.burchert@igd.fraunhofer.de>
    Reviewed-on: http://gerrit.esdi-community.eu/887
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.report/src/eu/esdihumboldt/hale/ui/views/report/properties/tree/ReportGroupInfo.java
@@ -0,0 +1,30 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.report.properties.tree;
+
+/**
+ * @author Andreas Burchert
+ * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+ */
+public class ReportGroupInfo extends AbstractList {
+	
+	/**
+	 * Version
+	 */
+	private static final long serialVersionUID = 6107922259340435511L;
+	
+	@Override
+	public String toString() {
+		return "Info";
+	}
+}
---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.report/src/eu/esdihumboldt/hale/ui/views/report/properties/tree/ReportGroupWarning.java
@@ -0,0 +1,30 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.report.properties.tree;
+
+/**
+ * @author Andreas Burchert
+ * @partner 01 / Fraunhofer Institute for Computer Graphics Research
+ */
+public class ReportGroupWarning extends AbstractList {
+
+	/**
+	 * Version
+	 */
+	private static final long serialVersionUID = -195634992466475534L;
+	
+	@Override
+	public String toString() {
+		return "Warning";
+	}
+}
