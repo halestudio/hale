e2a5ced04b4d52be9948bde58023a7c2d88aefa6
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-03-06 09:45:08 +0100

    made last query definition index thread local
    Also introduced common base class for geotools based filters.
    
    refs #312
    
    Change-Id: I9073fff520f3a434c44d57a63bcba7da17853771

--- common/plugins/eu.esdihumboldt.hale.common.filter/src/eu/esdihumboldt/hale/common/filter/FilterGeoCqlImpl.java
+++ common/plugins/eu.esdihumboldt.hale.common.filter/src/eu/esdihumboldt/hale/common/filter/FilterGeoCqlImpl.java
@@ -16,10 +16,7 @@ import org.geotools.filter.text.cql2.CQL;
 import org.geotools.filter.text.cql2.CQLException;
 import org.opengis.filter.Filter;
 
-import de.cs3d.util.logging.ALogger;
-import de.cs3d.util.logging.ALoggerFactory;
-
-import eu.esdihumboldt.hale.common.instance.model.Instance;
+import eu.esdihumboldt.hale.common.filter.internal.AbstractGeotoolsFilter;
 
 /**
  * CQL Filter. Two CQL filters are seen as equal if they are based on the same
@@ -27,71 +24,20 @@ import eu.esdihumboldt.hale.common.instance.model.Instance;
  * @author Sebastian Reinhardt
  * @author Simon Templer
  */
-public class FilterGeoCqlImpl implements
-		eu.esdihumboldt.hale.common.instance.model.Filter {
+public class FilterGeoCqlImpl extends AbstractGeotoolsFilter {
 	
-	private static final ALogger log = ALoggerFactory.getLogger(FilterGeoCqlImpl.class);
-
-	private final String filterTerm;
-	private final Filter internFilter;
-
 	/**
 	 * Create a CQL filter.
 	 * @param filterTerm the CQL expression
 	 * @throws CQLException if parsing the CQL fails
 	 */
 	public FilterGeoCqlImpl(String filterTerm) throws CQLException {
-		this.filterTerm = filterTerm;
-
-		internFilter = CQL.toFilter(this.filterTerm);
-		if (internFilter == Filter.EXCLUDE) {
-			log.warn("Parsed filter will not match any instance");
-		}
+		super(filterTerm);
 	}
 
 	@Override
-	public boolean match(Instance instance) {
-		return internFilter.evaluate(instance);
-	}
-
-	/**
-	 * Get the CQL expression the filter is based on.
-	 * @return the CQL expression
-	 */
-	public String getFilterTerm() {
-		return filterTerm;
-	}
-
-	/**
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((filterTerm == null) ? 0 : filterTerm.hashCode());
-		return result;
-	}
-
-	/**
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		FilterGeoCqlImpl other = (FilterGeoCqlImpl) obj;
-		if (filterTerm == null) {
-			if (other.filterTerm != null)
-				return false;
-		} else if (!filterTerm.equals(other.filterTerm))
-			return false;
-		return true;
+	protected Filter createFilter(String filterTerm) throws CQLException {
+		return CQL.toFilter(filterTerm);
 	}
 
 }
