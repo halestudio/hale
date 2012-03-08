67820033b8b21871d0ce5d618a0793135faf8790
Kai Schwierczek <kai@schwierczek.de>
2012-03-08 14:55:13 +0100

    "used" types can no longer be marked as unmappable
    refs #351
    
    Change-Id: I461d5f73585b0acb37b3eba634de19a901997200
    Reviewed-on: http://gerrit.esdi-community.eu/908
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/schema/tester/TypeEntityDefinitionTester.java
@@ -0,0 +1,55 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.service.schema.tester;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.ui.PlatformUI;
+
+import eu.esdihumboldt.hale.common.align.model.impl.TypeEntityDefinition;
+import eu.esdihumboldt.hale.ui.service.align.AlignmentService;
+
+/**
+ * Tests on {@link TypeEntityDefinition}s based on the {@link AlignmentService}.
+ *
+ * @author Kai Schwierczek
+ */
+public class TypeEntityDefinitionTester extends PropertyTester {
+	
+	/**
+	 * The property namespace for this tester.
+	 */
+	public static final String NAMESPACE = "eu.esdihumboldt.hale.ui.service.shema.type";
+
+	/**
+	 * The property that specifies if a cell may be removed.
+	 */
+	public static final String PROPERTY_TYPE_ALLOW_MARK_UNMAPPABLE = "allow_mark_unmappable";
+	
+	/**
+	 * @see org.eclipse.core.expressions.IPropertyTester#test(java.lang.Object, java.lang.String, java.lang.Object[], java.lang.Object)
+	 */
+	@Override
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		if (receiver == null)
+			return false;
+		
+		if (property.equals(PROPERTY_TYPE_ALLOW_MARK_UNMAPPABLE) && receiver instanceof TypeEntityDefinition) {
+			AlignmentService as = (AlignmentService) PlatformUI.getWorkbench().getService(AlignmentService.class);
+			TypeEntityDefinition entityDef = (TypeEntityDefinition) receiver;
+			return as.getAlignment().getCells(entityDef.getType(), entityDef.getSchemaSpace()).isEmpty();
+		}
+		
+		return false;
+	}
+}
