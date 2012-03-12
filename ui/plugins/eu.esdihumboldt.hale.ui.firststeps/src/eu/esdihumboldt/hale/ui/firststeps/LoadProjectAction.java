0c09bf6216e3cc4583267932e55efa7e536bb5fa
Kai Schwierczek <kai@schwierczek.de>
2012-03-12 15:52:15 +0100

    open a cheat sheet/a project in help with a link
    refs #332
    
    Change-Id: Ie1c796af56b6828c3bac16522a950601af7b47a9

--- ui/plugins/eu.esdihumboldt.hale.ui.firststeps/src/eu/esdihumboldt/hale/ui/firststeps/LoadProjectAction.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.firststeps/src/eu/esdihumboldt/hale/ui/firststeps/LoadProjectAction.java
@@ -18,6 +18,7 @@ import java.net.URISyntaxException;
 import java.util.Properties;
 
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.help.ILiveHelpAction;
 import org.eclipse.jface.action.Action;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.cheatsheets.ICheatSheetAction;
@@ -30,10 +31,11 @@ import eu.esdihumboldt.hale.common.core.io.project.ProjectInfo;
 import eu.esdihumboldt.hale.ui.service.project.ProjectService;
 
 /**
- * An IIntroAction and ICheatSheetAction to open a project. <br>
- * Parameters are <code>closeIntro</code> (or param1 in case of a cheatsheet),
+ * An IIntroAction, ILiveHelpAction and ICheatSheetAction to open a project. <br>
+ * Parameters are <code>closeIntro</code> (or param1 in case of a cheatsheet/live help),
  * <code>path</code> (param2), <code>type</code> (param3) and
  * <code>bundle</code> (param4). All are optional. <br>
+ * Parameters for live help are to be separated by "||".<br>
  * <br>
  * If <code>closeIntro</code> is set to "true" an opened intro is closed after a
  * project was successfully opened. <br>
@@ -50,17 +52,54 @@ import eu.esdihumboldt.hale.ui.service.project.ProjectService;
  * 
  * @author Kai Schwierczek
  */
-public class LoadProjectAction extends Action implements IIntroAction, ICheatSheetAction {
+public class LoadProjectAction extends Action implements IIntroAction, ICheatSheetAction, ILiveHelpAction {
+	/**
+	 * Value for type parameter, specifying that the path is a file path.
+	 */
 	public static final String TYPE_FILE = "file";
+	/**
+	 * Value for type parameter, specifying that the path is an uri.
+	 */
 	public static final String TYPE_URI = "uri";
+	/**
+	 * Value for type parameter, specifying that the path is a bundle internal path.
+	 */
 	public static final String TYPE_BUNDLE = "bundle";
+
+	private boolean closeIntro;
+	private String path;
+	private String type;
+	private String bundle;
 
 	/**
 	 * @see org.eclipse.jface.action.Action#run()
 	 */
 	@Override
 	public void run() {
-		execute(false, null, null, null);
+		// executes event with last configuration
+		ProjectService ps = (ProjectService) PlatformUI.getWorkbench().getService(ProjectService.class);
+		ProjectInfo before = ps.getProjectInfo();
+		// load a given file or show open project dialog
+		if (path != null) {
+			if (TYPE_FILE.equalsIgnoreCase(type) || type == null)
+				ps.load(new File(path).toURI());
+			else if (TYPE_URI.equalsIgnoreCase(type))
+				ps.load(URI.create(path));
+			else if (TYPE_BUNDLE.equalsIgnoreCase(type))
+				try {
+					ps.load(Platform.getBundle(bundle).getEntry(path).toURI());
+				} catch (URISyntaxException e) {
+					throw new IllegalArgumentException(e);
+				}
+		} else
+			ps.open();
+
+		// closeIntro if a project was loaded
+		if (before != ps.getProjectInfo() && closeIntro) {
+			IIntroPart introPart = PlatformUI.getWorkbench().getIntroManager().getIntro();
+			if (introPart != null)
+				PlatformUI.getWorkbench().getIntroManager().closeIntro(introPart);
+		}
 	}
 
 	/**
@@ -97,6 +136,26 @@ public class LoadProjectAction extends Action implements IIntroAction, ICheatShe
 		if (params.length > 3)
 			bundle = params[3];
 		execute(closeIntro, path, type, bundle);
+	}
+
+	/**
+	 * @see org.eclipse.help.ILiveHelpAction#setInitializationString(java.lang.String)
+	 */
+	@Override
+	public void setInitializationString(String data) {
+		String[] params = data.split("\\|\\|");
+		closeIntro = false;
+		if (params.length > 0)
+			closeIntro = "true".equals(params[0]);
+		path = null;
+		if (params.length > 1)
+			path = params[1];
+		type = TYPE_FILE;
+		if (params.length > 2)
+			type = params[2] == null ? type : params[2];
+		bundle = null;
+		if (params.length > 3)
+			bundle = params[3];
 	}
 
 	/**
@@ -143,28 +202,10 @@ public class LoadProjectAction extends Action implements IIntroAction, ICheatShe
 	 *            type path
 	 */
 	public void execute(boolean closeIntro, String path, String type, String bundle) {
-		ProjectService ps = (ProjectService) PlatformUI.getWorkbench().getService(ProjectService.class);
-		ProjectInfo before = ps.getProjectInfo();
-		// load a given file or show open project dialog
-		if (path != null) {
-			if (TYPE_FILE.equalsIgnoreCase(type) || type == null)
-				ps.load(new File(path).toURI());
-			else if (TYPE_URI.equalsIgnoreCase(type))
-				ps.load(URI.create(path));
-			else if (TYPE_BUNDLE.equalsIgnoreCase(type))
-				try {
-					ps.load(Platform.getBundle(bundle).getEntry(path).toURI());
-				} catch (URISyntaxException e) {
-					throw new IllegalArgumentException(e);
-				}
-		} else
-			ps.open();
-
-		// closeIntro if a project was loaded
-		if (before != ps.getProjectInfo() && closeIntro) {
-			IIntroPart introPart = PlatformUI.getWorkbench().getIntroManager().getIntro();
-			if (introPart != null)
-				PlatformUI.getWorkbench().getIntroManager().closeIntro(introPart);
-		}
+		this.closeIntro = closeIntro;
+		this.path = path;
+		this.type = type;
+		this.bundle = bundle;
+		run();
 	}
 }
