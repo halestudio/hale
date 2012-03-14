36d9e4768c3a99df87f9128b9239eab676c01a77
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-03-14 13:35:40 +0100

    added first version of CurveHandler
    Change-Id: If4caf09b2b74ac901431426f7368686e76a7e882

---
+++ io/plugins/eu.esdihumboldt.hale.io.gml.geometry/src/eu/esdihumboldt/hale/io/gml/geometry/handler/CurveHandler.java
@@ -0,0 +1,178 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.gml.geometry.handler;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Point;
+
+import eu.esdihumboldt.hale.common.instance.geometry.DefaultGeometryProperty;
+import eu.esdihumboldt.hale.common.instance.helper.PropertyResolver;
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+import eu.esdihumboldt.hale.common.schema.geometry.CRSDefinition;
+import eu.esdihumboldt.hale.common.schema.geometry.GeometryProperty;
+import eu.esdihumboldt.hale.common.schema.model.TypeConstraint;
+import eu.esdihumboldt.hale.common.schema.model.constraint.type.Binding;
+import eu.esdihumboldt.hale.common.schema.model.constraint.type.GeometryType;
+import eu.esdihumboldt.hale.io.gml.geometry.AbstractGeometryHandler;
+import eu.esdihumboldt.hale.io.gml.geometry.FixedConstraintsGeometryHandler;
+import eu.esdihumboldt.hale.io.gml.geometry.GMLGeometryUtil;
+import eu.esdihumboldt.hale.io.gml.geometry.GeometryHandler;
+import eu.esdihumboldt.hale.io.gml.geometry.GeometryNotSupportedException;
+import eu.esdihumboldt.hale.io.gml.geometry.constraint.GeometryFactory;
+
+/**
+ * Handler for Curve Geometries
+ * 
+ * @author Kevin Mais
+ */
+public class CurveHandler extends FixedConstraintsGeometryHandler {
+
+	private static final String CURVE_TYPE = "CurveType";
+
+	/**
+	 * @see GeometryHandler#createGeometry(Instance)
+	 */
+	@Override
+	public Object createGeometry(Instance instance)
+			throws GeometryNotSupportedException {
+		Point point = null;
+
+		// Curve is either defined by a CoordinatesType named coordinates
+		// GML 3.0, 3.1, 3.2
+		Collection<Object> values = PropertyResolver.getValues(instance,
+				"coordinates", false);
+		if (values != null && !values.isEmpty()) {
+			Object value = values.iterator().next();
+			if (value instanceof Instance) {
+				try {
+					Coordinate[] cs = GMLGeometryUtil
+							.parseCoordinates((Instance) value);
+					if (cs != null && cs.length > 0) {
+						point = getGeometryFactory().createPoint(cs[0]);
+					}
+				} catch (ParseException e) {
+					throw new GeometryNotSupportedException(
+							"Could not parse coordinates", e);
+				}
+			}
+		}
+
+		// or by a DirectPositionType named pos
+		// GML 3.0, 3.1, 3.2
+		if (point == null) {
+			values = PropertyResolver.getValues(instance, "pos", false);
+			if (values != null && !values.isEmpty()) {
+				Object value = values.iterator().next();
+				if (value instanceof Instance) {
+					Coordinate c = GMLGeometryUtil
+							.parseDirectPosition((Instance) value);
+					if (c != null) {
+						point = getGeometryFactory().createPoint(c);
+					}
+				}
+			}
+		}
+
+		// or by a PointPropertyType named pointRep
+		// GML 3.0, 3.1, 3.2
+		if (point == null) {
+			values = PropertyResolver.getValues(instance, "pointRep", false);
+			if (values != null && !values.isEmpty()) {
+				Object value = values.iterator().next();
+				if (value instanceof Instance) {
+					Coordinate c = GMLGeometryUtil.parseCoord((Instance) value);
+					if (c != null) {
+						point = getGeometryFactory().createPoint(c);
+					}
+				}
+			}
+		}
+
+		// or by a PointPropertyType named pointProperty
+		// GML 3.1, 3.2
+		if (point == null) {
+			values = PropertyResolver.getValues(instance, "pointProperty",
+					false);
+			if (values != null && !values.isEmpty()) {
+				Object value = values.iterator().next();
+				if (value instanceof Instance) {
+					Coordinate c = GMLGeometryUtil.parseCoord((Instance) value);
+					if (c != null) {
+						point = getGeometryFactory().createPoint(c);
+					}
+				}
+			}
+		}
+
+		// or by a DirectPositionListType named posList
+		// GML 3.1, 3.2
+		if (point == null) {
+			values = PropertyResolver.getValues(instance, "posList", false);
+			if (values != null && !values.isEmpty()) {
+				Object value = values.iterator().next();
+				if (value instanceof Instance) {
+					Coordinate c = GMLGeometryUtil.parseCoord((Instance) value);
+					if (c != null) {
+						point = getGeometryFactory().createPoint(c);
+					}
+				}
+			}
+		}
+
+		if (point != null) {
+			CRSDefinition crsDef = GMLGeometryUtil.findCRS(instance);
+			return new DefaultGeometryProperty<Point>(crsDef, point);
+		}
+
+		throw new GeometryNotSupportedException(); // XXX
+	}
+
+	/**
+	 * @see FixedConstraintsGeometryHandler#initConstraints()
+	 */
+	@Override
+	protected Collection<? extends TypeConstraint> initConstraints() {
+		Collection<TypeConstraint> constraints = new ArrayList<TypeConstraint>(
+				2);
+
+		// contains one point
+		constraints.add(Binding.get(GeometryProperty.class));
+		constraints.add(GeometryType.get(Point.class));
+		// set geometry factory constraint
+		constraints.add(new GeometryFactory(this));
+
+		return constraints;
+	}
+
+	/**
+	 * @see AbstractGeometryHandler#initSupportedTypes()
+	 */
+	@Override
+	protected Set<? extends QName> initSupportedTypes() {
+		Set<QName> types = new HashSet<QName>();
+
+		types.add(new QName(NS_GML, CURVE_TYPE));
+		types.add(new QName(NS_GML_32, CURVE_TYPE));
+
+		return types;
+	}
+
+}
