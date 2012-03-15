6059004220c8fd25acbfdc4cc3c16b3c58981ceb
Kai Schwierczek <kai@schwierczek.de>
2012-03-15 13:58:12 +0100

    modified ImportCheatSheetAction for live help
    refs #387
    
    Change-Id: I00ad612ae14b98fd7096668160c39e86d4a71f18

--- ui/plugins/eu.esdihumboldt.hale.ui.firststeps/src/eu/esdihumboldt/hale/ui/firststeps/ImportAction.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.firststeps/src/eu/esdihumboldt/hale/ui/firststeps/ImportAction.java
@@ -12,6 +12,7 @@
 
 package eu.esdihumboldt.hale.ui.firststeps;
 
+import org.eclipse.help.ILiveHelpAction;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -24,12 +25,50 @@ import org.eclipse.ui.cheatsheets.ICheatSheetManager;
 import eu.esdihumboldt.hale.ui.io.action.IOWizardAction;
 
 /**
- * Action to open dialogs via an actionId from within a cheatsheet. <br />
- * It needs one param, the actionId.
+ * Action to open dialogs via an actionId from within a cheatsheet or from the help. <br />
+ * It needs one parameter, the actionId.
  * 
  * @author Kai Schwierczek
  */
-public class ImportCheatSheetAction extends Action implements ICheatSheetAction {
+public class ImportAction extends Action implements ICheatSheetAction, ILiveHelpAction {
+	private String actionId;
+
+	/**
+	 * @see org.eclipse.help.ILiveHelpAction#setInitializationString(java.lang.String)
+	 */
+	@Override
+	public void setInitializationString(String data) {
+		actionId = data;
+	}
+
+	/**
+	 * @see org.eclipse.jface.action.Action#run()
+	 */
+	@Override
+	public void run() {
+		if (actionId == null)
+			return;
+
+		IOWizardAction action = new IOWizardAction(actionId);
+		if (action.isEnabled()) {
+			action.addPropertyChangeListener(new IPropertyChangeListener() {
+				@Override
+				public void propertyChange(PropertyChangeEvent event) {
+					if (IAction.RESULT.equals(event.getProperty()))
+						notifyResult((Boolean) event.getNewValue());
+				}
+			});
+			action.run();
+		} else {
+			MessageDialog.openWarning(
+					Display.getCurrent().getActiveShell(),
+					"Action disabled", action.getFactory()
+							.getDisabledReason());
+			notifyResult(false);
+		}
+		action.dispose();
+	}
+
 	/**
 	 * @see org.eclipse.ui.cheatsheets.ICheatSheetAction#run(java.lang.String[],
 	 *      org.eclipse.ui.cheatsheets.ICheatSheetManager)
@@ -37,24 +76,8 @@ public class ImportCheatSheetAction extends Action implements ICheatSheetAction 
 	@Override
 	public void run(String[] params, ICheatSheetManager manager) {
 		if (params.length > 0) {
-			IOWizardAction action = new IOWizardAction(params[0]);
-			if (action.isEnabled()) {
-				action.addPropertyChangeListener(new IPropertyChangeListener() {
-					@Override
-					public void propertyChange(PropertyChangeEvent event) {
-						if (IAction.RESULT.equals(event.getProperty()))
-							notifyResult((Boolean) event.getNewValue());
-					}
-				});
-				action.run();
-			} else {
-				MessageDialog.openWarning(
-						Display.getCurrent().getActiveShell(),
-						"Action disabled", action.getFactory()
-								.getDisabledReason());
-				notifyResult(false);
-			}
-			action.dispose();
+			actionId = params[0];
+			run();
 		}
 	}
 }
