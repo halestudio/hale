9b142e15dc7141cb10585e0636faffa5ed717cc9
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2012-03-19 18:15:15 +0100

    improved and added new handlers for gml geometries
    refs #341
    
    Change-Id: I0601d2214ff5c13c788ded8ac997dec0842fc853
    Reviewed-on: http://gerrit.esdi-community.eu/970
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- io/plugins/eu.esdihumboldt.hale.io.gml.geometry.test/src/eu/esdihumboldt/hale/io/gml/geometry/handler/LineStringHandlerTest.java
+++ io/plugins/eu.esdihumboldt.hale.io.gml.geometry.test/src/eu/esdihumboldt/hale/io/gml/geometry/handler/LineStringHandlerTest.java
@@ -23,7 +23,7 @@ import javax.xml.namespace.QName;
 import org.junit.Test;
 
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.LineString;
 
 import eu.esdihumboldt.hale.common.instance.helper.PropertyResolver;
 import eu.esdihumboldt.hale.common.instance.model.Instance;
@@ -33,49 +33,49 @@ import eu.esdihumboldt.hale.common.schema.geometry.GeometryProperty;
 import eu.esdihumboldt.hale.io.gml.geometry.handler.internal.AbstractHandlerTest;
 
 /**
- * Tests for reading point geometries.
- * @author Simon Templer
+ * Test for reading line string geometries
+ * 
+ * @author Patrick Lieb
  */
-public class PointHandlerTest extends AbstractHandlerTest {
-	
-	/**
-	 * The reference point geometry for checking if a point that was read is 
-	 * correct
-	 */
-	private Point reference;
-	
+public class LineStringHandlerTest extends AbstractHandlerTest {
+
+	private LineString reference;
+
 	@Override
 	public void init() {
 		super.init();
-		
-		// create the reference point geometry
-		reference = geomFactory.createPoint(new Coordinate(127906.229, 489141.472));
+
+		Coordinate[] coordinates = new Coordinate[] {
+				new Coordinate(-39799.68820381, 273207.53980172),
+				new Coordinate(-39841.185, 273182.863),
+				new Coordinate(-39882.89, 273153.86) };
+		reference = geomFactory.createLineString(coordinates);
 	}
 
 	/**
-	 * Test point geometries read from a GML 2 file.
+	 * Test linestring geometries read from a GML 2 file
 	 * @throws Exception if an error occurs
 	 */
 	@Test
-	public void testPointGml2() throws Exception {
+	public void testLineStringGml2() throws Exception {
 		InstanceCollection instances = AbstractHandlerTest.loadXMLInstances(
 				getClass().getResource("/data/geom-gml2.xsd").toURI(), 
-				getClass().getResource("/data/sample-point-gml2.xml").toURI());
+				getClass().getResource("/data/sample-linestring-gml2.xml").toURI());
 		
 		// three instances expected
 		ResourceIterator<Instance> it = instances.iterator();
 		try {
-			// 1. PointProperty with Point defined through coordinates
+			// 1. LineStringProperty with LineString defined through coordinates
 			assertTrue("First sample feature missing", it.hasNext());
 			Instance instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 2. PointProperty with Point defined through coord
+			// 2. LineStringProperty with LineString defined through coord
 			assertTrue("Second sample feature missing", it.hasNext());
 			instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 3. GeometryProperty with Point defined through coordinates
+			// 3. LineStringProperty with LineString defined through coord
 			assertTrue("Third sample feature missing", it.hasNext());
 			instance = it.next();
 			checkGeometryPropertyInstance(instance);
@@ -85,128 +85,138 @@ public class PointHandlerTest extends AbstractHandlerTest {
 	}
 	
 	/**
-	 * Test point geometries read from a GML 3 file.
+	 * Test linestring geometries read from a GML 3 file
 	 * @throws Exception if an error occurs
 	 */
 	@Test
-	public void testPointGml3() throws Exception {
-		InstanceCollection instances = loadXMLInstances(
-				getClass().getResource("/data/geom-gml3.xsd").toURI(), 
-				getClass().getResource("/data/sample-point-gml3.xml").toURI());
+	public void testLineStringGml3() throws Exception {
+		InstanceCollection instances = AbstractHandlerTest.loadXMLInstances(
+		getClass().getResource("/data/geom-gml3.xsd").toURI(), 
+		getClass().getResource("/data/sample-linestring-gml3.xml").toURI());
 		
-		// three instances expected
+		// five instances expected
 		ResourceIterator<Instance> it = instances.iterator();
 		try {
-			// 1. PointProperty with Point defined through coordinates
+			// 1. LineStringProperty with LineString defined through coordinates
 			assertTrue("First sample feature missing", it.hasNext());
 			Instance instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 2. PointProperty with Point defined through coord
+			// 2. LineStringProperty with LineString defined through coord
 			assertTrue("Second sample feature missing", it.hasNext());
 			instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 3. GeometryProperty with Point defined through coord
+			// 3. GeometryProperty with LineString defined through coord
 			assertTrue("Third sample feature missing", it.hasNext());
 			instance = it.next();
 			checkGeometryPropertyInstance(instance);
 			
-			// 4. PointProperty with Point defined through pos
+			// 4. LineStringProperty with LineString defined through pos
 			assertTrue("Fourth sample feature missing", it.hasNext());
 			instance = it.next();
-			checkPointPropertyInstance(instance);
-			
-			// 5. GeometryProperty with Point defined through pos
+			checkLineStringPropertyInstance(instance);
+						
+			// 5. LineStringProperty with LineString defined through pointRep
 			assertTrue("Fifth sample feature missing", it.hasNext());
 			instance = it.next();
-			checkGeometryPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 		} finally {
 			it.close();
 		}
 	}
 	
 	/**
-	 * Test point geometries read from a GML 3.1 file.
+	 * Test linestring geometries read from a GML 3.1 file
 	 * @throws Exception if an error occurs
 	 */
 	@Test
-	public void testPointGml31() throws Exception {
-		InstanceCollection instances = loadXMLInstances(
-				getClass().getResource("/data/geom-gml31.xsd").toURI(), 
-				getClass().getResource("/data/sample-point-gml31.xml").toURI());
+	public void testLineStringGml31() throws Exception {
+		InstanceCollection instances = AbstractHandlerTest.loadXMLInstances(
+		getClass().getResource("/data/geom-gml31.xsd").toURI(), 
+		getClass().getResource("/data/sample-linestring-gml31.xml").toURI());
 		
-		// three instances expected
+		// five instances expected
 		ResourceIterator<Instance> it = instances.iterator();
 		try {
-			// 1. PointProperty with Point defined through coordinates
+			// 1. LineStringProperty with LineString defined through coordinates
 			assertTrue("First sample feature missing", it.hasNext());
 			Instance instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 2. PointProperty with Point defined through coord
+			// 2. LineStringProperty with LineString defined through coord
 			assertTrue("Second sample feature missing", it.hasNext());
 			instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 3. GeometryProperty with Point defined through coord
+			// 3. LineStringProperty with LineString defined through pos
 			assertTrue("Third sample feature missing", it.hasNext());
 			instance = it.next();
-			checkGeometryPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 4. PointProperty with Point defined through pos
+			// 4. LineStringProperty with LineString defined through pointRep
 			assertTrue("Fourth sample feature missing", it.hasNext());
 			instance = it.next();
-			checkPointPropertyInstance(instance);
-			
-			// 5. GeometryProperty with Point defined through pos
+			checkLineStringPropertyInstance(instance);
+						
+			// 5. LineStringProperty with LineString defined through pointProperty
 			assertTrue("Fifth sample feature missing", it.hasNext());
 			instance = it.next();
-			checkGeometryPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
+			
+			// 6. LineStringProperty with LineString defined through posList
+			assertTrue("Sixth sample feature missing", it.hasNext());
+			instance = it.next();
+			checkLineStringPropertyInstance(instance);
 		} finally {
 			it.close();
 		}
 	}
 	
 	/**
-	 * Test point geometries read from a GML 3.2 file.
+	 * Test linestring geometries read from a GML 3.2 file
 	 * @throws Exception if an error occurs
 	 */
 	@Test
-	public void testPointGml32() throws Exception {
-		InstanceCollection instances = loadXMLInstances(
-				getClass().getResource("/data/geom-gml32.xsd").toURI(), 
-				getClass().getResource("/data/sample-point-gml32.xml").toURI());
+	public void testLineStringGml32() throws Exception {
+		InstanceCollection instances = AbstractHandlerTest.loadXMLInstances(
+		getClass().getResource("/data/geom-gml32.xsd").toURI(), 
+		getClass().getResource("/data/sample-linestring-gml32.xml").toURI());
 		
-		// three instances expected
+		// five instances expected
 		ResourceIterator<Instance> it = instances.iterator();
 		try {
-			// 1. PointProperty with Point defined through coordinates
+			// 1. LineStringProperty with LineString defined through coordinates
 			assertTrue("First sample feature missing", it.hasNext());
 			Instance instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 2. GeometryProperty with Point defined through coordinates
+			// 2. LineStringProperty with LineString defined through pos
 			assertTrue("Second sample feature missing", it.hasNext());
 			instance = it.next();
-			checkGeometryPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 3. PointProperty with Point defined through pos
+			// 3. LineStringProperty with LineString defined through pointRep
 			assertTrue("Third sample feature missing", it.hasNext());
 			instance = it.next();
-			checkPointPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
 			
-			// 4. GeometryProperty with Point defined through pos
+			// 4. LineStringProperty with LineString defined through pointProperty
 			assertTrue("Fourth sample feature missing", it.hasNext());
 			instance = it.next();
-			checkGeometryPropertyInstance(instance);
+			checkLineStringPropertyInstance(instance);
+						
+			// 5. LineStringProperty with LineString defined through posList
+			assertTrue("Fifth sample feature missing", it.hasNext());
+			instance = it.next();
+			checkLineStringPropertyInstance(instance);
 		} finally {
 			it.close();
 		}
 	}
-
+	
 	private void checkGeometryPropertyInstance(Instance instance) {
-		Collection<Object> geomVals = PropertyResolver.getValues(instance, "Point", false);
+		Collection<Object> geomVals = PropertyResolver.getValues(instance, "LineString", false);
 		assertNotNull(geomVals);
 		assertEquals(1, geomVals.size());
 		
@@ -216,8 +226,8 @@ public class PointHandlerTest extends AbstractHandlerTest {
 		Instance geomInstance = (Instance) geom;
 		checkGeomInstance(geomInstance);
 	}
-
-	private void checkPointPropertyInstance(Instance instance) {
+	
+	private void checkLineStringPropertyInstance(Instance instance) {
 		Object[] geomVals = instance.getProperty(
 				new QName(NS_TEST, "geometry"));
 		assertNotNull(geomVals);
@@ -229,16 +239,13 @@ public class PointHandlerTest extends AbstractHandlerTest {
 		Instance geomInstance = (Instance) geom;
 		checkGeomInstance(geomInstance);
 	}
-
+	
 	private void checkGeomInstance(Instance geomInstance) {
 		assertTrue(geomInstance.getValue() instanceof GeometryProperty<?>);
 		
 		@SuppressWarnings("unchecked")
-		Point point = ((GeometryProperty<Point>) geomInstance.getValue()).getGeometry();
+		LineString linestring = ((GeometryProperty<LineString>) geomInstance.getValue()).getGeometry();
 		assertTrue("Read geometry does not match the reference geometry", 
-				point.equalsExact(reference));
-		
-		//TODO check CRS!
+				linestring.equalsExact(reference));
 	}
-
 }
---
+++ io/plugins/eu.esdihumboldt.hale.io.gml.geometry/src/eu/esdihumboldt/hale/io/gml/geometry/handler/LinearRingHandler.java
@@ -0,0 +1,96 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.gml.geometry.handler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
+import eu.esdihumboldt.hale.common.instance.geometry.DefaultGeometryProperty;
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+import eu.esdihumboldt.hale.common.schema.geometry.CRSDefinition;
+import eu.esdihumboldt.hale.common.schema.geometry.GeometryProperty;
+import eu.esdihumboldt.hale.common.schema.model.TypeConstraint;
+import eu.esdihumboldt.hale.common.schema.model.constraint.type.Binding;
+import eu.esdihumboldt.hale.common.schema.model.constraint.type.GeometryType;
+import eu.esdihumboldt.hale.io.gml.geometry.FixedConstraintsGeometryHandler;
+import eu.esdihumboldt.hale.io.gml.geometry.GMLGeometryUtil;
+import eu.esdihumboldt.hale.io.gml.geometry.GeometryNotSupportedException;
+import eu.esdihumboldt.hale.io.gml.geometry.constraint.GeometryFactory;
+
+/**
+ * Handler for linear ring geometries
+ * @author Patrick Lieb
+ */
+public class LinearRingHandler extends FixedConstraintsGeometryHandler{
+
+	private static final String LINEAR_RING_TYPE = "LinearRingType";
+	
+	/**
+	 * @see eu.esdihumboldt.hale.io.gml.geometry.GeometryHandler#createGeometry(eu.esdihumboldt.hale.common.instance.model.Instance)
+	 */
+	@Override
+	public Object createGeometry(Instance instance)
+			throws GeometryNotSupportedException {
+		
+		LinearRing ring = null;
+		LineStringHandler handler = new LineStringHandler();
+		
+		// for use with GML 2
+		// XXX support for other types in GML 3.2
+		@SuppressWarnings("unchecked")
+		DefaultGeometryProperty<LineString> linestring = (DefaultGeometryProperty<LineString>)handler.createGeometry(instance);
+		ring = getGeometryFactory().createLinearRing(linestring.getGeometry().getCoordinates());
+		
+		if (ring != null) {
+			CRSDefinition crsDef = GMLGeometryUtil.findCRS(instance);
+			return new DefaultGeometryProperty<LinearRing>(crsDef, ring);
+		}
+		throw new GeometryNotSupportedException();
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.io.gml.geometry.FixedConstraintsGeometryHandler#initConstraints()
+	 */
+	@Override
+	protected Collection<? extends TypeConstraint> initConstraints() {
+		Collection<TypeConstraint> constraints = new ArrayList<TypeConstraint>(
+				2);
+
+		constraints.add(Binding.get(GeometryProperty.class));
+		constraints.add(GeometryType.get(LinearRing.class));
+
+		constraints.add(new GeometryFactory(this));
+
+		return constraints;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.io.gml.geometry.AbstractGeometryHandler#initSupportedTypes()
+	 */
+	@Override
+	protected Set<? extends QName> initSupportedTypes() {
+		Set<QName> types = new HashSet<QName>();
+
+		types.add(new QName(NS_GML, LINEAR_RING_TYPE));
+		types.add(new QName(NS_GML_32, LINEAR_RING_TYPE));
+
+		return types;
+	}
+
+}
