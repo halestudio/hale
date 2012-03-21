ad5e92e2cef33d2dd43adb7d19a3f84613acb842
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-03-21 21:04:19 +0100

    fixed possible endless loop introduced with definition sorting
    Change-Id: I1329c86b38580622c9d1d14d928e32ac85a861ce
    Reviewed-on: http://gerrit.esdi-community.eu/1028
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/definition/AbstractObjectFactory.java
+++ util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/definition/AbstractObjectFactory.java
@@ -14,21 +14,24 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Provides support for converting certain objects to a definition string and 
- * vice versa based on the {@link ObjectDefinition}ies available for the supported
- * object type and its sub-types.
- * @param <T> the supported object type
- * @param <D> the supported definition type
- *    
+ * Provides support for converting certain objects to a definition string and
+ * vice versa based on the {@link ObjectDefinition}ies available for the
+ * supported object type and its sub-types.
+ * 
+ * @param <T>
+ *            the supported object type
+ * @param <D>
+ *            the supported definition type
+ * 
  * @author Simon Templer
  */
 public abstract class AbstractObjectFactory<T, D extends ObjectDefinition<? extends T>> {
-	
+
 	/**
 	 * The cached sorted definitions.
 	 */
 	private Iterable<D> sortedDefinitions;
-	
+
 	/**
 	 * Default constructor.
 	 */
@@ -37,20 +40,24 @@ public abstract class AbstractObjectFactory<T, D extends ObjectDefinition<? exte
 	}
 
 	/**
-	 * Get all available definitions compatible with the supported type. 
+	 * Get all available definitions compatible with the supported type.
+	 * 
 	 * @return the definitions
 	 */
 	protected abstract List<D> getDefinitions();
-	
+
 	/**
-	 * Represent the given object as a definition string, so that it can be 
-	 * used to again create an object instance using {@link #parse(String)}.
-	 * @param <X> the object type, an {@link ObjectDefinition} supporting this
-	 *   type must be available
-	 * @param object the object to create a string representation for
+	 * Represent the given object as a definition string, so that it can be used
+	 * to again create an object instance using {@link #parse(String)}.
+	 * 
+	 * @param <X>
+	 *            the object type, an {@link ObjectDefinition} supporting this
+	 *            type must be available
+	 * @param object
+	 *            the object to create a string representation for
 	 * @return the string representation of the object or <code>null</code> if
-	 *   no corresponding {@link ObjectDefinition} is available
-	 *   
+	 *         no corresponding {@link ObjectDefinition} is available
+	 * 
 	 * @see #getDefinitions()
 	 */
 	@SuppressWarnings("unchecked")
@@ -59,42 +66,46 @@ public abstract class AbstractObjectFactory<T, D extends ObjectDefinition<? exte
 		if (object == null) {
 			return null;
 		}
-		
+
 		for (D definition : getSortedDefinitions()) {
 			if (definition.getObjectClass() == null) {
 				continue;
 			}
-			
+
 			// comparison based on classes
-			if (definition.getObjectClass() != null 
+			if (definition.getObjectClass() != null
 					&& definition.getObjectClass().equals(object.getClass())) {
-				return definition.getIdentifier() + ":" + ((ObjectDefinition<T>) definition).asString(object); //$NON-NLS-1$
+				return definition.getIdentifier()
+						+ ":" + ((ObjectDefinition<T>) definition).asString(object); //$NON-NLS-1$
 			}
 			// compare based on interfaces
-			else if (definition.getObjectClass().isInterface() 
+			else if (definition.getObjectClass().isInterface()
 					&& compare(object, definition)) {
-				return definition.getIdentifier() + ":" + ((ObjectDefinition<T>) definition).asString(object); //$NON-NLS-1$
+				return definition.getIdentifier()
+						+ ":" + ((ObjectDefinition<T>) definition).asString(object); //$NON-NLS-1$
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Compares two objects based on there implemented interfaces.
 	 * 
-	 * @param object object to check
-	 * @param definition definition to check on
+	 * @param object
+	 *            object to check
+	 * @param definition
+	 *            definition to check on
 	 * @return true if they implemented the same interfaces
 	 */
 	private boolean compare(T object, D definition) {
 		if (definition.getObjectClass().isAssignableFrom(object.getClass())) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Provides the definitions in a topological order.
 	 * 
@@ -102,29 +113,35 @@ public abstract class AbstractObjectFactory<T, D extends ObjectDefinition<? exte
 	 */
 	private Iterable<D> getSortedDefinitions() {
 		if (sortedDefinitions == null) {
-			List<D> list = getDefinitions();
-			ArrayList<D> result = new ArrayList<D>(list.size());
+			// create a copy of the definition list to ensure we are dealing
+			// with the same instances
+			List<D> list = new ArrayList<D>(getDefinitions());
 			
+			// result list
+			ArrayList<D> result = new ArrayList<D>(list.size());
+
 			while (!list.isEmpty()) {
 				Iterator<D> iter = list.iterator();
 				while (iter.hasNext()) {
 					D def = iter.next();
 					boolean isSuper = false;
-		
+
 					for (D d : list) {
 						// skip if it's the same object
-						if (def.equals(d)) continue;
-						
+						if (def.equals(d))
+							continue;
+
 						// check if it's super class
-						if (def.getObjectClass().isAssignableFrom(d.getObjectClass())) {
+						if (def.getObjectClass().isAssignableFrom(
+								d.getObjectClass())) {
 							isSuper = true;
 							break;
 						}
 					}
-					
+
 					/*
-					 * if it's not a super class/interface add it to result
-					 * and remove it from list
+					 * if it's not a super class/interface add it to result and
+					 * remove it from list
 					 */
 					if (!isSuper) {
 						result.add(def);
@@ -132,25 +149,26 @@ public abstract class AbstractObjectFactory<T, D extends ObjectDefinition<? exte
 					}
 				}
 			}
-			
+
 			sortedDefinitions = result;
 		}
-		
+
 		// return the sorted list
 		return sortedDefinitions;
 	}
-	
+
 	/**
 	 * Parse the given definition string and create a CRS definition instance.
 	 * 
-	 * @param value the definition string to parse
+	 * @param value
+	 *            the definition string to parse
 	 * @return the CRS definition instance or <code>null</code>
 	 */
 	public T parse(String value) {
 		if (value == null || value.isEmpty()) {
 			return null;
 		}
-		
+
 		for (D definition : getDefinitions()) {
 			String prefix = definition.getIdentifier() + ":"; //$NON-NLS-1$
 			if (value.startsWith(prefix)) {
@@ -158,8 +176,8 @@ public abstract class AbstractObjectFactory<T, D extends ObjectDefinition<? exte
 				return definition.parse(main);
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 }
