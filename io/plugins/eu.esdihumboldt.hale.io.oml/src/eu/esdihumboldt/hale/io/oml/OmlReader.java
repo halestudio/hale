fb9e1ba30d34bdc3697498f9fdb85cf31b70d2b8
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-03-29 14:55:53 +0200

    added basic implementation of OmlReader
    refs #364
    
    Change-Id: Ied3bec3bfc38fe7d299209a010cbffc584bf0a35

---
+++ io/plugins/eu.esdihumboldt.hale.io.oml/src/eu/esdihumboldt/hale/io/oml/OmlReader.java
@@ -0,0 +1,307 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.io.oml;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import eu.esdihumboldt.commons.goml.align.Alignment;
+import eu.esdihumboldt.commons.goml.oml.io.OmlRdfReader;
+import eu.esdihumboldt.commons.goml.omwg.ComposedFeatureClass;
+import eu.esdihumboldt.commons.goml.omwg.ComposedProperty;
+import eu.esdihumboldt.commons.goml.omwg.FeatureClass;
+import eu.esdihumboldt.commons.goml.omwg.Property;
+import eu.esdihumboldt.commons.goml.rdf.DetailedAbout;
+import eu.esdihumboldt.commons.goml.rdf.IDetailedAbout;
+import eu.esdihumboldt.hale.common.align.io.AlignmentReader;
+import eu.esdihumboldt.hale.common.align.io.impl.AbstractAlignmentReader;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.AlignmentBean;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.CellBean;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.ChildContextBean;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.EntityBean;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.NamedEntityBean;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.ParameterValue;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.PropertyBean;
+import eu.esdihumboldt.hale.common.align.io.impl.internal.TypeBean;
+import eu.esdihumboldt.hale.common.align.model.MutableAlignment;
+import eu.esdihumboldt.hale.common.core.io.IOProviderConfigurationException;
+import eu.esdihumboldt.hale.common.core.io.ProgressIndicator;
+import eu.esdihumboldt.hale.common.core.io.report.IOReport;
+import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
+import eu.esdihumboldt.hale.common.schema.model.Schema;
+import eu.esdihumboldt.hale.common.schema.model.SchemaSpace;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.common.schema.model.TypeIndex;
+import eu.esdihumboldt.hale.io.xsd.constraint.XmlElements;
+import eu.esdihumboldt.hale.io.xsd.model.XmlElement;
+import eu.esdihumboldt.hale.io.xsd.model.XmlIndex;
+import eu.esdihumboldt.specification.cst.align.ICell;
+import eu.esdihumboldt.specification.cst.align.IEntity;
+import eu.esdihumboldt.specification.cst.align.ext.IParameter;
+
+/**
+ * This class reads the OML Document into Java Object.
+ * 
+ * @author Kevin Mais
+ */
+@SuppressWarnings("restriction")
+public class OmlReader extends AbstractAlignmentReader implements
+		AlignmentReader {
+
+	private MutableAlignment mutableAlignment = null;
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.IOProvider#isCancelable()
+	 */
+	@Override
+	public boolean isCancelable() {
+		return false;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.align.io.AlignmentReader#getAlignment()
+	 */
+	@Override
+	public MutableAlignment getAlignment() {
+		return mutableAlignment;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.impl.AbstractIOProvider#execute(eu.esdihumboldt.hale.common.core.io.ProgressIndicator,
+	 *      eu.esdihumboldt.hale.common.core.io.report.IOReporter)
+	 */
+	@Override
+	protected IOReport execute(ProgressIndicator progress, IOReporter reporter)
+			throws IOProviderConfigurationException, IOException {
+
+		try {
+			progress.begin("Load ontology mapping file",
+					ProgressIndicator.UNKNOWN);
+
+			OmlRdfReader reader = new OmlRdfReader();
+
+			Alignment alignment = reader
+					.read(getSource().getLocation().toURL());
+
+			AlignmentBean align = new AlignmentBean();
+
+			List<CellBean> cells = new ArrayList<CellBean>();
+
+			List<ICell> map = alignment.getMap();
+
+			for (ICell cell : map) {
+
+				// create a new CellBean for each ICell
+				CellBean cellBean = new CellBean();
+
+				IEntity entity = cell.getEntity1();
+				IEntity entity2 = cell.getEntity2();
+
+				// temporary list to be copied into the CellBean
+				List<NamedEntityBean> temp_source = new ArrayList<NamedEntityBean>();
+				List<NamedEntityBean> temp_target = new ArrayList<NamedEntityBean>();
+
+				setBeanLists(entity, temp_source, getSourceSchema());
+				setBeanLists(entity2, temp_target, getTargetSchema());
+
+				// set source/target lists of the CellBean
+				cellBean.setSource(temp_source);
+				cellBean.setTarget(temp_target);
+
+				// check if one of the entities has a transformation
+				if (entity.getTransformation() != null
+						|| entity2.getTransformation() != null) {
+					// set parameters and transformation id for the cellBean
+					// from
+					// entity 1 if it has the transformation
+					if (entity.getTransformation() != null) {
+						setParameters(cellBean, entity);
+						setTransformationId(cellBean, entity);
+					} else {
+						// else set parameters and transformation id from entity
+						// 2
+						setParameters(cellBean, entity2);
+						setTransformationId(cellBean, entity2);
+					}
+				}
+
+				// add the CellBean to a list of CellBeans
+				cells.add(cellBean);
+			}
+
+			// set the cells for the alignment after the all iterations
+			align.setCells(cells);
+
+			mutableAlignment = align.createAlignment(reporter,
+					getSourceSchema(), getTargetSchema());
+
+			reporter.setSuccess(true);
+
+			return reporter;
+		} finally {
+			progress.end();
+		}
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.impl.AbstractIOProvider#getDefaultTypeName()
+	 */
+	@Override
+	protected String getDefaultTypeName() {
+		return null;
+	}
+
+	private void setBeanLists(IEntity entity, List<NamedEntityBean> list,
+			TypeIndex schema) {
+
+		if (entity instanceof FeatureClass) {
+			// get the detailed about-information
+			IDetailedAbout about = DetailedAbout.getDetailedAbout(
+					entity.getAbout(), false);
+
+			TypeBean typeBean = new TypeBean();
+			
+			setTypeNameBean(typeBean, about, schema);
+
+			NamedEntityBean namedEntityBean = new NamedEntityBean();
+
+			namedEntityBean.setEntity(typeBean);
+			namedEntityBean.setName(null);
+
+			list.add(namedEntityBean);
+
+		} else if (entity instanceof ComposedFeatureClass) {
+			ComposedFeatureClass cfc = (ComposedFeatureClass) entity;
+
+			List<FeatureClass> coll = cfc.getCollection();
+
+			for (FeatureClass fc : coll) {
+				setBeanLists(fc, list, schema);
+			}
+
+		} else if (entity instanceof Property) {
+			// get the detailed about-information
+			IDetailedAbout about = DetailedAbout.getDetailedAbout(
+					entity.getAbout(), true);
+
+			PropertyBean prop = new PropertyBean();
+			List<ChildContextBean> childList = new ArrayList<ChildContextBean>();
+
+			List<String> props = about.getProperties();
+
+			for (String property : props) {
+				ChildContextBean ccb = new ChildContextBean();
+				ccb.setChildName(new QName(property));
+
+				childList.add(ccb);
+			}
+			
+			setTypeNameBean(prop, about, schema);
+			prop.setProperties(childList);
+
+			NamedEntityBean namedEntityBean = new NamedEntityBean();
+
+			namedEntityBean.setEntity(prop);
+			namedEntityBean.setName(null);
+
+			list.add(namedEntityBean);
+
+		} else if (entity instanceof ComposedProperty) {
+			ComposedProperty cp = (ComposedProperty) entity;
+
+			List<Property> coll = cp.getCollection();
+
+			for (Property prop : coll) {
+				setBeanLists(prop, list, schema);
+			}
+
+		}
+
+	}
+
+	private void setTypeNameBean(EntityBean<?> entityBean, IDetailedAbout about, TypeIndex schema) {
+		
+		QName name = new QName(about.getNamespace(),
+				about.getFeatureClass());
+
+		if (schema.getType(name) == null) {
+			name = findElementType(schema, name);
+		}
+
+		entityBean.setTypeName(name);
+		
+	}
+
+	private QName findElementType(TypeIndex schema, QName elementName) {
+		if (schema instanceof SchemaSpace) {
+			SchemaSpace ss = (SchemaSpace) schema;
+			for (Schema schem : ss.getSchemas()) {
+				if (schem instanceof XmlIndex) {
+					XmlElement xmlelem = ((XmlIndex) schem).getElements().get(
+							elementName);
+					if (xmlelem != null) {
+						return xmlelem.getType().getName();
+					}
+				}
+			}
+		} else {
+			for(TypeDefinition typedef : schema.getTypes()) {
+				XmlElements xmlelem = typedef.getConstraint(XmlElements.class);
+				for(XmlElement elem : xmlelem.getElements()) {
+					if(elem.getName().equals(elementName)) {
+						return typedef.getName();
+					}
+				}
+			}
+		}
+		return elementName;
+	}
+
+	private void setParameters(CellBean cellBean, IEntity entity) {
+
+		// get the list of parameters
+		List<IParameter> list = entity.getTransformation().getParameters();
+
+		// create a list of ParameterValue (because
+		// setTransformationParameters needs a list)
+		List<ParameterValue> params = new ArrayList<ParameterValue>();
+
+		for (int i = 0; i < list.size(); i++) {
+			String name = list.get(i).getName();
+			String value = list.get(i).getValue();
+
+			// create the ParameterValue for the CellBean
+			ParameterValue paramVal = new ParameterValue(name, value);
+
+			// add the ParameterValue
+			params.add(paramVal);
+		}
+
+		// set the new transformation parameters
+		cellBean.setTransformationParameters(params);
+
+	}
+
+	private void setTransformationId(CellBean cellBean, IEntity entity) {
+
+		// set the new transformation identifier
+		cellBean.setTransformationIdentifier(entity.getTransformation()
+				.getService().getLocation());
+
+	}
+
+}
