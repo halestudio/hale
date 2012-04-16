394f804c815809628357fd96a6410cef3d903178
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-04-16 14:08:12 +0200

    feedback on processed instances for loading and transforming
    Change-Id: I3a24c04a8d66520400919496b814940bade9d895
    Reviewed-on: http://gerrit.esdi-community.eu/1072
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/io/impl/ProgressIndicatorDecorator.java
@@ -0,0 +1,74 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.core.io.impl;
+
+import eu.esdihumboldt.hale.common.core.io.ProgressIndicator;
+
+/**
+ * Progress indicator decorator
+ * @author Simon Templer
+ */
+public abstract class ProgressIndicatorDecorator implements ProgressIndicator {
+
+	private final ProgressIndicator decoratee;
+
+	/**
+	 * Create a progress indicator decorator
+	 * @param decoratee the decoratee
+	 */
+	public ProgressIndicatorDecorator(ProgressIndicator decoratee) {
+		super();
+		this.decoratee = decoratee;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.ProgressIndicator#begin(java.lang.String, int)
+	 */
+	@Override
+	public void begin(String taskName, int totalWork) {
+		decoratee.begin(taskName, totalWork);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.ProgressIndicator#setCurrentTask(java.lang.String)
+	 */
+	@Override
+	public void setCurrentTask(String taskName) {
+		decoratee.setCurrentTask(taskName);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.ProgressIndicator#advance(int)
+	 */
+	@Override
+	public void advance(int workUnits) {
+		decoratee.advance(workUnits);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.ProgressIndicator#isCanceled()
+	 */
+	@Override
+	public boolean isCanceled() {
+		return decoratee.isCanceled();
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.ProgressIndicator#end()
+	 */
+	@Override
+	public void end() {
+		decoratee.end();
+	}
+
+}
