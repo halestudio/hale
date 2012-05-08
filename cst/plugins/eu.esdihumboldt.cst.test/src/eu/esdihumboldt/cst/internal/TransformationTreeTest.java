231f948c70ad9b12798566d23fb54adde0f02c39
Kai Schwierczek <kai@schwierczek.de>
2012-05-08 19:18:15 +0200

    simple equals for transformation graphs
    load* TestUtil class
    some prefs updated
    
    Change-Id: I4256c1f56f58e676707e121786cac0814df21d2d
    Signed-off-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ cst/plugins/eu.esdihumboldt.cst.test/src/eu/esdihumboldt/cst/internal/TransformationTreeTest.java
@@ -0,0 +1,94 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.internal;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.zest.core.widgets.Graph;
+import org.eclipse.zest.core.widgets.GraphConnection;
+import org.eclipse.zest.core.widgets.GraphNode;
+
+/**
+ * Tests for the transformation tree
+ * 
+ * @author Kai Schwierczek
+ */
+public class TransformationTreeTest {
+	/**
+	 * Compares the two graphs with each other.
+	 * It expects the connections to point from target to source.
+	 * It expects exactly one target with no outgoing connections.
+	 * Source values schould be unique for it to work correctly.
+	 *
+	 * @param a one graph
+	 * @param b the other graph
+	 * @return true, if the two graphs equal each other
+	 */
+	@SuppressWarnings("unchecked")
+	public static boolean graphEquals(Graph a, Graph b) {
+		List<GraphNode> nodes1 = a.getNodes();
+		List<GraphNode> nodes2 = b.getNodes();
+
+		// compare node count
+		if (nodes1.size() != nodes2.size())
+			return false;
+
+		// find target nodes
+		GraphNode target1 = null, target2 = null;
+		for (GraphNode node1 : nodes1)
+			if (node1.getSourceConnections().size() == 0) {
+				target1 = node1;
+				break;
+			}
+		for (GraphNode node2 : nodes2)
+			if (node2.getSourceConnections().size() == 0) {
+				target2 = node2;
+				break;
+			}
+
+		// compare them
+		return nodesEqual(target1, target2);
+	}
+
+	// Compares the two given graph nodes.
+	@SuppressWarnings("unchecked")
+	private static boolean nodesEqual(GraphNode a, GraphNode b) {
+		// compare text
+		if (!a.getText().equals(b.getText()))
+			return false;
+
+		List<GraphConnection> aConnections = a.getSourceConnections();
+		List<GraphConnection> bConnections = b.getSourceConnections();
+		// compare outgoing connection count
+		if (aConnections.size() != bConnections.size())
+			return false;
+
+		// compare outgoing connections
+		for (GraphConnection aConnection : aConnections) {
+			boolean found = false;
+			Iterator<GraphConnection> bIter = bConnections.iterator();
+			while (!found && bIter.hasNext()) {
+				GraphConnection bConnection = bIter.next();
+				if (nodesEqual(aConnection.getDestination(), bConnection.getDestination())) {
+					bIter.remove();
+					found = true;
+				}
+			}
+			if (!found)
+				return false;
+		}
+
+		return true;
+	}
+}
