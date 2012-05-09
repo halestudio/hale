fe4ffc18220eda03fe673e1ebd806ad1818326e0
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-05-09 10:37:56 +0200

    added and fixed some test cases
    refs #364
    
    Change-Id: Iaf24a33ebd1a894edad92f3073b2b55ecabec8aa

--- io/plugins/eu.esdihumboldt.hale.io.oml.test/src/eu/esdihumboldt/hale/oml/test/OMLReaderTest.java
+++ io/plugins/eu.esdihumboldt.hale.io.oml.test/src/eu/esdihumboldt/hale/oml/test/OMLReaderTest.java
@@ -1,18 +1,24 @@
 package eu.esdihumboldt.hale.oml.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import org.exolab.castor.mapping.MappingException;
 import org.exolab.castor.xml.MarshalException;
 import org.exolab.castor.xml.ValidationException;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.google.common.collect.ListMultimap;
 
 import eu.esdihumboldt.hale.common.align.model.Alignment;
 import eu.esdihumboldt.hale.common.align.model.Cell;
@@ -26,77 +32,146 @@ import eu.esdihumboldt.hale.io.oml.OmlReader;
 import eu.esdihumboldt.hale.io.xsd.reader.XmlSchemaReader;
 
 public class OMLReaderTest {
-	
+
 	static Alignment alignment = null;
-	
+	static Alignment alignment2 = null;
+	static Alignment alignment3 = null;
+
 	@BeforeClass
-	public static void load() throws MarshalException, ValidationException, IOProviderConfigurationException, IOException, MappingException, URISyntaxException {
+	public static void load() throws MarshalException, ValidationException,
+			IOProviderConfigurationException, IOException, MappingException,
+			URISyntaxException {
+
 		alignment = loadAlignment(
-				OMLReaderTest.class.getResource("/testdata/testOML/t2.xsd").toURI(), 
-				OMLReaderTest.class.getResource("/testdata/testOML/t2.xsd").toURI(), 
-				OMLReaderTest.class.getResource("/testdata/testOML/testOMLmapping.goml").toURI());
+				OMLReaderTest.class.getResource("/testdata/testOML/t2.xsd")
+						.toURI(),
+				OMLReaderTest.class.getResource("/testdata/testOML/t2.xsd")
+						.toURI(),
+				OMLReaderTest.class.getResource(
+						"/testdata/testOML/testOMLmapping.goml").toURI());
+
+		alignment2 = loadAlignment(
+				OMLReaderTest.class.getResource(
+						"/testdata/sample_wva/wfs_va.xsd").toURI(),
+				OMLReaderTest.class
+						.getResource(
+								"/testdata/sample_wva/inspire3/HydroPhysicalWaters.xsd")
+						.toURI(),
+				OMLReaderTest.class.getResource(
+						"/testdata/sample_wva/watercourse_va.xml.goml").toURI());
+
+		alignment3 = loadAlignment(
+				OMLReaderTest.class.getResource(
+						"/testdata/aaa2inspire_cp/NAS_6.0.1/schema/aaa.xsd")
+						.toURI(),
+				OMLReaderTest.class
+						.getResource(
+								"/testdata/aaa2inspire_cp/inspire3/CadastralParcels.xsd")
+						.toURI(),
+				OMLReaderTest.class.getResource(
+						"/testdata/aaa2inspire_cp/aaa2inspire_cp.xml.goml")
+						.toURI());
+
 	}
-	
+
 	@Test
 	public void testOMLreader() throws Exception {
-		
+
 		assertNotNull(alignment);
+		assertNotNull(alignment2);
+		assertNotNull(alignment3);
 	}
-	
+
 	@Test
 	public void testCellCount() {
-		
-		Collection<? extends Cell> cells = alignment.getCells();
-		
-		assertEquals(2, cells.size());
-	}
-	
-	private static Alignment loadAlignment(URI sourceSchemaLocation, 
-			URI targetSchemaLocation, final URI alignmentLocation) throws IOProviderConfigurationException, IOException, MarshalException, ValidationException, MappingException, URISyntaxException {
-		
-		// load source schema
-		Schema source = readXMLSchema(new DefaultInputSupplier(sourceSchemaLocation));
-		
-		// load target schema
-		Schema target = readXMLSchema(new DefaultInputSupplier(targetSchemaLocation));
 
+		Collection<? extends Cell> cells = alignment.getCells();
+		Collection<? extends Cell> cells2 = alignment2.getCells();
+
+		assertEquals(2, cells.size());
+		assertEquals(11, cells2.size());
+	}
+
+	@Test
+	public void testFunction() {
+
+		Collection<? extends Cell> cells = alignment.getCells();
+
+		Iterator<? extends Cell> it = cells.iterator();
+
+		Cell cell1 = it.next();
+		Cell cell2 = it.next();
+
+		assertEquals("eu.esdihumboldt.hale.align.retype",
+				cell1.getTransformationIdentifier());
+		assertEquals("eu.esdihumboldt.hale.align.formattedstring",
+				cell2.getTransformationIdentifier());
+
+		ListMultimap<String, String> params = cell2
+				.getTransformationParameters();
+		List<String> values = params.get("pattern");
+
+		assertEquals("{id}-xxx-{details.address.street}", values.get(0));
+
+		// TODO: test for alignment3 pattern =
+		// "{flurstuecksnummer.AX_Flurstuecksnummer.zaehler}/{flurstuecksnummer.AX_Flurstuecksnummer.nenner}"
+
+	}
+
+	private static Alignment loadAlignment(URI sourceSchemaLocation,
+			URI targetSchemaLocation, final URI alignmentLocation)
+			throws IOProviderConfigurationException, IOException,
+			MarshalException, ValidationException, MappingException,
+			URISyntaxException {
+
+		// load source schema
+		Schema source = readXMLSchema(new DefaultInputSupplier(
+				sourceSchemaLocation));
+
+		// load target schema
+		Schema target = readXMLSchema(new DefaultInputSupplier(
+				targetSchemaLocation));
 
 		OmlReader reader = new OmlReader();
-		
+
 		reader.setSourceSchema(source);
 		reader.setTargetSchema(target);
 		reader.setSource(new DefaultInputSupplier(alignmentLocation));
-		
+
 		reader.validate();
-		
+
 		IOReport report = reader.execute(null);
-		
+
 		assertTrue(report.isSuccess());
-		
+
 		return reader.getAlignment();
 	}
-	
-	
+
 	/**
 	 * Reads a XML schema
 	 * 
-	 * @param input the input supplier
+	 * @param input
+	 *            the input supplier
 	 * @return the schema
-	 * @throws IOProviderConfigurationException if the configuration of the
-	 *   reader is invalid
-	 * @throws IOException if reading the schema fails
+	 * @throws IOProviderConfigurationException
+	 *             if the configuration of the reader is invalid
+	 * @throws IOException
+	 *             if reading the schema fails
 	 */
-	private static Schema readXMLSchema(LocatableInputSupplier<? extends InputStream> input) throws IOProviderConfigurationException, IOException {
+	private static Schema readXMLSchema(
+			LocatableInputSupplier<? extends InputStream> input)
+			throws IOProviderConfigurationException, IOException {
 		XmlSchemaReader reader = new XmlSchemaReader();
 		reader.setSharedTypes(new DefaultTypeIndex());
 		reader.setSource(input);
-		
+
 		reader.validate();
 		IOReport report = reader.execute(null);
-		
+
 		assertTrue(report.isSuccess());
-		assertTrue("Errors are contained in the report", report.getErrors().isEmpty());
-		
+		assertTrue("Errors are contained in the report", report.getErrors()
+				.isEmpty());
+
 		return reader.getSchema();
 	}
 }
