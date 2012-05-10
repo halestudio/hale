f8724547373232fa4c7ffa0ba1ecdcde107076e3
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-05-10 14:35:43 +0200

    updated project settings
    Change-Id: I1c485f20b06e05717d4a42874eae9d8e8b16697a
    Reviewed-on: http://gerrit.esdi-community.eu/1105
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- io/plugins/eu.esdihumboldt.hale.io.oml.test/src/eu/esdihumboldt/hale/oml/test/OMLReaderTest.java
+++ io/plugins/eu.esdihumboldt.hale.io.oml.test/src/eu/esdihumboldt/hale/oml/test/OMLReaderTest.java
@@ -1,3 +1,15 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2012.
+ */
+
 package eu.esdihumboldt.hale.oml.test;
 
 import static org.junit.Assert.*;
@@ -39,7 +51,6 @@ public class OMLReaderTest {
 	
 	@Test
 	public void testOMLreader() throws Exception {
-		
 		assertNotNull(alignment);
 	}
 	
