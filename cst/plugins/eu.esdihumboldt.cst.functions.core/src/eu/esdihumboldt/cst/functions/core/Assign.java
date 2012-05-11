163b7d623ceeed0fd2f84d204b6b9910a5440feb
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-05-11 15:06:32 +0200

    bound assign
    Added anchor as possible source property for Assign.
    
    refs #396
    
    Conflicts:
    	cst/plugins/eu.esdihumboldt.cst.functions.core/src/eu/esdihumboldt/cst/functions/core/Assign.java
    
    Change-Id: Ica1ea26b92a802226a9005eb16c45bd95135292f
    Reviewed-on: http://gerrit.esdi-community.eu/2236
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- cst/plugins/eu.esdihumboldt.cst.functions.core/src/eu/esdihumboldt/cst/functions/core/Assign.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.core/src/eu/esdihumboldt/cst/functions/core/Assign.java
@@ -16,17 +16,20 @@
 
 package eu.esdihumboldt.cst.functions.core;
 
+import java.util.List;
 import java.util.Map;
 
 import net.jcip.annotations.Immutable;
 
 import com.google.common.collect.ListMultimap;
 
+import eu.esdihumboldt.hale.common.align.model.Entity;
 import eu.esdihumboldt.hale.common.align.model.functions.AssignFunction;
 import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
 import eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine;
 import eu.esdihumboldt.hale.common.align.transformation.function.PropertyValue;
 import eu.esdihumboldt.hale.common.align.transformation.function.TransformationException;
+import eu.esdihumboldt.hale.common.align.transformation.function.impl.NoResultException;
 import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
 import eu.esdihumboldt.hale.common.scripting.transformation.AbstractSingleTargetScriptedPropertyTransformation;
 
@@ -44,7 +47,31 @@ public class Assign extends
 	protected Object evaluate(String transformationIdentifier, TransformationEngine engine,
 			ListMultimap<String, PropertyValue> variables, String resultName,
 			PropertyEntityDefinition resultProperty, Map<String, String> executionParameters,
-			TransformationLog log) throws TransformationException {
+			TransformationLog log) throws TransformationException, NoResultException {
+		// check anchor
+
+		/*
+		 * Source node that are not defined will not result in a variable being
+		 * set (if the variable is optional), thus the only way we can check if
+		 * there should be an anchor, is checking the cell.
+		 */
+		boolean hasAnchor = false;
+		ListMultimap<String, ? extends Entity> source = getCell().getSource();
+		if (source != null) {
+			hasAnchor = source.containsKey(ENTITY_ANCHOR);
+		}
+
+		if (hasAnchor) {
+			// check if anchor is present
+			List<PropertyValue> anchors = variables.get(ENTITY_ANCHOR);
+			if (anchors.isEmpty() || anchors.get(0).getValue() == null) {
+				// no or null value for anchor
+				throw new NoResultException();
+			}
+		}
+
+		// assign the value supplied as parameter
+		// conversion will be applied automatically to fit the binding
 		return getTransformedParameterChecked(PARAMETER_VALUE);
 	}
 
