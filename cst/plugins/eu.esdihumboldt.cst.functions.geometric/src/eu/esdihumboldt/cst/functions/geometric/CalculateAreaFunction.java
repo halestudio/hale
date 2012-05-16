2d9139a90a39c6abbe446432381c16b189d5af23
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-05-16 13:09:35 +0200

    created new functions and their associated translators
    refs #364
    
    Change-Id: Ib5397176aefe37e6da11920c415ff01bdc5da642

---
+++ cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/CalculateAreaFunction.java
@@ -0,0 +1,27 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.geometric;
+
+/**
+ * Calculate area function constants
+ * 
+ * @author Kevin Mais
+ */
+public interface CalculateAreaFunction {
+
+	/**
+	 * the calculate area function ID
+	 */
+	public static final String ID = "eu.esdihumboldt.cst.functions.geometric.calculatearea";
+	
+}
---
+++ cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/CalculateLengthFunction.java
@@ -0,0 +1,26 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.geometric;
+
+/**
+ * Calculate length function constants
+ * @author Kevin Mais
+ */
+public interface CalculateLengthFunction {
+
+	/**
+	 * the calculate length function ID
+	 */
+	public static final String ID = "eu.esdihumboldt.cst.functions.geometric.calculatelength";
+	
+}
