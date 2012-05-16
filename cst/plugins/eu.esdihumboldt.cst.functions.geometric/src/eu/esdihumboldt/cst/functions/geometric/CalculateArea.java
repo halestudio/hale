da0dc13711eff04538dc619a2db0896670426fa1
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-05-16 13:09:35 +0200

    created new functions and their associated translators
    refs #364
    
    Change-Id: Ib5397176aefe37e6da11920c415ff01bdc5da642
    Reviewed-on: http://gerrit.esdi-community.eu/1131
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/CalculateArea.java
@@ -0,0 +1,80 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.geometric;
+
+import java.util.Map;
+
+import com.google.common.collect.ListMultimap;
+import com.vividsolutions.jts.geom.Geometry;
+
+import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
+import eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine;
+import eu.esdihumboldt.hale.common.align.transformation.function.PropertyValue;
+import eu.esdihumboldt.hale.common.align.transformation.function.TransformationException;
+import eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation;
+import eu.esdihumboldt.hale.common.align.transformation.function.impl.NoResultException;
+import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+import eu.esdihumboldt.hale.common.schema.geometry.GeometryProperty;
+
+/**
+ * Calculate area function
+ * 
+ * @author Kevin Mais
+ */
+public class CalculateArea extends
+		AbstractSingleTargetPropertyTransformation<TransformationEngine>
+		implements CalculateAreaFunction {
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation#evaluate(java.lang.String,
+	 *      eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine,
+	 *      com.google.common.collect.ListMultimap, java.lang.String,
+	 *      eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition,
+	 *      java.util.Map,
+	 *      eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog)
+	 */
+	@Override
+	protected Object evaluate(String transformationIdentifier,
+			TransformationEngine engine,
+			ListMultimap<String, PropertyValue> variables, String resultName,
+			PropertyEntityDefinition resultProperty,
+			Map<String, String> executionParameters, TransformationLog log)
+			throws TransformationException, NoResultException {
+
+		// get input geometry
+		PropertyValue input = variables.get(null).get(0);
+		Object inputValue = input.getValue();
+		if (inputValue instanceof Instance) {
+			inputValue = ((Instance) inputValue).getValue();
+		}
+
+		Geometry geom = null;
+
+		if (inputValue instanceof GeometryProperty<?>) {
+			GeometryProperty<?> geomProp = (GeometryProperty<?>) inputValue;
+			geom = geomProp.getGeometry();
+		} else if (inputValue instanceof Geometry) {
+			geom = (Geometry) inputValue;
+		}
+
+		if (geom != null) {
+			return geom.getArea();
+		} else {
+			throw new TransformationException(
+					"Geometry for calculate area could not be retrieved.");
+		}
+
+	}
+
+}
