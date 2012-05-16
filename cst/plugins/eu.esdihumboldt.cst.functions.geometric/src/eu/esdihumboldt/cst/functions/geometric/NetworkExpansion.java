b2a94958665292348c0fe2e331d3ae8d7e2da52c
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-05-16 20:05:04 +0200

    allow mathematical expressions for buffer
    ...in NetworkExpansion function.
    Also fixed propagation of the CRS to the result.
    
    refs #413
    
    Change-Id: I491fecce43a4cb0f02ae42f1ab6f8e0ea322c0dc
    Reviewed-on: http://gerrit.esdi-community.eu/1146
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/NetworkExpansion.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/NetworkExpansion.java
@@ -12,13 +12,18 @@
 
 package eu.esdihumboldt.cst.functions.geometric;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.convert.ConversionException;
+
 import com.google.common.collect.ListMultimap;
+import com.iabcinc.jmep.XExpression;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.operation.buffer.BufferBuilder;
 import com.vividsolutions.jts.operation.buffer.BufferParameters;
 
+import eu.esdihumboldt.cst.functions.numeric.MathematicalExpression;
 import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
 import eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine;
 import eu.esdihumboldt.hale.common.align.transformation.function.PropertyValue;
@@ -26,12 +31,13 @@ import eu.esdihumboldt.hale.common.align.transformation.function.TransformationE
 import eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation;
 import eu.esdihumboldt.hale.common.align.transformation.function.impl.NoResultException;
 import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
+import eu.esdihumboldt.hale.common.convert.ConversionUtil;
 import eu.esdihumboldt.hale.common.instance.geometry.DefaultGeometryProperty;
 import eu.esdihumboldt.hale.common.instance.geometry.GeometryFinder;
 import eu.esdihumboldt.hale.common.instance.helper.DepthFirstInstanceTraverser;
 import eu.esdihumboldt.hale.common.instance.helper.InstanceTraverser;
 import eu.esdihumboldt.hale.common.instance.model.Instance;
-import eu.esdihumboldt.hale.common.schema.geometry.CRSDefinition;
+import eu.esdihumboldt.hale.common.schema.geometry.GeometryProperty;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.Binding;
 
@@ -60,7 +66,23 @@ public class NetworkExpansion extends
 			throws TransformationException, NoResultException {
 		// get the buffer width parameter
 		String bufferWidthString = getParameterChecked(PARAMETER_BUFFER_WIDTH);
-		double bufferWidth = Double.parseDouble(bufferWidthString);
+		
+		double bufferWidth;
+		try {
+			// try simple number
+			bufferWidth = Double.parseDouble(bufferWidthString);
+		} catch (NumberFormatException e) {
+			// evaluate as math expression
+			List<PropertyValue> vars = variables.get(ENTITY_VARIABLE);
+			try {
+				Object result = MathematicalExpression.evaluateExpression(bufferWidthString, vars);
+				bufferWidth = ConversionUtil.getAs(result, Double.class);
+			} catch (XExpression e1) {
+				throw new TransformationException("Failed to evaluate buffer width expression.", e1);
+			} catch (ConversionException e2) {
+				throw new TransformationException("Failed to convert buffer width expression result to double.", e2);
+			}
+		}
 
 		// get input geometry
 		PropertyValue input = variables.get(null).get(0);
@@ -69,22 +91,30 @@ public class NetworkExpansion extends
 			inputValue = ((Instance) inputValue).getValue();
 		}
 
-		CRSDefinition crs = null;
-
+		// find contained geometries
 		InstanceTraverser traverser = new DepthFirstInstanceTraverser(true);
-
 		GeometryFinder geoFind = new GeometryFinder(null);
-
 		traverser.traverse(inputValue, geoFind);
 
-		Geometry old_geometry = geoFind.getGeometries().get(0).getGeometry();
+		List<GeometryProperty<?>> geometries = geoFind.getGeometries();
+		
+		GeometryProperty<?> old_geometry = null;
+		if (!geometries.isEmpty()) {
+			old_geometry = geometries.get(0);
+			
+			if (geometries.size() > 1) {
+				log.warn(log.createMessage(
+						"Multiple geometries found, but network expansion is only done on the first.",
+						null));
+			}
+		}
 
 		if (old_geometry != null) {
 			Geometry new_geometry = null;
 			BufferParameters bufferParameters = new BufferParameters();
 			bufferParameters.setEndCapStyle(CAP_STYLE);
 			BufferBuilder bb = new BufferBuilder(new BufferParameters());
-			new_geometry = bb.buffer(old_geometry, bufferWidth);
+			new_geometry = bb.buffer(old_geometry.getGeometry(), bufferWidth);
 
 			// try to yield a result compatible to the target
 			TypeDefinition targetType = resultProperty.getDefinition()
@@ -96,11 +126,12 @@ public class NetworkExpansion extends
 					&& binding.isAssignableFrom(new_geometry.getClass())) {
 				return new_geometry;
 			}
-			return new DefaultGeometryProperty<Geometry>(crs, new_geometry);
-		} else {
-			throw new TransformationException(
-					"Geometry for network expansion could not be retrieved.");
+			return new DefaultGeometryProperty<Geometry>(
+					old_geometry.getCRSDefinition(), new_geometry);
 		}
+		
+		throw new TransformationException(
+				"Geometry for network expansion could not be retrieved.");
 	}
 
 }
