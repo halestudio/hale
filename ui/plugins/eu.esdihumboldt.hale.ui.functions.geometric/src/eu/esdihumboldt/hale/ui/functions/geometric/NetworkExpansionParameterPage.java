422ff2f26b13aa41b7a476b9db7671da3c858ba9
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-05-17 12:08:22 +0200

    added parameter page for NetworkExpansion bufferWidth
    refs #413
    
    Change-Id: Ibc2f6ab05f606b425d78a792bc08c4916027d258
    Reviewed-on: http://gerrit.esdi-community.eu/1151
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.geometric/src/eu/esdihumboldt/hale/ui/functions/geometric/NetworkExpansionParameterPage.java
@@ -0,0 +1,40 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.functions.geometric;
+
+import eu.esdihumboldt.cst.functions.geometric.NetworkExpansionFunction;
+import eu.esdihumboldt.hale.ui.functions.numeric.MathExpressionParameterPage;
+
+/**
+ * Parameter page for specifying the buffer width expression.
+ * @author Simon Templer
+ */
+public class NetworkExpansionParameterPage extends MathExpressionParameterPage implements NetworkExpansionFunction {
+
+	/**
+	 * @see MathExpressionParameterPage#getParameterName()
+	 */
+	@Override
+	protected String getParameterName() {
+		return PARAMETER_BUFFER_WIDTH;
+	}
+
+	/**
+	 * @see MathExpressionParameterPage#getSourcePropertyName()
+	 */
+	@Override
+	protected String getSourcePropertyName() {
+		return ENTITY_VARIABLE;
+	}
+
+}
