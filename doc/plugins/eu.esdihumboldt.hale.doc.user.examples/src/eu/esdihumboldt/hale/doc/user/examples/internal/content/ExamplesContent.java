2e1c0edaa70509cde53f85b1b900b170e512a5f0
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-05-17 12:26:28 +0200

    added log transactions for example help generation
    ...where loading the project files is involved.
    
    refs #408
    
    Change-Id: Ib78b3a149446f8faae274f43d5e9ec7a0ba9bbe7
    Reviewed-on: http://gerrit.esdi-community.eu/1152
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- doc/plugins/eu.esdihumboldt.hale.doc.user.examples/src/eu/esdihumboldt/hale/doc/user/examples/internal/content/ExamplesContent.java
+++ doc/plugins/eu.esdihumboldt.hale.doc.user.examples/src/eu/esdihumboldt/hale/doc/user/examples/internal/content/ExamplesContent.java
@@ -21,6 +21,7 @@ import org.eclipse.help.IHelpContentProducer;
 
 import de.cs3d.util.logging.ALogger;
 import de.cs3d.util.logging.ALoggerFactory;
+import de.cs3d.util.logging.ATransaction;
 
 import eu.esdihumboldt.hale.doc.user.examples.internal.ExamplesConstants;
 import eu.esdihumboldt.hale.doc.user.examples.internal.extension.ExampleProjectExtension;
@@ -44,25 +45,30 @@ public class ExamplesContent extends AbstractVelocityContent implements Examples
 	@Override
 	public InputStream getInputStream(String pluginID, String href,
 			Locale locale) {
-		if (href.startsWith(PATH_PREFIX_PROJECT)) {
-			// references a project
-			
-			// determine the project id
-			String projectId = href.substring(PATH_PREFIX_PROJECT.length());
-			// strip everything after a ?
-			int ind = projectId.indexOf('?');
-			if (ind >= 0) {
-				projectId = projectId.substring(0, ind);
+		ATransaction trans = log.begin("Generating examples help content");
+		try {
+			if (href.startsWith(PATH_PREFIX_PROJECT)) {
+				// references a project
+				
+				// determine the project id
+				String projectId = href.substring(PATH_PREFIX_PROJECT.length());
+				// strip everything after a ?
+				int ind = projectId.indexOf('?');
+				if (ind >= 0) {
+					projectId = projectId.substring(0, ind);
+				}
+				// strip the .*htm? ending
+				if (projectId.endsWith("html") || projectId.endsWith("htm")) {
+					projectId = projectId.substring(0, projectId.lastIndexOf('.'));
+				}
+				
+				return getProjectContent(projectId);
 			}
-			// strip the .*htm? ending
-			if (projectId.endsWith("html") || projectId.endsWith("htm")) {
-				projectId = projectId.substring(0, projectId.lastIndexOf('.'));
+			else if (href.startsWith(PATH_OVERVIEW)) {
+				return getOverviewContent();
 			}
-			
-			return getProjectContent(projectId);
-		}
-		else if (href.startsWith(PATH_OVERVIEW)) {
-			return getOverviewContent();
+		} finally {
+			trans.end();
 		}
 		
 		return null;
