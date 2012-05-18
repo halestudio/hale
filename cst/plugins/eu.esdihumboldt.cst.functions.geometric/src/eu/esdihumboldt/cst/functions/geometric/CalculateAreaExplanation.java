51746c596674f9d7b283d2736bb4f900e17d20d5
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-05-18 12:51:56 +0200

    added explanation classes for the 3 new functions
    refs #364
    
    Change-Id: Ibccd1b2fee695ce39af1a64e4ce7658199a23140
    Signed-off-by: Simon Templer <simon.templer@igd.fraunhofer.de>
    Reviewed-on: http://gerrit.esdi-community.eu/1170
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>

---
+++ cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/CalculateAreaExplanation.java
@@ -0,0 +1,46 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.geometric;
+
+import java.text.MessageFormat;
+
+import eu.esdihumboldt.hale.common.align.model.Cell;
+import eu.esdihumboldt.hale.common.align.model.CellUtil;
+import eu.esdihumboldt.hale.common.align.model.Entity;
+import eu.esdihumboldt.hale.common.align.model.impl.AbstractCellExplanation;
+
+/**
+ * Explanation for CalculateArea cells
+ * 
+ * @author Kevin Mais
+ */
+public class CalculateAreaExplanation extends AbstractCellExplanation {
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.align.model.impl.AbstractCellExplanation#getExplanation(eu.esdihumboldt.hale.common.align.model.Cell, boolean)
+	 */
+	@Override
+	protected String getExplanation(Cell cell, boolean html) {
+		
+		Entity source = CellUtil.getFirstEntity(cell.getSource());
+		Entity target = CellUtil.getFirstEntity(cell.getTarget());
+		
+		if(target != null && source != null) {
+			String message = "Calculates the area of the geometry contained in the {1} property and assignes the result to the {0} property.";
+			
+			return MessageFormat.format(message, formatEntity(target, html, true), formatEntity(source, html, true));
+		}
+		return null;
+	}
+
+}
