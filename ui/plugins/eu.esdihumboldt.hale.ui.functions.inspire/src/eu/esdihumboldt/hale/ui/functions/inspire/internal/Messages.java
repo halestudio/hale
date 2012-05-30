968fc3a839af6813ae7cadf18607c72e520c2b61
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-05-30 16:10:47 +0200

    added IdentifierFunction
    refs #364
    
    Change-Id: I7add862a11b8e872ddcbba061e9a84dc0ac14c34

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.inspire/src/eu/esdihumboldt/hale/ui/functions/inspire/internal/Messages.java
@@ -0,0 +1,25 @@
+package eu.esdihumboldt.hale.ui.functions.inspire.internal;
+
+import org.eclipse.osgi.util.NLS;
+
+@SuppressWarnings("all")
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "eu.esdihumboldt.hale.ui.functions.inspire.internal.messages"; //$NON-NLS-1$
+	public static String IdentifierFunctionWizard_0;
+	public static String IdentifierFunctionWizardPage_0;
+	public static String IdentifierFunctionWizardPage_11;
+	public static String IdentifierFunctionWizardPage_14;
+	public static String IdentifierFunctionWizardPage_16;
+	public static String IdentifierFunctionWizardPage_2;
+	public static String IdentifierFunctionWizardPage_4;
+	public static String IdentifierFunctionWizardPage_5;
+	public static String IdentifierFunctionWizardPage_7;
+	public static String IdentifierFunctionWizardPage_9;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
