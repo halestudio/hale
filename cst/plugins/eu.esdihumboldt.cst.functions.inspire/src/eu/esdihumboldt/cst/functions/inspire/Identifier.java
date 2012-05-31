d84a2395b59bc2daf07542f412e1d82e98185029
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-05-31 14:14:22 +0200

    implemented first version of identifier function
    refs #364
    
    Change-Id: Id0ac9c9ead9d57ab56c5fb9ab9b49227320a2432

--- cst/plugins/eu.esdihumboldt.cst.functions.inspire/src/eu/esdihumboldt/cst/functions/inspire/Identifier.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.inspire/src/eu/esdihumboldt/cst/functions/inspire/Identifier.java
@@ -14,6 +14,8 @@ package eu.esdihumboldt.cst.functions.inspire;
 
 import java.util.Map;
 
+import javax.xml.namespace.QName;
+
 import com.google.common.collect.ListMultimap;
 
 import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
@@ -23,14 +25,18 @@ import eu.esdihumboldt.hale.common.align.transformation.function.TransformationE
 import eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation;
 import eu.esdihumboldt.hale.common.align.transformation.function.impl.NoResultException;
 import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
+import eu.esdihumboldt.hale.common.instance.model.impl.DefaultInstance;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 
 /**
  * Class for the identifier function
+ * 
  * @author Kevin Mais
- *
+ * 
  */
-public class Identifier extends AbstractSingleTargetPropertyTransformation<TransformationEngine>
-implements IdentifierFunction {
+public class Identifier extends
+		AbstractSingleTargetPropertyTransformation<TransformationEngine>
+		implements IdentifierFunction {
 
 	@Override
 	protected Object evaluate(String transformationIdentifier,
@@ -39,8 +45,48 @@ implements IdentifierFunction {
 			PropertyEntityDefinition resultProperty,
 			Map<String, String> executionParameters, TransformationLog log)
 			throws TransformationException, NoResultException {
-		// TODO Auto-generated method stub
-		return null;
+
+		// get input
+		PropertyValue input = variables.get(null).get(0);
+		// source value as string (will be written into localid)
+		String inputValue = input.getValueAs(String.class);
+
+		// get all values for the parameters set by the parameter page
+		String countryName = getParameterChecked(COUNTRY_PARAMETER_NAME);
+		String providerName = getParameterChecked(DATA_PROVIDER_PARAMETER_NAME);
+		String productName = getParameterChecked(PRODUCT_PARAMETER_NAME);
+		String version = getParameterChecked(VERSION);
+		String versionNilReason = getParameterChecked(VERSION_NIL_REASON);
+
+		// definition of the target property (inspireId in this case)
+		TypeDefinition def = resultProperty.getDefinition().getParentType();
+
+		// instance that can be changed (add property/instance as child)
+		DefaultInstance inst = new DefaultInstance(def, null);
+
+		// child instance of "inst" (Identifier in this case)
+		DefaultInstance ident = new DefaultInstance(resultProperty
+				.getDefinition().getPropertyType(), null);
+
+		// versionId instance with own value + one property
+		DefaultInstance versionId = new DefaultInstance(ident.getDefinition(), null);
+		
+		// set the child properties of "ident"
+		ident.addProperty(new QName("localid"), inputValue);
+		ident.addProperty(new QName("namespace"), countryName + ":"
+				+ providerName + ":" + productName + ":" + resultName);
+		
+		// Set value and property of "versionId"
+		versionId.setValue(version);
+		versionId.addProperty(new QName(""), versionNilReason);
+		
+		// "add versionId" to "ident"
+		ident.addProperty(new QName("versionId"), versionId);
+		
+		// add the full "ident" to "inst"
+		inst.addProperty(new QName("Identifier"), ident);
+
+		return inst;
 	}
 
 }
