6ce70748e3b6d9db80e364a4230047dc0252096f
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-05-31 13:46:36 +0200

    added identifier to product and modified oml reader to support this
    refs #364
    
    Change-Id: Ie65d02a75767d5bb4f37f51b980b3b4df085a092

--- io/plugins/eu.esdihumboldt.hale.io.oml/src/eu/esdihumboldt/hale/io/oml/OmlReader.java
+++ io/plugins/eu.esdihumboldt.hale.io.oml/src/eu/esdihumboldt/hale/io/oml/OmlReader.java
@@ -56,6 +56,7 @@ import eu.esdihumboldt.hale.io.oml.helper.ClassificationMappingTranslator;
 import eu.esdihumboldt.hale.io.oml.helper.DateExtractionTranslator;
 import eu.esdihumboldt.hale.io.oml.helper.FormattedStringTranslator;
 import eu.esdihumboldt.hale.io.oml.helper.FunctionTranslator;
+import eu.esdihumboldt.hale.io.oml.helper.IdentifierTranslator;
 import eu.esdihumboldt.hale.io.oml.helper.MathematicalExpressionTranslator;
 import eu.esdihumboldt.hale.io.oml.helper.NetworkExpansionTranslator;
 import eu.esdihumboldt.hale.io.oml.helper.NotSupportedTranslator;
@@ -118,8 +119,8 @@ public class OmlReader extends AbstractAlignmentReader implements
 				new NotSupportedTranslator());
 		map.put("eu.esdihumboldt.cst.corefunctions.GeographicalNameFunction",
 				new NotSupportedTranslator());
-		map.put("eu.esdihumboldt.cst.corefunctions.IdentifierFunction",
-				new NotSupportedTranslator());
+		map.put("eu.esdihumboldt.cst.corefunctions.inspire.IdentifierFunction",
+				new IdentifierTranslator());
 	}
 
 	/**
