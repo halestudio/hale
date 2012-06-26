bb143ef85ff3fd91df2775b156a3e9c99e3647c5
Kai Schwierczek <kai@schwierczek.de>
2012-06-26 15:43:23 +0200

    show instance validation results in data views
    refs #410
    
    Change-Id: I71346e461ead480617e855311cd8605ee2ebc32f

--- ui/plugins/eu.esdihumboldt.hale.ui.common/src/eu/esdihumboldt/hale/ui/common/definition/viewer/TypeDefinitionContentProvider.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.common/src/eu/esdihumboldt/hale/ui/common/definition/viewer/TypeDefinitionContentProvider.java
@@ -12,14 +12,16 @@
 
 package eu.esdihumboldt.hale.ui.common.definition.viewer;
 
+import java.util.Collection;
+
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 
 /**
- * Content provider for a tree representing the structure of a 
- * {@link TypeDefinition}. Requires a {@link TypeDefinition} as input. 
+ * Content provider for a tree representing the structure of 
+ * {@link TypeDefinition}s. Requires a {@link Collection} of {@link TypeDefinition} as input. 
  * @author Simon Templer
  */
 public class TypeDefinitionContentProvider extends TypeIndexContentProvider {
@@ -36,12 +38,10 @@ public class TypeDefinitionContentProvider extends TypeIndexContentProvider {
 	 */
 	@Override
 	public Object[] getElements(Object inputElement) {
-		if (inputElement instanceof TypeDefinition) {
-			return ((TypeDefinition) inputElement).getChildren().toArray();
-		}
-		else {
-			throw new IllegalArgumentException("Content provider only applicable for type definitions.");
-		}
+		if (inputElement instanceof Collection<?>)
+			return ((Collection<?>) inputElement).toArray();
+		else
+			throw new IllegalArgumentException("Content provider only applicable for a collection of type definitions.");
 	}
 
 }
