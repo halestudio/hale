7c5443c9074b90ab42662624f6cd8e30cfb17bc4
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-06-27 11:31:44 +0200

    fixed ui inspire identifier function
    refs #364
    
    Change-Id: I813b329a9b43ebfda92b3fab571405d5cc8fc2f0

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.inspire/src/eu/esdihumboldt/hale/ui/functions/inspire/internal/Messages.java
@@ -0,0 +1,41 @@
+package eu.esdihumboldt.hale.ui.functions.inspire.internal;
+
+import org.eclipse.osgi.util.NLS;
+
+@SuppressWarnings("all")
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "eu.esdihumboldt.hale.ui.functions.inspire.internal.messages"; //$NON-NLS-1$
+	public static String GeographicNameFunctionWizard_1;
+	public static String GeographicNameFunctionWizard_2;
+	public static String GeographicNamePage_0;
+	public static String GeographicNamePage_10;
+	public static String GeographicNamePage_11;
+	public static String GeographicNamePage_12;
+	public static String GeographicNamePage_13;
+	public static String GeographicNamePage_14;
+	public static String GeographicNamePage_15;
+	public static String GeographicNamePage_16;
+	public static String GeographicNamePage_3;
+	public static String GeographicNamePage_5;
+	public static String GeographicNamePage_6;
+	public static String GeographicNamePage_7;
+	public static String GeographicNamePage_8;
+	public static String GeographicNamePage_9;
+	public static String IdentifierFunctionWizard_0;
+	public static String IdentifierFunctionWizardPage_0;
+	public static String IdentifierFunctionWizardPage_11;
+	public static String IdentifierFunctionWizardPage_14;
+	public static String IdentifierFunctionWizardPage_16;
+	public static String IdentifierFunctionWizardPage_2;
+	public static String IdentifierFunctionWizardPage_4;
+	public static String IdentifierFunctionWizardPage_5;
+	public static String IdentifierFunctionWizardPage_7;
+	public static String IdentifierFunctionWizardPage_9;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
