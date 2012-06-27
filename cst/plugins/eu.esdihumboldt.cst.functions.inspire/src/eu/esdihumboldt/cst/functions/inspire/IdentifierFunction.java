016281c8373f0d070a9342ca66d9bd2115c30d81
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-06-27 10:25:17 +0200

    fixed some non-functional code
    refs #364
    
    Change-Id: Id01e2bf10cbfc67e50f778469c19cd7bd5b4bc82

---
+++ cst/plugins/eu.esdihumboldt.cst.functions.inspire/src/eu/esdihumboldt/cst/functions/inspire/IdentifierFunction.java
@@ -0,0 +1,33 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.inspire;
+/**
+ * Constants for the identifier function
+ * 
+ * @author Kevin Mais
+ *
+ */
+@SuppressWarnings("javadoc")
+public interface IdentifierFunction {
+
+	public static final String ID = "eu.esdihumboldt.cst.functions.inspire.identifier";
+	
+	public static final String COUNTRY_PARAMETER_NAME = "countryName"; //$NON-NLS-1$
+	public static final String DATA_PROVIDER_PARAMETER_NAME = "providerName"; //$NON-NLS-1$
+	public static final String PRODUCT_PARAMETER_NAME = "productName"; //$NON-NLS-1$
+	public static final String VERSION = "version"; //$NON-NLS-1$
+	public static final String VERSION_NIL_REASON = "versionNilReason"; //$NON-NLS-1$
+	
+	public static final String INSPIRE_IDENTIFIER_PREFIX = "urn:x-inspire:object:id"; //$NON-NLS-1$
+	
+}
