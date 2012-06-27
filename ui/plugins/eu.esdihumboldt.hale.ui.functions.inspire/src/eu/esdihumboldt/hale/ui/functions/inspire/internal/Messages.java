8c493f8fb8ab553c9bd223a31f5c481c0dc19d1c
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-06-27 10:25:59 +0200

    added geographical name messages
    refs #364
    
    Change-Id: I69935d0ac0f206ba9989b6ca12a17e8adb1c6bee

--- ui/plugins/eu.esdihumboldt.hale.ui.functions.inspire/src/eu/esdihumboldt/hale/ui/functions/inspire/internal/Messages.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.inspire/src/eu/esdihumboldt/hale/ui/functions/inspire/internal/Messages.java
@@ -5,6 +5,22 @@ import org.eclipse.osgi.util.NLS;
 @SuppressWarnings("all")
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "eu.esdihumboldt.hale.ui.functions.inspire.internal.messages"; //$NON-NLS-1$
+	public static String GeographicNameFunctionWizard_1;
+	public static String GeographicNameFunctionWizard_2;
+	public static String GeographicNamePage_0;
+	public static String GeographicNamePage_10;
+	public static String GeographicNamePage_11;
+	public static String GeographicNamePage_12;
+	public static String GeographicNamePage_13;
+	public static String GeographicNamePage_14;
+	public static String GeographicNamePage_15;
+	public static String GeographicNamePage_16;
+	public static String GeographicNamePage_3;
+	public static String GeographicNamePage_5;
+	public static String GeographicNamePage_6;
+	public static String GeographicNamePage_7;
+	public static String GeographicNamePage_8;
+	public static String GeographicNamePage_9;
 	public static String IdentifierFunctionWizard_0;
 	public static String IdentifierFunctionWizardPage_0;
 	public static String IdentifierFunctionWizardPage_11;
