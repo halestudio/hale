a9917d5dd38222eec55e2f7bd411e0d65c24fcd0
Kevin Mais <kevin.mais@igd.fraunhofer.de>
2012-07-04 12:42:37 +0200

    created an interface with constants for GeoNamePage
    refs #364
    
    Change-Id: I93188fd97cae90478630634221073d825474b2ce

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.inspire/src/eu/esdihumboldt/hale/ui/functions/inspire/internal/GeographicalNamePageConstants.java
@@ -0,0 +1,57 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.functions.inspire.internal;
+
+
+/**
+ * Constants for the geographical name page
+ * 
+ * @author Kevin Mais
+ */
+@SuppressWarnings("javadoc")
+public interface GeographicalNamePageConstants {
+
+	public final static String SOURCE_OF_NAME_PROMT = Messages.GeographicNamePage_0;
+
+	public static final String GENDER_NULL_VAUE_MENU_ENTRY = ""; //$NON-NLS-1$
+
+	public static final String NUMBER_NULL_VALUE_MENU_ENTRY = ""; //$NON-NLS-1$
+
+	public static final String LANGUAGE_LABEL_TEXT = Messages.GeographicNamePage_3;
+
+	public static final String ISO_CODE_ENG = "eng"; //$NON-NLS-1$
+
+	public static final String SOURCE_OF_NAME_LABEL_TEXT = Messages.GeographicNamePage_5;
+
+	public static final String NAME_STATUS_LABEL_TEXT = Messages.GeographicNamePage_6;
+
+	public static final String NATIVENESS_LABEL_TEXT = Messages.GeographicNamePage_7;
+
+	public static final String GRAMMATICAL_GENDER_LABEL_TEXT = Messages.GeographicNamePage_8;
+
+	public static final String GRAMMATICAL_NUMBER_LABEL_TEXT = Messages.GeographicNamePage_9;
+
+	public static final String PRONOUNCIATION_GRPOUP_TEXT = Messages.GeographicNamePage_10;
+
+	public static final String SOUNDLINK_LABEL_TEXT = Messages.GeographicNamePage_11;
+
+	public static final String IPA_LABEL_TEXT = Messages.GeographicNamePage_12;
+
+	public static final String SPELLING_GROUP_TEXT = Messages.GeographicNamePage_13;
+
+	public static final String SPELLING_TEXT_LABEL_TEXT = Messages.GeographicNamePage_14;
+
+	public static final String SCRIPT_LABEL_TEXT = Messages.GeographicNamePage_15;
+
+	public static final String TRANSLITERATION_LABEL_TEXT = Messages.GeographicNamePage_16;
+}
