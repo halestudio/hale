e63f3f66a6ba6f864d8155cac4891cfcc5d544a6
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-08-01 17:07:13 +0200

    added parameter editor extension point
    Change-Id: I2c414705f0ead87efc95d5d1b6831e9e648ca07d

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.common/src/eu/esdihumboldt/hale/ui/common/EditorFactory.java
@@ -0,0 +1,32 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.common;
+
+import org.eclipse.swt.widgets.Composite;
+
+import eu.esdihumboldt.hale.common.schema.model.Definition;
+
+/**
+ * Factory for attribute editors not coupled to {@link Definition}s.
+ * @author Simon Templer
+ */
+public interface EditorFactory {
+	
+	/**
+	 * Create an editor.
+	 * @param parent the parent composite
+	 * @return the created editor
+	 */
+	public Editor<?> createEditor(Composite parent);
+
+}
---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/function/extension/ParameterEditorFactory.java
@@ -0,0 +1,46 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.function.extension;
+
+import de.cs3d.util.eclipse.extension.ExtensionObjectFactory;
+import eu.esdihumboldt.hale.common.align.extension.function.FunctionParameter;
+import eu.esdihumboldt.hale.ui.common.EditorFactory;
+
+/**
+ * Factory for parameter editors.
+ * 
+ * @author Simon Templer
+ */
+public interface ParameterEditorFactory extends ExtensionObjectFactory<EditorFactory> {
+	
+	/**
+	 * Get the ID of the associated function.
+	 * 
+	 * @return the function ID
+	 */
+	public String getFunctionId();
+	
+	/**
+	 * Get the name of the associated parameter.
+	 * 
+	 * @return the name of the parameter the editor is associated with
+	 */
+	public String getParameterName();
+
+	/**
+	 * Get the associated parameter.
+	 * 
+	 * @return the associated parameter
+	 */
+	public FunctionParameter getAssociatedParameter();
+}
