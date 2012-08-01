740ea94a75541c009c63d65dc75fd82677d5ea33
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-08-01 10:11:10 +0200

    added helper method for getting a single optional parameter
    ...to AbstractTransformationFunction. Use in Rename.
    
    Change-Id: I41771577b094620a15997841baa3f41f46750e64

--- cst/plugins/eu.esdihumboldt.cst.functions.core/src/eu/esdihumboldt/cst/functions/core/Rename.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.core/src/eu/esdihumboldt/cst/functions/core/Rename.java
@@ -60,10 +60,7 @@ public class Rename extends AbstractSingleTargetPropertyTransformation<Transform
 		// get the source value
 		Object sourceValue = variables.values().iterator().next().getValue();
 
-		String structuralRename = "false";
-		if (getParameters() != null && !getParameters().get(PARAMETER_STRUCTURAL_RENAME).isEmpty()) {
-			structuralRename = getParameters().get(PARAMETER_STRUCTURAL_RENAME).get(0);
-		}
+		String structuralRename = getOptionalParameter(PARAMETER_STRUCTURAL_RENAME, "false");
 		boolean structuralRenameEnabled = Boolean.parseBoolean(structuralRename);
 
 		// not a group? just return value.
