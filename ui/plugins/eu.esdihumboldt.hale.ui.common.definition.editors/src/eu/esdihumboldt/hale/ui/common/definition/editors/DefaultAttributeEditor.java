5e28714bada04c5e5436bf5ebb04a0e8fbbf420c
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-08-01 17:12:37 +0200

    renamed common editors bundle
    Change-Id: Ic30b975523526badc3b885a3c8765fb15c14dd58

--- ui/plugins/eu.esdihumboldt.hale.ui.common.definition.editors/src/eu/esdihumboldt/hale/ui/common/definition/editors/DefaultAttributeEditor.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.common.definition.editors/src/eu/esdihumboldt/hale/ui/common/definition/editors/DefaultAttributeEditor.java
@@ -10,7 +10,7 @@
  * (c) the HUMBOLDT Consortium, 2007 to 2011.
  */
 
-package eu.esdihumboldt.hale.ui.common.editors;
+package eu.esdihumboldt.hale.ui.common.definition.editors;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
