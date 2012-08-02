7dcc07a7dbbe5fed36621e422a66ef65421c08a5
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-08-02 18:25:57 +0200

    added command to open mapping view
    Also improved mapping view behavior.
    
    refs #442
    
    Change-Id: Ia8ee16d01698bc32e79aa724c2453ea4ad971dda

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.mapping/src/eu/esdihumboldt/hale/ui/views/mapping/handler/OpenMappingViewHandler.java
@@ -0,0 +1,48 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.views.mapping.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import de.cs3d.util.logging.ALogger;
+import de.cs3d.util.logging.ALoggerFactory;
+import eu.esdihumboldt.hale.ui.views.mapping.MappingView;
+
+/**
+ * Shows the mapping view
+ * @author Simon Templer
+ */
+public class OpenMappingViewHandler extends AbstractHandler {
+	
+	private static final ALogger log = ALoggerFactory.getLogger(OpenMappingViewHandler.class);
+
+	/**
+	 * @see IHandler#execute(ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage()
+					.showView(MappingView.ID);
+		} catch (PartInitException e) {
+			log.error("Error opening mapping view", e);
+		}
+		return null;
+	}
+
+}
