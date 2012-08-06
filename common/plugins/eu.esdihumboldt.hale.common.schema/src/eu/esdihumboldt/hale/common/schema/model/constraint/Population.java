682f11755663edbc76848922ae3533db99f0bdca
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-08-06 11:47:16 +0200

    added Population constraint
    Change-Id: I5615f44b1d0726a77f6fc91cbc83820389342d74

---
+++ common/plugins/eu.esdihumboldt.hale.common.schema/src/eu/esdihumboldt/hale/common/schema/model/constraint/Population.java
@@ -0,0 +1,69 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.schema.model.constraint;
+
+import eu.esdihumboldt.hale.common.schema.model.Constraint;
+import eu.esdihumboldt.hale.common.schema.model.GroupPropertyConstraint;
+import eu.esdihumboldt.hale.common.schema.model.PropertyConstraint;
+import eu.esdihumboldt.hale.common.schema.model.TypeConstraint;
+
+/**
+ * Constraint that represents the population of a definition, e.g. how many
+ * instances of a type are there and how many overall occurrences of a property
+ * are there. 
+ * @author Simon Templer
+ */
+@Constraint(mutable = true)
+public class Population implements PropertyConstraint, TypeConstraint,
+		GroupPropertyConstraint {
+	
+	private int count = 0;
+
+	/**
+	 * Create a population constraint initialized with a zero population. 
+	 */
+	public Population() {
+		super();
+	}
+
+	/**
+	 * Get the population count.
+	 * @return the population count
+	 */
+	public int getCount() {
+		return count;
+	}
+	
+	/**
+	 * Reset the population count to zero.
+	 */
+	public void reset() {
+		count = 0;
+	}
+	
+	/**
+	 * Increase the population count by one.
+	 */
+	public void increase() {
+		count++;
+	}
+
+	/**
+	 * @see TypeConstraint#isInheritable()
+	 */
+	@Override
+	public boolean isInheritable() {
+		return false;
+	}
+
+}
