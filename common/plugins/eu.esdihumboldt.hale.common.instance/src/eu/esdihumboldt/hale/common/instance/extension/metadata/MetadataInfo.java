a35e484f75028a25bf82cb7ed86779f9f8a208df
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2012-08-14 18:42:59 +0200

    implemented extension point and schema for meta datas
    Change-Id: Ibbe69d080a82d611ffc8e25ebfe02e53bf9b2753

---
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/extension/metadata/MetadataInfo.java
@@ -0,0 +1,68 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.instance.extension.metadata;
+
+
+import org.eclipse.core.runtime.IConfigurationElement;
+
+import de.cs3d.util.eclipse.extension.simple.IdentifiableExtension.Identifiable;
+
+/**
+ * Represents a declared Metadata Info
+ * @author Sebastian Reinhardt
+ */
+public class MetadataInfo  implements Identifiable {
+
+	private final String key;
+	private final String label;
+	private final String description;
+	
+	
+	/**
+	 * Create a metadata object from a configuration element.
+	 * @param key the data key
+	 * @param conf the configuration element
+	 */
+	public MetadataInfo(String key, IConfigurationElement conf){
+		super();
+		
+		this.key = key;
+		this.label = conf.getAttribute("label");
+		this.description = conf.getAttribute("description");
+	}
+		
+		
+	/**
+	 * @see de.cs3d.util.eclipse.extension.simple.IdentifiableExtension.Identifiable#getId()
+	 */
+	@Override
+	public String getId() {
+		return key;
+	}
+
+
+	/**
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label;
+	}
+
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+}
