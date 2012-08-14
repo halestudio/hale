763bc795bb95946ceb6e860bcd842315c6e8309b
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2012-08-14 18:47:29 +0200

    added interface for metadata generators and implemented an uniqueID generator
    Change-Id: If476f07f8be6771ea1de63225258492fdb4cc4b3

---
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/extension/metadata/MetadataGenerator.java
@@ -0,0 +1,30 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.common.instance.extension.metadata;
+
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+
+/**
+ * Generator Interface for Instance Metadatas
+ * @author Sebastan Reinhardt
+ */
+public interface MetadataGenerator {
+
+	/**
+	 * generates certain metadata values for an instance
+	 * @param inst the instance
+	 * @return an array of metadata values
+	 */
+	public Object[] generate(Instance inst);
+	
+}
