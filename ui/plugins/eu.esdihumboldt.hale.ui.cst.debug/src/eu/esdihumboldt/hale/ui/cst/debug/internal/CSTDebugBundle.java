d777d78b7004a99bd315332c7b81a9c8fd5e615d
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-08-15 15:34:49 +0200

    added transformation tree metadata hook stub
    refs #460
    
    Change-Id: Ic3d66c82f967653839f4ee95c2ad2b89ba99dd15

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.cst.debug/src/eu/esdihumboldt/hale/ui/cst/debug/internal/CSTDebugBundle.java
@@ -0,0 +1,66 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ */
+
+package eu.esdihumboldt.hale.ui.cst.debug.internal;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class CSTDebugBundle extends AbstractUIPlugin {
+
+	/**
+	 * The plug-in ID
+	 */
+	public static final String PLUGIN_ID = "eu.esdihumboldt.hale.ui.cst.debug"; //$NON-NLS-1$
+
+	// The shared instance
+	private static CSTDebugBundle plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public CSTDebugBundle() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static CSTDebugBundle getDefault() {
+		return plugin;
+	}
+
+}
