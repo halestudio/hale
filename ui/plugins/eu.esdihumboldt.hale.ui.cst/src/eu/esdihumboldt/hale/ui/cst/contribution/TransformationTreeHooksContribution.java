607b35908815d0fc2024fb7fecd5bdcb17a09371
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-08-15 09:29:21 +0200

    added transformation tree hooks contribution to menu
    refs #458
    
    Change-Id: Ibda5ed4a740ea3421936896c6d6f5b65946db602
    Reviewed-on: http://gerrit.esdi-community.eu/1366
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.cst/src/eu/esdihumboldt/hale/ui/cst/contribution/TransformationTreeHooksContribution.java
@@ -0,0 +1,60 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.hale.ui.cst.contribution;
+
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+
+import de.cs3d.ui.util.eclipse.extension.AbstractExtensionContribution;
+import de.cs3d.ui.util.eclipse.extension.selective.SelectiveExtensionContribution;
+import de.cs3d.util.eclipse.extension.selective.SelectiveExtension;
+import de.fhg.igd.osgi.util.OsgiUtils;
+import eu.esdihumboldt.cst.extension.hooks.TransformationTreeHook;
+import eu.esdihumboldt.cst.extension.hooks.TransformationTreeHookFactory;
+import eu.esdihumboldt.cst.extension.hooks.TransformationTreeHooks;
+
+/**
+ * Contribution for transformation tree hooks.
+ * @author Simon Templer
+ */
+public class TransformationTreeHooksContribution
+		extends
+		SelectiveExtensionContribution<TransformationTreeHook, TransformationTreeHookFactory> {
+
+	/**
+	 * @see AbstractExtensionContribution#initExtension()
+	 */
+	@Override
+	protected SelectiveExtension<TransformationTreeHook, TransformationTreeHookFactory> initExtension() {
+		return OsgiUtils.getService(TransformationTreeHooks.class);
+	}
+
+	/**
+	 * @see AbstractExtensionContribution#fillWithFactories(Menu, List, int)
+	 */
+	@Override
+	protected int fillWithFactories(Menu parent,
+			List<TransformationTreeHookFactory> factories, int index) {
+		if (factories.isEmpty()) {
+			// add a dummy item
+			MenuItem item = new MenuItem(parent, SWT.PUSH, index++);
+			item.setEnabled(false);
+			item.setText("None available");
+		}
+		return super.fillWithFactories(parent, factories, index);
+	}
+
+}
