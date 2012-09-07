a21183631bcd83fc8463481d79599c2c0a60c7f8
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2012-09-07 11:48:23 +0200

    implemented action on metadata entries
    Change-Id: I30178a56ab5477bc319efb9337ad227be84a9aeb

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.data/src/eu/esdihumboldt/hale/ui/views/data/internal/explore/MetadataExploreActionProvider.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     HUMBOLDT EU Integrated Project #030962
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.views.data.internal.explore;
+
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.ViewerCell;
+
+import eu.esdihumboldt.hale.ui.views.data.internal.MetadataActionProvider;
+import eu.esdihumboldt.util.Pair;
+
+/**
+ * Provides metadata actions for {@link TreeViewer}s used for data views explore
+ * mode
+ * 
+ * @author Sebastian Reinhardt
+ */
+public class MetadataExploreActionProvider extends MetadataActionProvider {
+
+	/**
+	 * Standard Constructor
+	 * 
+	 * @param treeViewer the TreeViewer to which the action will be applied
+	 */
+	public MetadataExploreActionProvider(TreeViewer treeViewer) {
+		super(treeViewer);
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.views.data.internal.MetadataActionProvider#retrieveMetadata(org.eclipse.jface.viewers.ViewerCell)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	protected Pair<Object, Object> retrieveMetadata(ViewerCell cell) {
+		return (Pair<Object, Object>) cell.getElement();
+
+	}
+
+}
