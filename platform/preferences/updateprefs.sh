14f605f49764d47f433ec578eff4774f737f9413
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-09-10 13:33:32 +0200

    added linux update preferences script
    Change-Id: Ib5b9976035d2fff5187c266e24560f446c79833c

---
+++ platform/preferences/updateprefs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+java -jar proprefapp.jar common ../../common
+java -jar proprefapp.jar common ../../cst
+java -jar proprefapp.jar common ../../io
+java -jar proprefapp.jar common ../../server
+java -jar proprefapp.jar common ../../doc
+java -jar proprefapp.jar common ../../ui
+java -jar proprefapp.jar common ../../util
+java -jar proprefapp.jar common ../../ext/styledmap
+java -jar proprefapp.jar mdl ../../ext/mdl eu.xsdi.mdl eu.xsdi.mdlui
