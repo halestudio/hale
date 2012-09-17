dd57ee37fa6dd5d3581f23edca16fc051e82ec66
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-09-17 16:46:31 +0200

    added first version of sequential ID function
    refs #472
    
    Change-Id: I883bdb610cc69d558f994d7099194555348f63da

---
+++ cst/plugins/eu.esdihumboldt.cst.functions.numeric/src/eu/esdihumboldt/cst/functions/numeric/sequentialid/SequentialID.java
@@ -0,0 +1,92 @@
+/*
+ * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
+ * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * 
+ * For more information on the project, please refer to the this web site:
+ * http://www.esdi-humboldt.eu
+ * 
+ * LICENSE: For information on the license under which this program is 
+ * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
+ * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ */
+
+package eu.esdihumboldt.cst.functions.numeric.sequentialid;
+
+import java.util.Collections;
+import java.util.Map;
+
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.SetMultimap;
+
+import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
+import eu.esdihumboldt.hale.common.align.transformation.engine.TransformationEngine;
+import eu.esdihumboldt.hale.common.align.transformation.function.PropertyValue;
+import eu.esdihumboldt.hale.common.align.transformation.function.TransformationException;
+import eu.esdihumboldt.hale.common.align.transformation.function.impl.AbstractSingleTargetPropertyTransformation;
+import eu.esdihumboldt.hale.common.align.transformation.function.impl.NoResultException;
+import eu.esdihumboldt.hale.common.align.transformation.report.TransformationLog;
+
+/**
+ * Function that generates a sequential identifier.
+ * 
+ * @author Simon Templer
+ */
+public class SequentialID extends AbstractSingleTargetPropertyTransformation<TransformationEngine>
+		implements SequentialIDConstants {
+
+	/**
+	 * @see AbstractSingleTargetPropertyTransformation#evaluate(String,
+	 *      TransformationEngine, ListMultimap, String,
+	 *      PropertyEntityDefinition, Map, TransformationLog)
+	 */
+	@Override
+	protected Object evaluate(String transformationIdentifier, TransformationEngine engine,
+			ListMultimap<String, PropertyValue> variables, String resultName,
+			PropertyEntityDefinition resultProperty, Map<String, String> executionParameters,
+			TransformationLog log) throws TransformationException, NoResultException {
+		// get parameter values
+
+		String prefix = getOptionalParameter(PARAM_PREFIX, "");
+		String suffix = getOptionalParameter(PARAM_SUFFIX, "");
+		// assume type as default for sequence
+		String sequenceStr = getOptionalParameter(PARAM_SEQUENCE, Sequence.type.name());
+
+		// select appropriate context and key
+		Sequence sequence = Sequence.valueOf(sequenceStr);
+		SetMultimap<Object, Object> context;
+		String key;
+		switch (sequence) {
+		case overall:
+			// use function context
+			context = getExecutionContext().getFunctionContext();
+			key = PARAM_SEQUENCE;
+			break;
+		case type:
+		default:
+			// use cell context
+			context = getExecutionContext().getCellContext();
+			key = getTargetType().getName().toString();
+			break;
+		}
+
+		long id;
+		synchronized (context) {
+			if (context.containsKey(key)) {
+				Long seqValue = (Long) context.get(key).iterator().next();
+				id = seqValue++;
+				context.replaceValues(key, Collections.singleton(id));
+			}
+			else {
+				id = START_VALUE;
+				context.put(key, id);
+			}
+		}
+
+		if (prefix.isEmpty() && suffix.isEmpty()) {
+			return id;
+		}
+		else {
+			return prefix + id + suffix;
+		}
+	}
+}
