43656602fb48ba131e4805c61450f70656b6ace6
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-09-18 14:20:46 +0200

    updated file headers to be consistent
    Main file header now reflects the transfer of rights from
    the HUMBOLDT project consortium to the Data Harmonisation
    Panel.
    Exceptions from the main file header:
    - Adapted source files originating from third party libraries
    - Thorsten's MDL projects
    
    Change-Id: I8bedc7700b35affc3077c0ef2eef0a3e780dafdc

--- io/plugins/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/reader/internal/HumboldtURIResolver.java
+++ io/plugins/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/reader/internal/HumboldtURIResolver.java
@@ -1,43 +1,20 @@
 /*
- * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
- * EU Integrated Project #030962                  01.10.2006 - 30.09.2010
+ * Copyright (c) 2012 Data Harmonisation Panel
  * 
- * For more information on the project, please refer to this website:
- * http://www.esdi-humboldt.eu
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  * 
- * LICENSE: For information on the license under which this program is 
- * available, please refer to : http:/www.esdi-humboldt.eu/license.html#core
- * (c) the HUMBOLDT Consortium, 2007 to 2010.
- *
- * Componet     : HALE
- * 	 
- * Classname    : eu.esdihumboldt.hale.models.impl/HumboldtURIResolver.java 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
  * 
- * Author       : schneidersb
- * 
- * Created on   : May 11, 2009 -- 3:14:38 PM
- *
+ * Contributors:
+ *     HUMBOLDT EU Integrated Project #030962
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
  */
 package eu.esdihumboldt.hale.io.xsd.reader.internal;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 import java.io.File;
 import java.io.InputStream;
 import java.net.URI;
@@ -56,6 +33,9 @@ import eu.esdihumboldt.util.resource.Resources;
  * This resolver provides the means of resolving the imports and includes of a
  * given schema document. The system will call this default resolver if there is
  * no other resolver present in the system.
+ * 
+ * @author Bernd Schneiders
+ * @author Simon Templer
  */
 public class HumboldtURIResolver implements CollectionURIResolver {
 
@@ -151,89 +131,6 @@ public class HumboldtURIResolver implements CollectionURIResolver {
 	protected boolean isAbsolute(String uri) {
 		return uri.startsWith("http://"); //$NON-NLS-1$
 	}
-
-//    /**
-//     * This is essentially a call to "new URL(contextURL, spec)"
-//     * with extra handling in case spec is
-//     * a file.
-//     *
-//     * @param contextURL
-//     * @param spec
-//     * @return the URL
-//     * @throws java.io.IOException
-//     */
-//    protected URL getURL(URL contextURL, String spec) throws IOException {
-//        // First, fix the slashes as windows filenames may have backslashes
-//        // in them, but the URL class wont do the right thing when we later
-//        // process this URL as the contextURL.
-//        String path = spec.replace('\\', '/');
-//
-//        // See if we have a good URL.
-//        URL url;
-//
-//        try {
-//
-//            // first, try to treat spec as a full URL
-//            url = new URL(contextURL, path);
-//
-//            // if we are deail with files in both cases, create a url
-//            // by using the directory of the context URL.
-//            if ((contextURL != null) && url.getProtocol().equals("file")
-//                    && contextURL.getProtocol().equals("file")) {
-//                url = getFileURL(contextURL, path);
-//            }
-//        } catch (MalformedURLException me) {
-//
-//            // try treating is as a file pathname
-//            url = getFileURL(contextURL, path);
-//        }
-//
-//        // Everything is OK with this URL, although a file url constructed
-//        // above may not exist.  This will be caught later when the URL is
-//        // accessed.
-//        return url;
-//    }    // getURL
-//
-//    /**
-//     * Method getFileURL
-//     *
-//     * @param contextURL
-//     * @param path
-//     * @return the URL
-//     * @throws IOException
-//     */
-//    protected URL getFileURL(URL contextURL, String path)
-//            throws IOException {
-//
-//        if (contextURL != null) {
-//
-//            // get the parent directory of the contextURL, and append
-//            // the spec string to the end.
-//            String contextFileName = contextURL.getFile();
-//            URL parent = null;
-//            //the logic for finding the parent file is this.
-//            //1.if the contextURI represents a file then take the parent file
-//            //of it
-//            //2. If the contextURI represents a directory, then take that as
-//            //the parent
-//            File parentFile;
-//            File contextFile = new File(contextFileName);
-//            if (contextFile.isDirectory()){
-//                parentFile = contextFile;
-//            }else{
-//                parentFile = contextFile.getParentFile();
-//            }
-//
-//            if (parentFile != null) {
-//            	parent = parentFile.toURI().toURL();
-//            }
-//            if (parent != null) {
-//                return new URL(parent, path);
-//            }
-//        }
-//
-//        return new URL("file", "", path);
-//    }    // getFileURL
 
 	/**
 	 * @see CollectionURIResolver#getCollectionBaseURI()
--- ui/plugins/eu.esdihumboldt.hale.ui.util/src/eu/esdihumboldt/hale/ui/util/viewer/tree/TreePathFilteredTree.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.util/src/eu/esdihumboldt/hale/ui/util/viewer/tree/TreePathFilteredTree.java
@@ -1,13 +1,17 @@
 /*
- * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
- * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * Copyright (c) 2012 Data Harmonisation Panel
  * 
- * For more information on the project, please refer to the this web site:
- * http://www.esdi-humboldt.eu
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  * 
- * LICENSE: For information on the license under which this program is 
- * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
- * (c) the HUMBOLDT Consortium, 2007 to 2011.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     HUMBOLDT EU Integrated Project #030962
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
  */
 
 package eu.esdihumboldt.hale.ui.util.viewer.tree;
