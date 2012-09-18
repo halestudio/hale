1e3dcbf522b089a9e84289a08d2343272313064e
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-09-18 10:48:09 +0200

    added example validation to sequential id param page
    ...to allow verifying in the UI if the generated ID
    will be valid for the target property.
    
    refs #472
    
    Change-Id: Ic5441fee453c9ffb090244844865c148f5901455

--- ui/plugins/eu.esdihumboldt.hale.ui.functions.numeric/src/eu/esdihumboldt/hale/ui/functions/numeric/SequentialIDParameterPage.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.numeric/src/eu/esdihumboldt/hale/ui/functions/numeric/SequentialIDParameterPage.java
@@ -12,14 +12,24 @@
 
 package eu.esdihumboldt.hale.ui.functions.numeric;
 
+import java.util.List;
+
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
@@ -29,9 +39,16 @@ import com.google.common.collect.ListMultimap;
 
 import eu.esdihumboldt.cst.functions.numeric.sequentialid.SequentialIDConstants;
 import eu.esdihumboldt.hale.common.align.extension.function.PropertyFunctionExtension;
+import eu.esdihumboldt.hale.common.align.model.Cell;
+import eu.esdihumboldt.hale.common.align.model.Entity;
+import eu.esdihumboldt.hale.common.schema.model.Definition;
+import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.common.schema.model.constraint.type.ValidationConstraint;
 import eu.esdihumboldt.hale.ui.HaleWizardPage;
 import eu.esdihumboldt.hale.ui.function.generic.pages.AbstractParameterPage;
 import eu.esdihumboldt.hale.ui.util.viewer.EnumContentProvider;
+import eu.esdihumboldt.util.validator.Validator;
 
 /**
  * Parameter page for the sequential ID function.
@@ -46,6 +63,10 @@ public class SequentialIDParameterPage extends AbstractParameterPage implements
 	private Text prefix;
 
 	private Text suffix;
+
+	private Label example;
+
+	private ControlDecoration exampleDecoration;
 
 	/**
 	 * Default constructor.
@@ -86,7 +107,7 @@ public class SequentialIDParameterPage extends AbstractParameterPage implements
 	 */
 	@Override
 	protected void createContent(Composite page) {
-		page.setLayout(GridLayoutFactory.swtDefaults().numColumns(2).create());
+		page.setLayout(GridLayoutFactory.swtDefaults().numColumns(2).spacing(10, 8).create());
 
 		Label label;
 		GridDataFactory labelLayout = GridDataFactory.swtDefaults().align(SWT.END, SWT.CENTER);
@@ -124,6 +145,14 @@ public class SequentialIDParameterPage extends AbstractParameterPage implements
 			Sequence initialValue = Sequence.valueOf(getOptionalInitialValue(PARAM_SEQUENCE,
 					Sequence.type.name()));
 			sequence.setSelection(new StructuredSelection(initialValue));
+
+			sequence.addSelectionChangedListener(new ISelectionChangedListener() {
+
+				@Override
+				public void selectionChanged(SelectionChangedEvent event) {
+					updateStatus();
+				}
+			});
 		}
 
 		// specify prefix
@@ -136,6 +165,14 @@ public class SequentialIDParameterPage extends AbstractParameterPage implements
 			controlLayout.applyTo(prefix);
 
 			prefix.setText(getOptionalInitialValue(PARAM_PREFIX, ""));
+
+			prefix.addModifyListener(new ModifyListener() {
+
+				@Override
+				public void modifyText(ModifyEvent e) {
+					updateStatus();
+				}
+			});
 		}
 
 		// specify suffix
@@ -148,21 +185,120 @@ public class SequentialIDParameterPage extends AbstractParameterPage implements
 			controlLayout.applyTo(suffix);
 
 			suffix.setText(getOptionalInitialValue(PARAM_SUFFIX, ""));
+
+			suffix.addModifyListener(new ModifyListener() {
+
+				@Override
+				public void modifyText(ModifyEvent e) {
+					updateStatus();
+				}
+			});
+		}
+
+		// show example
+		if (sequence != null && prefix != null && suffix != null) {
+			label = new Label(page, SWT.NONE);
+			label.setText("Example");
+			labelLayout.applyTo(label);
+
+			example = new Label(page, SWT.NONE);
+			example.setFont(JFaceResources.getTextFont());
+			controlLayout.applyTo(example);
+
+			// error decoration
+			exampleDecoration = new ControlDecoration(example, SWT.LEFT | SWT.TOP, page);
+			FieldDecoration fieldDecoration = FieldDecorationRegistry.getDefault()
+					.getFieldDecoration(FieldDecorationRegistry.DEC_ERROR);
+			exampleDecoration.setImage(fieldDecoration.getImage());
+			exampleDecoration.hide();
 		}
 
 		updateStatus();
 	}
 
 	/**
-	 * Update the page status
+	 * Update the example and the page status
 	 */
 	protected void updateStatus() {
 		boolean complete = true;
+
+		if (example != null) {
+			String exampleStr = generateExample();
+			example.setText(exampleStr);
+
+			boolean valid = validateValue(exampleStr);
+			if (!valid) {
+				complete = false;
+				setMessage("The generated identifier is not valid for the target property", ERROR);
+			}
+			else {
+				setMessage(null);
+			}
+		}
 
 		if (sequence != null && sequence.getSelection().isEmpty()) {
 			complete = false;
 		}
 
 		setPageComplete(complete);
+	}
+
+	/**
+	 * @see HaleWizardPage#onShowPage(boolean)
+	 */
+	@Override
+	protected void onShowPage(boolean firstShow) {
+		super.onShowPage(firstShow);
+
+		// the target property might have changed
+		updateStatus();
+	}
+
+	/**
+	 * Validates if the given value is valid for the target property.
+	 * 
+	 * @param value the value to validate
+	 * @return if the value is valid
+	 */
+	protected boolean validateValue(String value) {
+		Cell cell = getWizard().getUnfinishedCell();
+		List<? extends Entity> targets = cell.getTarget().get(null);
+
+		if (!targets.isEmpty()) {
+			Entity entity = targets.get(0);
+			Definition<?> def = entity.getDefinition().getDefinition();
+			if (def instanceof PropertyDefinition) {
+				TypeDefinition propertyType = ((PropertyDefinition) def).getPropertyType();
+				Validator validator = propertyType.getConstraint(ValidationConstraint.class)
+						.getValidator();
+				// TODO conversion to binding needed?
+				String error = validator.validate(value);
+				// update the example decoration
+				if (error == null) {
+					exampleDecoration.hide();
+				}
+				else {
+					exampleDecoration.setDescriptionText(error);
+					exampleDecoration.show();
+				}
+
+				return error == null;
+			}
+		}
+
+		// no validation possible
+		return true;
+	}
+
+	/**
+	 * Generate an identifier example from the current configuration.
+	 * 
+	 * @return the identifier example
+	 */
+	protected String generateExample() {
+		String prefix = (this.prefix == null) ? ("") : (this.prefix.getText());
+		String suffix = (this.suffix == null) ? ("") : (this.suffix.getText());
+
+		return prefix + START_VALUE + suffix;
 	}
 }
