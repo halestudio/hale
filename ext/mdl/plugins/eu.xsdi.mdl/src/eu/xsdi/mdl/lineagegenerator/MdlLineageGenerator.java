1f66e41bc0f583702c7f04cda3e0a598194d2efb
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-09-18 14:20:46 +0200

    updated file headers to be consistent
    Main file header now reflects the transfer of rights from
    the HUMBOLDT project consortium to the Data Harmonisation
    Panel.
    Exceptions from the main file header:
    - Adapted source files originating from third party libraries
    - Generated source files
    - Thorsten's MDL projects
    
    Change-Id: I8bedc7700b35affc3077c0ef2eef0a3e780dafdc

--- ext/mdl/plugins/eu.xsdi.mdl/src/eu/xsdi/mdl/lineagegenerator/MdlLineageGenerator.java
+++ ext/mdl/plugins/eu.xsdi.mdl/src/eu/xsdi/mdl/lineagegenerator/MdlLineageGenerator.java
@@ -1,13 +1,13 @@
 /*
- * HUMBOLDT: A Framework for Data Harmonisation and Service Integration.
- * EU Integrated Project #030962                 01.10.2006 - 30.09.2010
+ * LICENSE: This program is being made available under the LGPL 3.0 license.
+ * For more information on the license, please read the following:
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
  * 
- * For more information on the project, please refer to the this web site:
- * http://www.esdi-humboldt.eu
- * 
- * LICENSE: For information on the license under which this program is 
- * available, please refer to http:/www.esdi-humboldt.eu/license.html#core
- * (c) the HUMBOLDT Consortium, 2007 to 2010.
+ * For additional information on the Model behind Mismatches, please refer to
+ * the following publication(s):
+ * Thorsten Reitz (2010): A Mismatch Description Language for Conceptual Schema 
+ * Mapping and Its Cartographic Representation, Geographic Information Science,
+ * http://www.springerlink.com/content/um2082120r51232u/
  */
 
 package eu.xsdi.mdl.lineagegenerator;
--- ext/mdl/plugins/eu.xsdi.mdl/test/eu/xsdi/mdl/lineagegenerator/MdlLineageGeneratorTest.java
+++ ext/mdl/plugins/eu.xsdi.mdl/test/eu/xsdi/mdl/lineagegenerator/MdlLineageGeneratorTest.java
@@ -1,3 +1,14 @@
+/*
+ * LICENSE: This program is being made available under the LGPL 3.0 license.
+ * For more information on the license, please read the following:
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * 
+ * For additional information on the Model behind Mismatches, please refer to
+ * the following publication(s):
+ * Thorsten Reitz (2010): A Mismatch Description Language for Conceptual Schema 
+ * Mapping and Its Cartographic Representation, Geographic Information Science,
+ * http://www.springerlink.com/content/um2082120r51232u/
+ */
 package eu.xsdi.mdl.lineagegenerator;
 
 import static org.junit.Assert.*;
