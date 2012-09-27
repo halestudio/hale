6fdff734f774dd22343bfa8a19c958f907adfaf0
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-09-27 15:26:35 +0200

    added first version of schema presets extension point
    refs #485
    
    Change-Id: I7a2c478b7e031c47dc9bd5d2c3348cd865419294
    Reviewed-on: http://gerrit.esdi-community.eu/1506
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.schema.prefedined/src/eu/esdihumboldt/hale/ui/schema/presets/internal/SchemaPresetSelector.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.schema.presets.internal;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+
+import eu.esdihumboldt.hale.ui.schema.presets.extension.SchemaPreset;
+import eu.esdihumboldt.hale.ui.util.selector.AbstractSelector;
+import eu.esdihumboldt.hale.ui.util.selector.AbstractViewerSelectionDialog;
+
+/**
+ * Selector for {@link SchemaPreset}s.
+ * 
+ * @author Simon Templer
+ */
+public class SchemaPresetSelector extends AbstractSelector<SchemaPreset> {
+
+	/**
+	 * Create a selector for {@link SchemaPreset}s.
+	 * 
+	 * @param parent the parent composite
+	 */
+	public SchemaPresetSelector(Composite parent) {
+		super(parent, new SchemaPresetLabelProvider(), null);
+	}
+
+	@Override
+	protected AbstractViewerSelectionDialog<SchemaPreset, ?> createSelectionDialog(
+			Shell parentShell) {
+		return new SchemaPresetSelectionDialog(parentShell, getSelectedObject());
+	}
+
+}
