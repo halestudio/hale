bc0604a9f45decd3a9b8fdad514c0dc8ee77a745
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2012-10-18 19:48:13 +0200

    new version of archive project export
    not ready yet
    
    refs #420
    
    Change-Id: I5995b710ad93f7214777eea6975c2184cc89e144

--- common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/io/project/impl/ArchiveProjectWriter.java
+++ common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/io/project/impl/ArchiveProjectWriter.java
@@ -18,7 +18,9 @@ package eu.esdihumboldt.hale.common.core.io.project.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -30,6 +32,7 @@ import java.util.zip.ZipOutputStream;
 
 import org.eclipse.core.runtime.FileLocator;
 
+import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 
 import de.cs3d.util.logging.ALogger;
@@ -41,6 +44,7 @@ import eu.esdihumboldt.hale.common.core.io.project.model.IOConfiguration;
 import eu.esdihumboldt.hale.common.core.io.project.util.XMLSchemaUpdater;
 import eu.esdihumboldt.hale.common.core.io.report.IOReport;
 import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
+import eu.esdihumboldt.hale.common.core.io.supplier.DefaultInputSupplier;
 import eu.esdihumboldt.hale.common.core.io.supplier.FileIOSupplier;
 import eu.esdihumboldt.hale.common.core.io.supplier.LocatableOutputSupplier;
 
@@ -73,7 +77,7 @@ public class ArchiveProjectWriter extends AbstractProjectWriter {
 		ZipOutputStream zip = new ZipOutputStream(getTarget().getOutput());
 
 		// copy resources to the temp directory and update xml schemas
-		updateResources(tempDir);
+		updateResources(tempDir, false);
 
 		// update target save configuration of the project
 		IOConfiguration config = getProject().getSaveConfiguration();
@@ -98,16 +102,8 @@ public class ArchiveProjectWriter extends AbstractProjectWriter {
 		return report;
 	}
 
-	/**
-	 * @see eu.esdihumboldt.hale.common.core.io.impl.AbstractIOProvider#getDefaultTypeName()
-	 */
-	@Override
-	protected String getDefaultTypeName() {
-		return null;
-	}
-
 	// update the resources and copy them into target directory
-	private void updateResources(File targetDirectory) throws IOException {
+	private void updateResources(File targetDirectory, boolean allResources) throws IOException {
 
 		List<IOConfiguration> resources = getProject().getResources();
 		// every resource needs his own directory
@@ -115,36 +111,44 @@ public class ArchiveProjectWriter extends AbstractProjectWriter {
 		for (IOConfiguration resource : resources) {
 			Map<String, String> providerConfig = resource.getProviderConfiguration();
 			Map<String, String> newProvConf = new HashMap<String, String>();
-			URI path;
+			String path = providerConfig.get(ImportProvider.PARAM_SOURCE);
+			URI pathUri;
 			try {
-				path = new URI(providerConfig.get(ImportProvider.PARAM_SOURCE));
+				pathUri = new URI(path);
 			} catch (URISyntaxException e1) {
 				log.debug("Path of resource is invalid", e1);
 				continue;
 			}
-			String scheme = path.getScheme();
+			String scheme = pathUri.getScheme();
+			InputStream input = null;
 			// if scheme is null it has to be a local file represented by a
 			// relative path
 			if (scheme != null) {
-				if (scheme.equals("bundleentry")) {
+				if (allResources && (scheme.equals("http") || scheme.equals("https"))
+						|| scheme.equals("resource")) {
+					DefaultInputSupplier supplier = new DefaultInputSupplier(pathUri);
+					input = supplier.getInput();
+				}
+				else if (scheme.equals("bundleentry")) {
 					try {
-						// get absolute path of the bundleentry file
-						path = FileLocator.toFileURL(path.toURL()).toURI();
+						File in = new File(FileLocator.toFileURL(pathUri.toURL()).toURI());
+						input = new FileInputStream(in);
 					} catch (URISyntaxException e) {
-						log.debug("Bundleentry is invalid", e);
-						continue;
+						e.printStackTrace();
 					}
 				}
-				else if (!scheme.equals("file")) {
-					// URI represents no local file and can not be updated
-					continue;
+				else if (scheme.equals("file")) {
+					input = new FileInputStream(new File(pathUri));
 				}
+				else
+					continue;
 			}
+			else
+				input = new FileInputStream(new File(pathUri));
 
 			// only xml schemas have to be updated
 			String contentType = providerConfig.get(ImportProvider.PARAM_CONTENT_TYPE);
 			if (contentType.equals(XSD_CONTENT_TYPE)) {
-				File file = new File(path);
 
 				// every resource file is copied into an own resource directory
 				// in the target directory
@@ -154,14 +158,17 @@ public class ArchiveProjectWriter extends AbstractProjectWriter {
 				} catch (SecurityException e) {
 					throw new IOException("Can not create directory " + newDirectory.toString(), e);
 				}
-				File newFile = new File(newDirectory, file.getName());
-				Files.copy(file, newFile);
+				// get name of the file
+				String name = path.toString().substring(path.lastIndexOf("/"), path.length());
+				File newFile = new File(newDirectory, name);
+				OutputStream output = new FileOutputStream(newFile);
+				ByteStreams.copy(input, output);
 
 				// the XMLSchemaUpdater manipulates the current schema and
 				// copies the included and imported schemas to the directory
-				XMLSchemaUpdater.update(newFile, file);
+				XMLSchemaUpdater.update(newFile, pathUri, allResources);
 				newProvConf.put(ImportProvider.PARAM_SOURCE, new File(new File(targetDirectory,
-						"resource" + count), file.getName()).toURI().toString());
+						"resource" + count), name).toURI().toString());
 				count++;
 			}
 
@@ -170,7 +177,6 @@ public class ArchiveProjectWriter extends AbstractProjectWriter {
 				resource.getProviderConfiguration().remove(key);
 				resource.getProviderConfiguration().put(key, newProvConf.get(key));
 			}
-
 		}
 	}
 
@@ -221,7 +227,6 @@ public class ArchiveProjectWriter extends AbstractProjectWriter {
 					} catch (SecurityException e) {
 						log.debug("Can not delete directories because of SecurityManager", e);
 					}
-
 				}
 			}
 		}
