70d5884a26165d10b821ea77a9b615c0079f4498
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-10-22 12:53:10 +0200

    project management webapp (wip)
    Change-Id: Ia1eb134fc0bd889e264171bdd43767e7489a96ec

---
+++ server/plugins/eu.esdihumboldt.hale.server.projects.war/src/eu/esdihumboldt/hale/server/projects/war/pages/ProjectsPage.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.server.projects.war.pages;
+
+import org.apache.wicket.PageParameters;
+
+import eu.esdihumboldt.hale.server.webapp.pages.BasePage;
+import eu.esdihumboldt.hale.server.webapp.util.PageDescription;
+
+/**
+ * TODO Type description
+ * 
+ * @author Simon Templer
+ */
+@PageDescription(title = "Projects")
+public class ProjectsPage extends BasePage {
+
+	/**
+	 * 
+	 */
+	public ProjectsPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param parameters
+	 */
+	public ProjectsPage(PageParameters parameters) {
+		super(parameters);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ server/plugins/eu.esdihumboldt.hale.server.projects.war/src/eu/esdihumboldt/hale/server/projects/war/pages/UploadPage.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.server.projects.war.pages;
+
+import org.apache.wicket.PageParameters;
+
+import eu.esdihumboldt.hale.server.webapp.pages.BasePage;
+import eu.esdihumboldt.hale.server.webapp.util.PageDescription;
+
+/**
+ * TODO Type description
+ * 
+ * @author Simon Templer
+ */
+@PageDescription(title = "Upload", parent = ProjectsPage.class)
+public class UploadPage extends BasePage {
+
+	/**
+	 * 
+	 */
+	public UploadPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param parameters
+	 */
+	public UploadPage(PageParameters parameters) {
+		super(parameters);
+		// TODO Auto-generated constructor stub
+	}
+
+}
