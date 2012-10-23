99fe4eab4e735f6eb2e1f28f1ce3f66470ae14a9
Willem Weiss <weisswillem@gmail.com>
2012-10-23 15:57:35 +0200

    added Thorsten perspective
    Change-Id: Iaf9be3d7c237c8f2c02e3d66fe0aa0e302196110

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.application/src/eu/esdihumboldt/hale/ui/application/ThorstenPerspective.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.application;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+import eu.esdihumboldt.hale.ui.views.data.SourceDataView;
+import eu.esdihumboldt.hale.ui.views.data.TransformedDataView;
+import eu.esdihumboldt.hale.ui.views.mapping.AlignmentView;
+import eu.esdihumboldt.hale.ui.views.schemas.SchemasView;
+import eu.esdihumboldt.hale.ui.views.styledmap.StyledMapView;
+
+/**
+ * Thorsten's perspective
+ * 
+ * @author Willem Weiss
+ */
+public class ThorstenPerspective implements IPerspectiveFactory {
+
+	/**
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
+	@Override
+	public void createInitialLayout(IPageLayout _layout) {
+		String editorArea = _layout.getEditorArea();
+
+		// bottom left
+		IFolderLayout bottomLeft = _layout.createFolder(
+				"bottomLeft", IPageLayout.BOTTOM, 0.5f, editorArea); //$NON-NLS-1$
+		bottomLeft.addView(IPageLayout.ID_PROP_SHEET);
+		bottomLeft.addView(AlignmentView.ID);
+		bottomLeft.addView(StyledMapView.ID);
+
+		// bottom right
+		IFolderLayout bottomRight = _layout.createFolder(
+				"bottomRight", IPageLayout.RIGHT, 0.5f, "bottomLeft"); //$NON-NLS-1$ //$NON-NLS-2$
+		bottomRight.addView(TransformedDataView.ID);
+
+		// top left
+		IFolderLayout topLeft = _layout.createFolder("topLeft", IPageLayout.TOP, 0.5f, editorArea); //$NON-NLS-1$
+		topLeft.addView(SchemasView.ID);
+//	topLeft.addView(MapView.ID);
+
+		// top right
+		IFolderLayout topRight = _layout.createFolder(
+				"topRight", IPageLayout.RIGHT, 0.5f, "topLeft"); //$NON-NLS-1$ //$NON-NLS-2$
+		topRight.addView(SourceDataView.ID);
+
+		_layout.addShowViewShortcut(SchemasView.ID);
+		_layout.addShowViewShortcut(AlignmentView.ID);
+		_layout.addShowViewShortcut(SourceDataView.ID);
+		_layout.addShowViewShortcut(TransformedDataView.ID);
+
+		_layout.setEditorAreaVisible(false);
+	}
+}
