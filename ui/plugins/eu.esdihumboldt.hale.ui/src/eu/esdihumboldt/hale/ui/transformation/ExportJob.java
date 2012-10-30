d1f1a6be4cb5e9a29a64d6e3847fd03b165ca0e9
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-10-30 18:33:53 +0100

    first step in preparing batch transformation for reuse
    refs #508
    
    Change-Id: Ifa9a8219c60be25be85711ba15d80e4bc0b0d309

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/transformation/ExportJob.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.transformation;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+import de.cs3d.util.logging.ALogger;
+import de.cs3d.util.logging.ALoggerFactory;
+import de.cs3d.util.logging.ATransaction;
+import eu.esdihumboldt.hale.common.core.io.IOAdvisor;
+import eu.esdihumboldt.hale.common.core.io.report.IOReport;
+import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
+import eu.esdihumboldt.hale.common.core.io.report.impl.IOMessageImpl;
+import eu.esdihumboldt.hale.common.core.report.ReportHandler;
+import eu.esdihumboldt.hale.common.instance.io.InstanceWriter;
+import eu.esdihumboldt.hale.ui.io.util.ProgressMonitorIndicator;
+
+/**
+ * Job for exporting transformed data supplied in a
+ * {@link TransformDataInstanceSink}.
+ * 
+ * @author Kai Schwierczek
+ * @author Simon Templer
+ */
+public class ExportJob extends Job {
+
+	private static final ALogger log = ALoggerFactory.getLogger(ExportJob.class);
+
+	private final TransformDataInstanceSink targetSink;
+	private final InstanceWriter writer;
+	private final IOAdvisor<InstanceWriter> advisor;
+	private final ReportHandler reportHandler;
+
+	/**
+	 * Create a job for exporting transformed data supplied in the given target
+	 * sink.
+	 * 
+	 * @param targetSink the target sink
+	 * @param writer the instance writer
+	 * @param advisor the advisor
+	 * @param reportHandler the report handler
+	 */
+	public ExportJob(TransformDataInstanceSink targetSink, InstanceWriter writer,
+			IOAdvisor<InstanceWriter> advisor, ReportHandler reportHandler) {
+		super("Export");
+
+		this.targetSink = targetSink;
+		this.writer = writer;
+		this.advisor = advisor;
+		this.reportHandler = reportHandler;
+	}
+
+	/**
+	 * @see org.eclipse.core.runtime.jobs.Job#canceling()
+	 */
+	@Override
+	protected void canceling() {
+		// must cancel sink, otherwise it may be blocking
+		targetSink.done(true);
+	}
+
+	/**
+	 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		IOReporter defaultReporter = writer.createReporter();
+		defaultReporter.setSuccess(false);
+		IOReport report = defaultReporter;
+		try {
+			ATransaction trans = log.begin(defaultReporter.getTaskName());
+			try {
+				IOReport result = writer.execute(new ProgressMonitorIndicator(monitor));
+				if (result != null) {
+					report = result;
+				}
+				else {
+					defaultReporter.setSuccess(true);
+				}
+			} catch (Throwable e) {
+				defaultReporter.error(new IOMessageImpl(e.getLocalizedMessage(), e));
+			} finally {
+				trans.end();
+			}
+		} catch (Throwable e) {
+			defaultReporter.error(new IOMessageImpl(e.getLocalizedMessage(), e));
+		}
+
+		if (monitor.isCanceled())
+			return Status.CANCEL_STATUS;
+
+		// add report to report service
+		reportHandler.publishReport(report);
+
+		// show message to user
+		if (report.isSuccess()) {
+			// no message, we rely on the report being
+			// shown/processed
+
+			// let advisor handle results
+			advisor.handleResults(writer);
+		}
+		else {
+			// error message
+			log.userError(report.getSummary());
+		}
+
+		// XXX return something depending on report success?
+		return Status.OK_STATUS;
+	}
+
+}
--- ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/transformation/TransformDataWizard.java
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/transformation/TransformDataWizard.java
@@ -16,36 +16,16 @@
 
 package eu.esdihumboldt.hale.ui.transformation;
 
-import java.io.File;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.PlatformUI;
 
-import com.google.common.io.Files;
-
-import de.fhg.igd.osgi.util.OsgiUtils;
 import eu.esdihumboldt.hale.common.align.model.Alignment;
-import eu.esdihumboldt.hale.common.align.model.Cell;
-import eu.esdihumboldt.hale.common.align.model.functions.RetypeFunction;
 import eu.esdihumboldt.hale.common.align.model.impl.DefaultAlignment;
-import eu.esdihumboldt.hale.common.align.transformation.report.TransformationReport;
-import eu.esdihumboldt.hale.common.align.transformation.service.TransformationService;
-import eu.esdihumboldt.hale.common.instance.model.DataSet;
 import eu.esdihumboldt.hale.common.instance.model.InstanceCollection;
 import eu.esdihumboldt.hale.common.instance.model.impl.MultiInstanceCollection;
 import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
-import eu.esdihumboldt.hale.ui.io.util.ProgressMonitorIndicator;
+import eu.esdihumboldt.hale.ui.DefaultReportHandler;
 import eu.esdihumboldt.hale.ui.service.align.AlignmentService;
-import eu.esdihumboldt.hale.ui.service.instance.internal.orient.BrowseOrientInstanceCollection;
-import eu.esdihumboldt.hale.ui.service.instance.internal.orient.LocalOrientDB;
-import eu.esdihumboldt.hale.ui.service.instance.internal.orient.StoreInstancesJob;
-import eu.esdihumboldt.hale.ui.service.report.ReportService;
 import eu.esdihumboldt.hale.ui.service.schema.SchemaService;
 
 /**
@@ -87,138 +67,20 @@ public class TransformDataWizard extends Wizard {
 	public boolean performFinish() {
 		InstanceCollection rawSources = new MultiInstanceCollection(
 				sourceSelectionPage.getSourceInstances());
-		final InstanceCollection sourceToUse;
 
 		// Create a copy of the current alignment to be independent and run
 		// everything in a job.
 		AlignmentService alignmentService = (AlignmentService) PlatformUI.getWorkbench()
 				.getService(AlignmentService.class);
-		final Alignment alignment = new DefaultAlignment(alignmentService.getAlignment());
+		Alignment alignment = new DefaultAlignment(alignmentService.getAlignment());
 
-		// Check whether to create a temporary database or not.
-		// Currently do not create a temporary DB is there are Retypes only.
-		boolean useTempDatabase = false;
-		final LocalOrientDB db;
-		for (Cell cell : alignment.getTypeCells())
-			if (!RetypeFunction.ID.equals(cell.getTransformationIdentifier())) {
-				useTempDatabase = true;
-				break;
-			}
+		// schema service for getting source schema
+		SchemaService ss = (SchemaService) PlatformUI.getWorkbench()
+				.getService(SchemaService.class);
 
-		// Create temporary database if necessary.
-		if (useTempDatabase) {
-			// create db
-			File tmpDir = Files.createTempDir();
-			db = new LocalOrientDB(tmpDir);
-			tmpDir.deleteOnExit();
-
-			// get instance collection
-			SchemaService ss = (SchemaService) PlatformUI.getWorkbench().getService(
-					SchemaService.class);
-			sourceToUse = new BrowseOrientInstanceCollection(db,
-					ss.getSchemas(SchemaSpaceID.SOURCE), DataSet.SOURCE);
-		}
-		else {
-			sourceToUse = rawSources;
-			db = null;
-		}
-
-		// get / create jobs
-		final Job exportJob = sourceSelectionPage.getExportJob();
-		final Job validationJob = sourceSelectionPage.getValidationJob();
-		final Job transformJob = new Job("Transform") {
-
-			/**
-			 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
-			 */
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				TransformationService transformationService = OsgiUtils
-						.getService(TransformationService.class);
-
-				TransformationReport report = transformationService.transform(alignment,
-						sourceToUse, targetSink, new ProgressMonitorIndicator(monitor));
-
-				if (monitor.isCanceled()) {
-					targetSink.done(true);
-					return Status.CANCEL_STATUS;
-				}
-				else
-					targetSink.done(false);
-
-				// publish report
-				ReportService rs = (ReportService) PlatformUI.getWorkbench().getService(
-						ReportService.class);
-				rs.addReport(report);
-
-				// XXX return something else, if report is not successful
-				return Status.OK_STATUS;
-			}
-		};
-
-		exportJob.setUser(true);
-
-		// the jobs should cancel each other
-		transformJob.addJobChangeListener(new JobChangeAdapter() {
-
-			/**
-			 * @see org.eclipse.core.runtime.jobs.JobChangeAdapter#done(org.eclipse.core.runtime.jobs.IJobChangeEvent)
-			 */
-			@Override
-			public void done(IJobChangeEvent event) {
-				if (!event.getResult().isOK())
-					exportJob.cancel();
-
-				if (db != null)
-					db.delete();
-			}
-		});
-		// after export is done, validation should run
-		exportJob.addJobChangeListener(new JobChangeAdapter() {
-
-			/**
-			 * @see org.eclipse.core.runtime.jobs.JobChangeAdapter#done(org.eclipse.core.runtime.jobs.IJobChangeEvent)
-			 */
-			@Override
-			public void done(IJobChangeEvent event) {
-				if (!event.getResult().isOK())
-					transformJob.cancel();
-				else if (validationJob != null)
-					validationJob.schedule();
-			}
-		});
-
-		if (useTempDatabase) {
-			// run store instance job first...
-			StoreInstancesJob storeJob = new StoreInstancesJob(
-					"Load source instances into temporary database", db, rawSources) {
-
-				@Override
-				protected void onComplete() {
-					// onComplete is also called if monitor is cancelled...
-				}
-			};
-			// and schedule jobs on successful completion
-			storeJob.addJobChangeListener(new JobChangeAdapter() {
-
-				/**
-				 * @see org.eclipse.core.runtime.jobs.JobChangeAdapter#done(org.eclipse.core.runtime.jobs.IJobChangeEvent)
-				 */
-				@Override
-				public void done(IJobChangeEvent event) {
-					if (event.getResult().isOK()) {
-						exportJob.schedule();
-						transformJob.schedule();
-					}
-				}
-			});
-			storeJob.schedule();
-		}
-		else {
-			// otherwise schedule jobs directly
-			exportJob.schedule();
-			transformJob.schedule();
-		}
+		Transformation.transform(rawSources, targetSink, sourceSelectionPage.getExportJob(),
+				sourceSelectionPage.getValidationJob(), alignment,
+				ss.getSchemas(SchemaSpaceID.SOURCE), DefaultReportHandler.getInstance());
 
 		return true;
 	}
