739228c6dfd7e57bba8d87d7ee573c69886febe7
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-11-02 12:29:47 +0100

    status page shows transformation results
    refs #505
    
    Change-Id: I234e45eaf28caa9722f810d73d59a010d142dad5

--- server/plugins/eu.esdihumboldt.hale.server.webtransform.war/src/eu/esdihumboldt/hale/server/webtransform/war/pages/StatusPage.java
+++ server/plugins/eu.esdihumboldt.hale.server.webtransform.war/src/eu/esdihumboldt/hale/server/webtransform/war/pages/StatusPage.java
@@ -15,16 +15,34 @@
 
 package eu.esdihumboldt.hale.server.webtransform.war.pages;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.wicket.ajax.AbstractAjaxTimerBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.DownloadLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.http.flow.AbortWithHttpErrorCodeException;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.time.Duration;
+import org.eclipse.core.runtime.jobs.Job;
+import org.joda.time.format.DateTimeFormat;
 
 import eu.esdihumboldt.hale.common.headless.WorkspaceService;
 import eu.esdihumboldt.hale.common.headless.transform.AbstractTransformationJob;
+import eu.esdihumboldt.hale.common.headless.transform.TransformationWorkspace;
 import eu.esdihumboldt.hale.server.webapp.components.JobPanel;
 import eu.esdihumboldt.hale.server.webapp.pages.BasePage;
 import eu.esdihumboldt.hale.server.webapp.util.PageDescription;
@@ -60,7 +78,7 @@ public class StatusPage extends BasePage {
 	protected void addControls(boolean loggedIn) {
 		super.addControls(loggedIn);
 
-		String workspaceId = getPageParameters().get(PARAMETER_WORKSPACE).toOptionalString();
+		final String workspaceId = getPageParameters().get(PARAMETER_WORKSPACE).toOptionalString();
 		if (workspaceId == null || workspaceId.isEmpty()) {
 			throw new AbortWithHttpErrorCodeException(HttpServletResponse.SC_NOT_FOUND,
 					"Workspace ID not specified.");
@@ -73,7 +91,128 @@ public class StatusPage extends BasePage {
 					"Workspace does not exist.");
 		}
 
-		add(new JobPanel("jobs", AbstractTransformationJob.createFamily(workspaceId)));
+		final IModel<TransformationWorkspace> workspace = new LoadableDetachableModel<TransformationWorkspace>() {
+
+			private static final long serialVersionUID = 2600444242247550094L;
+
+			@Override
+			protected TransformationWorkspace load() {
+				return new TransformationWorkspace(workspaceId);
+			}
+		};
+
+		// job panel
+		final Serializable family = AbstractTransformationJob.createFamily(workspaceId);
+		final JobPanel jobs = new JobPanel("jobs", family, true);
+		add(jobs);
+
+		// status
+		final Label status = new Label("status", new LoadableDetachableModel<String>() {
+
+			private static final long serialVersionUID = -4351763182104835300L;
+
+			@Override
+			protected String load() {
+				if (workspace.getObject().isTransformationFinished()) {
+					if (workspace.getObject().isTransformationSuccessful()) {
+						return "Transformation completed.";
+					}
+					else {
+						return "Transformation failed.";
+					}
+				}
+				else {
+					if (Job.getJobManager().find(family).length > 0) {
+						return "Transformation is running:";
+					}
+					else {
+						return "No transformation running.";
+					}
+				}
+			}
+		});
+		status.setOutputMarkupId(true);
+		add(status);
+
+		// result
+		final WebMarkupContainer result = new WebMarkupContainer("result");
+		result.setOutputMarkupId(true);
+		add(result);
+
+		final WebMarkupContainer update = new WebMarkupContainer("update") {
+
+			private static final long serialVersionUID = -2591480922683644827L;
+
+			@Override
+			public boolean isVisible() {
+				return workspace.getObject().isTransformationFinished();
+			}
+
+		};
+		result.add(update);
+
+		// result : report
+		File reportFile = workspace.getObject().getReportFile();
+		DownloadLink report = new DownloadLink("log", reportFile, reportFile.getName());
+		update.add(report);
+
+		// result : file list
+		IModel<? extends List<File>> resultFilesModel = new LoadableDetachableModel<List<File>>() {
+
+			private static final long serialVersionUID = -7971872898614031331L;
+
+			@Override
+			protected List<File> load() {
+				return Arrays.asList(workspace.getObject().getTargetFolder().listFiles());
+			}
+
+		};
+		final ListView<File> fileList = new ListView<File>("file", resultFilesModel) {
+
+			private static final long serialVersionUID = -8045643864251639540L;
+
+			@Override
+			protected void populateItem(ListItem<File> item) {
+				// download link
+				DownloadLink download = new DownloadLink("download", item.getModelObject(), item
+						.getModelObject().getName());
+				item.add(download);
+
+				// file name
+				download.add(new Label("name", item.getModelObject().getName()));
+			}
+
+		};
+		update.add(fileList);
+
+		// leaseEnd
+		String leaseEnd;
+		try {
+			leaseEnd = workspaces.getLeaseEnd(workspaceId)
+					.toString(DateTimeFormat.mediumDateTime());
+		} catch (IOException e) {
+			leaseEnd = "unknown";
+		}
+		add(new Label("leaseEnd", leaseEnd));
+
+		// timer
+		add(new AbstractAjaxTimerBehavior(Duration.milliseconds(1500)) {
+
+			private static final long serialVersionUID = -3726349470723024150L;
+
+			@Override
+			protected void onTimer(AjaxRequestTarget target) {
+				if (workspace.getObject().isTransformationFinished()) {
+					// update status and result
+					target.add(status);
+					target.add(result);
+
+					// stop timers
+					stop(target);
+					jobs.getTimer().stopOnNextUpdate();
+				}
+			}
+		});
 	}
 
 }
