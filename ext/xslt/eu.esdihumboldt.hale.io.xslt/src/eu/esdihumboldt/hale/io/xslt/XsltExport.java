07ada7ebc2708072d2a490bc7108969b07bcd178
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-11-07 15:37:24 +0100

    added xslt export stub and transformation tests
    refs #116
    
    Change-Id: I301a7bf2aadd183b5476c42146cbabc188a5236b
    Reviewed-on: http://gerrit.esdi-community.eu/1667
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ext/xslt/eu.esdihumboldt.hale.io.xslt/src/eu/esdihumboldt/hale/io/xslt/XsltExport.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2012 Fraunhofer IGD
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Fraunhofer IGD
+ */
+
+package eu.esdihumboldt.hale.io.xslt;
+
+import java.io.IOException;
+
+import javax.swing.GroupLayout.Alignment;
+
+import eu.esdihumboldt.hale.common.align.io.impl.AbstractAlignmentWriter;
+import eu.esdihumboldt.hale.common.core.io.IOProviderConfigurationException;
+import eu.esdihumboldt.hale.common.core.io.ProgressIndicator;
+import eu.esdihumboldt.hale.common.core.io.report.IOReport;
+import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
+
+/**
+ * Exports an {@link Alignment} to XSLT.
+ * 
+ * @author Simon Templer
+ */
+public class XsltExport extends AbstractAlignmentWriter {
+
+	@Override
+	public boolean isCancelable() {
+		return false;
+	}
+
+	@Override
+	protected IOReport execute(ProgressIndicator progress, IOReporter reporter)
+			throws IOProviderConfigurationException, IOException {
+
+		// TODO
+
+		reporter.setSuccess(false);
+
+		return reporter;
+	}
+
+	@Override
+	protected String getDefaultTypeName() {
+		return "XSLT file";
+	}
+
+}
