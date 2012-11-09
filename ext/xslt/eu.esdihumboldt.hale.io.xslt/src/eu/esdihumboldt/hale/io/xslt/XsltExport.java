5049548f7f3e353487cf4e33d9e7884dc779dc2f
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-11-09 09:11:42 +0100

    move logic for XSLT generation to own class
    refs #116
    
    Change-Id: If6538c49a0087d28fc16d74ee9471f3fd6cb27e0
    Reviewed-on: http://gerrit.esdi-community.eu/1671
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ext/xslt/eu.esdihumboldt.hale.io.xslt/src/eu/esdihumboldt/hale/io/xslt/XsltExport.java
+++ ext/xslt/eu.esdihumboldt.hale.io.xslt/src/eu/esdihumboldt/hale/io/xslt/XsltExport.java
@@ -17,16 +17,10 @@ package eu.esdihumboldt.hale.io.xslt;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 
 import javax.swing.GroupLayout.Alignment;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
 
 import com.google.common.io.Files;
 
@@ -38,7 +32,7 @@ import eu.esdihumboldt.hale.common.core.io.ProgressIndicator;
 import eu.esdihumboldt.hale.common.core.io.report.IOReport;
 import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
 import eu.esdihumboldt.hale.common.core.io.report.impl.IOMessageImpl;
-import eu.esdihumboldt.hale.io.xslt.internal.Templates;
+import eu.esdihumboldt.hale.io.xslt.internal.XsltGenerator;
 
 /**
  * Exports an {@link Alignment} to XSLT.
@@ -55,39 +49,25 @@ public class XsltExport extends AbstractAlignmentWriter {
 	}
 
 	@Override
+	public void validate() throws IOProviderConfigurationException {
+		super.validate();
+
+		if (getTargetSchema() == null) {
+			fail("Target schema not supplied");
+		}
+	}
+
+	@Override
 	protected IOReport execute(ProgressIndicator progress, IOReporter reporter)
 			throws IOProviderConfigurationException, IOException {
 		File templateDir = Files.createTempDir();
 		progress.begin("Generate XSLT", ProgressIndicator.UNKNOWN);
 		try {
 			log.info("Template directory: " + templateDir.getAbsolutePath());
-			Templates.copyTemplates(templateDir);
 
-			VelocityEngine ve = new VelocityEngine();
-
-//			ve.setProperty("resource.loader", "main, file");
-//			ve.setProperty("main.resource.loader.class",
-//					eu.esdihumboldt.hale.io.xslt.internal.Templates.class);
-			// custom resource loader does not work in OSGi context, so copy
-			// templates to template folder
-			ve.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH, templateDir.getAbsolutePath());
-			ve.init();
-
-			Template root = ve.getTemplate(Templates.ROOT, "UTF-8");
-
-			VelocityContext context = new VelocityContext();
-
-			OutputStream out = getTarget().getOutput();
-			Writer writer = new OutputStreamWriter(out, "UTF-8");
-			try {
-				root.merge(context, writer);
-				writer.flush();
-			} finally {
-				writer.close();
-			}
-
-			reporter.setSuccess(reporter.getErrors().isEmpty());
-			return reporter;
+			XsltGenerator generator = new XsltGenerator(templateDir, getAlignment(),
+					getTargetSchema(), reporter, progress);
+			return generator.write(getTarget());
 		} catch (Exception e) {
 			reporter.error(new IOMessageImpl("XSLT generation failed", e));
 			reporter.setSuccess(false);
