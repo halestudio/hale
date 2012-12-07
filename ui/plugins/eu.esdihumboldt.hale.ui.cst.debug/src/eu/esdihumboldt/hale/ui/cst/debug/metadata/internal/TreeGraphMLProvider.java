a15e07ac70f68546515928ee17eb1469e84c5d7d
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2012-12-07 20:02:13 +0100

    tree to graph conversion provider with GraphML subclass
    Change-Id: I2bace1d5079f0443849c4edcd0a514118aad416c

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.cst.debug/src/eu/esdihumboldt/hale/ui/cst/debug/metadata/internal/TreeGraphMLProvider.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.cst.debug.metadata.internal;
+
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import com.google.common.collect.SetMultimap;
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
+
+import eu.esdihumboldt.hale.common.align.extension.function.FunctionUtil;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.CellNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.SourceNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TargetNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationTree;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.visitor.TreeToGraphVisitor;
+import eu.esdihumboldt.hale.common.instance.model.Group;
+import eu.esdihumboldt.hale.common.instance.model.Instance;
+
+/**
+ * Provider for converting TransformationTrees into the graphML-format
+ * 
+ * @author SebastianReinhardt
+ */
+public class TreeGraphMLProvider implements TreeGraphProvider {
+
+	TransformationTree tree;
+	TreeToGraphVisitor graphVisitor;
+
+	/**
+	 * @param tree The Tree to get the graph from
+	 */
+	public TreeGraphMLProvider(TransformationTree tree) {
+		this.tree = tree;
+		graphVisitor = new TreeToGraphVisitor();
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.cst.debug.metadata.internal.TreeGraphProvider#generateGraph()
+	 */
+	@Override
+	public Graph generateGraph() {
+
+		tree.accept(graphVisitor);
+
+		SetMultimap<String, String> connections = graphVisitor.getAllConnections();
+		Set<String> ids = graphVisitor.getAllIds();
+
+		TinkerGraph graph = new TinkerGraph();
+
+		// add nodes to the graph
+		for (String key : ids) {
+			TransformationNode node = graphVisitor.getNode(key);
+			Vertex vertex = graph.addVertex(key);
+			setVertexProperty(node, vertex);
+		}
+
+		for (String key : connections.keySet()) {
+			for (String value : connections.get(key)) {
+				graph.addEdge(null, graph.getVertex(key), graph.getVertex(value), " ");
+			}
+		}
+		return graph;
+	}
+
+	/**
+	 * sets the property of a [@link]Vertex from a [@link]TransformationNode
+	 * 
+	 * @param node the node-object to get the name from
+	 * @param vertex the vertex to set the property
+	 */
+	private void setVertexProperty(TransformationNode node, Vertex vertex) {
+
+		if (node instanceof TransformationTree) {
+			vertex.setProperty("name", ((TransformationTree) node).getType().getDisplayName());
+			vertex.setProperty("type", "root");
+		}
+
+		if (node instanceof TargetNode) {
+			vertex.setProperty("name", ((TargetNode) node).getDefinition().getDisplayName());
+			vertex.setProperty("type", "target");
+		}
+
+		if (node instanceof SourceNode) {
+			SourceNode snode = (SourceNode) node;
+			Object value = snode.getValue();
+			String name = ((SourceNode) node).getDefinition().getDisplayName();
+
+			if (value instanceof Group) {
+				vertex.setProperty("name", name);
+				vertex.setProperty("group", getChildrencountString(value));
+				vertex.setProperty("type", "source");
+			}
+			if (value instanceof Instance) {
+				if (((Instance) value).getValue() != null) {
+					vertex.setProperty("value", shortenString(((Instance) value).getValue()
+							.toString()));
+					vertex.setProperty("type", "source");
+				}
+			}
+			else {
+				vertex.setProperty("name", name);
+				vertex.setProperty("type", "source");
+			}
+
+		}
+
+		if (node instanceof CellNode) {
+			vertex.setProperty(
+					"name",
+					FunctionUtil.getFunction(
+							((CellNode) node).getCell().getTransformationIdentifier())
+							.getDisplayName());
+			vertex.setProperty("type", "cell");
+		}
+	}
+
+	/**
+	 * method for gathering the string of a node value
+	 * 
+	 * @param value the node value
+	 * @return the dot-format-string
+	 */
+	private String getChildrencountString(Object value) {
+
+		if (value instanceof Instance) {
+			int i = 0;
+
+			for (@SuppressWarnings("unused")
+			QName name : ((Instance) value).getPropertyNames()) {
+				i++;
+			}
+			String ivalue = "Instance(" + i + ")";
+			return ivalue;
+
+		}
+		if (value instanceof Group) {
+			int i = 0;
+			for (@SuppressWarnings("unused")
+			QName name : ((Group) value).getPropertyNames()) {
+				i++;
+			}
+			return "Group(" + i + ")";
+		}
+
+		return shortenString(value.toString());
+
+	}
+
+	/**
+	 * utility method for shortening strings in a graph
+	 * 
+	 * @param string the string to shorten
+	 * @return the shortened string
+	 */
+	private String shortenString(String string) {
+
+		if (string.length() > 40) {
+			string = string.substring(0, 37) + "...";
+		}
+		return string;
+
+	}
+}
