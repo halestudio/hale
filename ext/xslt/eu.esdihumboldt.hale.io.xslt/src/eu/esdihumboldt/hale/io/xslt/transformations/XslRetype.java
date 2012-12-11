01296cc5ecb7240c7476165c7cd9ed9a60d13629
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-12-11 11:50:01 +0100

    XslRetype wip
    refs #116
    refs #513
    refs #514
    
    Change-Id: I9a5750e2016ec3bcaf9f221f400260d76143d01d

--- ext/xslt/eu.esdihumboldt.hale.io.xslt/src/eu/esdihumboldt/hale/io/xslt/transformations/XslRetype.java
+++ ext/xslt/eu.esdihumboldt.hale.io.xslt/src/eu/esdihumboldt/hale/io/xslt/transformations/XslRetype.java
@@ -15,14 +15,28 @@
 
 package eu.esdihumboldt.hale.io.xslt.transformations;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Collection;
+
+import javax.xml.stream.XMLStreamWriter;
+
 import org.apache.velocity.VelocityContext;
 
+import eu.esdihumboldt.hale.common.align.model.Alignment;
+import eu.esdihumboldt.hale.common.align.model.AlignmentUtil;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.CellUtil;
 import eu.esdihumboldt.hale.common.align.model.Type;
 import eu.esdihumboldt.hale.common.align.model.impl.TypeEntityDefinition;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationTree;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.impl.TransformationTreeImpl;
+import eu.esdihumboldt.hale.common.align.tgraph.TGraph;
+import eu.esdihumboldt.hale.common.align.tgraph.impl.TGraphImpl;
+import eu.esdihumboldt.hale.common.align.transformation.function.TransformationException;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.io.xslt.XslTransformationUtil;
 import eu.esdihumboldt.hale.io.xslt.XslTypeTransformation;
+import eu.esdihumboldt.hale.io.xslt.transformations.base.AbstractTransformationTraverser;
 import eu.esdihumboldt.hale.io.xslt.transformations.base.AbstractVelocityXslTypeTransformation;
 
 /**
@@ -35,13 +49,76 @@ public class XslRetype extends AbstractVelocityXslTypeTransformation implements
 
 	private static final String CONTEXT_PARAM_SELECT_INSTANCES = "select_instances";
 
+	private static final String CONTEXT_PARAM_PROPERTIES = "properties";
+
 	@Override
-	protected void configureTemplate(VelocityContext context, Cell typeCell) {
+	protected void configureTemplate(final VelocityContext context, final Cell typeCell)
+			throws TransformationException {
 		Type source = (Type) CellUtil.getFirstEntity(typeCell.getSource());
 
 		TypeEntityDefinition ted = source.getDefinition();
 		context.put(CONTEXT_PARAM_SELECT_INSTANCES,
 				XslTransformationUtil.selectInstances(ted, "/", context().getNamespaceContext()));
+
+		String properties = createPropertiesFragment(typeCell);
+		context.put(CONTEXT_PARAM_PROPERTIES, properties);
 	}
 
+	/**
+	 * 
+	 * @param typeCell
+	 * @return
+	 * @throws TransformationException
+	 */
+	protected String createPropertiesFragment(final Cell typeCell) throws TransformationException {
+		/*
+		 * Create the transformation tree with only those property cells related
+		 * to the type cell.
+		 */
+		Type target = (Type) CellUtil.getFirstEntity(typeCell.getTarget());
+		final TransformationTree tree = new TransformationTreeImpl(target.getDefinition()
+				.getDefinition(), context().getAlignment()) {
+
+			@Override
+			protected Collection<? extends Cell> getRelevantPropertyCells(Alignment alignment,
+					TypeDefinition targetType) {
+				return AlignmentUtil.getPropertyCellsFromTypeCell(alignment, typeCell);
+			}
+
+		};
+
+		/*
+		 * Create the transformation graph derived from the transformation tree
+		 * and perform context matching.
+		 */
+		final TGraph graph = new TGraphImpl(tree).proxyMultiResultNodes().performContextMatching();
+
+		// TODO tree as GraphML as informative annotation into XSLT?
+		try {
+			ByteArrayOutputStream propsOut = new ByteArrayOutputStream();
+			try {
+				final XMLStreamWriter writer = XslTransformationUtil.setupXMLWriter(propsOut,
+						context().getNamespaceContext());
+
+				/*
+				 * TODO Do an additional traversal of the transformation tree
+				 * first, where for each target node the condition that it is
+				 * actually needed is determined.
+				 * 
+				 * This would have to be from source to target, as parent target
+				 * nodes must take into account the conditions present on their
+				 * children.
+				 */
+
+				AbstractTransformationTraverser trav = new RetypeTraverser(writer);
+				trav.traverse(graph);
+			} finally {
+				propsOut.close();
+			}
+
+			return propsOut.toString("UTF-8");
+		} catch (Exception e) {
+			throw new TransformationException("Failed to create property transformations", e);
+		}
+	}
 }
