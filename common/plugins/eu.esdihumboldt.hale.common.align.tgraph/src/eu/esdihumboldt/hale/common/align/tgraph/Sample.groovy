7a40b1f615f403df7b32d02688cac7aaff77c85e
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-12-18 17:30:28 +0100

    groovy build test
    Change-Id: I2137d47abde0c96b699931949a048166bdcb76de

---
+++ common/plugins/eu.esdihumboldt.hale.common.align.tgraph/src/eu/esdihumboldt/hale/common/align/tgraph/Sample.groovy
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package eu.esdihumboldt.hale.common.align.tgraph
+
+/**
+ * @author Simon Templer
+ *
+ */
+class Sample {
+	
+	static void main(def args){
+		def mylist=[1,2,"Blub","4"]
+		mylist.each{ println it }
+	  }
+
+}
