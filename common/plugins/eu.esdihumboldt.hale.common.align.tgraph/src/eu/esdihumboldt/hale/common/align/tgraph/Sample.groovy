0b8192019d2060832cbd37c5295488cdfb474869
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-12-18 17:30:28 +0100

    groovy build test
    Change-Id: I2137d47abde0c96b699931949a048166bdcb76de

---
+++ common/plugins/eu.esdihumboldt.hale.common.align.tgraph/src/eu/esdihumboldt/hale/common/align/tgraph/Sample.groovy
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.align.tgraph
+
+/**
+ * Groovy sample class for testing the build.
+ * 
+ * @author Simon Templer
+ */
+class Sample {
+
+	static void main(def args){
+		def mylist=[1,2,"Blub","4"]
+		mylist.each{ println it }
+	}
+
+}
