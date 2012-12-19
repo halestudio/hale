b1e13feec8068ea7d942040176e7c896bbb50b81
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-12-19 15:20:06 +0100

    transformation graph wip
    Change-Id: Ib1094389d0ebe5e340ed405fb9b91a35017564c4

---
+++ common/plugins/eu.esdihumboldt.hale.common.align.tgraph/src/eu/esdihumboldt/hale/common/align/tgraph/TransformationGraph.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.align.tgraph;
+
+import java.util.Set;
+
+import com.google.common.collect.SetMultimap;
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
+
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.CellNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.GroupNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.SourceNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TargetNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationNode;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationTree;
+import eu.esdihumboldt.hale.common.align.model.transformation.tree.visitor.TreeToGraphVisitor;
+
+/**
+ * Helper for transformation graphs based on a {@link TransformationTree}.
+ * 
+ * @author Simon Templer
+ */
+public class TransformationGraph implements TransformationGraphConstants {
+
+	/**
+	 * Create a transformation graph from a transformation tree.
+	 * 
+	 * @param ttree the transformation tree
+	 * @return an in-memory graph created from the transformation tree
+	 */
+	public static Graph create(TransformationTree ttree) {
+		TreeToGraphVisitor graphVisitor = new TreeToGraphVisitor();
+		ttree.accept(graphVisitor);
+
+		SetMultimap<String, String> connections = graphVisitor.getAllConnections();
+		Set<String> ids = graphVisitor.getAllIds();
+
+		Graph graph = new TinkerGraph();
+
+		// add nodes to the graph
+		for (String key : ids) {
+			// create a vertex for each transformation node
+			TransformationNode node = graphVisitor.getNode(key);
+			Vertex vertex = graph.addVertex(key);
+			setVertexProperties(vertex, node);
+		}
+
+		for (String key : connections.keySet()) {
+			for (String value : connections.get(key)) {
+				Vertex targetSide = graph.getVertex(key);
+				Vertex sourceSide = graph.getVertex(value);
+
+				TransformationNode targetSideNode = graphVisitor.getNode(key);
+				TransformationNode sourceSideNode = graphVisitor.getNode(value);
+
+				String edgeLabel;
+				if (sourceSideNode instanceof SourceNode && targetSideNode instanceof SourceNode) {
+					edgeLabel = EDGE_CHILD;
+				}
+				else if (sourceSideNode instanceof SourceNode && targetSideNode instanceof CellNode) {
+					edgeLabel = EDGE_VARIABLE;
+				}
+				else if (sourceSideNode instanceof CellNode && targetSideNode instanceof GroupNode) {
+					edgeLabel = EDGE_RESULT;
+				}
+				else if (sourceSideNode instanceof GroupNode && targetSideNode instanceof GroupNode) {
+					edgeLabel = EDGE_PARENT;
+				}
+				else {
+					throw new IllegalStateException("Invalid relation in transformation tree");
+				}
+
+				Edge edge = graph.addEdge(null, sourceSide, targetSide, edgeLabel);
+				setEdgeProperties(edge, sourceSideNode, targetSideNode);
+			}
+		}
+		return graph;
+	}
+
+	/**
+	 * @param edge
+	 * @param sourceSideNode
+	 * @param targetSideNode
+	 */
+	private static void setEdgeProperties(Edge edge, TransformationNode sourceSideNode,
+			TransformationNode targetSideNode) {
+		if (sourceSideNode instanceof SourceNode && targetSideNode instanceof SourceNode) {
+			setEdgeProperties(edge, (SourceNode) sourceSideNode, (SourceNode) targetSideNode);
+		}
+		else if (sourceSideNode instanceof SourceNode && targetSideNode instanceof CellNode) {
+			setEdgeProperties(edge, (SourceNode) sourceSideNode, (CellNode) targetSideNode);
+		}
+		else if (sourceSideNode instanceof CellNode && targetSideNode instanceof GroupNode) {
+			setEdgeProperties(edge, (CellNode) sourceSideNode, (GroupNode) targetSideNode);
+		}
+		else if (sourceSideNode instanceof GroupNode && targetSideNode instanceof GroupNode) {
+			setEdgeProperties(edge, (GroupNode) sourceSideNode, (GroupNode) targetSideNode);
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static void setEdgeProperties(Edge edge, SourceNode sourceSideNode,
+			SourceNode targetSideNode) {
+		// do nothing
+	}
+
+	@SuppressWarnings("unused")
+	private static void setEdgeProperties(Edge edge, SourceNode sourceSideNode,
+			CellNode targetSideNode) {
+		// do nothing
+	}
+
+	@SuppressWarnings("unused")
+	private static void setEdgeProperties(Edge edge, CellNode sourceSideNode,
+			GroupNode targetSideNode) {
+		// do nothing
+	}
+
+	@SuppressWarnings("unused")
+	private static void setEdgeProperties(Edge edge, GroupNode sourceSideNode,
+			GroupNode targetSideNode) {
+		// do nothing
+	}
+
+	/**
+	 * Set the vertex properties based on the associated transformation tree
+	 * node.
+	 * 
+	 * @param node the transformation tree node
+	 * @param vertex the vertex to update
+	 */
+	private static void setVertexProperties(Vertex vertex, TransformationNode node) {
+		vertex.setProperty(P_ORG_NODE, node);
+		if (node instanceof CellNode) {
+			setVertexProperties(vertex, (CellNode) node);
+		}
+		if (node instanceof SourceNode) {
+			setVertexProperties(vertex, (SourceNode) node);
+		}
+		if (node instanceof GroupNode) {
+			setVertexProperties(vertex, (GroupNode) node);
+		}
+	}
+
+	private static void setVertexProperties(Vertex vertex, CellNode node) {
+		vertex.setProperty(P_TYPE, NodeType.Cell);
+		vertex.setProperty(P_CELL, node.getCell());
+	}
+
+	private static void setVertexProperties(Vertex vertex, SourceNode node) {
+		vertex.setProperty(P_TYPE, NodeType.Source);
+		vertex.setProperty(P_ENTITY, node.getEntityDefinition());
+	}
+
+	private static void setVertexProperties(Vertex vertex, GroupNode node) {
+		vertex.setProperty(P_TYPE, NodeType.Target);
+		if (node instanceof TargetNode) {
+			setVertexProperties(vertex, (TargetNode) node);
+		}
+	}
+
+	private static void setVertexProperties(Vertex vertex, TargetNode node) {
+		vertex.setProperty(P_ENTITY, node.getEntityDefinition());
+	}
+
+}
