60c11522e5087e24e8b3a6e43f2d70f43569cb39
Simon Templer <simon.templer@igd.fraunhofer.de>
2012-12-21 15:24:09 +0100

    experimenting w/ context matching
    refs #376
    
    Change-Id: I9bdbe4664b97012fb7196f3d1222d5b8910cf56f

--- common/plugins/eu.esdihumboldt.hale.common.align.tgraph/src/eu/esdihumboldt/hale/common/align/tgraph/internal/TGraphImpl.groovy
+++ common/plugins/eu.esdihumboldt.hale.common.align.tgraph/src/eu/esdihumboldt/hale/common/align/tgraph/internal/TGraphImpl.groovy
@@ -20,10 +20,14 @@ import com.tinkerpop.blueprints.Edge
 import com.tinkerpop.blueprints.Graph
 import com.tinkerpop.blueprints.Vertex
 import com.tinkerpop.blueprints.util.ElementHelper
+import com.tinkerpop.gremlin.groovy.Gremlin
 
+import eu.esdihumboldt.hale.common.align.model.EntityDefinition
 import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationTree
 import eu.esdihumboldt.hale.common.align.tgraph.TGraph
 import eu.esdihumboldt.hale.common.align.tgraph.TGraphConstants.NodeType
+import eu.esdihumboldt.hale.common.schema.model.ChildDefinition
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.Cardinality
 
 
 
@@ -33,6 +37,29 @@ import eu.esdihumboldt.hale.common.align.tgraph.TGraphConstants.NodeType
  * @author Simon Templer
  */
 class TGraphImpl implements TGraph {
+	
+	static {
+		Gremlin.load()
+		
+		Vertex.metaClass.entity = { ->
+			delegate.getProperty(P_ENTITY)
+		}
+		Vertex.metaClass.cardinality = { ->
+			EntityDefinition entity = delegate.entity() 
+			switch (entity.getDefinition()) {
+				case ChildDefinition:
+					return delegate.entity().getDefinition().getConstraint(Cardinality)
+				default:
+					// e.g. a type
+					//TODO allow different type cardinalities for Join etc.
+					return Cardinality.CC_EXACTLY_ONCE; 
+			}
+			
+		}
+		Cardinality.metaClass.mayOccurMultipleTimes = { ->
+			delegate.maxOccurs == Cardinality.UNBOUNDED || delegate.maxOccurs > 0
+		}
+	}
 
 	/** The internal graph */
 	private Graph graph
@@ -78,7 +105,8 @@ class TGraphImpl implements TGraph {
 					// create a bypass
 
 					Vertex proxy = graph.addVertex()
-					proxy.setProperty P_TYPE, NodeType.Proxy
+					proxy.setProperty P_TYPE, NodeType.Target
+					proxy.setProperty P_PROXY, true
 
 					Edge newResult = graph.addEdge(null,
 							it.getVertex(Direction.IN), proxy, EDGE_RESULT)
@@ -94,4 +122,274 @@ class TGraphImpl implements TGraph {
 
 		this
 	}
+
+	@Override
+	TGraph performContextMatching() {
+		/*
+		 * XXX First experiments with context matching.
+		 * For now looking only at simple examples
+		 * w/o a context hierarchy (e.g. simple rename, cm_multi_2/4) 
+		 */
+
+		// get the target type node
+		def root = getTarget();
+
+		// get all its direct children
+		def children = root.in.has(P_TYPE, NodeType.Target)
+
+		for (Vertex target : children) {
+			// for each child (which is a target node)
+
+			// determine all paths leading to this node
+			/*
+			 * This is only working because there are no cycles in the graph
+			 * and all incoming connections are acceptable. 
+			 */
+			def paths = target.in.loop(1){
+				it.object.inE.hasNext() // loop while there is an incoming edge
+			}.path.toList()
+
+			// reverse paths so they start with source (type) nodes
+			paths = paths.collect{it.reverse()}
+
+			//XXX debug
+			println "Paths: $paths"
+
+			// find candidates for context match
+
+			/*
+			 * All paths have to have at least the one item in common -
+			 * otherwise there is no point on the source side where they
+			 * converge.
+			 */
+			// find the elements equal in all paths
+			def common = findCommonPaths(paths)
+
+			// only retain source nodes
+			common.retainAll{
+				it.getProperty(P_TYPE) == NodeType.Source
+			}
+
+			//XXX debug
+			println "Common: $common"
+			
+			/*
+			 * TODO Candidate configured in mapping???
+			 */
+
+			/*
+			 * TODO Analyze common elements
+			 * Are there any outgoing connections other than the determined paths?
+			 * Are they candidates for the context match?
+			 * Changes to the graph may be necessary to use an element as context.  
+			 */
+			def candidates = []
+			def sourcePath = []
+			for (source in common) {
+				// determine if a source node is a valid candidate
+				
+				// add the source node to the source path to obtain its path
+				sourcePath.add source
+
+				/*
+				 * Check if source and target form a self-contained sub-graph.
+				 * 
+				 * This means that there may be no other routes outgoing from
+				 * source through the graph than those in the paths list. 
+				 */
+				boolean sc = checkSelfContained(sourcePath, paths)
+				
+				if (sc) {
+					// is a valid candidate
+					candidates.add source
+				}
+				else {
+					/*
+					 * This doesn't necessarily mean that this is not a valid
+					 * candidate, but that a change to the graph may be needed
+					 * to use it as candidate. 
+					 */
+					//XXX for now, we stop here
+					
+					// get the node entity
+					EntityDefinition entity = source.getProperty(P_ENTITY)
+					//TODO
+				}
+			}
+
+			/*
+			 * TODO Check for alternative contexts by grouping child nodes that
+			 * occur in the paths. 
+			 */
+			
+			//XXX debug
+			println "Candidates: $candidates"
+			
+			/*
+			 * TODO Determine an appropriate default candidate?!
+			 */
+			def context = findBestCandidate(candidates, target)
+			
+			//XXX debug
+			println "Selected candidate: $context"
+			
+			//TODO create the context edge?!
+
+			/*
+			 * XXX Now what about the children of node?
+			 * XXX How to pass the node context as parent context?
+			 */
+		}
+	}
+	
+	/**
+	 * Find the best candidate for a context match from the given list of
+	 * candidates.
+	 * 
+	 * @param candidates the list of context match candidates, these are source
+	 *   nodes (XXX later maybe also sets of nodes)
+	 * @param target the target node
+	 * @return
+	 */
+	private def findBestCandidate(def candidates, def target) {
+		assert candidates
+		
+		if (candidates.size() == 1) {
+			return candidates[0]
+		}
+		
+		// find the best candidates based on cardinality
+		boolean targetMul = target.cardinality().mayOccurMultipleTimes()
+		
+		//XXX assuming that candidates are ordered by hierarchy
+		
+		if (targetMul) {
+			/*
+			 * Strategy: Find candidate highest in the hierarchy that also
+			 * allows multiple values.
+			 */
+			for (candidate in candidates) {
+				if (candidate.cardinality().mayOccurMultipleTimes()) {
+					return candidate
+				}
+			}
+		}
+		else {
+			/*
+			 * Strategy: Find candidate highest in the hierarchy
+			 */
+			return candidates[0]
+		}
+		
+		// fall back to the candidate highest in the hierarchy
+		return candidates[0]
+	}
+
+	/**
+	 * Find the common elements in a list of paths.
+	 * 
+	 * @param paths the paths, a list of lists
+	 * @return the list of common leading elements
+	 */
+	private def findCommonPaths(def paths) {
+		assert paths
+		assert paths.size() > 0
+
+		def common
+		for (path in paths) {
+			if (common == null) {
+				common = path
+			} else {
+				for (int i = 0; i < Math.min(common.size(), path.size()); i++) {
+					if (common[i] != path[i]) {
+						common = common.take(i)
+						break;
+					}
+				}
+			}
+		}
+		return common
+	}
+	
+	/**
+	 * Check if a source and target node form a self contained sub-graph.
+	 * 
+	 * @param sourcePath the path leading to the source node
+	 * @param targetPaths the list of paths leading to the target node
+	 * @return if the sub-graph is self contained
+	 */
+	private boolean checkSelfContained(def sourcePath, def targetPaths) {
+		/*
+		 * For the sub-graph to be self contained there may be no other routes
+		 * outgoing from the source node through the graph than those in the
+		 * target paths.
+		 */
+		
+		// the source node
+		def source = sourcePath.last()
+		
+		// cripple paths to represent path after source
+		def paths = targetPaths.collect{
+			// assure that target paths start with source path
+			assert sourcePath == it.take(sourcePath.size())
+			
+			it.subList(sourcePath.size(), it.size())
+		}
+		
+		// check if there are any other paths and
+		// check the same for the source children
+		checkOnlyPaths(source, paths)
+	}
+	
+	/**
+	 * Check if the given paths are the only paths leading from the given
+	 * source node. If there are no paths given, the check will terminate
+	 * successfully (as the paths target is reached).
+	 *
+	 * @param source the source node
+	 * @param paths the list of paths
+	 * @return if these paths are the only paths leading away from source
+	 */
+	private boolean checkOnlyPaths(def source, def paths) {
+		// remove all empty paths
+		paths.retainAll{
+			!it.empty
+		}
+		
+		// if no paths, terminate check
+		if (paths.empty) {
+			return true
+		}
+		
+		// collect the first nodes in the paths in a set
+		def targets = paths.collect{
+			it[0]
+		}.toSet()
+		
+		for (outgoing in source.out) { //TODO what about context edges
+			if (!targets.contains(outgoing)) {
+				/*
+				 * If the outgoing node is not contained in the targets, there
+				 * are additional paths 
+				 */
+				return false
+			}
+			// determine the paths that should be going out from this vertex
+			def outPaths = paths.findResults{
+				if (it[0] == outgoing) {
+					it.subList(1, it.size())
+				}
+				else {
+					null
+				}
+			}
+			if (!checkOnlyPaths(outgoing, outPaths)) {
+				// check paths from this node
+				return false
+			}
+		}
+		
+		return true
+	}
+
 }
