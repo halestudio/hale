a473a92f65d7f9b6fc17b4737ae9d3461997d34b
Simon Templer <simon@templer.cc>
2013-01-26 07:48:11 +0100

    save/load alignment using JAXB (wip)
    ...and added support for cell annotations/documentation.
    Implemented example AnnotationDescriptor used in test case.
    
    Change-Id: If201b39fd284a2f22159ac6224ecc37bbcb87f70

---
+++ common/plugins/eu.esdihumboldt.hale.common.align.test/src/eu/esdihumboldt/hale/common/align/io/TestAnnotationDescriptor.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.align.io
+
+import org.w3c.dom.Element
+
+import eu.esdihumboldt.hale.common.align.model.AnnotationDescriptor
+import groovy.xml.DOMBuilder
+import groovy.xml.dom.DOMCategory
+
+
+/**
+ * Annotation descriptor for test purposes only.<br>
+ * <br>
+ * Annotation markup looks like this:<br>
+ * <code>&lt;comment author=&quot;Name&quot;&gt;Text&lt;/comment&gt;</code>
+ * 
+ * @author Simon Templer
+ */
+class TestAnnotationDescriptor implements AnnotationDescriptor<TestAnnotation> {
+
+	private static final String NS = 'http://www.esdi-humboldt.eu/hale/test'
+
+	@Override
+	TestAnnotation create() {
+		return new TestAnnotation()
+	}
+
+	@Override
+	TestAnnotation fromDOM(Element fragment) {
+		// retrieve values using DOMCategory
+		use (DOMCategory) {
+			return new TestAnnotation(author: fragment.'@author', comment: fragment.text())
+		}
+	}
+
+	@Override
+	Element toDOM(TestAnnotation annotation) {
+		// using DOMBuilder create the DOM structure from the annotation object
+		def builder = DOMBuilder.newInstance()
+		// w/o namespace
+		//def fragment = builder.comment(author: annotation.author, annotation.comment)
+		// with namespace (cleaner document)
+		def fragment = builder.'test:comment'('xmlns:test': NS, author: annotation.author, annotation.comment)
+
+		return fragment;
+	}
+}
---
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/io/impl/internal/AlignmentToJaxb.groovy
@@ -0,0 +1,188 @@
+
+
+
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.align.io.impl.internal
+
+import java.util.Map.Entry
+
+import eu.esdihumboldt.hale.common.align.extension.annotation.AnnotationExtension
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.AlignmentType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.AnnotationType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.CellType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.ClassType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.ChildContextType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.ConditionType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.DocumentationType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.NamedEntityType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.ObjectFactory
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.ParameterType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.PropertyType
+import eu.esdihumboldt.hale.common.align.io.impl.internal.generated.ClassType.Type
+import eu.esdihumboldt.hale.common.align.model.Alignment
+import eu.esdihumboldt.hale.common.align.model.Cell
+import eu.esdihumboldt.hale.common.align.model.ChildContext
+import eu.esdihumboldt.hale.common.align.model.Entity
+import eu.esdihumboldt.hale.common.align.model.ParameterValue
+import eu.esdihumboldt.hale.common.align.model.Property
+import eu.esdihumboldt.hale.common.core.io.report.IOReporter
+import eu.esdihumboldt.hale.common.instance.extension.filter.FilterDefinitionManager
+import eu.esdihumboldt.hale.common.instance.model.Filter
+
+
+
+
+/**
+ * Converts an {@link Alignment} to a {@link AlignmentType} for serialization with JAXB.
+ * 
+ * @author Simon Templer
+ */
+class AlignmentToJaxb {
+
+	private final Alignment alignment
+	private final IOReporter reporter
+	private final ObjectFactory of = new ObjectFactory()
+
+	/**
+	 * Create a new converter.
+	 * 
+	 * @param alignment the alignment to convert
+	 * @param reporter the reporter to use for reporting problems, may be <code>null</code>
+	 */
+	AlignmentToJaxb(Alignment alignment, IOReporter reporter) {
+		this.alignment = alignment
+		this.reporter = reporter
+	}
+
+	/**
+	 * @return
+	 */
+	AlignmentType convert() throws Exception {
+		AlignmentType align = new AlignmentType()
+
+		// convert cells
+		for (Cell cell in alignment.cells) {
+			align.cell << convert(cell)
+		}
+
+		return align
+	}
+
+	protected CellType convert(Cell cell) {
+		CellType result = new CellType()
+
+		// the transformation id
+		result.relation = cell.transformationIdentifier
+
+		// the transformation parameters
+		cell.transformationParameters?.entries()?.each { Entry<String, ParameterValue> param ->
+			result.parameter << convert(param.key, param.value)
+		}
+
+		// source entities
+		cell.source?.entries()?.each { Entry<String, ? extends Entity> entity ->
+			result.source << convert(entity.key, entity.value)
+		}
+
+		// target entities
+		cell.target?.entries()?.each { Entry<String, ? extends Entity> entity ->
+			result.target << convert(entity.key, entity.value)
+		}
+		
+		// documentations
+		cell.documentation.entries().each {
+			// create documentation element from each multimap entry
+			result.documentationOrAnnotation << new DocumentationType(type: it.key, value: it.value)
+		}
+		
+		// annotations
+		for (String type in cell.annotationTypes) {
+			def descriptor = AnnotationExtension.instance.get(type)
+			if (descriptor) {
+				for (Object value : cell.getAnnotations(type)) {
+					result.documentationOrAnnotation << new AnnotationType(type: type, any: descriptor.toDOM(value))
+				}
+			}
+			else {
+				//TODO report
+			}
+		}
+
+		return result
+	}
+
+	protected ParameterType convert(String name, ParameterValue value) {
+		new ParameterType(name: name, value: value.value, type: value.type ?: ParameterValue.DEFAULT_TYPE)
+	}
+
+	protected NamedEntityType convert(String name, Entity entity) {
+		NamedEntityType result = new NamedEntityType()
+
+		// the entity name
+		result.name = name
+
+		// create the entity object
+		switch (entity) {
+			case eu.esdihumboldt.hale.common.align.model.Type:
+			result.abstractEntity = of.createClass(new ClassType())
+			break
+			case Property:
+			result.abstractEntity = of.createProperty(new PropertyType())
+			break
+			default:
+			throw new IllegalArgumentException("Illegal entity ${entity.class}")
+		}
+
+		// set the type
+		result.abstractEntity.value.type = new Type(
+			name: entity.definition.type.name.localPart,
+			ns: entity.definition.type.name.namespaceURI ?: null)
+		result.abstractEntity.value.type.condition = entity.definition.filter ? 
+			convert(entity.definition.filter) : null
+
+		// add children
+		if (entity instanceof Property) {
+			for (ChildContext child in entity.definition.propertyPath) {
+				result.abstractEntity.value.child << convert(child)
+			}
+		}
+
+		return result
+	}
+
+	protected ChildContextType convert(ChildContext context) {
+		ChildContextType result = new ChildContextType()
+
+		result.name = context.child.name.localPart
+		result.ns = context.child.name.namespaceURI ?: null
+		
+		result.context = context.contextName
+		result.index = context.index
+		result.condition = context.condition ? convert(context.condition.filter) : null
+
+		return result
+	}
+
+	protected ConditionType convert(Filter filter) {
+		if (!filter) return null
+
+		def rep = FilterDefinitionManager.getInstance().asPair(filter)
+		
+		new ConditionType(lang: rep.first, value: rep.second)
+	}
+
+}
