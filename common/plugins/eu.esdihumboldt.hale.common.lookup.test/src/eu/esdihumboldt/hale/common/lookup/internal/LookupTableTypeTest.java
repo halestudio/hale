13c8cda9ca72f1c723735bd266271ac7009f4fec
Simon Templer <simon@templer.cc>
2013-02-03 19:26:18 +0100

    lookup table complex value representation prototype
    Not yet tested with complex values inside the lookup table.
    
    Change-Id: Ia4e86f53f48eed0a97d5e7c1775c0dee35a328ee

---
+++ common/plugins/eu.esdihumboldt.hale.common.lookup.test/src/eu/esdihumboldt/hale/common/lookup/internal/LookupTableTypeTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.lookup.internal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+import org.w3c.dom.Element;
+
+import com.google.common.collect.Sets;
+
+import eu.esdihumboldt.hale.common.core.io.HaleIO;
+import eu.esdihumboldt.hale.common.core.io.Value;
+import eu.esdihumboldt.hale.common.lookup.LookupTable;
+import eu.esdihumboldt.hale.common.lookup.impl.LookupTableImpl;
+
+/**
+ * Tests for {@link LookupTable} complex value conversion.
+ * 
+ * @author Simon Templer
+ */
+public class LookupTableTypeTest {
+
+	/**
+	 * Test if a simple lookup table containing only string values is the same
+	 * when converted to DOM and back again.
+	 */
+	@Test
+	public void testStringLookup() {
+		Map<Value, Value> values = new HashMap<>();
+
+		values.put(Value.of("a"), Value.of("1"));
+		values.put(Value.of("b"), Value.of("1"));
+		values.put(Value.of("c"), Value.of("1"));
+		values.put(Value.of("d"), Value.of("2"));
+		values.put(Value.of("e"), Value.of("2"));
+
+		LookupTable org = new LookupTableImpl(values);
+
+		// convert to DOM
+		Element fragment = HaleIO.getComplexElement(org);
+
+		// convert back
+		LookupTable conv = HaleIO.getComplexValue(fragment, LookupTable.class);
+
+		checkTable(conv, values);
+	}
+
+	private void checkTable(LookupTable conv, Map<Value, Value> values) {
+		assertTrue(Sets.difference(conv.getKeys(), values.keySet()).isEmpty());
+
+		for (Entry<Value, Value> entry : values.entrySet()) {
+			assertEquals(entry.getValue(), conv.lookup(entry.getKey()));
+		}
+	}
+
+}
---
+++ common/plugins/eu.esdihumboldt.hale.common.lookup/src/eu/esdihumboldt/hale/common/lookup/internal/LookupTableType.groovy
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.lookup.internal
+
+import org.w3c.dom.Element
+
+import eu.esdihumboldt.hale.common.core.io.ComplexValueType
+import eu.esdihumboldt.hale.common.core.io.Value
+import eu.esdihumboldt.hale.common.core.io.impl.ElementValue
+import eu.esdihumboldt.hale.common.lookup.LookupTable
+import eu.esdihumboldt.hale.common.lookup.impl.LookupTableImpl
+import groovy.xml.DOMBuilder
+import groovy.xml.dom.DOMCategory
+
+
+/**
+ * Lookup table complex value type representation.
+ * 
+ * @author Simon Templer
+ */
+class LookupTableType implements ComplexValueType<LookupTable> {
+
+	private static Value fromTag(def element) {
+		if (element.'@value') {
+			// string representation
+			return Value.of(element.'@value')
+		}
+		else {
+			// DOM representation
+			return new ElementValue(element.'*'[0]);
+		}
+	}
+
+	@Override
+	public LookupTable fromDOM(Element fragment) {
+		Map<Value, Value> values = new HashMap<Value, Value>();
+
+		use (DOMCategory) {
+			for (entry in fragment.entry) {
+				Value key = fromTag(entry.key[0]);
+				Value value = fromTag(entry.value[0])
+				values.put(key, value)
+			}
+		}
+
+		return new LookupTableImpl(values);
+	}
+
+	@Override
+	public Element toDOM(LookupTable table) {
+		def builder = DOMBuilder.newInstance(false, true)
+
+		def valueTag = { String tagName, Value value ->
+			if (value.isRepresentedAsDOM()) {
+				def element = builder."$tagName"
+				def child = value.getDOMRepresentation();
+				//TODO somehow add child to element
+			}
+			else {
+				builder."$tagName"(value: value.stringRepresentation)
+			}
+		}
+
+		def fragment = builder.'lookup-table' {
+			for (Value key in table.keys) {
+				entry {
+					valueTag('key', key)
+					valueTag('value', table.lookup(key))
+				}
+			}
+		}
+
+		return fragment;
+	}
+}
