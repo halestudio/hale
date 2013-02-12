72b9246a4600662ec9e5ae1bf228fc049f694f94
Simon Templer <simon@templer.cc>
2013-02-12 21:38:50 +0100

    made documentation title optional in annotation
    Change-Id: Ic3afe7bd3555907ab72e79b5162295974f20d95e

--- server/plugins/eu.esdihumboldt.hale.server.api.war/src/eu/esdihumboldt/hale/server/api/internal/wadl/doc/WDoc.groovy
+++ server/plugins/eu.esdihumboldt.hale.server.api.war/src/eu/esdihumboldt/hale/server/api/internal/wadl/doc/WDoc.groovy
@@ -31,7 +31,7 @@ import java.lang.annotation.Target
 	/**
 	 * The documentation title.
 	 */
-	String title()
+	String title() default ''
 	/**
 	 * The closure describing the documentation content.
 	 * XXX
