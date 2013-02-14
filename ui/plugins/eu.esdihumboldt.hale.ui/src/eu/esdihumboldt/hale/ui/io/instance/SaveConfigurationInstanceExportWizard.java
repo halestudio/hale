294971385fcec30353094ff3ec8c581c69c07e64
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2013-02-14 15:31:20 +0100

    added wizards for saving and loading export configurations
    refs #398
    
    Change-Id: Ia0c8cfc575640bdef7ac66af90988f228acd8fe7

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/instance/SaveConfigurationInstanceExportWizard.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.io.instance;
+
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.ui.PlatformUI;
+
+import eu.esdihumboldt.hale.common.core.io.ExportProvider;
+import eu.esdihumboldt.hale.common.core.io.IOProvider;
+import eu.esdihumboldt.hale.common.core.io.project.model.IOConfiguration;
+import eu.esdihumboldt.hale.common.core.io.project.model.Project;
+import eu.esdihumboldt.hale.common.core.io.report.IOReport;
+import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
+import eu.esdihumboldt.hale.ui.service.project.ProjectService;
+
+/**
+ * Save instance export configuration in project file
+ * 
+ * @author Patrick Lieb
+ */
+public class SaveConfigurationInstanceExportWizard extends InstanceExportWizard {
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.IOWizard#execute(eu.esdihumboldt.hale.common.core.io.IOProvider,
+	 *      eu.esdihumboldt.hale.common.core.io.report.IOReporter)
+	 */
+	@Override
+	protected IOReport execute(IOProvider provider, IOReporter defaultReporter) {
+
+		IOConfiguration configuration = new IOConfiguration();
+		// store the (export) configuration of the provider in the new io
+		// configuration
+		configuration.setActionId(getActionId());
+		configuration.setProviderId(getProviderFactory().getIdentifier());
+		provider.storeConfiguration(configuration.getProviderConfiguration());
+
+		ProjectService ps = (ProjectService) PlatformUI.getWorkbench().getService(
+				ProjectService.class);
+		Project project = (Project) ps.getProjectInfo();
+		// target is not set here and also not needed for the configuration
+		configuration.getProviderConfiguration().remove(ExportProvider.PARAM_TARGET);
+		// add the configuration to the export configurations of the project
+		project.getExportConfigurations().add(configuration);
+
+		// no provider is executed so we return the default reporter
+		defaultReporter.setSuccess(true);
+		return defaultReporter;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.ExportWizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+		// add page to save current export configuration
+		addPage(new SaveConfigurationInstanceExportNamePage());
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.IOWizard#getNextPage(org.eclipse.jface.wizard.IWizardPage)
+	 */
+	@Override
+	public IWizardPage getNextPage(IWizardPage page) {
+		IWizardPage nextPage = super.getNextPage(page);
+		if (nextPage != null) {
+			if (!(nextPage.getName().equals("export.selTarget"))) {
+				return nextPage;
+			}
+			else {
+				// no selection of the export file is desired, so we skip the
+				// select export select target page
+				getSelectTargetPage().setPageComplete(true);
+				return super.getNextPage(getSelectTargetPage());
+			}
+		}
+		return nextPage;
+	}
+}
