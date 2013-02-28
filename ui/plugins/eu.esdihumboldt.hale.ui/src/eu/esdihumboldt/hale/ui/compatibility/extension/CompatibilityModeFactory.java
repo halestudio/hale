1e7ddecb169568cebe12855caa8155e09c735821
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2013-02-28 15:06:34 +0100

    Compatibility Mode service and UI components
    Change-Id: Ide4f89abca71e48a4571d71ecffc7f1acbe7b389

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/compatibility/extension/CompatibilityModeFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.compatibility.extension;
+
+import de.cs3d.util.eclipse.extension.ExtensionObjectFactory;
+import eu.esdihumboldt.hale.common.align.compatibility.CompatibilityMode;
+
+/**
+ * The compatibility mode factory interface
+ * 
+ * @author Sebastian Reinhardt
+ */
+public interface CompatibilityModeFactory extends ExtensionObjectFactory<CompatibilityMode> {
+	/* ignore */
+}
