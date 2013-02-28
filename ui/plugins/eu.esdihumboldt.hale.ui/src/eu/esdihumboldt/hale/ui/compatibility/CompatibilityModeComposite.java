71bd9da9b50cf3d05a650ba84ecf79daf7cb0a2f
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2013-02-28 15:06:34 +0100

    Compatibility Mode service and UI components
    Change-Id: Ide4f89abca71e48a4571d71ecffc7f1acbe7b389

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/compatibility/CompatibilityModeComposite.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.compatibility;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.menus.WorkbenchWindowControlContribution;
+
+import de.cs3d.util.eclipse.extension.exclusive.ExclusiveExtension.ExclusiveExtensionListener;
+import eu.esdihumboldt.hale.common.align.compatibility.CompatibilityMode;
+import eu.esdihumboldt.hale.ui.common.CommonSharedImages;
+import eu.esdihumboldt.hale.ui.compatibility.extension.CompatibilityModeFactory;
+import eu.esdihumboldt.hale.ui.compatibility.extension.CompatibilityService;
+
+/**
+ * UI Composite containing elements displaying the current compatibility mode
+ * and its status
+ * 
+ * @author Sebastian Reinhardt
+ */
+public class CompatibilityModeComposite extends WorkbenchWindowControlContribution {
+
+	CompatibilityService cs;
+	ExclusiveExtensionListener<CompatibilityMode, CompatibilityModeFactory> modeListener;
+	CompatibilityServiceListener compListener;
+
+	/**
+	 * standard constructor, retrieves the current compatibility service
+	 */
+	public CompatibilityModeComposite() {
+		super();
+		this.cs = (CompatibilityService) PlatformUI.getWorkbench().getService(
+				CompatibilityService.class);
+	}
+
+	/**
+	 * standart constructor with id
+	 * 
+	 * @param id the id
+	 */
+	public CompatibilityModeComposite(String id) {
+		super(id);
+	}
+
+	/**
+	 * @see org.eclipse.jface.action.ControlContribution#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected Control createControl(Composite parent) {
+
+		// initiate the composite for the compatibility elements
+		Composite comp = new Composite(parent, SWT.NONE);
+		comp.setLayout(new RowLayout(SWT.HORIZONTAL));
+
+		// label for displaying the status of the mode
+		final Label statusLabel = new Label(comp, SWT.NONE);
+		statusLabel.setImage(CommonSharedImages.getImageRegistry().get(
+				CommonSharedImages.IMG_SIGNED_YES));
+
+		// combo for selecting/changing compatibility modes
+		final CCombo combo = new CCombo(comp, SWT.READ_ONLY);
+		for (CompatibilityModeFactory fac : cs.getFactories()) {
+			combo.add(fac.getDisplayName());
+		}
+
+		combo.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				for (CompatibilityModeFactory fac : cs.getFactories()) {
+					if (fac.getDisplayName().equals(combo.getItem(combo.getSelectionIndex()))) {
+						cs.setCurrent(fac);
+					}
+				}
+
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// do nothing
+			}
+		});
+
+		modeListener = new ExclusiveExtensionListener<CompatibilityMode, CompatibilityModeFactory>() {
+
+			@Override
+			public void currentObjectChanged(final CompatibilityMode arg0,
+					final CompatibilityModeFactory arg1) {
+				PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+					@Override
+					public void run() {
+						for (int i = 0; i < combo.getItems().length; i++) {
+							if (combo.getItems()[i].equals(arg1.getDisplayName())) {
+								combo.select(i);
+							}
+						}
+					}
+				});
+			}
+
+		};
+		cs.addListener(modeListener);
+
+		compListener = new CompatibilityServiceListener() {
+
+			@Override
+			public void compatibilityChanged(final boolean isCompatible, final String notification) {
+				PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+					@Override
+					public void run() {
+						if (isCompatible) {
+							statusLabel.setImage(CommonSharedImages.getImageRegistry().get(
+									CommonSharedImages.IMG_SIGNED_YES));
+							statusLabel.setToolTipText(notification);
+						}
+						if (!isCompatible) {
+							statusLabel.setImage(CommonSharedImages.getImageRegistry().get(
+									CommonSharedImages.IMG_SIGNED_NO));
+							statusLabel.setToolTipText(notification);
+						}
+					}
+				});
+			}
+		};
+		cs.addCompatibilityListener(compListener);
+		if (combo.getItems().length != 0) {
+			combo.select(0);
+		}
+		return comp;
+	}
+
+	/**
+	 * @see org.eclipse.jface.action.ContributionItem#dispose()
+	 */
+	@Override
+	public void dispose() {
+		cs.removeListener(modeListener);
+		cs.removeCompatibilityListener(compListener);
+		super.dispose();
+	}
+}
---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/compatibility/extension/impl/CompatibilityServiceImpl.java
@@ -0,0 +1,331 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.compatibility.extension.impl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.ui.PlatformUI;
+
+import de.cs3d.ui.util.eclipse.extension.exclusive.PreferencesExclusiveExtension;
+import de.cs3d.util.eclipse.extension.AbstractConfigurationFactory;
+import de.cs3d.util.eclipse.extension.AbstractExtension;
+import de.cs3d.util.eclipse.extension.AbstractObjectFactory;
+import de.cs3d.util.eclipse.extension.ExtensionObjectDefinition;
+import de.cs3d.util.eclipse.extension.ExtensionObjectFactory;
+import de.cs3d.util.eclipse.extension.ObjectExtension;
+import eu.esdihumboldt.cst.internal.CSTCompatibilityMode;
+import eu.esdihumboldt.hale.common.align.compatibility.CompatibilityMode;
+import eu.esdihumboldt.hale.common.align.model.Cell;
+import eu.esdihumboldt.hale.ui.compatibility.CompatibilityServiceListener;
+import eu.esdihumboldt.hale.ui.compatibility.extension.CompatibilityModeFactory;
+import eu.esdihumboldt.hale.ui.compatibility.extension.CompatibilityService;
+import eu.esdihumboldt.hale.ui.internal.HALEUIPlugin;
+import eu.esdihumboldt.hale.ui.service.align.AlignmentService;
+import eu.esdihumboldt.hale.ui.service.align.AlignmentServiceListener;
+
+/**
+ * Compatibility Service, main class and extension for handling all issues for
+ * compatibility (e.g. CST or Xslt)
+ * 
+ * @author Sebastian Reinhardt
+ */
+public class CompatibilityServiceImpl extends
+		PreferencesExclusiveExtension<CompatibilityMode, CompatibilityModeFactory> implements
+		CompatibilityService {
+
+	// stored listeners of the service
+	private final CopyOnWriteArraySet<CompatibilityServiceListener> listeners = new CopyOnWriteArraySet<CompatibilityServiceListener>();
+
+	/**
+	 * Default factory based on a configuration element.
+	 */
+	private static class CompatibilityModeFactoryImpl extends
+			AbstractConfigurationFactory<CompatibilityMode> implements CompatibilityModeFactory {
+
+		/**
+		 * Create an instance view factory based on a configuration element.
+		 * 
+		 * @param conf the configuration element
+		 */
+		protected CompatibilityModeFactoryImpl(IConfigurationElement conf) {
+			super(conf, "class");
+		}
+
+		/**
+		 * @see ExtensionObjectFactory#dispose(java.lang.Object)
+		 */
+		@Override
+		public void dispose(CompatibilityMode instance) {
+			// dispose is handled externally
+		}
+
+		/**
+		 * @see ExtensionObjectDefinition#getIdentifier()
+		 */
+		@Override
+		public String getIdentifier() {
+			return conf.getAttribute("id");
+		}
+
+		/**
+		 * @see ExtensionObjectDefinition#getDisplayName()
+		 */
+		@Override
+		public String getDisplayName() {
+			return conf.getAttribute("name");
+		}
+
+	}
+
+	/**
+	 * Default fallback factory
+	 */
+	private static class CompatibilityDefaultFactory extends
+			AbstractObjectFactory<CompatibilityMode> implements CompatibilityModeFactory {
+
+		/**
+		 * @see ExtensionObjectFactory#createExtensionObject()
+		 */
+		@Override
+		public CompatibilityMode createExtensionObject() throws Exception {
+			return new CSTCompatibilityMode();
+		}
+
+		/**
+		 * @see ExtensionObjectFactory#dispose(java.lang.Object)
+		 */
+		@Override
+		public void dispose(CompatibilityMode instance) {
+			// dispose is handled externally
+		}
+
+		/**
+		 * @see ExtensionObjectDefinition#getIdentifier()
+		 */
+		@Override
+		public String getIdentifier() {
+			return "eu.esdihumboldt.hale.ui.compatibility";
+		}
+
+		/**
+		 * @see ExtensionObjectDefinition#getDisplayName()
+		 */
+		@Override
+		public String getDisplayName() {
+			return ("CST Compatibility");
+		}
+
+		/**
+		 * @see de.cs3d.util.eclipse.extension.ExtensionObjectDefinition#getTypeName()
+		 */
+		@SuppressWarnings("restriction")
+		@Override
+		public String getTypeName() {
+			return CSTCompatibilityMode.class.getName();
+		}
+
+	}
+
+	/**
+	 * default service constructor, also adds a listener to the alignment
+	 * service
+	 */
+	public CompatibilityServiceImpl() {
+		super(new CompatibilityModeExtension(), HALEUIPlugin.getDefault().getPreferenceStore(),
+				"compatibilityMode");
+		((AlignmentService) PlatformUI.getWorkbench().getService(AlignmentService.class))
+				.addListener(new CompatibilityAlignmentListener());
+	}
+
+	/**
+	 * The extension ID
+	 */
+	public static final String EXTENSION_ID = "eu.esdihumboldt.hale.align.compatibility";
+
+	/**
+	 * {@link CompatibilityMode} extension
+	 */
+	public static class CompatibilityModeExtension extends
+			AbstractExtension<CompatibilityMode, CompatibilityModeFactory> implements
+			ObjectExtension<CompatibilityMode, CompatibilityModeFactory> {
+
+		/**
+		 * Default constructor
+		 */
+		public CompatibilityModeExtension() {
+			super(EXTENSION_ID);
+		}
+
+		/**
+		 * @see AbstractExtension#createFactory(IConfigurationElement)
+		 */
+		@Override
+		protected CompatibilityModeFactory createFactory(IConfigurationElement conf)
+				throws Exception {
+			if (conf.getName().equals("compatibility")) {
+				return new CompatibilityModeFactoryImpl(conf);
+			}
+
+			return null;
+		}
+
+	}
+
+	/**
+	 * @see de.cs3d.ui.util.eclipse.extension.exclusive.PreferencesExclusiveExtension#getFallbackFactory()
+	 */
+	@Override
+	protected CompatibilityModeFactory getFallbackFactory() {
+		return new CompatibilityDefaultFactory();
+	}
+
+	/**
+	 * Compatibility Listener for the Alignment
+	 * 
+	 * @author Sebastian Reinhardt
+	 */
+	class CompatibilityAlignmentListener implements AlignmentServiceListener {
+
+		List<Cell> incompatibleCells;
+
+		/**
+		 * 
+		 */
+		public CompatibilityAlignmentListener() {
+			incompatibleCells = new ArrayList<Cell>();
+		}
+
+		/**
+		 * @see eu.esdihumboldt.hale.ui.service.align.AlignmentServiceListener#alignmentCleared()
+		 */
+		@Override
+		public void alignmentCleared() {
+			incompatibleCells.clear();
+			finish();
+		}
+
+		/**
+		 * @see eu.esdihumboldt.hale.ui.service.align.AlignmentServiceListener#cellsAdded(java.lang.Iterable)
+		 */
+		@Override
+		public void cellsAdded(Iterable<Cell> cells) {
+			Iterator<Cell> cit = cells.iterator();
+			while (cit.hasNext()) {
+				Cell cell = cit.next();
+				boolean isCompatibleNow = getCurrent().supportsFunction(
+						cell.getTransformationIdentifier());
+
+				if (!isCompatibleNow) {
+					incompatibleCells.add(cell);
+				}
+
+			}
+			finish();
+		}
+
+		/**
+		 * @see eu.esdihumboldt.hale.ui.service.align.AlignmentServiceListener#cellReplaced(eu.esdihumboldt.hale.common.align.model.Cell,
+		 *      eu.esdihumboldt.hale.common.align.model.Cell)
+		 */
+		@Override
+		public void cellReplaced(Cell oldCell, Cell newCell) {
+			if (incompatibleCells.contains(oldCell)) {
+				incompatibleCells.remove(oldCell);
+			}
+
+			if (!getCurrent().supportsFunction(newCell.getTransformationIdentifier())) {
+				incompatibleCells.add(newCell);
+			}
+
+			finish();
+		}
+
+		/**
+		 * @see eu.esdihumboldt.hale.ui.service.align.AlignmentServiceListener#cellsRemoved(java.lang.Iterable)
+		 */
+		@Override
+		public void cellsRemoved(Iterable<Cell> cells) {
+			Iterator<Cell> cit = cells.iterator();
+			while (cit.hasNext()) {
+				Cell cell = cit.next();
+
+				if (incompatibleCells.contains(cell)) {
+					incompatibleCells.remove(cell);
+				}
+			}
+			finish();
+		}
+
+		/**
+		 * @see eu.esdihumboldt.hale.ui.service.align.AlignmentServiceListener#cellsPropertyChanged(java.lang.Iterable,
+		 *      java.lang.String)
+		 */
+		@Override
+		public void cellsPropertyChanged(Iterable<Cell> cells, String propertyName) {
+			// Do nothing
+
+		}
+
+		/**
+		 * Completes the checkup after the alignment changes were checked
+		 */
+		public void finish() {
+			if (!incompatibleCells.isEmpty()) {
+				for (CompatibilityServiceListener listener : listeners) {
+					listener.compatibilityChanged(false, "Incompatibility detected!");
+				}
+			}
+
+			if (incompatibleCells.isEmpty())
+				for (CompatibilityServiceListener listener : listeners) {
+					listener.compatibilityChanged(true, "No compatibility problems detected!");
+				}
+		}
+
+		/**
+		 * @see eu.esdihumboldt.hale.ui.service.align.AlignmentServiceListener#alignmentChanged()
+		 */
+		@Override
+		public void alignmentChanged() {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.compatibility.extension.CompatibilityService#addCompatibilityListener(eu.esdihumboldt.hale.ui.compatibility.CompatibilityServiceListener)
+	 */
+	@Override
+	public void addCompatibilityListener(CompatibilityServiceListener listener) {
+		listeners.add(listener);
+
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.compatibility.extension.CompatibilityService#removeCompatibilityListener(CompatibilityServiceListener
+	 *      listener)
+	 */
+	@Override
+	public void removeCompatibilityListener(CompatibilityServiceListener listener) {
+		listeners.remove(listener);
+
+	}
+}
