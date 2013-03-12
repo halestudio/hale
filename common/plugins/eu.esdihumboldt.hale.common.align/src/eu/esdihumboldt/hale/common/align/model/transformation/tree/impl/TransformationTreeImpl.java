be4d7bf1b69c9cd2e2d41bdf558deddc124981e7
Kai Schwierczek <kai.schwierczek@igd.fraunhofer.de>
2013-03-12 17:38:28 +0100

    allow cell inheritance in the transformation tree
    refs #541
    
    Change-Id: Idb74a23f5442155f2744588d46fe304b753a6202
    Reviewed-on: http://gerrit.esdi-community.eu/2002
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/transformation/tree/impl/TransformationTreeImpl.java
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/transformation/tree/impl/TransformationTreeImpl.java
@@ -34,7 +34,11 @@ import eu.esdihumboldt.hale.common.align.model.CellUtil;
 import eu.esdihumboldt.hale.common.align.model.ChildContext;
 import eu.esdihumboldt.hale.common.align.model.Entity;
 import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
+import eu.esdihumboldt.hale.common.align.model.MutableCell;
 import eu.esdihumboldt.hale.common.align.model.Type;
+import eu.esdihumboldt.hale.common.align.model.impl.DefaultCell;
+import eu.esdihumboldt.hale.common.align.model.impl.DefaultProperty;
+import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
 import eu.esdihumboldt.hale.common.align.model.impl.TypeEntityDefinition;
 import eu.esdihumboldt.hale.common.align.model.transformation.tree.CellNode;
 import eu.esdihumboldt.hale.common.align.model.transformation.tree.SourceNode;
@@ -42,6 +46,8 @@ import eu.esdihumboldt.hale.common.align.model.transformation.tree.TargetNode;
 import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationNode;
 import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationNodeVisitor;
 import eu.esdihumboldt.hale.common.align.model.transformation.tree.TransformationTree;
+import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
+import eu.esdihumboldt.hale.common.schema.model.DefinitionUtil;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 
 /**
@@ -76,19 +82,14 @@ public class TransformationTreeImpl extends AbstractGroupNode implements Transfo
 		// partition cells by child
 		ListMultimap<EntityDefinition, CellNode> childCells = ArrayListMultimap.create();
 		for (Cell cell : cells) {
+			cell = reparentCell(cell, typeCell);
 			CellNode node = new CellNodeImpl(cell, sourceNodes);
 			for (Entity target : cell.getTarget().values()) {
-				if (target.getDefinition().getType().equals(type)) {
-					List<ChildContext> path = target.getDefinition().getPropertyPath();
-					if (path != null && !path.isEmpty()) {
-						// store cell with child
-						childCells.put(AlignmentUtil.deriveEntity(target.getDefinition(), 1), node);
-					}
-				}
-				else {
-					// now, that's bad - obviously a cell with targets in more
-					// than one type!
-					throw new IllegalStateException();
+				EntityDefinition targetDef = target.getDefinition();
+				List<ChildContext> path = targetDef.getPropertyPath();
+				if (path != null && !path.isEmpty()) {
+					// store cell with child
+					childCells.put(AlignmentUtil.deriveEntity(targetDef, 1), node);
 				}
 			}
 		}
@@ -103,6 +104,100 @@ public class TransformationTreeImpl extends AbstractGroupNode implements Transfo
 		}
 
 		children = Collections.unmodifiableList(childList);
+	}
+
+	/**
+	 * Creates a copy of the given property cell with all source and target
+	 * types matching those off the given type cell.<br>
+	 * If the types already match they are unchanged. If the types are sub types
+	 * of the types of the type cell they are changed. Otherwise an
+	 * {@link IllegalStateException} is thrown.
+	 * 
+	 * @param propertyCell the property cell to update
+	 * @param typeCell the type cell with the target types
+	 * @return the updated cell
+	 */
+	private Cell reparentCell(Cell propertyCell, Cell typeCell) {
+		ListMultimap<String, Entity> sources = propertyCell.getSource() == null ? null
+				: ArrayListMultimap.<String, Entity> create();
+		ListMultimap<String, Entity> targets = ArrayListMultimap.create();
+		boolean updateNecessary = false;
+
+		// XXX are updates to the property path needed?
+
+		TypeDefinition typeCellTargetType = ((Type) CellUtil.getFirstEntity(typeCell.getTarget()))
+				.getDefinition().getDefinition();
+		for (Entry<String, ? extends Entity> target : propertyCell.getTarget().entries()) {
+			TypeDefinition propertyCellTargetType = target.getValue().getDefinition().getType();
+			if (propertyCellTargetType.equals(typeCellTargetType))
+				targets.put(target.getKey(), target.getValue());
+			else if (DefinitionUtil.isSuperType(typeCellTargetType, propertyCellTargetType)) {
+				PropertyEntityDefinition oldDef = (PropertyEntityDefinition) target.getValue()
+						.getDefinition();
+				targets.put(target.getKey(), new DefaultProperty(new PropertyEntityDefinition(
+						typeCellTargetType, oldDef.getPropertyPath(), SchemaSpaceID.TARGET, null)));
+				updateNecessary = true;
+			}
+			else {
+				// a cell with targets in more than one type
+				throw new IllegalStateException();
+			}
+		}
+
+		if (sources != null) {
+			// collect source entity definitions
+			Collection<TypeEntityDefinition> typeCellSourceTypes = new ArrayList<TypeEntityDefinition>();
+			for (Entity entity : typeCell.getSource().values())
+				typeCellSourceTypes.add((TypeEntityDefinition) entity.getDefinition());
+			for (Entry<String, ? extends Entity> source : propertyCell.getSource().entries()) {
+				TypeEntityDefinition propertyCellSourceType = AlignmentUtil.getTypeEntity(source
+						.getValue().getDefinition());
+				if (typeCellSourceTypes.contains(propertyCellSourceType))
+					sources.put(source.getKey(), source.getValue());
+				else {
+					boolean matchFound = false;
+					// try to find a matching source
+					// XXX what if multiple sources match?
+					// currently all are added
+					// other choices: first match
+					// or maybe prioritize matching filter and closest super
+					// type
+					// maybe the whole cell should be duplicated?
+					for (TypeEntityDefinition typeCellSourceType : typeCellSourceTypes) {
+						if (DefinitionUtil.isSuperType(typeCellSourceType.getDefinition(),
+								propertyCellSourceType.getDefinition())
+								&& (propertyCellSourceType.getFilter() == null || propertyCellSourceType
+										.getFilter().equals(typeCellSourceType.getFilter()))) {
+							matchFound = true;
+							PropertyEntityDefinition oldDef = (PropertyEntityDefinition) source
+									.getValue().getDefinition();
+							sources.put(
+									source.getKey(),
+									new DefaultProperty(new PropertyEntityDefinition(
+											typeCellSourceType.getDefinition(), oldDef
+													.getPropertyPath(), SchemaSpaceID.SOURCE,
+											typeCellSourceType.getFilter())));
+							updateNecessary = true;
+							// XXX break; if only one match should be added
+						}
+					}
+					if (!matchFound) {
+						// a cell with a source that does not match the type
+						// cell
+						throw new IllegalStateException();
+					}
+				}
+			}
+		}
+
+		if (updateNecessary) {
+			MutableCell copy = new DefaultCell(propertyCell);
+			copy.setSource(sources);
+			copy.setTarget(targets);
+			propertyCell = copy;
+		}
+
+		return propertyCell;
 	}
 
 	/**
