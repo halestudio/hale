6bf7cd9589000d6cfcc762a0f92a5edec60e33a1
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-04-02 13:38:19 +0200

    fixed XslForEach path element
    ...so that is handled correctly by Descent.
    
    Change-Id: Ia7af80ca7a83fd345eb4daea833a52944ff539bf

--- ext/xslt/eu.esdihumboldt.hale.io.xslt/src/eu/esdihumboldt/hale/io/xslt/internal/XslForEach.java
+++ ext/xslt/eu.esdihumboldt.hale.io.xslt/src/eu/esdihumboldt/hale/io/xslt/internal/XslForEach.java
@@ -77,8 +77,21 @@ public class XslForEach implements PathElement {
 
 	@Override
 	public boolean isUnique() {
-		// by default may not be repeated
-		return true;
+		// may be repeated as it is independent of the target structure
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		/*
+		 * This is kind of dirty but needed for correct handling in Descent.
+		 */
+		return obj instanceof XslForEach;
 	}
 
 }
