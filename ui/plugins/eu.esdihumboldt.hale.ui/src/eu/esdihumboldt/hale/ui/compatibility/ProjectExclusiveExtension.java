4c7f5d64bd01bd72081defa12140f7fa1531b7b7
Sebastian Reinhardt <sebastian.reinhardt@igd.fraunhofer.de>
2013-04-19 17:16:56 +0200

    Compatibility Mode to saved projects integration
    Change-Id: I1aa9bb88872dc602f3754a68def062c4d6d8abda
    Reviewed-on: http://gerrit.esdi-community.eu/2125
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/compatibility/ProjectExclusiveExtension.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.compatibility;
+
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.ui.PlatformUI;
+
+import de.cs3d.util.eclipse.extension.ExtensionObjectFactory;
+import de.cs3d.util.eclipse.extension.ObjectExtension;
+import de.cs3d.util.eclipse.extension.exclusive.AbstractExclusiveExtension;
+import de.cs3d.util.eclipse.extension.exclusive.ExclusiveExtension;
+import eu.esdihumboldt.hale.common.core.io.project.model.ProjectFile;
+import eu.esdihumboldt.hale.ui.service.project.ProjectService;
+import eu.esdihumboldt.hale.ui.service.project.ProjectServiceAdapter;
+
+/**
+ * {@link ExclusiveExtension} that saves/loads the current extension object in a
+ * Project from the {@link ProjectService}.
+ * 
+ * @param <T> the extension object type
+ * @param <F> the extension object factory type
+ * 
+ * @author Sebastian Reinhardt
+ */
+public abstract class ProjectExclusiveExtension<T, F extends ExtensionObjectFactory<T>> extends
+		AbstractExclusiveExtension<T, F> {
+
+	/**
+	 * The preference key
+	 */
+	private final String preferenceKey;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param extension the internal extension
+	 * @param preferenceKey the preference key
+	 */
+	public ProjectExclusiveExtension(ObjectExtension<T, F> extension, final String preferenceKey) {
+		super(extension);
+
+		this.preferenceKey = preferenceKey;
+
+		final ProjectService ps = (ProjectService) PlatformUI.getWorkbench().getService(
+				ProjectService.class);
+
+		addListener(new ExclusiveExtensionListener<T, F>() {
+
+			@Override
+			public void currentObjectChanged(T current, F definition) {
+				if (isSaveAllowed(current, definition)) {
+					ps.getConfigurationService().set(preferenceKey, definition.getIdentifier());
+				}
+			}
+		});
+
+		ps.addListener(new ProjectServiceAdapter() {
+
+			@Override
+			public void afterLoad(ProjectService projectService,
+					Map<String, ProjectFile> projectFiles) {
+				String key = projectService.getConfigurationService().get(preferenceKey);
+				ProjectExclusiveExtension.this.setCurrent(key);
+			}
+		});
+	}
+
+	/**
+	 * Determines if saving the state to the project is allowed
+	 * 
+	 * @param current the extension object
+	 * @param definition the extension object definition
+	 * 
+	 * @return if saving the state is allowed
+	 */
+	protected boolean isSaveAllowed(T current, F definition) {
+		return true;
+	}
+
+	/**
+	 * Determines if loading the given factory from the project is allowed
+	 * 
+	 * @param definition the extension object definition
+	 * 
+	 * @return if loading the factory is allowed
+	 */
+	protected boolean isLoadAllowed(F definition) {
+		return true;
+	}
+
+	/**
+	 * @see AbstractExclusiveExtension#getInitialFactory()
+	 */
+	@Override
+	protected F getInitialFactory() {
+		String identifier = ((ProjectService) PlatformUI.getWorkbench().getService(
+				ProjectService.class)).getConfigurationService().get(preferenceKey);
+
+		List<F> factories = getFactories();
+
+		// find preferred factory to load
+		for (F factory : factories) {
+			if (isLoadAllowed(factory) && factory.getIdentifier().equals(identifier)) {
+				return factory;
+			}
+		}
+
+		// return default factory found
+		if (!factories.isEmpty()) {
+			return getDefaultFactory(factories);
+		}
+		else {
+			return getFallbackFactory();
+		}
+	}
+
+	/**
+	 * Get the default factory to use
+	 * 
+	 * @param factories the available factories (non-empty list)
+	 * 
+	 * @return the factory to set as initial factory
+	 */
+	protected F getDefaultFactory(List<F> factories) {
+		for (F factory : factories) {
+			if (isLoadAllowed(factory)) {
+				return factory;
+			}
+		}
+
+		return getFallbackFactory();
+	}
+
+	/**
+	 * Get the factory to use when no extensions are configured
+	 * 
+	 * @return the factory to use when no extensions are configured
+	 */
+	protected abstract F getFallbackFactory();
+
+}
