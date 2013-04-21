7ae48fe0c84ae46bf6ba0e230fd9fd0b8ab19a99
Simon Templer <simon@templer.cc>
2013-04-21 09:59:05 +0200

    first version of propagation app w/ only ADE target properties
    Change-Id: I33fbad846fa46a1fc0d98381a9b5c783afff2af6
    Reviewed-on: http://gerrit.esdi-community.eu/2131
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ext/ageobw/eu.esdihumboldt.hale.app.bgis.ade/src/eu/esdihumboldt/hale/app/bgis/ade/propagate/CityGMLPropagate.java
+++ ext/ageobw/eu.esdihumboldt.hale.app.bgis.ade/src/eu/esdihumboldt/hale/app/bgis/ade/propagate/CityGMLPropagate.java
@@ -13,12 +13,14 @@
  *     Fraunhofer IGD
  */
 
-package eu.esdihumboldt.hale.app.bgis.ade.duplicate;
+package eu.esdihumboldt.hale.app.bgis.ade.propagate;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.xml.namespace.QName;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
@@ -28,6 +30,8 @@ import com.google.common.collect.SetMultimap;
 
 import eu.esdihumboldt.hale.app.bgis.ade.common.BGISAppConstants;
 import eu.esdihumboldt.hale.app.bgis.ade.common.BGISAppUtil;
+import eu.esdihumboldt.hale.app.bgis.ade.propagate.config.ExcelFeatureMap;
+import eu.esdihumboldt.hale.app.bgis.ade.propagate.config.FeatureMap;
 import eu.esdihumboldt.hale.common.align.io.AlignmentWriter;
 import eu.esdihumboldt.hale.common.align.model.Alignment;
 import eu.esdihumboldt.hale.common.align.model.Cell;
@@ -37,6 +41,7 @@ import eu.esdihumboldt.hale.common.align.model.MutableAlignment;
 import eu.esdihumboldt.hale.common.align.model.impl.DefaultAlignment;
 import eu.esdihumboldt.hale.common.align.model.impl.TypeEntityDefinition;
 import eu.esdihumboldt.hale.common.core.io.HaleIO;
+import eu.esdihumboldt.hale.common.core.io.IOProviderConfigurationException;
 import eu.esdihumboldt.hale.common.core.io.extension.IOProviderDescriptor;
 import eu.esdihumboldt.hale.common.core.io.impl.NullProgressIndicator;
 import eu.esdihumboldt.hale.common.core.io.report.IOReport;
@@ -47,27 +52,34 @@ import eu.esdihumboldt.hale.common.core.report.Report;
 import eu.esdihumboldt.hale.common.core.report.ReportHandler;
 import eu.esdihumboldt.hale.common.headless.impl.ProjectTransformationEnvironment;
 import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
+import eu.esdihumboldt.hale.common.schema.io.SchemaReader;
 import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
+import eu.esdihumboldt.hale.common.schema.model.Schema;
 import eu.esdihumboldt.hale.common.schema.model.SchemaSpace;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.io.gml.CityGMLConstants;
 
 /**
- * Generates an extended mappings for BGIS CityGML ADE based on an example
+ * Generates an extended mapping for BGIS CityGML ADE based on an example
  * mapping.
  * 
  * @author Simon Templer
  */
-public class GenerateDuplicates implements BGISAppConstants {
+public class CityGMLPropagate implements BGISAppConstants, CityGMLConstants {
 
 	private SchemaSpace sourceSchema;
 
 	private SchemaSpace targetSchema;
 
+	private Schema cityGMLSource;
+
 	private Alignment examples;
 
 	private Alignment alignment;
 
-	private GenerateDuplicatesContext context;
+	private FeatureMap config;
+
+	private CityGMLPropagateContext context;
 
 	/**
 	 * Generate the default value mapping based on the given configuration.
@@ -75,11 +87,17 @@ public class GenerateDuplicates implements BGISAppConstants {
 	 * @param context the configuration for the mapping generation
 	 * @throws Exception if an unrecoverable error occurs during the generation
 	 */
-	public void generate(GenerateDuplicatesContext context) throws Exception {
+	public void generate(CityGMLPropagateContext context) throws Exception {
 		this.context = context;
 
 		// load project
 		if (loadProject()) {
+			// load CityGML schema
+			loadCityGML();
+
+			// load the feature map
+			loadConfig();
+
 			// generate mapping
 			generateMapping();
 
@@ -119,22 +137,56 @@ public class GenerateDuplicates implements BGISAppConstants {
 		return false;
 	}
 
+	private void loadCityGML() throws IOProviderConfigurationException, IOException {
+		System.out.println("Loading schema...");
+
+		LocatableInputSupplier<? extends InputStream> schemaIn = new DefaultInputSupplier(
+				context.getSourceSchema());
+		SchemaReader schemaReader = HaleIO.findIOProvider(SchemaReader.class, schemaIn, context
+				.getSourceSchema().getPath());
+		schemaReader.setSource(schemaIn);
+		IOReport report = schemaReader.execute(new NullProgressIndicator());
+		if (!report.isSuccess() || !report.getErrors().isEmpty()) {
+			throw new IllegalStateException("Failed to load schema");
+		}
+		cityGMLSource = schemaReader.getSchema();
+	}
+
+	private void loadConfig() {
+		System.out.println("Loading feature map...");
+
+		try {
+			config = new ExcelFeatureMap(context.getConfig());
+		} catch (Exception e) {
+			throw new IllegalStateException("Failed to load feature map configuration", e);
+		}
+	}
+
 	private void generateMapping() {
 		System.out.println("Indexing example cells...");
 
 		// index all cells based on the target property name
-		SetMultimap<String, Cell> exampleCells = HashMultimap.create();
+		SetMultimap<String, Cell> bgisExamples = HashMultimap.create();
+		SetMultimap<QName, Cell> cityGMLExamples = HashMultimap.create();
+
 		for (Cell cell : examples.getCells()) {
 			if (cell.getTarget().size() == 1) {
 				// only supports cells with one target
 				EntityDefinition entityDef = CellUtil.getFirstEntity(cell.getTarget())
 						.getDefinition();
+				// XXX check source?!
 				if (entityDef.getDefinition() instanceof PropertyDefinition) {
-					if (ADE_NS.equals(entityDef.getDefinition().getName().getNamespaceURI())) {
-						exampleCells.put(entityDef.getDefinition().getName().getLocalPart(), cell);
+					QName name = entityDef.getDefinition().getName();
+					if (ADE_NS.equals(name.getNamespaceURI())) {
+						bgisExamples.put(name.getLocalPart(), cell);
+					}
+					else if (name.getNamespaceURI().startsWith(CITYGML_NAMESPACE_CORE)) {
+						// XXX only support level 1 properties?
+						cityGMLExamples.put(name, cell);
 					}
 					else
-						System.out.println("WARNING: ignoring cell with non-ADE target property");
+						System.out
+								.println("WARNING: ignoring cell with target property neither from CityGML nor from BGIS ADE");
 				}
 				else
 					System.out.println("WARNING: ignoring type cell");
@@ -146,9 +198,16 @@ public class GenerateDuplicates implements BGISAppConstants {
 		// collect all ADE feature types
 		List<TypeDefinition> featureTypes = BGISAppUtil.getADEFeatureTypes(targetSchema);
 
+		// TODO collect all source CityGML feature types
+		/*
+		 * XXX ensure to remove all those that share a local name with an ADE
+		 * type
+		 */
+
 		// visit ADE properties and create cells
 		System.out.println("Generating mapping from example cells for");
-		DuplicateVisitor visitor = new DuplicateVisitor(exampleCells);
+		CityGMLPropagateVisitor visitor = new CityGMLPropagateVisitor(cityGMLSource, bgisExamples,
+				cityGMLExamples, config);
 		for (TypeDefinition type : featureTypes) {
 			System.out.println(type.getDisplayName() + "...");
 			visitor.accept(new TypeEntityDefinition(type, SchemaSpaceID.TARGET, null));
