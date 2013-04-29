5aac55af034310e9a80dce37283e09b75f0b88f0
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-04-29 18:04:33 +0200

    removed restriction on removing type cells
    Change-Id: I606aa85d28308a09a06033a955da5f7cdfd21c3e

--- ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/align/internal/tester/CellTester.java
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/align/internal/tester/CellTester.java
@@ -17,7 +17,6 @@
 package eu.esdihumboldt.hale.ui.service.align.internal.tester;
 
 import org.eclipse.core.expressions.PropertyTester;
-import org.eclipse.ui.PlatformUI;
 
 import eu.esdihumboldt.hale.common.align.model.AlignmentUtil;
 import eu.esdihumboldt.hale.common.align.model.Cell;
@@ -86,24 +85,8 @@ public class CellTester extends PropertyTester {
 			// never allow removing base alignment cells
 			return false;
 		}
-		if (!AlignmentUtil.isTypeCell(cell)) {
-			// always allow removing property cells
-			return true;
-		}
-
-		// only allow removing a type cell if there are no property cells
-		// associated
-		/*
-		 * FIXME must we test if there are other type cells that are related to
-		 * the cell? XXX but even then we would not be safe to remove the type
-		 * cell, as this might be in the context of the removal of multiple type
-		 * cells, removing both might leave us again in an invalid state. XXX
-		 * solutions could be: only allow deleting one cell at a time and not
-		 * multiple or performing the check on all cells to be deleted.
-		 */
-		AlignmentService as = (AlignmentService) PlatformUI.getWorkbench().getService(
-				AlignmentService.class);
-		return as.getAlignment().getPropertyCells(cell, true).isEmpty();
+		// always allow removing type & property cells
+		return true;
 	}
 
 }
