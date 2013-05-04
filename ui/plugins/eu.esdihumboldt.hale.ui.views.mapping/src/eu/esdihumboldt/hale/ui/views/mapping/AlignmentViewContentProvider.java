f4f8bf87aaf12e9e7db1c59a1a383256f07483c6
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-05-04 11:37:54 +0200

    cell sorting w/ comparator
    Change-Id: I15937f84219d7e8770529eeb76e2d990a1e8802c
    Reviewed-on: http://gerrit.esdi-community.eu/2177
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ui/plugins/eu.esdihumboldt.hale.ui.views.mapping/src/eu/esdihumboldt/hale/ui/views/mapping/AlignmentViewContentProvider.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.mapping/src/eu/esdihumboldt/hale/ui/views/mapping/AlignmentViewContentProvider.java
@@ -18,7 +18,6 @@ package eu.esdihumboldt.hale.ui.views.mapping;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
@@ -32,10 +31,8 @@ import eu.esdihumboldt.hale.common.align.model.Alignment;
 import eu.esdihumboldt.hale.common.align.model.AlignmentUtil;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.CellUtil;
-import eu.esdihumboldt.hale.common.align.model.ChildContext;
 import eu.esdihumboldt.hale.common.align.model.Entity;
 import eu.esdihumboldt.hale.common.align.model.Type;
-import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
 import eu.esdihumboldt.hale.ui.common.graph.content.Edge;
 import eu.esdihumboldt.hale.ui.common.graph.content.ReverseCellGraphContentProvider;
 import eu.esdihumboldt.hale.ui.service.align.AlignmentService;
@@ -151,54 +148,7 @@ public class AlignmentViewContentProvider extends ReverseCellGraphContentProvide
 	 */
 	private Collection<? extends Cell> sortCells(Collection<? extends Cell> cells) {
 		Cell[] cellArr = cells.toArray(new Cell[cells.size()]);
-
-		for (int i = cellArr.length; i > 1; i--) {
-			for (int j = 0; j < i - 1; j++) {
-
-				if (cellArr[j].getSource() == null) {
-					Cell temp = cellArr[j + 1];
-					cellArr[j + 1] = cellArr[j];
-					cellArr[j] = temp;
-				}
-
-				if (cellArr[j + 1].getSource() == null) {
-					continue;
-				}
-
-				else {
-
-					Iterator<ChildContext> pathJiterator = cellArr[j].getSource().values()
-							.iterator().next().getDefinition().getPropertyPath().iterator();
-					Iterator<ChildContext> pathJ1iterator = cellArr[j + 1].getSource().values()
-							.iterator().next().getDefinition().getPropertyPath().iterator();
-
-					while (pathJiterator.hasNext() && pathJ1iterator.hasNext()) {
-
-						PropertyDefinition defJ = pathJiterator.next().getChild().asProperty();
-						PropertyDefinition defJ1 = pathJ1iterator.next().getChild().asProperty();
-
-						if (defJ != null && defJ1 != null) {
-							if (defJ.getName().getLocalPart()
-									.compareToIgnoreCase(defJ1.getName().getLocalPart()) > 0) {
-								Cell temp = cellArr[j + 1];
-								cellArr[j + 1] = cellArr[j];
-								cellArr[j] = temp;
-								break;
-							}
-						}
-						else if (defJ != null && defJ1 == null) {
-							break;
-						}
-						else if (defJ == null && defJ1 != null) {
-							Cell temp = cellArr[j + 1];
-							cellArr[j + 1] = cellArr[j];
-							cellArr[j] = temp;
-							break;
-						}
-					}
-				}
-			}
-		}
+		Arrays.sort(cellArr, new CellComparator());
 		return Arrays.asList(cellArr);
 	}
 
---
+++ ui/plugins/eu.esdihumboldt.hale.ui.views.mapping/src/eu/esdihumboldt/hale/ui/views/mapping/CellComparator.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.views.mapping;
+
+import java.util.Comparator;
+
+import eu.esdihumboldt.hale.common.align.model.Cell;
+import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
+
+/**
+ * Comparator for cells.
+ */
+final class CellComparator implements Comparator<Cell> {
+
+	private final Comparator<EntityDefinition> edComp = new EntityDefinitionComparator();
+
+	@Override
+	public int compare(Cell o1, Cell o2) {
+		if (o1.getSource() == null && o2.getSource() == null) {
+			// compare first target
+			EntityDefinition e1 = o1.getTarget().values().iterator().next().getDefinition();
+			EntityDefinition e2 = o2.getTarget().values().iterator().next().getDefinition();
+			return edComp.compare(e1, e2);
+		}
+		else if (o1.getSource() == null) {
+			// o1 after o2
+			return 1;
+		}
+		else if (o2.getSource() == null) {
+			// o2 after o1
+			return -1;
+		}
+
+		// compare first entity in source
+		// XXX what about multiple sources?
+		EntityDefinition e1 = o1.getSource().values().iterator().next().getDefinition();
+		EntityDefinition e2 = o2.getSource().values().iterator().next().getDefinition();
+		return edComp.compare(e1, e2);
+	}
+}
