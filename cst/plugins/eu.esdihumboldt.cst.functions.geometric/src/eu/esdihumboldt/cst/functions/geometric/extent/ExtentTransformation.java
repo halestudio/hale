055e4ae0f5a367f8c05bdfec28267c1e398aaaa7
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-06-21 19:20:51 +0200

    removed output to console
    Output directly to system streams should only be used
    locally for debugging purposes.
    
    Change-Id: Ifee52f1f94aca8b606b71bcc838d59463be722e9

--- cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/extent/ExtentTransformation.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.geometric/src/eu/esdihumboldt/cst/functions/geometric/extent/ExtentTransformation.java
@@ -57,12 +57,12 @@ public class ExtentTransformation extends
 		CRSDefinition commonCrs = null;
 		Geometry extent = null;
 
-		int count = 0;
+//		int count = 0;
 		for (PropertyValue pv : variables.get(null)) {
 			// find contained geometries
 			Object value = pv.getValue();
 			traverser.traverse(value, geoFind);
-			count += geoFind.getGeometries().size();
+//			count += geoFind.getGeometries().size();
 
 			for (GeometryProperty<?> geom : geoFind.getGeometries()) {
 				// check CRS
@@ -99,7 +99,7 @@ public class ExtentTransformation extends
 			geoFind.reset();
 		}
 
-		System.err.println(count);
+//		System.err.println(count);
 
 		if (extent != null) {
 			return new DefaultGeometryProperty<Geometry>(commonCrs, extent);
