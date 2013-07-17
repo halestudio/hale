fcba4e3ac1c02d8a85c47c768366237cbc6aba2a
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2013-07-17 09:45:31 +0200

    archive project as import and export
    refs #420
    
    Change-Id: I0ff64bbf7a46b00206f2c0dcfad408cbb3faf162

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/project/ArchiveProjectImportSelectLocationPage.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.io.project;
+
+import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Composite;
+
+import eu.esdihumboldt.hale.common.core.io.Value;
+import eu.esdihumboldt.hale.common.core.io.project.impl.ArchiveProjectImport;
+import eu.esdihumboldt.hale.ui.io.config.AbstractConfigurationPage;
+
+/**
+ * Selection page for the directory to chose where the project archive should be
+ * imported
+ * 
+ * @author Patrick Lieb
+ */
+public class ArchiveProjectImportSelectLocationPage extends
+		AbstractConfigurationPage<ArchiveProjectImport, ArchiveProjectImportWizard> {
+
+	private String location = "";
+
+	/**
+	 * Default Constructor
+	 */
+	protected ArchiveProjectImportSelectLocationPage() {
+		super("archiveProjectImportConfiguration");
+		setTitle("Choose directory for archive extraction");
+		setDescription("The project archive will be extracted and loaded in the specific directory.");
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.config.AbstractConfigurationPage#enable()
+	 */
+	@Override
+	public void enable() {
+		// not needed here
+
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.config.AbstractConfigurationPage#disable()
+	 */
+	@Override
+	public void disable() {
+		// not needed here
+
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.IOWizardPage#updateConfiguration(eu.esdihumboldt.hale.common.core.io.IOProvider)
+	 */
+	@Override
+	public boolean updateConfiguration(ArchiveProjectImport provider) {
+		provider.setParameter("PARAM_IMPORT_LOCATION", Value.of(location));
+		return true;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.HaleWizardPage#createContent(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected void createContent(Composite page) {
+		final DirectoryFieldEditor dialog = new DirectoryFieldEditor("Import location",
+				"Select directory", page);
+		dialog.getTextControl(page).addModifyListener(new ModifyListener() {
+
+			@Override
+			public void modifyText(ModifyEvent e) {
+				location = dialog.getStringValue();
+				setPageComplete(true);
+			}
+		});
+	}
+
+	/**
+	 * @return the location of the directory
+	 */
+	protected String getLocation() {
+		return location;
+	}
+}
