19523200189f4b0a83900c880d1e2c5d626d581d
Kai Schwierczek <kai.schwierczek@igd.fraunhofer.de>
2013-08-01 15:53:39 +0200

    Join parameter page
    along with a more clean way to safe the join parameter
    
    Last problem to solve is that JoinParameterType currently needs access to
    the SchemaService.
    
    refs #372
    
    Change-Id: I92fb46921254ffb6b0020e14cf580f98591cd630

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.core/src/eu/esdihumboldt/hale/ui/functions/core/join/JoinParameterType.groovy
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.functions.core.join;
+
+import javax.xml.parsers.DocumentBuilderFactory
+
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+
+import eu.esdihumboldt.hale.common.align.io.LoadAlignmentContext
+import eu.esdihumboldt.hale.common.align.io.impl.DOMEntityDefinitionHelper
+import eu.esdihumboldt.hale.common.align.model.functions.join.JoinParameter
+import eu.esdihumboldt.hale.common.align.model.functions.join.JoinParameter.JoinCondition
+import eu.esdihumboldt.hale.common.align.model.impl.TypeEntityDefinition
+import eu.esdihumboldt.hale.common.core.io.ComplexValueType
+import eu.esdihumboldt.hale.common.schema.SchemaSpaceID
+import eu.esdihumboldt.hale.common.schema.model.TypeIndex
+import groovy.xml.dom.DOMCategory
+
+/**
+ * Descriptor for conversion between DOM and JoinParameter.
+ * 
+ * @author Kai Schwierczek
+ */
+public class JoinParameterType implements ComplexValueType<JoinParameter, LoadAlignmentContext> {
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.ComplexValueType#fromDOM(org.w3c.dom.Element)
+	 */
+	@Override
+	public JoinParameter fromDOM(Element fragment, LoadAlignmentContext context) {
+		assert context
+
+		TypeIndex sIndex = context.sourceTypes
+		SchemaSpaceID ssid = SchemaSpaceID.SOURCE
+
+		use (DOMCategory) {
+			// get type list
+			// Cannot use fragment.'class' because that results in the class object of fragment.
+			// getElementsByTagName is okay, because there are only direct 'class' children...
+			def domTypes = fragment.getElementsByTagName('class')
+			List<TypeEntityDefinition> types = new ArrayList<>(domTypes.size())
+			domTypes.each{ type ->
+				types.add(DOMEntityDefinitionHelper.typeFromDOM(type, sIndex, ssid))
+			}
+
+			// get conditions
+			def domConditions = fragment.'jp:condition'
+			Set<JoinCondition> conditions = new HashSet<>(domConditions.size())
+			domConditions.each{ condition ->
+				def domProperties = condition.'property'
+				conditions.add(new JoinCondition(
+						DOMEntityDefinitionHelper.propertyFromDOM(domProperties[0], sIndex, ssid),
+						DOMEntityDefinitionHelper.propertyFromDOM(domProperties[1], sIndex, ssid)))
+			}
+
+			return new JoinParameter(types, conditions)
+		}
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.common.core.io.ComplexValueType#toDOM(java.lang.Object)
+	 */
+	@Override
+	public Element toDOM(JoinParameter value) {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance()
+		dbf.setNamespaceAware(true);
+		Document doc = dbf.newDocumentBuilder().newDocument()
+		def result = doc.createElementNS("http://www.esdi-humboldt.eu/hale/join", "jp:join-parameter")
+		value.types.each{ type ->
+			result.appendChild(result.ownerDocument.adoptNode(
+					DOMEntityDefinitionHelper.typeToDOM(type)))
+		}
+		value.conditions.each{ condition ->
+			def conditionNode = doc.createElementNS("http://www.esdi-humboldt.eu/hale/join", "jp:condition")
+			result.appendChild(conditionNode)
+			conditionNode.appendChild(conditionNode.ownerDocument.adoptNode(
+					DOMEntityDefinitionHelper.propertyToDOM(condition.baseProperty)))
+			conditionNode.appendChild(conditionNode.ownerDocument.adoptNode(
+					DOMEntityDefinitionHelper.propertyToDOM(condition.joinProperty)))
+		}
+		return result
+	}
+
+	@Override
+	public Class<LoadAlignmentContext> getContextType() {
+		LoadAlignmentContext.class
+	}
+}
