3e7bf771956b639a6a38d150dbdefddef41b2811
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-08-13 15:56:41 +0200

    added utility method for human readable byte count
    Change-Id: I3f9155b2e3441d90ee4b2f055dd3f78f297e58b1

--- util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/io/IOUtils.java
+++ util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/io/IOUtils.java
@@ -203,4 +203,23 @@ public final class IOUtils {
 			return URI.create(path);
 		}
 	}
+
+	/**
+	 * Get the human readable notation of a size in bytes.
+	 * 
+	 * {@link "http://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java"}
+	 * 
+	 * @param bytes the number of bytes
+	 * @param si if the SI or binary unit should be used
+	 * @return the human readable string representation of the number of bytes
+	 */
+	public static String humanReadableByteCount(long bytes, boolean si) {
+		int unit = si ? 1000 : 1024;
+		if (bytes < unit)
+			return bytes + " B";
+		int exp = (int) (Math.log(bytes) / Math.log(unit));
+		String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+	}
+
 }
