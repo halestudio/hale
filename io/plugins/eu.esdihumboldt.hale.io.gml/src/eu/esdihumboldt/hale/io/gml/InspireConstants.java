de3998c812a7018897a1a3b6f445efa55ad9644e
Kai Schwierczek <kai.schwierczek@igd.fraunhofer.de>
2013-09-02 13:59:46 +0200

    support export to INSPIRE SpatialDataSet
    The exported SpatialDataSet always contains mandatory attributes, which may
    be specified on an additional configuration page.
    
    refs #585
    
    Change-Id: I68d15ad80187b89fd25788c04e8d23487b1ccd35

---
+++ io/plugins/eu.esdihumboldt.hale.io.gml/src/eu/esdihumboldt/hale/io/gml/InspireConstants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.io.gml;
+
+/**
+ * Common constants on Inspire.
+ * 
+ * @author Kai Schwierczek
+ */
+public interface InspireConstants {
+
+	/**
+	 * The Inspire base type namespace.
+	 */
+	public static final String INSPIRE_NAMESPACE_BASETYPES = "urn:x-inspire:specification:gmlas:BaseTypes:3.2";
+}
