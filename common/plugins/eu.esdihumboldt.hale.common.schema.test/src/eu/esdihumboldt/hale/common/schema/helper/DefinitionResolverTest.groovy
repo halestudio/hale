46de9d31c80ab7c266d95bfb5a58789e0fa44fd7
Simon Templer <simon@templer.cc>
2013-09-14 07:40:30 +0200

    added basic definition resolver
    The resolver is able to yield different definition paths
    from a given definition group that match a given property
    name.
    
    Change-Id: I8a198524f5b418cc251532de6333dd02f950329d

---
+++ common/plugins/eu.esdihumboldt.hale.common.schema.test/src/eu/esdihumboldt/hale/common/schema/helper/DefinitionResolverTest.groovy
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.schema.helper
+
+import eu.esdihumboldt.hale.common.schema.groovy.SchemaBuilder
+import eu.esdihumboldt.hale.common.schema.model.GroupPropertyDefinition
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition
+import groovy.transform.CompileStatic
+import groovy.transform.TypeCheckingMode
+
+
+
+/**
+ * TODO Type description
+ * @author simon
+ */
+@CompileStatic
+class DefinitionResolverTest extends GroovyTestCase {
+
+	private static final String MAIN_NS = 'http://www.example.com'
+
+	private static final String PROPERTY_NS = 'http://www.example.com/property'
+
+	private static final String SPECIAL_NS = 'totally-different-namespace'
+
+	private TypeDefinition itemType;
+
+	private TypeDefinition orderType;
+
+	@CompileStatic(TypeCheckingMode.SKIP)
+	@Override
+	protected void setUp() throws Exception {
+		new SchemaBuilder(defaultPropertyTypeNamespace: PROPERTY_NS).schema(MAIN_NS) {
+			itemType = Item {
+				name()
+				price(Double)
+				alternateNames { name(cardinality: '*') }
+				_(cardinality: 0..1) {
+					name(namespace: SPECIAL_NS)
+					link(URI)
+				}
+			}
+
+			orderType = Order {
+				_(cardinality: '+') {
+					item(itemType)
+					quantity(Integer)
+				}
+			}
+		}
+	}
+
+	/**
+	 * Tests finding properties w/o a fixed namespace.
+	 */
+	void testFindPropertyIgnoreNamespace() {
+		// item.name
+		List<DefinitionPath> paths = DefinitionResolver.findProperty(itemType, 'name', null);
+		assertEquals 2, paths.size()
+
+		// direct property
+		DefinitionPath path1 = paths[0]
+		assertEquals 1, path1.path.size()
+
+		// inside group
+		DefinitionPath path2 = paths[1]
+		assertEquals 2, path2.path.size()
+		assertTrue path2.path[0] instanceof GroupPropertyDefinition
+
+		// order.name
+		paths = DefinitionResolver.findProperty(orderType, 'name', null);
+		assertEquals 0, paths.size()
+
+		// order.item
+		paths = DefinitionResolver.findProperty(orderType, 'item', null);
+		assertEquals 1, paths.size()
+	}
+
+	/**
+	 * Tests finding properties w/o a fixed namespace and using the caching constraint.
+	 */
+	void testFindPropertyCached() {
+		// item.name
+		List<DefinitionPath> paths = DefinitionResolver.findPropertyCached(itemType, 'name', null);
+		assertEquals 2, paths.size()
+
+		// direct property
+		DefinitionPath path1 = paths[0]
+		assertEquals 1, path1.path.size()
+
+		// inside group
+		DefinitionPath path2 = paths[1]
+		assertEquals 2, path2.path.size()
+		assertTrue path2.path[0] instanceof GroupPropertyDefinition
+	}
+
+	/**
+	 * Tests finding properties w/ a fixed namespace.
+	 */
+	void testFindPropertyNamespace() {
+		// item.name
+		List<DefinitionPath> paths = DefinitionResolver.findProperty(itemType, 'name', SPECIAL_NS);
+		assertEquals 1, paths.size()
+
+		// inside group
+		DefinitionPath path1 = paths[0]
+		assertEquals 2, path1.path.size()
+		assertTrue path1.path[0] instanceof GroupPropertyDefinition
+	}
+}
