43e9a97d9f689433dc1b00288673234749f09548
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-09-26 18:50:19 +0200

    added actions, advisors and basic UI for exporting schemas
    Change-Id: Ia0d37e05a6110beff6b56e30f984d2bc3ef43929

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/schema/SchemaExportAdvisor.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     HUMBOLDT EU Integrated Project #030962
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.io.schema;
+
+import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
+import eu.esdihumboldt.hale.common.schema.io.SchemaWriter;
+import eu.esdihumboldt.hale.ui.io.DefaultIOAdvisor;
+import eu.esdihumboldt.hale.ui.service.schema.SchemaService;
+
+/**
+ * Advisor for schema export from the {@link SchemaService}
+ * 
+ * @author Simon Templer
+ * @since 2.7
+ */
+public class SchemaExportAdvisor extends DefaultIOAdvisor<SchemaWriter> {
+
+	private final SchemaSpaceID spaceID;
+
+	/**
+	 * Create a schema export advisor
+	 * 
+	 * @param spaceID the schema space ID, either {@link SchemaSpaceID#SOURCE}
+	 *            or {@link SchemaSpaceID#TARGET}
+	 */
+	public SchemaExportAdvisor(SchemaSpaceID spaceID) {
+		super();
+		this.spaceID = spaceID;
+	}
+
+	@Override
+	public void prepareProvider(SchemaWriter provider) {
+		super.prepareProvider(provider);
+
+		SchemaService ss = getService(SchemaService.class);
+		provider.setSchemas(ss.getSchemas(spaceID));
+	}
+
+}
