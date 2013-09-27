5600b40d919dfc92d75bbda6b33bc60a46aef725
Thorsten Reitz <treitz@esri.com>
2013-09-27 15:43:11 +0200

    Added JsonSchemafromXsdTest.java to test JsonSchemaWriter with exampels from XSDs

---
+++ io/plugins/eu.esdihumboldt.hale.io.json.test/src/eu/esdihumboldt/hale/io/json/JsonSchemafromXsdTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.io.json;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+import eu.esdihumboldt.hale.common.core.io.IOProviderConfigurationException;
+import eu.esdihumboldt.hale.common.core.io.report.IOReport;
+import eu.esdihumboldt.hale.common.core.io.supplier.FileIOSupplier;
+import eu.esdihumboldt.hale.common.schema.io.SchemaWriter;
+import eu.esdihumboldt.hale.common.schema.model.Schema;
+import eu.esdihumboldt.hale.common.schema.model.impl.DefaultSchemaSpace;
+import eu.esdihumboldt.hale.common.test.TestUtil;
+
+/**
+ * A comprehensive test of the {@link JsonSchemaWriter} using XSDs.
+ * 
+ * @author Thorsten Reitz
+ */
+public class JsonSchemafromXsdTest {
+
+	@Test
+	public void testFeatureData() {
+		try {
+			Schema schema = TestUtil.loadSchema(new URI(this.getClass().getClassLoader()
+					.getResource("xsd/FeatureData.xsd").toString()));
+
+			assertNotNull(schema);
+
+			Path tempFile = Files.createTempFile("hale-json-schema-test", ".json");
+			SchemaWriter writer = new JsonSchemaWriter();
+			writer.setSchemas(new DefaultSchemaSpace().addSchema(schema));
+			writer.setTarget(new FileIOSupplier(tempFile.toFile()));
+			IOReport report = writer.execute(null);
+
+		} catch (IOProviderConfigurationException | IOException | URISyntaxException e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
