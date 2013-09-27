1d76a6d4c56b7c6b21f61d370458bfcd1fd4f7dc
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-09-27 10:58:50 +0200

    ask user for approval on resource removal
    refs #326
    
    Change-Id: I9c1c507036824bc1dfd13b18b9e2cb73b787b17a

--- ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/action/handler/RemoveResourceHandler.java
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/io/action/handler/RemoveResourceHandler.java
@@ -15,9 +15,12 @@
 
 package eu.esdihumboldt.hale.ui.io.action.handler;
 
+import java.text.MessageFormat;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.PlatformUI;
@@ -26,6 +29,7 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import de.cs3d.util.logging.ALogger;
 import de.cs3d.util.logging.ALoggerFactory;
 import eu.esdihumboldt.hale.common.core.io.project.model.Resource;
+import eu.esdihumboldt.hale.ui.io.action.ActionUI;
 import eu.esdihumboldt.hale.ui.io.action.ActionUIAdvisor;
 import eu.esdihumboldt.hale.ui.io.action.ActionUIExtension;
 import eu.esdihumboldt.hale.ui.service.project.ProjectService;
@@ -50,19 +54,53 @@ public class RemoveResourceHandler extends AbstractHandler {
 				Resource resource = (Resource) element;
 
 				// retrieve action UI advisor
-				ActionUIAdvisor<?> advisor = ActionUIExtension.getInstance()
-						.findActionUI(resource.getActionId()).getUIAdvisor();
-				if (advisor != null && advisor.supportsRemoval()) {
-					String id = resource.getResourceId();
-					if (advisor.removeResource(id)) {
-						// removal succeeded, so remove from project as well
-						ProjectService ps = (ProjectService) PlatformUI.getWorkbench().getService(
-								ProjectService.class);
-						ps.removeResource(id);
+				ActionUI actionUI = ActionUIExtension.getInstance().findActionUI(
+						resource.getActionId());
+				if (actionUI != null) {
+					ActionUIAdvisor<?> advisor = actionUI.getUIAdvisor();
+					if (advisor != null && advisor.supportsRemoval()) {
+						String name = null;
+						if (resource.getSource() != null) {
+							String location = resource.getSource().toString();
+							int index = location.lastIndexOf('/');
+							if (index > 0 && index < location.length()) {
+								name = location.substring(index + 1);
+							}
+						}
+
+						String resourceType = actionUI.getResourceName();
+						if (resourceType == null) {
+							resourceType = "resource";
+						}
+
+						String message;
+						if (name == null) {
+							message = MessageFormat.format(
+									"Do you really want to remove this {0}?", resourceType);
+						}
+						else {
+							message = MessageFormat
+									.format("Do you really want to remove the {0} {1}?",
+											resourceType, name);
+						}
+
+						if (MessageDialog.openQuestion(HandlerUtil.getActiveShell(event),
+								"Remove resource", message)) {
+							// do the actual removal
+
+							String id = resource.getResourceId();
+							if (advisor.removeResource(id)) {
+								// removal succeeded, so remove from project as
+								// well
+								ProjectService ps = (ProjectService) PlatformUI.getWorkbench()
+										.getService(ProjectService.class);
+								ps.removeResource(id);
+							}
+						}
 					}
-				}
-				else {
-					log.userError("Removing this resource is not supported.");
+					else {
+						log.userError("Removing this resource is not supported.");
+					}
 				}
 			}
 		}
