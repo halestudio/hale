b2ce6ec796d577f2ef2fa5537ec55a8e0d5618a3
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2013-10-23 13:02:21 +0200

    support for import/export of xls lookup table files
    refs #603
    
    Change-Id: Ie77e5df66ac3ea82ce2bfd045712046396f034a1
    Reviewed-on: http://gerrit.esdi-community.eu/2498
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- io/plugins/eu.esdihumboldt.hale.io.csv.ui/src/eu/esdihumboldt/hale/io/csv/ui/LookupTablePage.java
+++ io/plugins/eu.esdihumboldt.hale.io.csv.ui/src/eu/esdihumboldt/hale/io/csv/ui/LookupTablePage.java
@@ -16,7 +16,14 @@
 package eu.esdihumboldt.hale.io.csv.ui;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
@@ -30,8 +37,9 @@ import org.eclipse.swt.widgets.Label;
 import au.com.bytecode.opencsv.CSVReader;
 import eu.esdihumboldt.hale.common.core.io.Value;
 import eu.esdihumboldt.hale.common.lookup.LookupTableImport;
-import eu.esdihumboldt.hale.io.csv.reader.internal.CSVConstants;
+import eu.esdihumboldt.hale.io.csv.reader.internal.CSVLookupReader;
 import eu.esdihumboldt.hale.io.csv.reader.internal.CSVUtil;
+import eu.esdihumboldt.hale.io.csv.writer.LookupTableExportConstants;
 import eu.esdihumboldt.hale.ui.lookup.LookupTableImportConfigurationPage;
 
 /**
@@ -105,11 +113,13 @@ public class LookupTablePage extends LookupTableImportConfigurationPage implemen
 	 */
 	@Override
 	public boolean updateConfiguration(LookupTableImport provider) {
-		provider.setParameter(CSVConstants.PARAM_SKIP_FIRST_LINE, Value.of(skip));
+		provider.setParameter(LookupTableExportConstants.PARAM_SKIP_FIRST_LINE, Value.of(skip));
 
 		if (keyColumn.getSelectionIndex() != -1 && valueColumn.getSelectionIndex() != -1) {
-			provider.setParameter(CSVConstants.LOOKUP_KEY_COLUMN, Value.of(keyColumn.getSelectionIndex()));
-			provider.setParameter(CSVConstants.LOOKUP_VALUE_COLUMN, Value.of(valueColumn.getSelectionIndex()));
+			provider.setParameter(LookupTableExportConstants.LOOKUP_KEY_COLUMN,
+					Value.of(keyColumn.getSelectionIndex()));
+			provider.setParameter(LookupTableExportConstants.LOOKUP_VALUE_COLUMN,
+					Value.of(valueColumn.getSelectionIndex()));
 			return true;
 		}
 		else {
@@ -123,40 +133,71 @@ public class LookupTablePage extends LookupTableImportConfigurationPage implemen
 	 */
 	@Override
 	public void widgetSelected(SelectionEvent e) {
-		try {
-			if (e.getSource() != null) {
-				if (e.getSource().equals(choose)) {
-					l.setVisible(true);
-					keyColumn.setVisible(true);
-					valueColumn.setVisible(true);
-					CSVReader reader = CSVUtil.readFirst(getWizard().getProvider());
-					if (((Combo) e.getSource()).getSelectionIndex() == 0) {
-						// yes is selected
-						// Load the csv-file to get the column informations
-						skip = true;
-						String[] items = reader.readNext();
-						keyColumn.setItems(items);
-						valueColumn.setItems(items);
-						setPageComplete(true);
+		if (e.getSource() != null) {
+			if (e.getSource().equals(choose)) {
+				l.setVisible(true);
+				keyColumn.setVisible(true);
+				valueColumn.setVisible(true);
+				String[] header = readHeader();
+				if (((Combo) e.getSource()).getSelectionIndex() == 0) {
+					// yes is selected
+					// Load the csv-file to get the column informations
+					skip = true;
+					keyColumn.setItems(header);
+					valueColumn.setItems(header);
+					setPageComplete(true);
+				}
+				else {
+					// no is selected
+					skip = false;
+					int numberOfColumns = header.length;
+					String[] items = new String[numberOfColumns];
+					for (int i = 0; i < numberOfColumns; i++) {
+						int tmp = i + 1;
+						items[i] = "Column " + tmp;
 					}
-					else {
-						// no is selected
-						skip = false;
-						int numberOfColumns = reader.readNext().length;
-						String[] items = new String[numberOfColumns];
-						for (int i = 0; i < numberOfColumns; i++) {
-							int tmp = i + 1;
-							items[i] = "Column " + tmp;
-						}
-						keyColumn.setItems(items);
-						valueColumn.setItems(items);
-						setPageComplete(true);
-					}
+					keyColumn.setItems(items);
+					valueColumn.setItems(items);
+					setPageComplete(true);
 				}
 			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
 		}
+	}
+
+	private String[] readHeader() {
+		LookupTableImport provider = getWizard().getProvider();
+		List<String> items = new ArrayList<String>();
+		try {
+			if (provider instanceof CSVLookupReader) {
+				CSVReader reader = CSVUtil.readFirst(getWizard().getProvider());
+				return reader.readNext();
+			}
+			else {
+				Workbook workbook;
+				// write xls file
+				String file = provider.getSource().getLocation().getPath();
+				String fileExtension = file.substring(file.lastIndexOf("."), file.length());
+				if (fileExtension.equals(".xls")) {
+					workbook = new HSSFWorkbook(provider.getSource().getInput());
+				}
+				// write xlsx file
+				else if (fileExtension.equals(".xlsx")) {
+					workbook = new XSSFWorkbook(provider.getSource().getInput());
+				}
+				else
+					return new String[0];
+				Sheet sheet = workbook.getSheetAt(0);
+				Row currentRow = sheet.getRow(0);
+				for (int cell = 0; cell < currentRow.getPhysicalNumberOfCells(); cell++) {
+					items.add(currentRow.getCell(cell).getStringCellValue());
+				}
+				return items.toArray(new String[0]);
+			}
+		} catch (IOException e) {
+			// TODO logging
+			e.printStackTrace();
+		}
+		return new String[0];
 	}
 
 	/**
