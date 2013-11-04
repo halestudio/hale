e2d099932a4b40ac061f8f0fd9b6d98db9fa18e2
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-11-04 11:23:51 +0100

    show example code for accessing Groovy Retype source structure
    Change-Id: I63a2eab0201d62c769c6c5695dc05d985218f683
    Reviewed-on: http://gerrit.esdi-community.eu/2534
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- ui/plugins/eu.esdihumboldt.hale.ui.functions.groovy/src/eu/esdihumboldt/hale/ui/functions/groovy/TypeStructureTray.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.groovy/src/eu/esdihumboldt/hale/ui/functions/groovy/TypeStructureTray.java
@@ -53,11 +53,13 @@ import eu.esdihumboldt.cst.functions.groovy.GroovyConstants;
 import eu.esdihumboldt.hale.common.schema.Classification;
 import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
 import eu.esdihumboldt.hale.common.schema.groovy.DefinitionAccessor;
+import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
 import eu.esdihumboldt.hale.common.schema.model.Definition;
 import eu.esdihumboldt.hale.common.schema.model.DefinitionGroup;
 import eu.esdihumboldt.hale.common.schema.model.DefinitionUtil;
 import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.Cardinality;
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.HasValueFlag;
 import eu.esdihumboldt.hale.ui.HaleWizardPage;
 import eu.esdihumboldt.hale.ui.common.CommonSharedImages;
@@ -207,10 +209,6 @@ public class TypeStructureTray extends DialogTray {
 		/*
 		 * Groovy specific part
 		 */
-		if (schemaSpace == SchemaSpaceID.SOURCE) {
-			// XXX not yet supported for source
-			return page;
-		}
 
 		// caption
 		Label example = new Label(page, SWT.NONE);
@@ -247,21 +245,25 @@ public class TypeStructureTray extends DialogTray {
 			public void selectionChanged(SelectionChangedEvent event) {
 				IDocument doc = new Document();
 				GroovySourceViewerUtil.setupDocument(doc);
-				if (event.getSelection().isEmpty()) {
+				String example = null;
+				if (!event.getSelection().isEmpty()) {
+					switch (schemaSpace) {
+					case SOURCE:
+						example = createSourceSample(event.getSelection(), types);
+						break;
+					case TARGET:
+						example = createTargetSample(event.getSelection(), types);
+						break;
+					}
+				}
+
+				if (example == null || example.isEmpty()) {
 					doc.set("// Please select a schema element");
 				}
 				else {
-					switch (schemaSpace) {
-					case SOURCE:
-						doc.set(""); // TODO
-						break;
-					case TARGET:
-						doc.set(createTargetSample(event.getSelection(), types));
-						break;
-					default:
-						doc.set("");
-					}
+					doc.set(example);
 				}
+
 				viewer.setDocument(doc);
 			}
 		});
@@ -269,6 +271,176 @@ public class TypeStructureTray extends DialogTray {
 		tree.setSelection(StructuredSelection.EMPTY);
 
 		return page;
+	}
+
+	/**
+	 * Create sample code for accessing a source property.
+	 * 
+	 * @param selection the selection in the tree viewer
+	 * @param types the types serving as input
+	 * @return the sample code
+	 */
+	protected String createSourceSample(ISelection selection,
+			Collection<? extends TypeDefinition> types) {
+		ITreeSelection treeSel = (ITreeSelection) selection;
+
+		TreePath[] paths = treeSel.getPaths();
+		if (paths != null && paths.length > 0) {
+			// XXX for now only use the first path
+
+			TreePath path = paths[0];
+
+			DefinitionGroup parent;
+			int startIndex = 0;
+			String prefix;
+
+			// determine parent type
+			if (path.getFirstSegment() instanceof TypeDefinition) {
+				// types are the top level elements
+//				parent = (DefinitionGroup) path.getFirstSegment();
+//				startIndex = 1;
+				// TODO (join)
+				return null;
+			}
+			else {
+				// types are not in the tree, single type must be root
+				parent = types.iterator().next();
+				// assuming Retype/Merge
+				prefix = GroovyConstants.BINDING_SOURCE;
+			}
+
+			StringBuilder access = new StringBuilder();
+			access.append(prefix);
+			if (path.getSegmentCount() > startIndex) {
+				// a property is referenced
+				access.append(".p");
+			}
+			else {
+				// XXX possible?
+			}
+
+			for (int i = startIndex; i < path.getSegmentCount(); i++) {
+				Definition<?> def = (Definition<?>) path.getSegment(i);
+				if (def instanceof PropertyDefinition) {
+					// property name
+					access.append('.');
+					access.append(def.getName().getLocalPart());
+
+					// test if uniquely accessible from parent
+					boolean useNamespace = true;
+					if (parent instanceof Definition<?>) {
+						try {
+							new DefinitionAccessor((Definition<?>) parent).findChildren(
+									def.getName().getLocalPart()).eval();
+							useNamespace = false;
+						} catch (IllegalStateException e) {
+							// ignore - namespace needed
+						}
+					}
+
+					// add namespace if necessary
+					if (useNamespace) {
+						access.append("('");
+						access.append(def.getName().getNamespaceURI());
+						access.append("')");
+					}
+				}
+				else {
+					// groups are ignored
+				}
+
+				// set the new parent
+				parent = DefinitionUtil.getDefinitionGroup(def);
+			}
+
+			if (parent instanceof TypeDefinition) {
+				// only properties at the end of the path are supported
+				TypeDefinition propertyType = (TypeDefinition) parent;
+				StringBuilder example = new StringBuilder();
+
+				boolean canOccurMultipleTimes = false;
+				/*
+				 * Instances/values may occur multiple times if any element in
+				 * the path may occur multiple times.
+				 */
+				for (int i = path.getSegmentCount() - 1; i >= 0 && !canOccurMultipleTimes; i--) {
+					if (path.getSegment(i) instanceof ChildDefinition<?>) {
+						Cardinality card = DefinitionUtil.getCardinality((ChildDefinition<?>) path
+								.getSegment(i));
+						canOccurMultipleTimes = card.getMaxOccurs() == Cardinality.UNBOUNDED
+								|| card.getMaxOccurs() > 1;
+					}
+				}
+
+				// check different cases
+
+				if (propertyType.getConstraint(HasValueFlag.class).isEnabled()) {
+					// referenced property is a value or has a value
+
+					// single value
+					if (canOccurMultipleTimes) {
+						example.append("// access first value\n");
+					}
+					else {
+						example.append("// access value\n");
+					}
+					example.append("def value = ");
+					example.append(access);
+					example.append(".value()\n\n");
+
+					// multiple values
+					if (canOccurMultipleTimes) {
+						example.append("// access all values as list\n");
+						example.append("def valueList = ");
+						example.append(access);
+						example.append(".values()\n\n");
+					}
+				}
+
+				if (DefinitionUtil.hasChildren(propertyType)) {
+					// referenced property is an instance
+
+					// single instance
+					if (canOccurMultipleTimes) {
+						example.append("// access first instance\n");
+					}
+					else {
+						example.append("// access instance\n");
+					}
+					example.append("def instance = ");
+					example.append(access);
+					example.append(".first()\n\n");
+
+					if (canOccurMultipleTimes) {
+						// multiple values
+						example.append("// access all instances as list\n");
+						example.append("def instanceList = ");
+						example.append(access);
+						example.append(".all()\n\n");
+
+						// iterate over instances
+						example.append("// iterate over instances\n");
+						example.append(access);
+						example.append(".each {\n");
+						example.append("\tinstance ->\n");
+						example.append("}\n\n");
+					}
+				}
+				else if (canOccurMultipleTimes
+						&& propertyType.getConstraint(HasValueFlag.class).isEnabled()) {
+					// iterate over values
+					example.append("// iterate over values\n");
+					example.append(access);
+					example.append(".each {\n");
+					example.append("\tvalue ->\n");
+					example.append("}\n\n");
+				}
+
+				return example.toString();
+			}
+		}
+
+		return null;
 	}
 
 	/**
