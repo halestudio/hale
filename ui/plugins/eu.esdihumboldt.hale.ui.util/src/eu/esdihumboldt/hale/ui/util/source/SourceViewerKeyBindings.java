3233d97ec76b2eefd936f7bf8530bda3684500ad
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-11-13 10:08:40 +0100

    support CTRL+D to delete line in source viewer
    Change-Id: I659e53f91a710d67ae1efbde34228394bf6d029d

--- ui/plugins/eu.esdihumboldt.hale.ui.util/src/eu/esdihumboldt/hale/ui/util/source/SourceViewerKeyBindings.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.util/src/eu/esdihumboldt/hale/ui/util/source/SourceViewerKeyBindings.java
@@ -15,7 +15,19 @@
 
 package eu.esdihumboldt.hale.ui.util.source;
 
+import org.eclipse.jface.bindings.TriggerSequence;
+import org.eclipse.jface.bindings.keys.KeySequence;
+import org.eclipse.jface.bindings.keys.ParseException;
+import org.eclipse.jface.bindings.keys.SWTKeySupport;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextViewer;
 import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.swt.custom.VerifyKeyListener;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.graphics.Point;
+
+import de.cs3d.util.logging.ALogger;
+import de.cs3d.util.logging.ALoggerFactory;
 
 /**
  * Utility class for source viewer key bindings if used outside an editor.
@@ -23,6 +35,8 @@ import org.eclipse.jface.text.source.SourceViewer;
  * @author Simon Templer
  */
 public abstract class SourceViewerKeyBindings {
+
+	private static final ALogger log = ALoggerFactory.getLogger(SourceViewerKeyBindings.class);
 
 	/**
 	 * Install the default bindings for undo, redo and shift.
@@ -32,6 +46,68 @@ public abstract class SourceViewerKeyBindings {
 	public static void installDefault(SourceViewer viewer) {
 		SourceViewerUndoSupport.install(viewer);
 		TextViewerOperationSupport.installShift(viewer);
+		installDeleteLine(viewer);
+	}
+
+	/**
+	 * Install the ability to delete a line through <code>CTRL+D</code> on a
+	 * text viewer.
+	 * 
+	 * @param viewer the text viewer
+	 */
+	public static void installDeleteLine(final TextViewer viewer) {
+		try {
+			final TriggerSequence trigger = KeySequence.getInstance("CTRL+D");
+			viewer.appendVerifyKeyListener(new VerifyKeyListener() {
+
+				@Override
+				public void verifyKey(VerifyEvent event) {
+					int accelerator = SWTKeySupport.convertEventToUnmodifiedAccelerator(event);
+					KeySequence sequence = KeySequence.getInstance(SWTKeySupport
+							.convertAcceleratorToKeyStroke(accelerator));
+					if (trigger.equals(sequence)) {
+						// determine the current selection
+						int startOffset;
+						int endOffset;
+						Point sel = viewer.getTextWidget().getSelectionRange();
+						if (sel != null) {
+							startOffset = sel.x;
+							endOffset = startOffset + sel.y;
+						}
+						else {
+							startOffset = viewer.getTextWidget().getCaretOffset();
+							endOffset = startOffset;
+						}
+
+						try {
+							// determine the involved lines
+							IDocument doc = viewer.getDocument();
+							int startLine = doc.getLineOfOffset(startOffset);
+							int endLine = doc.getLineOfOffset(endOffset);
+							// derive start and end offset
+							startOffset = doc.getLineOffset(startLine);
+							if (startLine != endLine) {
+								// delete multiple lines
+								endOffset = doc.getLineOffset(endLine) + doc.getLineLength(endLine);
+							}
+							else {
+								// delete one line
+								endOffset = startOffset + doc.getLineLength(endLine);
+							}
+
+							// delete the line
+							doc.replace(startOffset, endOffset - startOffset, "");
+
+							event.doit = false;
+						} catch (Exception e) {
+							log.warn("Failed to delete line in document", e);
+						}
+					}
+				}
+			});
+		} catch (ParseException e) {
+			log.error("Failed to install delete line listener on source viewer", e);
+		}
 	}
 
 }
