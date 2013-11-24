f00a5a9f7a7fac38c5a0694c2cb76986b1c5112b
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2013-11-24 18:43:19 +0100

    support for xls/xlsx instance files
    refs #433
    
    Change-Id: I5cabfb99f4fe59ae375379abaa6da6cef5003897

--- io/plugins/eu.esdihumboldt.hale.io.csv.ui/src/eu/esdihumboldt/hale/io/csv/ui/TypeSelectionPage.java
+++ io/plugins/eu.esdihumboldt.hale.io.csv.ui/src/eu/esdihumboldt/hale/io/csv/ui/TypeSelectionPage.java
@@ -31,10 +31,10 @@ import org.eclipse.ui.PlatformUI;
 
 import eu.esdihumboldt.hale.common.core.io.Value;
 import eu.esdihumboldt.hale.common.instance.io.InstanceReader;
-import eu.esdihumboldt.hale.common.schema.SchemaConstants;
 import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.io.csv.reader.CSVConstants;
+import eu.esdihumboldt.hale.io.csv.reader.CommonSchemaConstants;
 import eu.esdihumboldt.hale.io.csv.reader.internal.CSVConfiguration;
 import eu.esdihumboldt.hale.io.csv.reader.internal.CSVInstanceReader;
 import eu.esdihumboldt.hale.ui.common.definition.selector.TypeDefinitionSelector;
@@ -130,7 +130,7 @@ public class TypeSelectionPage extends InstanceReaderConfigurationPage implement
 		if (sel.getSelectedObject() != null) {
 			QName name = sel.getSelectedObject().getName();
 			String param_name = name.toString();
-			provider.setParameter(SchemaConstants.PARAM_TYPENAME, Value.of(param_name));
+			provider.setParameter(CommonSchemaConstants.PARAM_TYPENAME, Value.of(param_name));
 		}
 		else {
 			return false;
