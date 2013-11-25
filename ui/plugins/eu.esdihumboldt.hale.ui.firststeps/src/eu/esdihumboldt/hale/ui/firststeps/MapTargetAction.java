947790ea7b47107325db212ce2216993194c1d24
Simon Templer <simon.templer@igd.fraunhofer.de>
2013-11-25 13:08:05 +0100

    added cheat sheet action for mapping a target entity
    Change-Id: I863331ba503712bafb57ff6f8567545d4a27b308
    Reviewed-on: http://gerrit.esdi-community.eu/2597
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.firststeps/src/eu/esdihumboldt/hale/ui/firststeps/MapTargetAction.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2013 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.firststeps;
+
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.cheatsheets.ICheatSheetManager;
+
+import eu.esdihumboldt.hale.common.align.model.EntityDefinition;
+import eu.esdihumboldt.hale.ui.function.FunctionWizardUtil;
+import eu.esdihumboldt.hale.ui.selection.impl.DefaultSchemaSelection;
+import eu.esdihumboldt.hale.ui.views.schemas.SchemasView;
+
+/**
+ * Cheat sheet action that opens a dialog/wizard for mapping to a specific
+ * target type or property.
+ * 
+ * @author Simon Templer
+ */
+public class MapTargetAction extends AbstractTargetAction {
+
+	@Override
+	protected void run(EntityDefinition target, ICheatSheetManager manager) {
+		// try selecting the target entity in the schema explorer
+		try {
+			DefaultSchemaSelection ss = new DefaultSchemaSelection();
+			ss.addTargetItem(target);
+
+			IViewPart view = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+					.findView(SchemasView.ID);
+			view.getSite().getSelectionProvider().setSelection(ss);
+		} catch (Exception e) {
+			// ignore
+		}
+
+		// launch the wizard
+		FunctionWizardUtil.addRelationForTarget(target);
+	}
+
+}
