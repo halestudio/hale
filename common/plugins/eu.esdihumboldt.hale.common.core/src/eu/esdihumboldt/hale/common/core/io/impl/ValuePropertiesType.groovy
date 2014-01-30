f1d6325bed9606db1782c84fcba0e9cc9b1cb50c
Simon Templer <simon.templer@igd.fraunhofer.de>
2014-01-30 14:20:40 +0100

    added ValueList, ValueMap and ValueProperties types
    Change-Id: Iad8ea8f1537832098946d477e14a66638dc7ff96
    Reviewed-on: http://gerrit.esdi-community.eu/2666
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ common/plugins/eu.esdihumboldt.hale.common.core/src/eu/esdihumboldt/hale/common/core/io/impl/ValuePropertiesType.groovy
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2014 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.core.io.impl
+
+import org.w3c.dom.Element
+
+import eu.esdihumboldt.hale.common.core.io.ComplexValueType
+import eu.esdihumboldt.hale.common.core.io.HaleIO
+import eu.esdihumboldt.hale.common.core.io.Value
+import eu.esdihumboldt.hale.common.core.io.ValueProperties
+import eu.esdihumboldt.util.groovy.xml.NSDOMBuilder
+import groovy.xml.dom.DOMCategory
+
+
+/**
+ * XML serialization for {@link ValueProperties}.
+ * 
+ * @author Simon Templer
+ */
+class ValuePropertiesType implements ComplexValueType<ValueProperties, Void> {
+
+	@Override
+	ValueProperties fromDOM(Element fragment, Void context) {
+		ValueProperties properties = new ValueProperties()
+
+		use (DOMCategory) {
+			for (entry in fragment.property) {
+				String key = entry.'@name'
+				Value value = ValuePropertiesType.fromTag(entry.value[0])
+				properties[key] = value
+			}
+		}
+
+		return properties;
+	}
+
+	@Override
+	Element toDOM(ValueProperties properties) {
+		def builder = NSDOMBuilder.newInstance(core: HaleIO.NS_HALE_CORE)
+
+		def fragment = builder.'core:properties' {
+			properties.each { String key, Value value ->
+				// ignore null values
+				if (value != null) {
+					property(name: key) {
+						ValueListType.valueTag(builder, 'value', value)
+					}
+				}
+			}
+		}
+
+		return fragment;
+	}
+
+	@Override
+	Class<Void> getContextType() {
+		return Void.class;
+	}
+}
