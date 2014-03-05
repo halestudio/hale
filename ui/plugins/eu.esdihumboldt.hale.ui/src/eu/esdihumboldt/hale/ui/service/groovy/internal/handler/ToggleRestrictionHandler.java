c9596eb758f70a9d879c5df34f61796657cf1d8f
Kai Schwierczek <kai.schwierczek@igd.fraunhofer.de>
2014-03-05 19:51:48 +0100

    WIP toggle button for groovy restriction option
    refs #589
    
    Change-Id: Ie9ff179121517fbf8dc5759b0522c562e94db1cc

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/groovy/internal/handler/ToggleRestrictionHandler.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.service.groovy.internal.handler;
+
+import java.util.Map;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.commands.IElementUpdater;
+import org.eclipse.ui.menus.UIElement;
+
+import eu.esdihumboldt.hale.ui.HaleUI;
+import eu.esdihumboldt.util.groovy.sandbox.GroovyService;
+
+/**
+ * Handler that toggles the groovy script restriction.
+ * 
+ * @author Kai
+ */
+public class ToggleRestrictionHandler extends AbstractHandler implements IElementUpdater {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		GroovyService gs = HaleUI.getServiceProvider().getService(GroovyService.class);
+		// TODO ask for confirmation
+		gs.setRestrictionActive(!gs.isRestrictionActive());
+		return null;
+	}
+
+	@Override
+	public void updateElement(UIElement element, @SuppressWarnings("rawtypes") Map parameters) {
+		GroovyService gs = HaleUI.getServiceProvider().getService(GroovyService.class);
+		element.setChecked(gs.isRestrictionActive());
+	}
+}
