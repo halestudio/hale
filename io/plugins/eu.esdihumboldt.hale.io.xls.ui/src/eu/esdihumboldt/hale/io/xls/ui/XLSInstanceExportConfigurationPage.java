bf8d57ef1831d4b7773c75e346599f6b6af02313
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2014-03-07 15:40:24 +0100

    solve simplified nested properties
    adapted xls instance reader and writer to solve nested properties (only
    one path in the hierarchy)
    
    only intermediate result:
    creation of nested properties but instances of nested properties are not integrated correctly
    adaption required to create one typedefinition per xls sheet
    
    refs #433
    
    Change-Id: Ief98bbb08f51a79c7abc88637003cd126a825dd8

---
+++ io/plugins/eu.esdihumboldt.hale.io.xls.ui/src/eu/esdihumboldt/hale/io/xls/ui/XLSInstanceExportConfigurationPage.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2014 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.io.xls.ui;
+
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import eu.esdihumboldt.hale.common.core.io.IOProvider;
+import eu.esdihumboldt.hale.common.core.io.Value;
+import eu.esdihumboldt.hale.io.xls.XLSConstants;
+import eu.esdihumboldt.hale.ui.io.IOWizard;
+import eu.esdihumboldt.hale.ui.io.config.AbstractConfigurationPage;
+
+/**
+ * Configuration page for xls instance export
+ * 
+ * @author Patrick Lieb
+ */
+public class XLSInstanceExportConfigurationPage extends
+		AbstractConfigurationPage<IOProvider, IOWizard<IOProvider>> {
+
+	private Button solveNestedProperties;
+
+	/**
+	 * 
+	 */
+	public XLSInstanceExportConfigurationPage() {
+		super("xlsInstanceExport.configPage");
+
+		setTitle("Additonal Export Options");
+		setDescription("Select if nested properties should be solved");
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.config.AbstractConfigurationPage#enable()
+	 */
+	@Override
+	public void enable() {
+		// not required
+
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.config.AbstractConfigurationPage#disable()
+	 */
+	@Override
+	public void disable() {
+		// not required
+
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.io.IOWizardPage#updateConfiguration(eu.esdihumboldt.hale.common.core.io.IOProvider)
+	 */
+	@Override
+	public boolean updateConfiguration(IOProvider provider) {
+		provider.setParameter(XLSConstants.SOLVE_NESTED_PROPERTIES,
+				Value.of(solveNestedProperties.getSelection()));
+		return true;
+	}
+
+	/**
+	 * @see eu.esdihumboldt.hale.ui.HaleWizardPage#createContent(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected void createContent(Composite page) {
+		page.setLayout(GridLayoutFactory.fillDefaults().create());
+		solveNestedProperties = new Button(page, SWT.CHECK);
+		solveNestedProperties.setText("Solve nested properties");
+		solveNestedProperties.setSelection(false);
+
+	}
+}
