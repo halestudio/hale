559a2de1a125bd6f44332b5d8ff61c90dfac7d84
Simon Templer <simon.templer@igd.fraunhofer.de>
2014-04-16 10:11:26 +0200

    retrieve INSPIRE code lists with Accept/Accept-Language headers
    refs #634
    
    Change-Id: Ia44f5ca6e9051f5f3b4a6e70c41aec1316ff2fc6

--- io/plugins/eu.esdihumboldt.hale.io.codelist.xml/src/eu/esdihumboldt/hale/io/codelist/inspire/reader/INSPIRECodeListReader.java
+++ io/plugins/eu.esdihumboldt.hale.io.codelist.xml/src/eu/esdihumboldt/hale/io/codelist/inspire/reader/INSPIRECodeListReader.java
@@ -3,16 +3,28 @@ package eu.esdihumboldt.hale.io.codelist.inspire.reader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.Locale;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import eu.esdihumboldt.hale.common.codelist.CodeList;
 import eu.esdihumboldt.hale.common.codelist.CodeList.CodeEntry;
@@ -51,19 +63,65 @@ public class INSPIRECodeListReader extends AbstractImportProvider implements Cod
 	protected IOReport execute(ProgressIndicator progress, IOReporter reporter)
 			throws IOProviderConfigurationException, IOException {
 		progress.begin("Loading code list.", ProgressIndicator.UNKNOWN);
-		try (InputStream is = getSource().getInput()) {
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(is);
 
-			URI loc = getSource().getLocation();
+		Document doc;
 
+		URI loc = getSource().getLocation();
+		if (loc != null && (loc.getScheme().equals("http") || loc.getScheme().equals("https"))) {
+			// load with HTTP client
+			// and provide headers to retrieve correct format and language
+			doc = Request.Get(loc)
+					.addHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_XML.getMimeType())
+					.addHeader(HttpHeaders.ACCEPT_LANGUAGE, Locale.getDefault().getLanguage())
+					.execute().handleResponse(new ResponseHandler<Document>() {
+
+						@Override
+						public Document handleResponse(HttpResponse response)
+								throws ClientProtocolException, IOException {
+							StatusLine statusLine = response.getStatusLine();
+							HttpEntity entity = response.getEntity();
+							if (statusLine.getStatusCode() >= 300) {
+								throw new HttpResponseException(statusLine.getStatusCode(),
+										statusLine.getReasonPhrase());
+							}
+							if (entity == null) {
+								throw new ClientProtocolException("Response contains no content");
+							}
+							DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+							try {
+								DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+								ContentType contentType = ContentType.getOrDefault(entity);
+								if (!contentType.getMimeType().equals(
+										ContentType.APPLICATION_XML.getMimeType())) {
+									throw new ClientProtocolException("Unexpected content type:"
+											+ contentType);
+								}
+								return docBuilder.parse(entity.getContent());
+							} catch (ParserConfigurationException ex) {
+								throw new IllegalStateException(ex);
+							} catch (SAXException ex) {
+								throw new ClientProtocolException("Malformed XML document", ex);
+							}
+						}
+					});
+		}
+		else {
+			// just access stream
+			try (InputStream is = getSource().getInput()) {
+				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+				DocumentBuilder db = dbf.newDocumentBuilder();
+				doc = db.parse(is);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		try {
 			reporter.setSuccess(parse(doc, loc, reporter));
-
-			progress.setCurrentTask("Code list loaded.");
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+		progress.setCurrentTask("Code list loaded.");
 
 		return reporter;
 	}
