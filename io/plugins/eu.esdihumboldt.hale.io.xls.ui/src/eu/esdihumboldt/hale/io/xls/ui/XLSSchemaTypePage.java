4170bc7b054bb1d98a3514d1ee30b955bcaa4e1b
Patrick Lieb <patrick.lieb@igd.fraunhofer.de>
2014-05-09 14:43:10 +0200

    import and export of flat xls instance files
    refs #433
    
    Change-Id: Ifc9fcf871f24a8a408cb21a8e4e43e24e2a21fc5
    Reviewed-on: http://gerrit.esdi-community.eu/2797
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- io/plugins/eu.esdihumboldt.hale.io.xls.ui/src/eu/esdihumboldt/hale/io/xls/ui/XLSSchemaTypePage.java
+++ io/plugins/eu.esdihumboldt.hale.io.xls.ui/src/eu/esdihumboldt/hale/io/xls/ui/XLSSchemaTypePage.java
@@ -15,6 +15,10 @@
 
 package eu.esdihumboldt.hale.io.xls.ui;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 import eu.esdihumboldt.hale.io.csv.ui.DefaultSchemaTypePage;
 import eu.esdihumboldt.hale.io.xls.AnalyseXLSSchemaTable;
 
@@ -22,27 +26,43 @@ import eu.esdihumboldt.hale.io.xls.AnalyseXLSSchemaTable;
  * Schema type configuration page for loading xls/xlsx schema files
  * 
  * @author Patrick Lieb
- *
+ * 
  */
-public class XLSSchemaTypePage extends DefaultSchemaTypePage{
-	
+public class XLSSchemaTypePage extends DefaultSchemaTypePage {
+
 	/**
 	 * Default constructor
 	 */
 	public XLSSchemaTypePage() {
 		super("XLS.SchemaTypePage");
 	}
-	
+
 	@Override
 	protected void onShowPage(boolean firstShow) {
-		
+
 		try {
-			AnalyseXLSSchemaTable analyser = new AnalyseXLSSchemaTable(getWizard().getProvider().getSource().getLocation());
+			AnalyseXLSSchemaTable analyser = new AnalyseXLSSchemaTable(getWizard().getProvider()
+					.getSource().getLocation());
 			setHeader(analyser.getHeader().toArray(new String[0]));
 			setSecondRow(analyser.getSecondRow().toArray(new String[0]));
 			super.onShowPage(firstShow);
+
+			Workbook wb = WorkbookFactory.create(getWizard().getProvider().getSource().getInput());
+			Sheet sheet = wb.getSheetAt(0);
+			sfe.setStringValue(sheet.getSheetName());
 		} catch (Exception e) {
 			setMessage("File cannot be loaded!", WARNING);
 		}
+	}
+
+	private String getFileName(String path) {
+		int indexStart = 0;
+		int indexEnd = path.length() - 1;
+
+		indexStart = path.lastIndexOf("/") + 1;
+		if (path.lastIndexOf(".") >= 0) {
+			indexEnd = path.lastIndexOf(".");
+		}
+		return path.substring(indexStart, indexEnd);
 	}
 }
