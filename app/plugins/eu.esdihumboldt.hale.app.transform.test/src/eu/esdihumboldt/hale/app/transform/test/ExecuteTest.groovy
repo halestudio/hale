8302a556e87b9adbfc2e3cc9dd10407635303c44
Simon Templer <simon.templer@igd.fraunhofer.de>
2014-05-26 17:00:57 +0200

    attempt on a command line application unit test
    Change-Id: I6f928b8d67a12a9e99e3ddabde9bc89130919a6e
    Reviewed-on: http://gerrit.esdi-community.eu/2820
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

---
+++ app/plugins/eu.esdihumboldt.hale.app.transform.test/src/eu/esdihumboldt/hale/app/transform/test/ExecuteTest.groovy
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2014 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.app.transform.test;
+
+import eu.esdihumboldt.hale.app.transform.ExecApplication
+
+
+
+/**
+ * Test command line application execution.
+ * 
+ * @author Simon Templer
+ */
+class ExecuteTest extends GroovyTestCase {
+
+	void testUsage() {
+		transform {
+			def lines = it.readLines()
+
+			assert lines[0].contains('Usage') || lines[1].contains('Usage')
+		}
+	}
+
+	// general stuff / utilities
+
+	/**
+	 * Run the transformation application.
+	 * @param exec
+	 * @return
+	 */
+	private int transform(List<String> args = [], Closure exec) {
+		PrintStream console = System.out
+		File output = File.createTempFile('app-test', '.log')
+		output.createNewFile()
+		output.deleteOnExit()
+		System.setOut(new PrintStream(output))
+		console.println ">> Writing output to ${output}..."
+		int res
+		try {
+			console.println ">> Executing application with the following arguments: ${args.join(' ')}"
+
+			/*
+			 * XXX conflicts with PDE JUnit application and is thus not
+			 * executable like this locally in Eclipse.
+			 */
+			//res = (int) ApplicationUtil.launchApplication('hale.transform', args)
+			res = ApplicationUtil.launchSyncApplication(new ExecApplication(), args)
+		} finally {
+			System.setOut(console)
+		}
+
+		console.println ">> Application exited with code $res"
+
+		output.eachLine { line, number ->
+			console.println "$number\t: $line"
+		}
+
+		exec(output)
+
+		return res
+	}
+}
