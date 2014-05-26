4811d6e9e61558a968b91683b3d464692117f03e
Simon Templer <simon.templer@igd.fraunhofer.de>
2014-05-26 15:26:40 +0200

    include app folder for preference updates
    Change-Id: I5172c9720e13121602b9418527802496bca1ef5c

--- app/plugins/eu.esdihumboldt.hale.app.transform/src/eu/esdihumboldt/hale/app/transform/ExecTransformation.java
+++ app/plugins/eu.esdihumboldt.hale.app.transform/src/eu/esdihumboldt/hale/app/transform/ExecTransformation.java
@@ -15,6 +15,11 @@
 
 package eu.esdihumboldt.hale.app.transform;
 
+import static eu.esdihumboldt.hale.app.transform.ExecUtil.error;
+import static eu.esdihumboldt.hale.app.transform.ExecUtil.fail;
+import static eu.esdihumboldt.hale.app.transform.ExecUtil.info;
+import static eu.esdihumboldt.hale.app.transform.ExecUtil.status;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -44,22 +49,20 @@ import eu.esdihumboldt.hale.common.headless.transform.Transformation;
 import eu.esdihumboldt.hale.common.instance.io.InstanceReader;
 import eu.esdihumboldt.hale.common.instance.io.InstanceWriter;
 
-import static eu.esdihumboldt.hale.app.transform.ExecUtil.*;
-
 /**
  * Executes a transformation.
  * 
  * @author Simon Templer
  */
 public class ExecTransformation implements ConsoleConstants {
-	
+
 	/**
 	 * ID for the transformation
 	 */
 	private final String id = UUID.randomUUID().toString();
-	
+
 	private ReportHandler reportHandler = null;
-	
+
 	private ProjectTransformationEnvironment env;
 
 	private ExecContext context;
@@ -67,32 +70,33 @@ public class ExecTransformation implements ConsoleConstants {
 	private InstanceWriter target;
 
 	private InstanceReader source;
-	
+
+	@SuppressWarnings("javadoc")
 	public void run(ExecContext context) throws Exception {
 		this.context = context;
-		
+
 		new ConsoleProgressManager();
-		
+
 		// set up report handler
 		setupReportHandler();
-		
+
 		// set up transformation environment
 		loadProject();
-		
+
 		// set up reader for source
 		setupReader();
-		
+
 		// set up writer for target
 		setupWriter();
-		
+
 		if (target == null) {
 			// writer could not be created
 			return;
 		}
-		
+
 		// set up validator
 //		setupValidator();
-		
+
 		// trigger transformation
 		transform();
 	}
@@ -105,12 +109,12 @@ public class ExecTransformation implements ConsoleConstants {
 		else {
 			delegateTo = null;
 		}
-		
+
 		/*
 		 * The report handler writes a summary to std out
 		 */
 		reportHandler = new ReportHandler() {
-			
+
 			@Override
 			public void publishReport(Report<?> report) {
 				ExecUtil.printSummary(report);
@@ -123,10 +127,11 @@ public class ExecTransformation implements ConsoleConstants {
 
 	private void loadProject() throws IOException {
 		status("Loading HALE project...");
-		
-		env = new ProjectTransformationEnvironment(id, new DefaultInputSupplier(context.getProject()), reportHandler);
+
+		env = new ProjectTransformationEnvironment(id, new DefaultInputSupplier(
+				context.getProject()), reportHandler);
 	}
-	
+
 	private void setupReader() {
 		LocatableInputSupplier<? extends InputStream> sourceIn = new DefaultInputSupplier(
 				context.getSource());
@@ -142,28 +147,28 @@ public class ExecTransformation implements ConsoleConstants {
 		}
 		if (source == null) {
 			// find applicable reader
-			source = HaleIO.findIOProvider(InstanceReader.class, sourceIn, context
-					.getSource().getPath());
+			source = HaleIO.findIOProvider(InstanceReader.class, sourceIn, context.getSource()
+					.getPath());
 		}
 		if (source == null) {
 			fail("Could not determine instance reader to use for source data");
 		}
-		
+
 		// apply custom settings
 		source.loadConfiguration(context.getSourceSettings());
-		
+
 		source.setSource(sourceIn);
 //		source.setCRSProvider(crsProvider)
 		// source schema is set in Transformation.transform
 	}
-	
+
 	private void setupWriter() {
 		String preset = context.getPreset();
 		String customProvider = context.getTargetProviderId();
 		if (preset == null && customProvider == null) {
 			fail("Please specify the name of a data export configuration preset or provide a specific provider ID for the instance writer");
 		}
-		
+
 		// create I/O configuration
 		IOConfiguration conf = null;
 		if (preset != null) {
@@ -180,10 +185,10 @@ public class ExecTransformation implements ConsoleConstants {
 		if (customProvider != null) {
 			conf.setProviderId(customProvider);
 		}
-		
+
 		// apply custom settings to configuration
 		conf.getProviderConfiguration().putAll(context.getTargetSettings());
-		
+
 		// create I/O provider
 		String writerId = conf.getProviderId();
 		target = HaleIO.createIOProvider(InstanceWriter.class, null, writerId);
@@ -191,23 +196,25 @@ public class ExecTransformation implements ConsoleConstants {
 		target.setTargetSchema(env.getTargetSchema());
 
 		// determine content type to use based on file extension
-		IOProviderDescriptor factory = HaleIO.findIOProviderFactory(InstanceWriter.class, null, writerId);
-		List<IContentType> cts = HaleIO.findContentTypesFor(factory.getSupportedTypes(), null, context.getTarget().getPath());
+		IOProviderDescriptor factory = HaleIO.findIOProviderFactory(InstanceWriter.class, null,
+				writerId);
+		List<IContentType> cts = HaleIO.findContentTypesFor(factory.getSupportedTypes(), null,
+				context.getTarget().getPath());
 		if (!cts.isEmpty()) {
 			target.setContentType(cts.get(0));
 		}
-		
+
 		// apply configuration (may override content type)
 		target.loadConfiguration(conf.getProviderConfiguration());
 	}
-	
+
 	private LocatableOutputSupplier<? extends OutputStream> createTargetSupplier(final URI uri) {
 		try {
 			File file = new File(uri);
 			return new FileIOSupplier(file);
 		} catch (IllegalArgumentException e) {
-			//TODO check for other supported URI types, e.g. FTP?
-			
+			// TODO check for other supported URI types, e.g. FTP?
+
 			// create dummy output supplier
 			// e.g. for JDBC URIs
 			return new LocatableOutputSupplier<OutputStream>() {
@@ -227,11 +234,12 @@ public class ExecTransformation implements ConsoleConstants {
 
 	private void transform() throws InterruptedException, ExecutionException {
 		status("Running HALE transformation...");
-		
+
 		List<InstanceReader> sources = new ArrayList<InstanceReader>();
 		sources.add(source);
-		ListenableFuture<Boolean> res = Transformation.transform(sources, target, env, reportHandler, id, null);
-		
+		ListenableFuture<Boolean> res = Transformation.transform(sources, target, env,
+				reportHandler, id, null);
+
 		if (res.get()) {
 			info("Transformation completed. Please check the reports for more details.");
 		}
