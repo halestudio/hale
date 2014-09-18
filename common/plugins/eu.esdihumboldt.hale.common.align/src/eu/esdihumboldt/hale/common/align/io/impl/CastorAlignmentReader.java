cdd102e060f72614f3aa163830addea5e353bd31
Simon Templer <simon.templer@igd.fraunhofer.de>
2014-09-18 09:42:09 +0200

    split Assign function in Assign and Bound Assign
    Change-Id: I657791e485427eaede53bb4ad95d3a5683f6a53e
    Reviewed-on: http://gerrit.esdi-community.eu/3163
    Tested-by: Jenkins <sitemple@igd.fraunhofer.de>
    Reviewed-by: Simon Templer <simon.templer@igd.fraunhofer.de>

--- common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/io/impl/CastorAlignmentReader.java
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/io/impl/CastorAlignmentReader.java
@@ -19,13 +19,11 @@ package eu.esdihumboldt.hale.common.align.io.impl;
 import java.io.IOException;
 import java.io.InputStream;
 
-import eu.esdihumboldt.hale.common.align.io.AlignmentReader;
 import eu.esdihumboldt.hale.common.align.model.MutableAlignment;
 import eu.esdihumboldt.hale.common.core.io.IOProvider;
 import eu.esdihumboldt.hale.common.core.io.IOProviderConfigurationException;
 import eu.esdihumboldt.hale.common.core.io.ProgressIndicator;
 import eu.esdihumboldt.hale.common.core.io.impl.AbstractIOProvider;
-import eu.esdihumboldt.hale.common.core.io.report.IOReport;
 import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
 import eu.esdihumboldt.hale.common.core.io.report.impl.IOMessageImpl;
 
@@ -35,16 +33,6 @@ import eu.esdihumboldt.hale.common.core.io.report.impl.IOMessageImpl;
  * @author Simon Templer
  */
 public class CastorAlignmentReader extends AbstractAlignmentReader {
-
-	private MutableAlignment alignment;
-
-	/**
-	 * @see AlignmentReader#getAlignment()
-	 */
-	@Override
-	public MutableAlignment getAlignment() {
-		return alignment;
-	}
 
 	/**
 	 * @see IOProvider#isCancelable()
@@ -58,33 +46,26 @@ public class CastorAlignmentReader extends AbstractAlignmentReader {
 	 * @see AbstractIOProvider#execute(ProgressIndicator, IOReporter)
 	 */
 	@Override
-	protected IOReport execute(ProgressIndicator progress, IOReporter reporter)
+	protected MutableAlignment loadAlignment(ProgressIndicator progress, IOReporter reporter)
 			throws IOProviderConfigurationException, IOException {
 		progress.begin("Load HALE alignment", ProgressIndicator.UNKNOWN);
 
 		InputStream in = getSource().getInput();
+		MutableAlignment alignment = null;
 		try {
 			alignment = CastorAlignmentIO.load(in, reporter, getSourceSchema(), getTargetSchema(),
 					getPathUpdater());
 		} catch (Exception e) {
 			reporter.error(new IOMessageImpl(e.getMessage(), e));
 			reporter.setSuccess(false);
-			return reporter;
+			return alignment;
 		} finally {
 			in.close();
 		}
 
 		progress.end();
 		reporter.setSuccess(true);
-		return reporter;
-	}
-
-	/**
-	 * @see AbstractIOProvider#getDefaultTypeName()
-	 */
-	@Override
-	protected String getDefaultTypeName() {
-		return "HALE alignment";
+		return alignment;
 	}
 
 }
