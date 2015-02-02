dc90eca481eb66974a7f8fc3458520f3985a8cb3
Simon Templer <simon@wetransform.to>
2015-02-02 13:10:57 +0100

    experimental WFS upload
    Change-Id: I049d29adc095b1c42dabcf0ad47b4e73bf564729

---
+++ io/plugins/eu.esdihumboldt.hale.io.wfs/src/eu/esdihumboldt/hale/io/wfs/transactions/WFSInsert.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.io.wfs.transactions;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import eu.esdihumboldt.hale.io.wfs.WFSVersion;
+
+/**
+ * WFS Insert.
+ * 
+ * @author Simon Templer
+ */
+public class WFSInsert extends AbstractWFSTransaction {
+
+	private final Map<String, String> params;
+
+	/**
+	 * @param wfsVersion the WFS version
+	 */
+	public WFSInsert(WFSVersion wfsVersion) {
+		super(wfsVersion);
+
+		Map<String, String> params = new HashMap<>();
+
+		// ID generation XXX only WFS 1.1.0?
+		params.put("idgen", "GenerateNew");
+		// XXX other values are "UseExisting" or "ReplaceDuplicate"
+
+		// input format
+		params.put("inputFormat", "text/xml; subtype=gml/3.2.1"); // FIXME
+
+		// reference system
+		// "srsName"
+
+		this.params = Collections.unmodifiableMap(params);
+	}
+
+	@Override
+	protected String getActionName() {
+		return "Insert";
+	}
+
+	@Override
+	protected Map<String, String> getActionAttributes() {
+		return params;
+	}
+
+}
