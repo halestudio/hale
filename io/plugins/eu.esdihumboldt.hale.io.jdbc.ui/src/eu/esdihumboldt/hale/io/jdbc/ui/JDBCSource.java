a482529cf70c596e9be0e66cb9b9db0d13a42e69
Simon Templer <simon@wetransform.to>
2015-02-17 13:17:09 +0100

    JDBC target selector (wip)
    Change-Id: Iba50ef6310f88cf6ddee517c8f06ca44a049f8fa

--- io/plugins/eu.esdihumboldt.hale.io.jdbc.ui/src/eu/esdihumboldt/hale/io/jdbc/ui/JDBCSource.java
+++ io/plugins/eu.esdihumboldt.hale.io.jdbc.ui/src/eu/esdihumboldt/hale/io/jdbc/ui/JDBCSource.java
@@ -18,23 +18,12 @@ package eu.esdihumboldt.hale.io.jdbc.ui;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 
-import org.eclipse.jface.dialogs.DialogPage;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -44,10 +33,7 @@ import org.eclipse.swt.widgets.Text;
 
 import eu.esdihumboldt.hale.common.core.io.ImportProvider;
 import eu.esdihumboldt.hale.common.core.io.supplier.LocatableInputSupplier;
-import eu.esdihumboldt.hale.io.jdbc.extension.DriverConfiguration;
-import eu.esdihumboldt.hale.io.jdbc.extension.DriverConfigurationExtension;
 import eu.esdihumboldt.hale.ui.io.source.AbstractProviderSource;
-import eu.esdihumboldt.util.Pair;
 
 /**
  * Source for configuring a JDBC connection.
@@ -75,48 +61,8 @@ public class JDBCSource extends AbstractProviderSource<ImportProvider> {
 		labelDriver.setText("Driver");
 		labelData.applyTo(labelDriver);
 
-		driver = new ComboViewer(page, SWT.BORDER | SWT.READ_ONLY);
-		driver.setContentProvider(ArrayContentProvider.getInstance());
-		driver.setLabelProvider(new LabelProvider() {
-
-			@Override
-			public String getText(Object element) {
-				if (element instanceof Pair<?, ?>) {
-					@SuppressWarnings("unchecked")
-					Pair<DriverConfiguration, Driver> driverInfo = (Pair<DriverConfiguration, Driver>) element;
-					return driverInfo.getFirst().getName();
-				}
-				return super.getText(element);
-			}
-
-		});
+		driver = JDBCComponents.createDriverSelector(page);
 		compData.applyTo(driver.getControl());
-
-		// driver input
-		List<Pair<DriverConfiguration, Driver>> drivers = new ArrayList<>();
-		for (DriverConfiguration dc : DriverConfigurationExtension.getInstance().getElements()) {
-
-			// determine associated driver
-			Driver driver = null;
-			Enumeration<Driver> enDrivers = DriverManager.getDrivers();
-			while (enDrivers.hasMoreElements()) {
-				Driver candidate = enDrivers.nextElement();
-				if (dc.matchesDriver(candidate)) {
-					driver = candidate;
-					break;
-				}
-			}
-
-//			if (driver != null) {
-			// XXX ignore if the driver is null, seems to work nonetheless
-			drivers.add(new Pair<>(dc, driver));
-//			}
-		}
-		driver.setInput(drivers);
-		if (!drivers.isEmpty()) {
-			// by default select a driver if possible
-			driver.setSelection(new StructuredSelection(drivers.get(0)));
-		}
 
 		// driver selection listener
 		driver.addSelectionChangedListener(new ISelectionChangedListener() {
@@ -173,32 +119,7 @@ public class JDBCSource extends AbstractProviderSource<ImportProvider> {
 	}
 
 	private URI buildURI() {
-		getPage().setErrorMessage(null);
-		if (driver != null) {
-			ISelection sel = driver.getSelection();
-			if (!sel.isEmpty() && sel instanceof IStructuredSelection) {
-				@SuppressWarnings({ "unchecked" })
-				Pair<DriverConfiguration, Driver> driverInfo = (Pair<DriverConfiguration, Driver>) ((IStructuredSelection) sel)
-						.getFirstElement();
-				try {
-					URI uri = driverInfo.getFirst().getURIBuilder()
-							.createJdbcUri(host.getText(), database.getText());
-					if (driverInfo.getSecond() == null
-							|| driverInfo.getSecond().acceptsURL(uri.toString())) {
-						return uri;
-					}
-				} catch (Exception e) {
-					getPage().setErrorMessage(e.getLocalizedMessage());
-				}
-			}
-
-			getPage().setMessage(null);
-		}
-		else {
-			getPage().setMessage("Please select a JDBC database driver", DialogPage.INFORMATION);
-		}
-
-		return null;
+		return JDBCComponents.buildURI(getPage(), driver, host, database);
 	}
 
 	@Override
