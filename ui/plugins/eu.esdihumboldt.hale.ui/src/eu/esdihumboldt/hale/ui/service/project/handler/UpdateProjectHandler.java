a59337399e94405f105eed9fbdc83cd45b76afa7
Simon Templer <simon@wetransform.to>
2015-02-24 13:30:13 +0100

    schema update wip
    Change-Id: I236a5ba54e1f1d7ad70af7e5ef27bb795f9fd36a

---
+++ ui/plugins/eu.esdihumboldt.hale.ui/src/eu/esdihumboldt/hale/ui/service/project/handler/UpdateProjectHandler.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2012 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     HUMBOLDT EU Integrated Project #030962
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.service.project.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.PlatformUI;
+
+import eu.esdihumboldt.hale.ui.service.project.ProjectService;
+
+/**
+ * Handler that reloads a project and allows updating it before completing the
+ * process.
+ * 
+ * @author Simon Templer
+ */
+public class UpdateProjectHandler extends AbstractHandler {
+
+	/**
+	 * @see IHandler#execute(ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ProjectService ps = (ProjectService) PlatformUI.getWorkbench().getService(
+				ProjectService.class);
+		ps.update();
+
+		return null;
+	}
+
+}
