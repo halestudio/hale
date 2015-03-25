a4ef96145b362ead3980063145c0545bc4ec3735
Simon Templer <simon@wetransform.to>
2015-03-25 11:23:54 +0100

    WFS schema source: set relevant types based on request URI
    Change-Id: Id5f8690f0e63745925adb6b6c5b66a23f62d0aa7

--- io/plugins/eu.esdihumboldt.hale.io.wfs.ui/src/eu/esdihumboldt/hale/io/wfs/ui/describefeature/WFSDescribeFeatureSource.java
+++ io/plugins/eu.esdihumboldt.hale.io.wfs.ui/src/eu/esdihumboldt/hale/io/wfs/ui/describefeature/WFSDescribeFeatureSource.java
@@ -15,27 +15,32 @@
 
 package eu.esdihumboldt.hale.io.wfs.ui.describefeature;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Maps.EntryTransformer;
 
 import eu.esdihumboldt.hale.common.core.io.ImportProvider;
 import eu.esdihumboldt.hale.io.wfs.ui.AbstractWFSSource;
+import eu.esdihumboldt.hale.io.xsd.reader.XmlSchemaReader;
 import eu.esdihumboldt.hale.ui.util.io.URIFieldEditor;
 import eu.esdihumboldt.hale.ui.util.wizard.HaleWizardDialog;
 
@@ -155,6 +160,88 @@ public class WFSDescribeFeatureSource extends AbstractWFSSource<ImportProvider> 
 		}
 		namespaces.put(namespace, candidate);
 		return candidate;
+	}
+
+	@Override
+	public boolean updateConfiguration(ImportProvider provider) {
+		boolean success = super.updateConfiguration(provider);
+		if (success && provider instanceof XmlSchemaReader) {
+			// analyze URI and determine relevant elements
+
+			URI loc = provider.getSource().getLocation();
+			if (loc != null) {
+				URIBuilder b = new URIBuilder(loc);
+				String namespace = null;
+				String typename = null;
+				for (NameValuePair param : b.getQueryParams()) {
+					switch (param.getName().toLowerCase()) {
+					case "namespace":
+					case "namespaces":
+						namespace = param.getValue();
+						break;
+					case "typename":
+					case "typenames":
+						typename = param.getValue();
+						break;
+					}
+				}
+
+				if (typename != null && !typename.isEmpty()) {
+					// parse namespaces
+					Map<String, String> prefixToNamespace = new HashMap<>();
+					if (namespace != null && !namespace.isEmpty()) {
+						for (String xmlns : Splitter.on(',').omitEmptyStrings().trimResults()
+								.split(namespace)) {
+							// XXX what if a namespace contains a comma?
+							if (xmlns.startsWith("xmlns(") && xmlns.endsWith(")")) {
+								String mapping = xmlns.substring("xmlns(".length(),
+										xmlns.length() - 1);
+								List<String> mp = Splitter.on('=').limit(2).trimResults()
+										.splitToList(mapping);
+								if (mp.size() == 2) {
+									prefixToNamespace.put(mp.get(0), mp.get(1));
+								}
+								else {
+									// mapping for default namespace
+									prefixToNamespace.put(XMLConstants.NULL_NS_URI, mp.get(0));
+								}
+							}
+						}
+					}
+
+					Set<QName> elements = new HashSet<>();
+					// parse type names
+					for (String type : Splitter.on(',').omitEmptyStrings().trimResults()
+							.split(typename)) {
+						List<String> nameParts = Splitter.on(':').limit(2).trimResults()
+								.splitToList(type);
+						QName name;
+						if (nameParts.size() == 2) {
+							// prefix and name
+							String prefix = nameParts.get(0);
+							String ns = prefixToNamespace.get(prefix);
+							if (ns != null) {
+								name = new QName(ns, nameParts.get(1), prefix);
+							}
+							else {
+								// namespace unknown - fall back to local name
+								// only
+								name = new QName(nameParts.get(1));
+							}
+						}
+						else {
+							// only local name
+							name = new QName(nameParts.get(0));
+						}
+
+						elements.add(name);
+					}
+
+					((XmlSchemaReader) provider).setRelevantElements(elements);
+				}
+			}
+		}
+		return success;
 	}
 
 	@Override
