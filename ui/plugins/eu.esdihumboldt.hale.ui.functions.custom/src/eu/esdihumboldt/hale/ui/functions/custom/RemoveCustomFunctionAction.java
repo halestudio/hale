3fec694a7138e293059d4ae4f6f245df3cfc9101
Simon Templer <simon@wetransform.to>
2015-05-23 13:33:50 +0200

    stub contributions for editing/removing custom functions
    Change-Id: I0dd9e30b7224ed3ae0a8a263dd70ddf72950df25

---
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.custom/src/eu/esdihumboldt/hale/ui/functions/custom/RemoveCustomFunctionAction.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.ui.functions.custom;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+
+import eu.esdihumboldt.hale.common.align.custom.DefaultCustomPropertyFunction;
+import eu.esdihumboldt.hale.ui.service.align.AlignmentService;
+
+/**
+ * TODO Type description
+ * 
+ * @author simon
+ */
+public class RemoveCustomFunctionAction extends Action {
+
+	/**
+	 * @param cf
+	 * @param alignmentService
+	 */
+	public RemoveCustomFunctionAction(DefaultCustomPropertyFunction cf, AlignmentService alignmentService) {
+		super(cf.getName(), IAction.AS_PUSH_BUTTON);
+	}
+
+	/**
+	 * @see org.eclipse.jface.action.Action#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+}
