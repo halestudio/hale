755b78d76f484fa885d3e8e4cb271010f22582e3
Simon Templer <simon@templer.cc>
2015-07-11 11:02:58 +0200

    Groovy Join wip
    Change-Id: I5e5aa5b2ee4e3baee1c141c15fb3cbc07379f460

--- ui/plugins/eu.esdihumboldt.hale.ui.functions.groovy/src/eu/esdihumboldt/hale/ui/functions/groovy/GroovyJoinPage.java
+++ ui/plugins/eu.esdihumboldt.hale.ui.functions.groovy/src/eu/esdihumboldt/hale/ui/functions/groovy/GroovyJoinPage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Data Harmonisation Panel
+ * Copyright (c) 2015 Data Harmonisation Panel
  * 
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the GNU Lesser General Public License as
@@ -18,16 +18,17 @@ package eu.esdihumboldt.hale.ui.functions.groovy;
 import java.util.Collection;
 import java.util.Collections;
 
-import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.swt.widgets.ToolBar;
-
-import com.google.common.collect.ImmutableList;
 
 import eu.esdihumboldt.cst.functions.groovy.GroovyRetype;
 import eu.esdihumboldt.cst.functions.groovy.internal.GroovyUtil;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.CellUtil;
+import eu.esdihumboldt.hale.common.align.model.ParameterValue;
 import eu.esdihumboldt.hale.common.align.model.Type;
+import eu.esdihumboldt.hale.common.align.model.functions.JoinFunction;
+import eu.esdihumboldt.hale.common.align.model.functions.join.JoinParameter;
+import eu.esdihumboldt.hale.common.align.model.impl.TypeEntityDefinition;
 import eu.esdihumboldt.hale.common.align.transformation.function.ExecutionContext;
 import eu.esdihumboldt.hale.common.align.transformation.function.impl.FamilyInstanceImpl;
 import eu.esdihumboldt.hale.common.align.transformation.report.impl.CellLog;
@@ -40,13 +41,9 @@ import eu.esdihumboldt.hale.common.instance.model.impl.DefaultInstance;
 import eu.esdihumboldt.hale.common.schema.SchemaSpaceID;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.ui.HaleUI;
-import eu.esdihumboldt.hale.ui.functions.groovy.internal.InstanceBuilderCompletions;
 import eu.esdihumboldt.hale.ui.functions.groovy.internal.PageHelp;
 import eu.esdihumboldt.hale.ui.functions.groovy.internal.TypeStructureTray;
 import eu.esdihumboldt.hale.ui.functions.groovy.internal.TypeStructureTray.TypeProvider;
-import eu.esdihumboldt.hale.ui.scripting.groovy.InstanceTestValues;
-import eu.esdihumboldt.hale.ui.scripting.groovy.TestValues;
-import eu.esdihumboldt.hale.ui.util.groovy.SimpleGroovySourceViewerConfiguration;
 import eu.esdihumboldt.hale.ui.util.groovy.ast.GroovyAST;
 import eu.esdihumboldt.hale.ui.util.source.CompilingSourceViewer;
 import eu.esdihumboldt.util.groovy.sandbox.GroovyService;
@@ -54,78 +51,67 @@ import groovy.lang.Binding;
 import groovy.lang.Script;
 
 /**
- * Configuration page for the Groovy Retype script.
+ * Configuration page for the Groovy Join script.
  * 
  * @author Simon Templer
  */
 @SuppressWarnings("restriction")
-public class GroovyRetypePage extends GroovyScriptPage {
-
-	private final TestValues testValues;
+public class GroovyJoinPage extends GroovyRetypePage {
 
 	/**
 	 * Default constructor.
 	 */
-	public GroovyRetypePage() {
+	public GroovyJoinPage() {
 		super();
-		setTitle("Convert instance script");
-		setDescription("Specify a Groovy script to build a target instance from a source instance");
 
-		testValues = new InstanceTestValues();
-	}
-
-	/**
-	 * @return the testValues
-	 */
-	public TestValues getTestValues() {
-		return testValues;
-	}
-
-	@Override
-	protected SourceViewerConfiguration createConfiguration() {
-		InstanceBuilderCompletions targetCompletions = new InstanceBuilderCompletions(
-				definitionImages) {
-
-			@Override
-			protected TypeDefinition getTargetType() {
-				Type typeEntity = (Type) CellUtil.getFirstEntity(getWizard().getUnfinishedCell()
-						.getTarget());
-				if (typeEntity != null) {
-					return typeEntity.getDefinition().getDefinition();
-				}
-				return null;
-			}
-		};
-
-		return new SimpleGroovySourceViewerConfiguration(colorManager, ImmutableList.of(
-				BINDING_BUILDER, BINDING_SOURCE, BINDING_TARGET, BINDING_SOURCE_TYPES,
-				BINDING_TARGET_TYPE, BINDING_CELL, BINDING_LOG, BINDING_CELL_CONTEXT,
-				BINDING_FUNCTION_CONTEXT, BINDING_TRANSFORMATION_CONTEXT),
-				ImmutableList.of(targetCompletions));
+		setDescription("Specify a Groovy script to build a target instance from joined source instances");
 	}
 
 	@Override
 	protected boolean validate(String document) {
 		super.validate(document);
 
+		ParameterValue param = CellUtil.getFirstParameter(getWizard().getUnfinishedCell(),
+				JoinFunction.PARAMETER_JOIN);
+		JoinParameter joinParameter = param.as(JoinParameter.class);
+
+		// check Join parameter
+		if (joinParameter == null) {
+			setValidationError("Missing join configuration");
+			return false;
+		}
+		else {
+			String error = joinParameter.validate();
+			if (!setValidationError(error)) {
+				return false;
+			}
+		}
+
+		// target type
 		Type targetType = (Type) CellUtil.getFirstEntity(getWizard().getUnfinishedCell()
 				.getTarget());
-		Type sourceType = (Type) CellUtil.getFirstEntity(getWizard().getUnfinishedCell()
-				.getSource());
-		if (sourceType == null || targetType == null) {
+		if (targetType == null) {
 			// not yet selected (NewRelationWizard)
 			return false;
 		}
 
-		InstanceBuilder builder = new InstanceBuilder(false);
+		/*
+		 * FIXME use JoinParameter to fake joined instances!
+		 * 
+		 * XXX for now just base instance
+		 */
+		TypeEntityDefinition sourceType = joinParameter.types.get(0);
 
-		Instance instance = testValues.get(sourceType.getDefinition());
+		InstanceBuilder builder = new InstanceBuilder(false);
+		Instance instance = getTestValues().get(sourceType);
 		if (instance == null) {
 			// use an empty instance as input for the script
-			instance = new DefaultInstance(sourceType.getDefinition().getDefinition(),
-					DataSet.SOURCE);
+			instance = new DefaultInstance(sourceType.getDefinition(), DataSet.SOURCE);
 		}
+
 		FamilyInstance source = new FamilyInstanceImpl(instance);
+
+		// prepare binding
 		Cell cell = getWizard().getUnfinishedCell();
 		CellLog log = new CellLog(new DefaultTransformationReporter("dummy", false), cell);
 		ExecutionContext context = new DummyExecutionContext(HaleUI.getServiceProvider());
@@ -151,14 +137,18 @@ public class GroovyRetypePage extends GroovyScriptPage {
 
 		PageHelp.createToolItem(toolbar, this);
 
+		// FIXME TypeStructureTray does not support FamilyInstances
+		// XXX for now only use Join base type
 		TypeStructureTray.createToolItem(toolbar, this, SchemaSpaceID.SOURCE, new TypeProvider() {
 
 			@Override
 			public Collection<? extends TypeDefinition> getTypes() {
-				Type sourceType = (Type) CellUtil.getFirstEntity(getWizard().getUnfinishedCell()
-						.getSource());
-				if (sourceType != null) {
-					return Collections.singleton(sourceType.getDefinition().getDefinition());
+				ParameterValue param = CellUtil.getFirstParameter(getWizard().getUnfinishedCell(),
+						JoinFunction.PARAMETER_JOIN);
+				JoinParameter joinParameter = param.as(JoinParameter.class);
+				if (joinParameter != null && joinParameter.types != null
+						&& !joinParameter.types.isEmpty()) {
+					return Collections.singleton(joinParameter.types.get(0).getDefinition());
 				}
 				return Collections.emptyList();
 			}
@@ -180,7 +170,7 @@ public class GroovyRetypePage extends GroovyScriptPage {
 
 	@Override
 	public String getHelpContext() {
-		return "eu.esdihumboldt.cst.functions.groovy.retype";
+		return "eu.esdihumboldt.cst.functions.groovy.join";
 	}
 
 }
