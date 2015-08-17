476051025278f7fde3af272cc0f5fa0e0c617b7c
sameer sheikh <sameer.sheikh@wetransform.to>
2015-08-17 19:16:28 +0200

    bug fix: Can't write to database tables using foreign keys
    Change-Id: I599a2f2223a74057237ee898ea2cccb354f6fee9

---
+++ io/plugins/eu.esdihumboldt.hale.io.jdbc.postgresql.test/src/eu/esdihumboldt/hale/io/jdbc/postgresql/test/CheckComplexTypeOrder.groovy
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2015 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.io.jdbc.postgresql.test;
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+import eu.esdihumboldt.hale.common.instance.groovy.InstanceBuilder
+import eu.esdihumboldt.hale.common.instance.model.InstanceCollection
+import eu.esdihumboldt.hale.common.schema.model.Schema
+import eu.esdihumboldt.hale.io.jdbc.test.AbstractDBTest
+import eu.esdihumboldt.hale.io.jdbc.test.DBConfigInstance
+import groovy.sql.Sql
+
+
+
+
+/**
+ * TODO Type description
+ * @author sameer
+ */
+public class CheckComplexTypeOrder extends AbstractDBTest{
+	private static final FIRST_TABLE_LINES = '''CREATE TABLE FIRST_TABLE
+		(
+	first_id VARCHAR(10) REFERENCES SECOND_TABLE(second_id),
+	name VARCHAR(254)
+	
+
+		);'''
+	private static final SECOND_TABLE_LINES = '''create table SECOND_TABLE(
+	second_id VARCHAR(10) primary key,
+	third_id VARCHAR(10) REFERENCES THIRD_TABLE(test_id),
+	fourth_id VARCHAR(10) REFERENCES FOURTH_TABLE(fourth_id),
+	name VARCHAR(254)
+
+	);'''
+	private static final THIRD_TABLE_LINES = '''CREATE TABLE THIRD_TABLE (
+	
+	test_id VARCHAR(10) primary key,
+	third_id VARCHAR(10),
+	year INTEGER,
+	physicians INTEGER,
+	unique(third_id)
+	);'''
+
+	private static final FOURTH_TABLE_LINES = '''CREATE TABLE FOURTH_TABLE (
+	fourth_id VARCHAR(10) primary key,
+	test_id VARCHAR(10) REFERENCES THIRD_TABLE(third_id),
+	description VARCHAR(254)
+	);'''
+
+	public CheckComplexTypeOrder(){
+		super(new DBConfigInstance("postgis",CheckComplexTypeOrder.class.getClassLoader()));
+	}
+
+	@Test
+	public void checkComplexOrder(){
+		Sql s = new Sql(waitForConnection());
+		try{
+
+			s.execute THIRD_TABLE_LINES;
+			s.execute FOURTH_TABLE_LINES;
+			s.execute SECOND_TABLE_LINES;
+			s.execute FIRST_TABLE_LINES;
+		}
+		finally{
+			s.close();
+		}
+		Schema schema = readSchema()
+
+		InstanceCollection instances = new InstanceBuilder(types: schema).createCollection {
+			for (i in 1..1) {
+				first_table{
+					first_id i
+					name "test $i"
+				}
+				second_table{
+					second_id i
+					third_id i
+					fourth_id i
+					name "test $i"
+				}
+				third_table{
+					test_id i
+					third_id i
+					year 2010 + i
+					physicians 10+ i
+				}
+				fourth_table{
+					fourth_id i
+					test_id i
+					description "description $i"
+				}
+			}
+		}
+		writeInstances(instances, schema);
+	}
+}
