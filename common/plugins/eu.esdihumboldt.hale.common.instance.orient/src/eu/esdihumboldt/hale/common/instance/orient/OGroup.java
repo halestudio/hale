b252de3793e0270f228c2dd267fce2ab49238bab
Simon Templer <simon@wetransform.to>
2015-10-05 19:50:46 +0200

    OGroup.configureDocument: fixed handling of lists
    Change-Id: I489a3d778311d9b7df295de5e6cfde2d76f56f4d

--- common/plugins/eu.esdihumboldt.hale.common.instance.orient/src/eu/esdihumboldt/hale/common/instance/orient/OGroup.java
+++ common/plugins/eu.esdihumboldt.hale.common.instance.orient/src/eu/esdihumboldt/hale/common/instance/orient/OGroup.java
@@ -159,7 +159,8 @@ public class OGroup implements MutableGroup {
 				List<ORecordAbstract<?>> recs = new ArrayList<ORecordAbstract<?>>();
 				if (field.getValue() instanceof Collection<?>) {
 					for (Object value : (Collection<?>) field.getValue()) {
-						if (value instanceof ODocument) {
+						if (value instanceof ODocument
+								&& !getSpecialFieldNames().contains(field.getKey())) {
 							docs.add((ODocument) value);
 						}
 						else if (value instanceof ORecordAbstract<?>) {
