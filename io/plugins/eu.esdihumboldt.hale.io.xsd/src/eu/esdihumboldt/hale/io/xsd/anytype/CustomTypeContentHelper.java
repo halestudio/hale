de9f35e1a04082f36ff45e76a97036320eab29d1
Simon Templer <simon@wetransform.to>
2015-11-10 12:22:45 +0100

    experimental custom type content for XML schemas via configuration
    TODOs:
    - support groups as parents
    - configurable via the UI
    
    Change-Id: I7f0781ac04180cfca49bde2e6282d3d5f874cf32

--- io/plugins/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/anytype/CustomTypeContentHelper.java
+++ io/plugins/eu.esdihumboldt.hale.io.xsd/src/eu/esdihumboldt/hale/io/xsd/anytype/CustomTypeContentHelper.java
@@ -22,10 +22,21 @@ import javax.xml.namespace.QName;
 
 import de.fhg.igd.slf4jplus.ALogger;
 import de.fhg.igd.slf4jplus.ALoggerFactory;
+import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
 import eu.esdihumboldt.hale.common.schema.model.Definition;
+import eu.esdihumboldt.hale.common.schema.model.DefinitionGroup;
 import eu.esdihumboldt.hale.common.schema.model.DefinitionUtil;
 import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.Cardinality;
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.ChoiceFlag;
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.NillableFlag;
+import eu.esdihumboldt.hale.common.schema.model.constraint.type.MappableFlag;
+import eu.esdihumboldt.hale.common.schema.model.impl.DefaultGroupPropertyDefinition;
+import eu.esdihumboldt.hale.common.schema.model.impl.DefaultPropertyDefinition;
+import eu.esdihumboldt.hale.common.schema.model.impl.DefaultTypeDefinition;
+import eu.esdihumboldt.hale.common.schema.model.impl.PropertyTypeOverrideProperty;
+import eu.esdihumboldt.hale.io.xsd.model.XmlElement;
 import eu.esdihumboldt.hale.io.xsd.model.XmlIndex;
 
 /**
@@ -62,6 +73,7 @@ public class CustomTypeContentHelper {
 		// property identified by a list of qualified names
 		List<QName> property = association.getProperty();
 		PropertyDefinition propDef = null;
+		DefinitionGroup propParent = null;
 		if (property != null && property.size() > 1) {
 			QName typeName = property.get(0);
 			TypeDefinition type = index.getType(typeName);
@@ -76,6 +88,22 @@ public class CustomTypeContentHelper {
 				}
 				if (nameQueue.isEmpty() && child instanceof PropertyDefinition) {
 					propDef = (PropertyDefinition) child;
+					if (parent instanceof TypeDefinition) {
+						propParent = (DefinitionGroup) parent;
+					}
+					else if (parent instanceof ChildDefinition<?>) {
+						ChildDefinition<?> pc = (ChildDefinition<?>) parent;
+						if (pc.asProperty() != null) {
+							propParent = pc.asProperty().getPropertyType();
+						}
+						else if (pc.asGroup() != null) {
+							propParent = pc.asGroup();
+						}
+					}
+					else {
+						log.error("Illegal parent for custom type content property");
+						return;
+					}
 				}
 				else {
 					log.warn("Cannot apply custom type content configuration due to invalid property path");
@@ -96,21 +124,71 @@ public class CustomTypeContentHelper {
 
 		switch (config.getMode()) {
 		case simple:
-			applySimpleMode(propDef, config);
+			applySimpleMode(propDef, propParent, config);
 		case elements:
-			applyElementsMode(propDef, config);
+			applyElementsMode(propDef, propParent, config, index);
 		default:
 			log.error("Unrecognized custom type content mode {}", config.getMode().name());
 		}
 
 	}
 
-	private static void applyElementsMode(PropertyDefinition propDef, CustomTypeContent config) {
-		// TODO Auto-generated method stub
+	private static void applyElementsMode(PropertyDefinition propDef, DefinitionGroup propParent,
+			CustomTypeContent config, XmlIndex index) {
+		// build new property type based on config
+		DefaultTypeDefinition type = new DefaultTypeDefinition(new QName(propDef.getIdentifier(),
+				"customElementsContentType"));
+		type.setConstraint(MappableFlag.DISABLED);
+		DefaultGroupPropertyDefinition choice = new DefaultGroupPropertyDefinition(new QName(
+				propDef.getIdentifier(), "customElementsContentChoice"), type, false);
+		choice.setConstraint(ChoiceFlag.ENABLED);
+		choice.setConstraint(Cardinality.CC_ANY_NUMBER);
 
+		for (QName elementName : config.getElements()) {
+			XmlElement element = index.getElements().get(elementName);
+			if (element != null) {
+				DefaultPropertyDefinition elementProp = new DefaultPropertyDefinition(elementName,
+						choice, element.getType());
+				elementProp.setConstraint(Cardinality.CC_EXACTLY_ONCE);
+				elementProp.setConstraint(NillableFlag.DISABLED);
+			}
+			else {
+				log.error("Element {} could not be found when creating custom type content",
+						elementName);
+			}
+		}
+
+		replaceTypeForProperty(propDef, propParent, type);
 	}
 
-	private static void applySimpleMode(PropertyDefinition propDef, CustomTypeContent config) {
+	private static void applySimpleMode(PropertyDefinition propDef, DefinitionGroup propParent,
+			CustomTypeContent config) {
 		// XXX currently no modification needed
+
+//		replaceTypeForProperty(propDef, propParent, type);
+	}
+
+	private static void replaceTypeForProperty(PropertyDefinition propDef,
+			DefinitionGroup propParent, TypeDefinition newPropertyType) {
+		PropertyDefinition newProperty;
+		if (propDef instanceof PropertyTypeOverrideProperty) {
+			newProperty = new PropertyTypeOverrideProperty(
+					((PropertyTypeOverrideProperty) propDef).getDecoratedProperty(),
+					newPropertyType);
+		}
+		else {
+			newProperty = new PropertyTypeOverrideProperty(propDef, newPropertyType);
+		}
+
+		if (propParent instanceof DefaultTypeDefinition) {
+			DefaultTypeDefinition type = (DefaultTypeDefinition) propParent;
+			type.overrideChild(newProperty);
+		}
+//		else if (propParent instanceof DefaultGroupPropertyDefinition) {
+//			// TODO
+//		}
+		else {
+			log.error("Could not update custom content property because of unsupported parent definition group");
+		}
 	}
 }
