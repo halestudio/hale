5eb51349544299d12999ad767cac86c9e712c514
Simon Templer <simon@wetransform.to>
2016-01-10 13:09:10 +0100

    try to autodetect NTLM credentials for proxy authentication
    Change-Id: I0f27f2f1cc0971872394e6affd4800c74d739974

--- util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/http/ProxyUtil.java
+++ util/plugins/eu.esdihumboldt.util/src/eu/esdihumboldt/util/http/ProxyUtil.java
@@ -28,6 +28,7 @@ import java.util.Set;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
+import org.apache.http.auth.NTCredentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.fluent.Executor;
@@ -128,9 +129,23 @@ public class ProxyUtil {
 			if (useProxyAuth) {
 				// set the proxy credentials
 				CredentialsProvider credsProvider = new BasicCredentialsProvider();
+
+				Credentials credentials;
+				int sepIndex = user.indexOf('\\');
+				if (sepIndex > 0 && sepIndex + 1 < user.length()) {
+					// assume this is DOMAIN \ user for NTLM authentication
+					String userName = user.substring(sepIndex + 1);
+					String domain = user.substring(0, sepIndex);
+					String workstation = null;
+					credentials = new NTCredentials(userName, password, workstation, domain);
+				}
+				else {
+					credentials = new UsernamePasswordCredentials(user, password);
+				}
+
 				credsProvider.setCredentials(
 						new AuthScope(proxyAddress.getHostName(), proxyAddress.getPort()),
-						new UsernamePasswordCredentials(user, password));
+						credentials);
 				builder = builder.setDefaultCredentialsProvider(credsProvider)
 						.setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
 			}
