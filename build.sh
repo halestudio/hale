4bba2495305be3d8293e43ab21d44244bc2fb29a
Simon Templer <simon@wetransform.to>
2016-02-25 13:43:14 +0100

    increase PermGen space for build
    Change-Id: I50952e432664df881840feb8db6ebf2911a9414d

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./build/gradlew -p build -Dorg.gradle.jvmargs="-XX:MaxPermSize=460M" -Dorg.gradle.daemon=true --stacktrace cli -Pargs="$*"
+./build/gradlew -p build -Dorg.gradle.jvmargs="-XX:MaxPermSize=512M" -Dorg.gradle.daemon=true --stacktrace cli -Pargs="$*"
