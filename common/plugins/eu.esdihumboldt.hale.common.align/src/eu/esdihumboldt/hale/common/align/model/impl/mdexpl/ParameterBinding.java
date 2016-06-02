259b9c0135ca18a71e619d9ff071ff046835073c
Simon Templer <simon@wetransform.to>
2016-06-02 09:31:32 +0200

    ParameterBinding: support default values and simple binding conversion
    Change-Id: Ia876a8cdec09f8b116ef1bef8860ab7e03a63d25

--- common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/impl/mdexpl/ParameterBinding.java
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/impl/mdexpl/ParameterBinding.java
@@ -17,11 +17,14 @@ package eu.esdihumboldt.hale.common.align.model.impl.mdexpl;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import eu.esdihumboldt.hale.common.align.extension.function.AbstractFunction;
+import eu.esdihumboldt.hale.common.align.extension.function.FunctionParameter;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.ParameterValue;
+import eu.esdihumboldt.hale.common.core.parameter.ParameterValueDescriptor;
 import groovy.lang.GroovyObjectSupport;
 
 /**
@@ -50,12 +53,23 @@ public class ParameterBinding extends GroovyObjectSupport {
 	 * Extract the value of a {@link ParameterValue}.
 	 * 
 	 * @param value the parameter value
+	 * @param paramDef the parameter definition
 	 * @return the extracted value
 	 */
-	protected Object extractParameterValue(ParameterValue value) {
+	protected Object extractParameterValue(ParameterValue value,
+			Optional<FunctionParameter> paramDef) {
 		// TODO handle scripted parameters?
 
-		// XXX for now just retrieve the internal value
+		if (paramDef.isPresent()) {
+			Class<?> binding = paramDef.get().getBinding();
+
+			if (binding != null) {
+				// handle simple binding case
+				return value.as(binding);
+			}
+		}
+
+		// as fall-back yield the internal value
 		return value.getValue();
 	}
 
@@ -63,11 +77,17 @@ public class ParameterBinding extends GroovyObjectSupport {
 	public Object getProperty(String property) {
 		boolean getAsList = true;
 
+		final Optional<FunctionParameter> paramDef;
 		if (function != null) {
-			boolean paramNotList = function.getDefinedParameters().stream().anyMatch(param -> {
-				return Objects.equals(property, param.getName()) && param.getMaxOccurrence() == 1;
-			});
-			if (paramNotList) {
+			paramDef = function.getDefinedParameters().stream()
+					.filter(param -> Objects.equals(property, param.getName())).findFirst();
+		}
+		else {
+			paramDef = Optional.empty();
+		}
+
+		if (paramDef.isPresent()) {
+			if (paramDef.get().getMaxOccurrence() == 1) {
 				getAsList = false;
 			}
 		}
@@ -75,15 +95,24 @@ public class ParameterBinding extends GroovyObjectSupport {
 		List<ParameterValue> values = cell.getTransformationParameters().get(property);
 		if (getAsList) {
 			// yield parameters as list
-			return values.stream().map(this::extractParameterValue).collect(Collectors.toList());
+			return values.stream().map(value -> extractParameterValue(value, paramDef))
+					.collect(Collectors.toList());
 		}
 		else {
 			// yield parameter value or null if there is none
 			if (values.isEmpty()) {
+				if (paramDef.isPresent()) {
+					ParameterValueDescriptor descriptor = paramDef.get().getValueDescriptor();
+					if (descriptor != null && descriptor.getDefaultValue() != null) {
+						// use default value as parameter value
+						return extractParameterValue(
+								new ParameterValue(descriptor.getDefaultValue()), paramDef);
+					}
+				}
 				return null;
 			}
 			else {
-				return extractParameterValue(values.get(0));
+				return extractParameterValue(values.get(0), paramDef);
 			}
 		}
 	}
