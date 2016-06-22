1ff82cf0dc7706a425a7a3164512837a603689cc
Simon Templer <simon@wetransform.to>
2016-06-22 16:39:12 +0200

    fix reprojection test
    Change-Id: I5366261b17715cef283afe0f92c4b6ae646a9880

--- cst/plugins/eu.esdihumboldt.cst.functions.geometric.test/src/eu/esdihumboldt/cst/functions/geometric/test/reprojectgeometry/ReprojectGeometryTest.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.geometric.test/src/eu/esdihumboldt/cst/functions/geometric/test/reprojectgeometry/ReprojectGeometryTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.namespace.QName;
 
@@ -19,6 +21,10 @@ import com.vividsolutions.jts.geom.Geometry;
 import eu.esdihumboldt.cst.ConceptualSchemaTransformer;
 import eu.esdihumboldt.cst.test.AbstractTransformationTest;
 import eu.esdihumboldt.cst.test.TransformationExample;
+import eu.esdihumboldt.hale.common.align.service.FunctionService;
+import eu.esdihumboldt.hale.common.align.service.TransformationFunctionService;
+import eu.esdihumboldt.hale.common.align.service.impl.AlignmentFunctionService;
+import eu.esdihumboldt.hale.common.align.service.impl.AlignmentTransformationFunctionService;
 import eu.esdihumboldt.hale.common.align.transformation.service.impl.DefaultInstanceSink;
 import eu.esdihumboldt.hale.common.core.io.impl.NullProgressIndicator;
 import eu.esdihumboldt.hale.common.core.service.ServiceManager;
@@ -38,8 +44,30 @@ public class ReprojectGeometryTest extends AbstractTransformationTest {
 	protected List<Instance> transformData(TransformationExample example) throws Exception {
 		ConceptualSchemaTransformer transformer = new ConceptualSchemaTransformer();
 		DefaultInstanceSink sink = new DefaultInstanceSink();
-		// FIXME global scope not supported yet
-		ServiceProvider serviceProvider = new ServiceManager(ServiceManager.SCOPE_PROJECT);
+
+		final Map<Class<?>, Object> customServices = new HashMap<>();
+		customServices.put(FunctionService.class,
+				new AlignmentFunctionService(example.getAlignment()));
+		customServices.put(TransformationFunctionService.class,
+				new AlignmentTransformationFunctionService(example.getAlignment()));
+
+		final ServiceProvider serviceProvider = new ServiceProvider() {
+
+			private final ServiceProvider projectScope = new ServiceManager(
+					ServiceManager.SCOPE_PROJECT);
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> T getService(Class<T> serviceInterface) {
+				if (customServices.containsKey(serviceInterface)) {
+					return (T) customServices.get(serviceInterface);
+				}
+
+				// FIXME global scope not supported yet
+				return projectScope.getService(serviceInterface);
+			}
+		};
+
 		transformer.transform(example.getAlignment(), example.getSourceInstances(), sink,
 				serviceProvider, new NullProgressIndicator());
 		return sink.getInstances();
@@ -57,12 +85,12 @@ public class ReprojectGeometryTest extends AbstractTransformationTest {
 		Iterator<Instance> sit = sourceInstances.iterator();
 		if (sit.hasNext()) {
 			Instance i = sit.next();
-			DefaultInstance di = (DefaultInstance) (i.getProperty(new QName(
-					"eu:esdihumboldt:hale:test", "geometry"))[0]);
-			DefaultGroup dg = (DefaultGroup) (di.getProperty(new QName(
-					"http://www.opengis.net/gml/_Geometry", "choice"))[0]);
-			DefaultInstance dig = (DefaultInstance) (dg.getProperty(new QName(
-					"http://www.opengis.net/gml", "Point"))[0]);
+			DefaultInstance di = (DefaultInstance) (i
+					.getProperty(new QName("eu:esdihumboldt:hale:test", "geometry"))[0]);
+			DefaultGroup dg = (DefaultGroup) (di
+					.getProperty(new QName("http://www.opengis.net/gml/_Geometry", "choice"))[0]);
+			DefaultInstance dig = (DefaultInstance) (dg
+					.getProperty(new QName("http://www.opengis.net/gml", "Point"))[0]);
 			DefaultGeometryProperty<?> value = (DefaultGeometryProperty<?>) dig.getValue();
 			DefaultGeometryProperty<?> geom = value;
 			MathTransform transform = CRS.findMathTransform(geom.getCRSDefinition().getCRS(),
