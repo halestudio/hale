591943a4c932bdba9a6bcf6636695c5adfbfef5e
Arun Verma <arun.verma87@gmail.com>
2016-12-09 12:19:51 +0100

    added test for skos reader

---
+++ io/plugins/eu.esdihumboldt.hale.io.codelist.skos.test/src/eu/esdihumboldt/hale/io/codelist/skos/test/SkosCodeListTest.java
@@ -0,0 +1,129 @@
+package eu.esdihumboldt.hale.io.codelist.skos.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+
+import eu.esdihumboldt.hale.common.codelist.CodeList;
+import eu.esdihumboldt.hale.common.codelist.CodeList.CodeEntry;
+import eu.esdihumboldt.hale.common.codelist.io.CodeListReader;
+import eu.esdihumboldt.hale.common.core.io.impl.LogProgressIndicator;
+import eu.esdihumboldt.hale.common.core.io.report.IOReport;
+import eu.esdihumboldt.hale.common.core.io.supplier.DefaultInputSupplier;
+import eu.esdihumboldt.hale.io.codelist.skos.reader.SkosCodeListReader;
+
+/**
+ * 
+ * SKOS Code list reader test
+ * 
+ * @author Arun
+ *
+ */
+public class SkosCodeListTest {
+
+	/**
+	 * test rdf file consisting only concepts
+	 * 
+	 * @throws Exception throws exception if something wrong occurs
+	 */
+	@Test
+	public void testSKOSFromRDF1() throws Exception {
+
+		CodeList codeList = readCodeList(getResourceURI("/data/test1.rdf"));
+
+		Collection<CodeEntry> entries = codeList.getEntries();
+		assertFalse(entries.isEmpty());
+
+		assertEquals(entries.size(), 1);
+
+		assertNotNull(codeList.getLocation());
+		assertNotNull(codeList.getIdentifier());
+
+		for (CodeEntry entry : entries) {
+			assertEquals("Data scientist", entry.getName());
+		}
+	}
+
+	/**
+	 * test rdf file consisting concept scheme and concepts
+	 * 
+	 * @throws Exception throws exception if something wrong occurs
+	 */
+	@Test
+	public void testSKOSFromRDF2() throws Exception {
+
+		CodeList codeList = readCodeList(getResourceURI("/data/test2.rdf"));
+
+		Collection<CodeEntry> entries = codeList.getEntries();
+		assertFalse(entries.isEmpty());
+
+		assertEquals(entries.size(), 3);
+
+		assertNotNull(codeList.getLocation());
+		assertNotNull(codeList.getIdentifier());
+		assertEquals("Types of aquifers.", codeList.getDescription());
+
+		List<String> concepts = Arrays.asList("confined subartesian", "confined artesian",
+				"unconfined");
+
+		for (CodeEntry entry : entries) {
+			assertTrue(concepts.contains(entry.getName()));
+		}
+
+	}
+
+	/**
+	 * 
+	 * test url consisting concepts.
+	 * 
+	 * @throws Exception throws exception if something wrong occurs
+	 */
+	@Test
+	public void testSKOSFromURL() throws Exception {
+
+		CodeList codeList = readCodeList(
+				URI.create("http://vocab.ndg.nerc.ac.uk/term/W080/1/CONT0001"));
+
+		Collection<CodeEntry> entries = codeList.getEntries();
+		assertFalse(entries.isEmpty());
+
+		assertEquals(entries.size(), 1);
+
+		assertNotNull(codeList.getLocation());
+		assertNotNull(codeList.getIdentifier());
+
+		for (CodeEntry entry : entries) {
+			assertEquals("Manufacturer", entry.getName());
+			assertEquals(
+					"Relevant contact information about the person or organisation that builds or develops a device or system.",
+					entry.getDescription());
+		}
+	}
+
+	private CodeList readCodeList(URI source) throws Exception {
+
+		CodeListReader reader = new SkosCodeListReader();
+
+		reader.setSource(new DefaultInputSupplier(source));
+
+		IOReport report = reader.execute(new LogProgressIndicator());
+		assertTrue(report.isSuccess());
+
+		return reader.getCodeList();
+
+	}
+
+	private URI getResourceURI(String location) throws URISyntaxException {
+		return getClass().getResource(location).toURI();
+	}
+
+}
