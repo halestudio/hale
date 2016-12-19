9c47898ac0c87e761ddad4d6037447ddb5370c34
Simon Templer <simon@wetransform.to>
2016-12-20 00:04:17 +0100

    release version 3.1.0

--- build/config.groovy
+++ build/config.groovy
@@ -5,7 +5,7 @@
 project = {
 	// version
 	// major.minor.micro-SNAPSHOT - remove -SNAPSHOT for releases
-	version = '3.1.0-SNAPSHOT'
+	version = '3.1.0'
 
 	// when increasing the version number, at least the version of the .application bundles
 	// should be increased so they state an updated application version
