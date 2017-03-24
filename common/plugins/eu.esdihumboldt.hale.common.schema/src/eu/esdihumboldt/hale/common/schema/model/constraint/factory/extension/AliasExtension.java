a438136e8265d0cd3bc5ec2c8e5a6bea396bfa20
Simon Templer <simon@wetransform.to>
2017-03-24 08:51:48 +0100

    add value constraint alias for backwards compatibility

---
+++ common/plugins/eu.esdihumboldt.hale.common.schema/src/eu/esdihumboldt/hale/common/schema/model/constraint/factory/extension/AliasExtension.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2014 Data Harmonisation Panel
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ */
+
+package eu.esdihumboldt.hale.common.schema.model.constraint.factory.extension;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+
+import de.fhg.igd.eclipse.util.extension.simple.IdentifiableExtension;
+import eu.esdihumboldt.hale.common.schema.model.constraint.factory.ValueConstraintFactory;
+
+/**
+ * Extension point for {@link ValueConstraintFactory} aliases.
+ * 
+ * @author Simon Templer
+ */
+public class AliasExtension extends IdentifiableExtension<Alias> {
+
+	/**
+	 * The extension instance.
+	 */
+	public static final AliasExtension INSTANCE = new AliasExtension();
+
+	/**
+	 * Default constructor.
+	 */
+	private AliasExtension() {
+		super(ValueConstraintExtension.ID);
+	}
+
+	@Override
+	protected Alias create(String id, IConfigurationElement conf) {
+		if ("alias".equals(conf.getName())) {
+			return new Alias(id, conf);
+		}
+		else {
+			return null;
+		}
+	}
+
+	@Override
+	protected String getIdAttributeName() {
+		return "id";
+	}
+
+}
