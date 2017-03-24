4b281f4376848c74e14604860656c7ec5ceec469
Simon Templer <simon@wetransform.to>
2017-03-24 09:46:34 +0100

    fix file header for new files

--- common/plugins/eu.esdihumboldt.hale.common.schema/src/eu/esdihumboldt/hale/common/schema/model/constraint/factory/extension/AliasExtension.java
+++ common/plugins/eu.esdihumboldt.hale.common.schema/src/eu/esdihumboldt/hale/common/schema/model/constraint/factory/extension/AliasExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Data Harmonisation Panel
+ * Copyright (c) 2017 wetransform GmbH
  * 
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the GNU Lesser General Public License as
@@ -10,7 +10,7 @@
  * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
  * 
  * Contributors:
- *     Data Harmonisation Panel <http://www.dhpanel.eu>
+ *     wetransform GmbH <http://www.wetransform.to>
  */
 
 package eu.esdihumboldt.hale.common.schema.model.constraint.factory.extension;
