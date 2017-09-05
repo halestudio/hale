9ca2f3c3312ba6eada129759a642f112381f261b
Simon Templer <simon@wetransform.to>
2017-09-05 12:01:16 +0200

    introduce partitioner interface

---
+++ common/plugins/eu.esdihumboldt.hale.common.instance/src/eu/esdihumboldt/hale/common/instance/tools/InstanceCollectionPartitioner.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 wetransform GmbH
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     wetransform GmbH <http://www.wetransform.to>
+ */
+
+package eu.esdihumboldt.hale.common.instance.tools;
+
+import java.util.Iterator;
+
+import eu.esdihumboldt.hale.common.instance.model.InstanceCollection;
+
+/**
+ * Interface for classes that are able to partition an instance collection.
+ * 
+ * @author Simon Templer
+ */
+public interface InstanceCollectionPartitioner {
+
+	/**
+	 * States if the partitioner uses instance references meaning that the
+	 * references should be efficiently resolvable and not keep the instances in
+	 * memory.
+	 * 
+	 * @return if the partitioner uses instance references
+	 */
+	boolean usesReferences();
+
+	/**
+	 * Partition an instance collection.
+	 * 
+	 * @param instances the instances to partition
+	 * @param maxObjects the maximum objects threshold that should be met for a
+	 *            part if possible
+	 * @return the iterator of the parts
+	 */
+	Iterator<InstanceCollection> partition(InstanceCollection instances, int maxObjects);
+
+}
