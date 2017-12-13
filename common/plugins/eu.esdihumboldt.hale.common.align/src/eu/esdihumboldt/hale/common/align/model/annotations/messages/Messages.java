fe6dc59ec201d4cb1da81a1e701ede1f3c7381b7
Simon Templer <simon@wetransform.to>
2017-12-13 17:24:01 +0100

    let Message implement SimpleLog

--- common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/annotations/messages/Messages.java
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/annotations/messages/Messages.java
@@ -15,19 +15,28 @@
 
 package eu.esdihumboldt.hale.common.align.model.annotations.messages;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import de.fhg.igd.slf4jplus.ALogger;
+import de.fhg.igd.slf4jplus.ALoggerFactory;
+import eu.esdihumboldt.hale.common.core.io.Text;
 import eu.esdihumboldt.hale.common.core.io.Value;
 import eu.esdihumboldt.hale.common.core.io.ValueList;
+import eu.esdihumboldt.hale.common.core.report.SimpleLog;
 
 /**
  * List of {@link Message}s.
  * 
  * @author Simon Templer
  */
-public class Messages {
+public class Messages implements SimpleLog {
+
+	private static final ALogger log = ALoggerFactory.getLogger(Messages.class);
 
 	private final List<Message> messages = new ArrayList<>();
 
@@ -81,6 +90,38 @@ public class Messages {
 		}
 
 		return vals.toValue();
+	}
+
+	private void log(String category, String message, Throwable e) {
+		Message m = new Message(message).setCategory(category);
+
+		if (e != null) {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			try (PrintStream ps = new PrintStream(baos, true, StandardCharsets.UTF_8.name())) {
+				e.printStackTrace(ps);
+			} catch (Exception e1) {
+				log.error("Error converting stacktrace to String ", e1);
+			}
+			String trace = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+			m.setCustomPayload(Value.of(new Text(trace)));
+		}
+
+		addMessage(m);
+	}
+
+	@Override
+	public void warn(String message, Throwable e) {
+		log("warning", message, e);
+	}
+
+	@Override
+	public void error(String message, Throwable e) {
+		log("error", message, e);
+	}
+
+	@Override
+	public void info(String message, Throwable e) {
+		log("info", message, e);
 	}
 
 }
