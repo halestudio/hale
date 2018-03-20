784acec737f3c2e99f835a8df283616a5eaeecd0
Florian Esser <fe@wetransform.to>
2018-03-20 16:30:04 +0100

    Implement `equals` and `hashCode` for `DefaultCell`

--- common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/impl/DefaultCell.java
+++ common/plugins/eu.esdihumboldt.hale.common.align/src/eu/esdihumboldt/hale/common/align/model/impl/DefaultCell.java
@@ -299,4 +299,41 @@ public class DefaultCell implements Cell, MutableCell {
 	public boolean isBaseCell() {
 		return baseCell;
 	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof DefaultCell)) {
+			return false;
+		}
+		DefaultCell other = (DefaultCell) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		}
+		else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
 }
