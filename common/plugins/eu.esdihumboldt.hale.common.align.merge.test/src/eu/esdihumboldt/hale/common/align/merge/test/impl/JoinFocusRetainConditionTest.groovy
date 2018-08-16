9ee2d7fe7d61bae37feeff9417ea6bbb43a829e4
Edward Nash <e.nash@dvz-mv.de>
2018-08-16 11:13:17 +0200

    Add test for transfering source conditions for GroovyRetype

--- common/plugins/eu.esdihumboldt.hale.common.align.merge.test/src/eu/esdihumboldt/hale/common/align/merge/test/impl/JoinFocusRetainConditionTest.groovy
+++ common/plugins/eu.esdihumboldt.hale.common.align.merge.test/src/eu/esdihumboldt/hale/common/align/merge/test/impl/JoinFocusRetainConditionTest.groovy
@@ -150,6 +150,55 @@ class JoinFocusRetainConditionTest extends AbstractMergeCellMigratorTest {
 		})
 	}
 
+	@Test
+	void testGroovyRetypeCondition() {
+		def toMigrate = this.class.getResource('/testcases/retain-condition-join/B-to-C.halex')
+		def cellId = 'B6toC4'
+
+		def matching = this.class.getResource('/testcases/retain-condition-join/A-to-B.halex')
+
+		def migrated = merge(cellId, toMigrate, matching)
+
+		// do checks
+
+		// filter
+		assertEquals(1, migrated.size())
+		JaxbAlignmentIO.printCell(migrated[0], System.out)
+
+		assertNotNull(migrated[0].source)
+		assertEquals(2, migrated[0].source.size())
+		Collection<? extends Entity> source = migrated[0].source.values()
+		((Collection<Entity>) source).each { e ->
+			def filter = e.definition.filter
+			if (e.definition.definition.displayName == 'A7') {
+				// expect filter to have been propagated to A7
+				assertNotNull(filter)
+				assertEquals('NOT ("a7" = 10)', filter.filterTerm)
+			}
+			else {
+				// no filter should be present
+				assertNull(filter)
+			}
+		}
+
+		JoinParameter param = CellUtil.getFirstParameter(migrated[0], JoinFunction.PARAMETER_JOIN).as(JoinParameter)
+		assertJoinOrder(param, ['A7', 'A8'])
+
+		// there should be a condition on the join focus, also in the order
+		assertNotNull(param.types[0].filter)
+
+		// there should also be a filter in the condition
+		def base = param.conditions.collect { it.baseProperty }.findAll { it.type.displayName == 'A7' }.toList()
+		assertEquals(1, base.size())
+		assertNotNull(base[0].filter)
+
+		// there should be a message about the condition having been translated automatically
+		def messages = getMigrationMessages(migrated[0])
+		assertTrue(messages.any { msg ->
+			msg.text.toLowerCase().contains('condition')
+		})
+	}
+
 	// helpers
 
 	void assertJoinOrder(JoinParameter param, List<String> expected) {
