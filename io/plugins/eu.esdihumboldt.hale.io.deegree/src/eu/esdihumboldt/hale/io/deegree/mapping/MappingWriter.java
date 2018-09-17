ca915154fa3168ce29b3851052ac2d8a33c0b0e1
Simon Templer <simon@wetransform.to>
2018-09-17 12:19:39 +0200

    include only configuration for types mapped to in the target schema

--- io/plugins/eu.esdihumboldt.hale.io.deegree/src/eu/esdihumboldt/hale/io/deegree/mapping/MappingWriter.java
+++ io/plugins/eu.esdihumboldt.hale.io.deegree/src/eu/esdihumboldt/hale/io/deegree/mapping/MappingWriter.java
@@ -24,10 +24,13 @@ import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamWriter;
 
@@ -36,9 +39,14 @@ import org.deegree.feature.persistence.sql.MappedAppSchema;
 import org.deegree.feature.persistence.sql.config.SQLFeatureStoreConfigWriter;
 import org.deegree.feature.persistence.sql.ddl.DDLCreator;
 import org.deegree.feature.types.AppSchema;
+import org.deegree.feature.types.FeatureType;
 
 import eu.esdihumboldt.hale.common.align.model.Alignment;
 import eu.esdihumboldt.hale.common.schema.model.Schema;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
+import eu.esdihumboldt.hale.io.deegree.mapping.model.AppSchemaDecorator;
+import eu.esdihumboldt.hale.io.xsd.constraint.XmlElements;
+import eu.esdihumboldt.hale.io.xsd.model.XmlElement;
 
 /**
  * Writer for deegree feature mappings based on hale schema and alignment.
@@ -126,12 +134,70 @@ public class MappingWriter {
 	public MappedAppSchema getMappedSchema() throws Exception {
 		if (mappedSchema == null) {
 			AppSchema appSchema = readApplicationSchema(targetSchema.getLocation().toString());
+			appSchema = adaptSchema(appSchema);
 			mappedSchema = mapApplicationSchema(appSchema, config);
 
 			// TODO adapt according to alignment?
 		}
 
 		return mappedSchema;
+	}
+
+	/**
+	 * Adapt the given schema based on the alignment.
+	 * 
+	 * @param appSchema the application schema to adapt
+	 * @return the adapted schema
+	 */
+	protected AppSchema adaptSchema(AppSchema appSchema) {
+		if (alignment != null) {
+			// determine types that are mapped to
+			Set<TypeDefinition> targetTypes = alignment.getTypeCells().stream().flatMap(c -> {
+				return c.getTarget().values().stream();
+			}).map(e -> e.getDefinition().getType()).collect(Collectors.toSet());
+			// the feature type name corresponds to the element name
+			Set<QName> targetElements = targetTypes.stream()
+					.<XmlElement> flatMap(
+							t -> t.getConstraint(XmlElements.class).getElements().stream())
+					.map(e -> e.getName()).collect(Collectors.toSet());
+
+			return new AppSchemaDecorator(appSchema) {
+
+				@Override
+				public List<FeatureType> getFeatureTypes(String namespace,
+						boolean includeCollections, boolean includeAbstracts) {
+					List<FeatureType> fts = super.getFeatureTypes(namespace, includeCollections,
+							includeAbstracts);
+
+					return fts.stream().filter(ft -> {
+						// only accept types that are part of the mapped-to
+						// types
+						return targetElements.contains(ft.getName());
+					}).collect(Collectors.toList());
+				}
+			};
+		}
+		else {
+			return appSchema;
+		}
+	}
+
+	/**
+	 * Determine if a hale type definition and a deegree feature type represent
+	 * the same type.
+	 * 
+	 * @param t1 the hale type definition
+	 * @param t2 the deegree feature type
+	 * @return <code>true</code> if the definitions refer to the same type,
+	 *         <code>false</code> otherwise
+	 */
+	protected boolean typeMatches(TypeDefinition t1, FeatureType t2) {
+		QName ftName = t2.getName();
+
+		// the feature type name corresponds to the element name
+		Collection<? extends XmlElement> elements = t1.getConstraint(XmlElements.class)
+				.getElements();
+		return elements.stream().anyMatch(element -> element.getName().equals(ftName));
 	}
 
 }
