8d1a6d82b697e388d189a2168b3216fb9e53ea07
Florian Esser <fe@wetransform.to>
2019-01-11 13:48:56 +0100

    build: set new development version

--- build/config.groovy
+++ build/config.groovy
@@ -5,7 +5,7 @@
 project = {
 	// version
 	// major.minor.micro-SNAPSHOT - remove -SNAPSHOT for releases
-	version = '3.5.0-SNAPSHOT'
+	version = '4.0.0-SNAPSHOT'
 
 	// when increasing the version number, at least the version of the .application bundles
 	// should be increased so they state an updated application version
