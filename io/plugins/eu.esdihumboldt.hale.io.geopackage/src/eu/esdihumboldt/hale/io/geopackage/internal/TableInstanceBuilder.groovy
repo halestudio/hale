c6341aa1eb25fcb5642be964e65808dee31188d3
Simon Templer <simon@wetransform.to>
2022-03-21 16:38:21 +0100

    feat(geopackage): better support loading with XML/GML schema
    When using an XML/GML schema that resembles the Geopackage structure,
    so far some information would get lost or not be represented correctly.
    
    The changes here support this use case and thus allow for converting
    Geopackage to XML/GML given a suited XML schema.
    
    ING-2671

--- io/plugins/eu.esdihumboldt.hale.io.geopackage/src/eu/esdihumboldt/hale/io/geopackage/internal/TableInstanceBuilder.groovy
+++ io/plugins/eu.esdihumboldt.hale.io.geopackage/src/eu/esdihumboldt/hale/io/geopackage/internal/TableInstanceBuilder.groovy
@@ -15,20 +15,31 @@
 
 package eu.esdihumboldt.hale.io.geopackage.internal
 
+import java.sql.SQLException
+
+import javax.xml.namespace.QName
+
 import org.locationtech.jts.io.WKBReader
 
+import eu.esdihumboldt.hale.common.align.helper.EntityFinder
+import eu.esdihumboldt.hale.common.align.model.AlignmentUtil
+import eu.esdihumboldt.hale.common.align.model.EntityDefinition
 import eu.esdihumboldt.hale.common.core.report.SimpleLog
 import eu.esdihumboldt.hale.common.instance.geometry.DefaultGeometryProperty
 import eu.esdihumboldt.hale.common.instance.geometry.impl.CodeDefinition
 import eu.esdihumboldt.hale.common.instance.geometry.impl.WKTDefinition
 import eu.esdihumboldt.hale.common.instance.groovy.InstanceBuilder
 import eu.esdihumboldt.hale.common.instance.model.Instance
+import eu.esdihumboldt.hale.common.schema.SchemaSpaceID
 import eu.esdihumboldt.hale.common.schema.geometry.CRSDefinition
 import eu.esdihumboldt.hale.common.schema.model.DefinitionUtil
 import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.Cardinality
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.NillableFlag
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.GeometryMetadata
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.GeometryType
+import eu.esdihumboldt.hale.io.geopackage.GeopackageSchemaBuilder
 import groovy.transform.CompileStatic
 import mil.nga.geopackage.features.user.FeatureResultSet
 import mil.nga.geopackage.user.UserResultSet
@@ -48,6 +59,11 @@ class TableInstanceBuilder {
 	//	private final CRSProvider crsProvider
 	private final WKBReader wkbReader
 
+	private final Map<TypeDefinition, Collection<PropertyDefinition>> cachedProperties = [:]
+	private final Map<TypeDefinition, QName> cachedGeometryProperty = [:]
+
+	private final Set<String> noSuchColumnLogged = new HashSet()
+
 	/**
 	 * Default constructor. 
 	 */
@@ -56,9 +72,93 @@ class TableInstanceBuilder {
 		//		this.crsProvider = crsProvider
 		this.log = log
 
-		builder = new InstanceBuilder(strictBinding: false)
+		builder = new InstanceBuilder(
+				strictBinding: false,
+				strictValueFlags: false // allow setting values even if no value is expected (mainly for use with XML schema and geometries)
+				)
 		wkbReader = new WKBReader()
 	}
+
+	/**
+	 * Filter properties to remove duplicates (related to the local name)
+	 * 
+	 * @param properties the properties to filter
+	 * @return the filtered properties that no longer contain duplicates related to the local name
+	 */
+	private Collection<PropertyDefinition> filterProperties(Collection<PropertyDefinition> properties) {
+		def byLocalName = properties.groupBy { property -> property.name.localPart }
+
+		byLocalName.collect { name, candidates ->
+			// prefer property with empty namespace as in schema created by GeopackageSchemaReader
+			def cand = candidates.find { it.name.namespaceURI == null || it.name.namespaceURI.isEmpty() }
+
+			if (cand == null) {
+				// prefer property with Geopackage namespace (because hale connect schema service applies the type/schema namespace to the properties as well)
+				cand = candidates.find { it.name.namespaceURI == GeopackageSchemaBuilder.DEFAULT_NAMESPACE }
+			}
+
+			if (cand == null) {
+				// prefer property that is mandatory
+				cand = candidates.find { it.getConstraint(Cardinality).getMinOccurs() > 0 && !it.getConstraint(NillableFlag).isEnabled() }
+			}
+
+			if (cand == null) {
+				// just pick the first one
+				cand = candidates[0]
+			}
+
+			cand
+		}
+	}
+
+	private Collection<PropertyDefinition> getProperties(TypeDefinition type) {
+		return cachedProperties.computeIfAbsent(type) { TypeDefinition t ->
+			Collection<PropertyDefinition> allProperties = (Collection<PropertyDefinition>) DefinitionUtil.getAllProperties(t) // Groovy CompileStatic can't deal properly with ? extends ...
+			filterProperties(allProperties)
+		}
+	}
+
+	private QName getGeometryProperty(TypeDefinition type) {
+		return cachedGeometryProperty.computeIfAbsent(type) { TypeDefinition t ->
+			// allow for geometry property types with choices
+			int checkLevels = 3
+
+			// create finder for geometry properties
+			EntityFinder finder = new EntityFinder({ EntityDefinition entity ->
+				// determine if the property classifies as
+				if (entity.getDefinition() instanceof PropertyDefinition) {
+					def propertyType = ((PropertyDefinition) entity.getDefinition()).getPropertyType()
+
+					boolean isGeometry = propertyType.getConstraint(GeometryType).isGeometry()
+					if (isGeometry) {
+						return true
+					}
+				}
+
+				false
+			}, checkLevels)
+
+			def parents = getProperties(type).collect { PropertyDefinition p ->
+				AlignmentUtil.createEntityFromDefinitions(type, [p], SchemaSpaceID.SOURCE, null)
+			}
+
+			def candidates = finder.find(parents)
+
+			if (candidates.empty) {
+				null
+			}
+			else {
+				// select candidate
+				//XXX for now just use first one
+				def name = candidates[0].propertyPath[0].child.name
+
+				log.info("Identified property $name as geometry property for type ${type.name.localPart}")
+
+				name
+			}
+		}
+	}
+
 	/**
 	 * Create an instance with the given type from a row in a SQL result set.
 	 * 
@@ -71,23 +171,23 @@ class TableInstanceBuilder {
 		// create instance
 		builder.createInstance(type) {
 			// create properties
-			Collection<PropertyDefinition> allProperties = (Collection<PropertyDefinition>) DefinitionUtil.getAllProperties(type) // Groovy CompileStatic can't deal properly with ? extends ...
-			allProperties.each { PropertyDefinition property ->
+			Collection<PropertyDefinition> filteredProperties = getProperties(type)
+			QName geometryProperty = getGeometryProperty(type)
+			filteredProperties.each { PropertyDefinition property ->
 				// get property value
 				try {
 					Object value = row.getResultSet().getObject(property.name.localPart)
 
 					// geometry conversion
 					if (value != null) {
-						GeometryType gType = property.propertyType.getConstraint(GeometryType)
-						if (gType.isGeometry() && row instanceof FeatureResultSet) {
+						if (geometryProperty != null && geometryProperty.equals(property.getName()) && row instanceof FeatureResultSet) {
 							def geomData = ((FeatureResultSet) row).geometry
 
 							// read JTS geometry from WKB
 							def jtsGeom = wkbReader.read(geomData.wkbBytes)
 
 							CRSDefinition crsDef = null
-							// determine CRS
+							// determine CRS from schema (usually possible if the schema is the Geopackage schema)
 							GeometryMetadata geomMetadata = property.propertyType.getConstraint(GeometryMetadata)
 							if (geomMetadata.getAuthName() && geomMetadata.srs) {
 								// prefer code definition
@@ -107,11 +207,22 @@ class TableInstanceBuilder {
 
 					// create property
 					if (value != null) {
-						builder.createProperty(property.name.localPart, value)
+						builder.createProperty(property.name.localPart, property.name.namespaceURI, value)
 					}
 				} catch (Exception e) {
 					// value could not be retrieved
-					log.error("Could not retrieve value for column $property.name.localPart", e)
+					if (e instanceof SQLException && e.message != null && e.message.contains('no such column')) {
+						if (!noSuchColumnLogged.contains(property.name.localPart)) {
+							// the column not being there is at max a warning, and he stacktrace is not important
+							log.warn("Could not retrieve value for column ${property.name.localPart}: ${e.message}")
+
+							// don't warn again
+							noSuchColumnLogged.add(property.name.localPart)
+						}
+					}
+					else {
+						log.error("Could not retrieve value for column $property.name.localPart", e)
+					}
 				}
 			}
 		}
