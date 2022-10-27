3df143a51d7e047f0a21f26db3c1cfcbec19982f
kapil-agnihotri <kapil.agnihotri@wetransform.to>
2022-10-27 10:28:15 +0200

    Fix: ClassNotFoundException for org.glassfish.jaxb.runtime.v2.ContextFactory
    ING-3267

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 if [ -z $HALE_GRADLE_CONSOLE ]; then
   HALE_GRADLE_CONSOLE=auto
 fi
-./build/gradlew -p build -Dorg.gradle.jvmargs="-XX:MaxPermSize=512M" --console $HALE_GRADLE_CONSOLE -Dorg.gradle.daemon=true --stacktrace cli -Pargs="$*"
+./build/gradlew -p build --console $HALE_GRADLE_CONSOLE -Dorg.gradle.daemon=true --stacktrace cli -Pargs="$*"
