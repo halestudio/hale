a2d056abcfa483e42204a8a114249c11ed7a7a52
kapil-agnihotri <kapil.agnihotri@wetransform.to>
2022-11-10 15:05:39 +0100

    build: fix running tests for CI
    ING-3267

--- build-hale.sh
+++ build-hale.sh
@@ -3,7 +3,7 @@
 # Default HALE builds
 ./build.sh clean
 ./build.sh product HALE -o linux -a x86_64
-./build.sh product HALE -o linux -a x86
+#./build.sh product HALE -o linux -a x86
 ./build.sh product HALE -o windows -a x86_64
-./build.sh product HALE -o windows -a x86
+#./build.sh product HALE -o windows -a x86
 ./build.sh product HALE -o macosx -a x86_64
