a777f690e42fb8c6121c0944935fa74a24f33500
kapil-agnihotri <kapil.agnihotri@wetransform.to>
2023-05-03 11:13:45 +0200

    refactor: update OptionAccessor with picocli imports
    ING-3616

--- app/plugins/eu.esdihumboldt.hale.app.cli.commands/src/eu/esdihumboldt/hale/app/cli/commands/alignment/ExportJsonCommand.groovy
+++ app/plugins/eu.esdihumboldt.hale.app.cli.commands/src/eu/esdihumboldt/hale/app/cli/commands/alignment/ExportJsonCommand.groovy
@@ -22,6 +22,7 @@ import eu.esdihumboldt.hale.common.core.report.ReportHandler
 import eu.esdihumboldt.hale.common.headless.impl.ProjectTransformationEnvironment
 import eu.esdihumboldt.hale.io.html.svg.mapping.json.JsonMappingExporter
 import eu.esdihumboldt.util.cli.CommandContext
+import groovy.cli.picocli.OptionAccessor
 import groovy.transform.CompileStatic
 
 /**
