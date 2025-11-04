4703e9444d2bfec9b3bc2ff3bccc77c060f1ee08
Florian Esser <fe@wetransform.to>
2025-11-04 21:44:22 +0100

    feat: add IntRange to Groovy restriction whitelist

--- util/plugins/eu.esdihumboldt.util.groovy.sandbox/src/eu/esdihumboldt/util/groovy/sandbox/internal/RestrictiveGroovyInterceptor.java
+++ util/plugins/eu.esdihumboldt.util.groovy.sandbox/src/eu/esdihumboldt/util/groovy/sandbox/internal/RestrictiveGroovyInterceptor.java
@@ -59,6 +59,7 @@ import org.kohsuke.groovy.sandbox.GroovyInterceptor;
 import eu.esdihumboldt.util.groovy.sandbox.GroovyRestrictionException;
 import groovy.lang.Closure;
 import groovy.lang.GString;
+import groovy.lang.IntRange;
 import groovy.lang.MissingPropertyException;
 import groovy.lang.Range;
 import groovy.lang.Script;
@@ -161,6 +162,7 @@ public class RestrictiveGroovyInterceptor extends GroovyInterceptor {
 		allowedClasses.add(LinkedHashSet.class);
 		allowedClasses.add(ArrayList.class);
 		allowedClasses.add(Range.class);
+		allowedClasses.add(IntRange.class);
 		allowedClasses.add(GStringImpl.class);
 
 		// Some more collections
