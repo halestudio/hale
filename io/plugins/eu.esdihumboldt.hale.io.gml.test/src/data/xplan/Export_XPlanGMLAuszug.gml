4bd3e3830e8857044dbed1e824fd1a35830963a6
Emanuela Epure <67077116+emanuelaepure10@users.noreply.github.com>
2026-09-24 10:25:54 +0200

    fix(xplan): add default CRS to XPlanGML (XPlanAuszug) export
    The XPlanAuszug export had no default CRS, failing XPlan Validator rule
    2.1.3.1 ("Angabe eines Standard CRS unbekannt").
    
    Write a gml:boundedBy/gml:Envelope (with srsName and srsDimension="2")
    for the container, with the extent computed from all exported geometries
    using the same CRS conversion as the features. Geometries in another CRS
    are reprojected (or skipped with a warning); if no CRS can be determined,
    srsName is omitted and a warning is reported. Only applies to GML 3.2
    containers that declare boundedBy; other cases keep the default handling.
    
    StreamGmlWriter.writeAdditionalElements now also receives the
    InstanceCollection; the old overload is kept (deprecated). WFS and
    INSPIRE writers are updated accordingly. Adds tests, incl. a real-world
    XPlanAuszug fixture.
    
    Ported from halestudio/hale-core@a03702db, adapted to the org.opengis
    GeoTools API and the eu.esdihumboldt.hale.io.gml.test bundle.
    
    ING-4920

---
+++ io/plugins/eu.esdihumboldt.hale.io.gml.test/src/data/xplan/Export_XPlanGMLAuszug.gml
@@ -0,0 +1,84 @@
+<?xml version="1.0" ?>
+<xplan:XPlanAuszug xmlns:wfs="http://www.opengis.net/wfs/2.0" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xplan="http://www.xplanung.de/xplangml/6/0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ns1="http://www.w3.org/1999/xhtml" xmlns:hfp="http://www.w3.org/2001/XMLSchema-hasFacetAndProperty" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" gml:id="_bdf07d78-ab15-4b58-b44a-d9fcd7bbdb22" xsi:schemaLocation="http://www.xplanung.de/xplangml/6/0 XPlanung-Operationen-BW-Profil.xsd">
+  <gml:featureMember>
+    <xplan:BP_Bereich gml:id="BP_Bereich_BP_082375003027_4463_11_01">
+      <xplan:nummer>1</xplan:nummer>
+      <xplan:geltungsbereich>
+        <gml:Polygon gml:id="_d620a412-a7ce-4766-a223-bf0c87f6e718" srsName="http://www.opengis.net/def/crs/EPSG/0/25832" srsDimension="2">
+          <gml:exterior>
+            <gml:LinearRing>
+              <gml:posList>483430.27 5367746.71 483330.04 5367717.77 483355.94 5367692.64 483375.17 5367564.75 483375.77 5367560.73 483404.17000000004 5367570.285 483424.46551618463 5367475.59301327 483453.9 5367481.14 483438.06 5367554.64 483437.27 5367558.26 483436.63 5367561.19 483440.91 5367562.61 483478.03 5367574.98 483479.46 5367577.61 483450.97 5367682.63 483430.27 5367746.71</gml:posList>
+            </gml:LinearRing>
+          </gml:exterior>
+        </gml:Polygon>
+      </xplan:geltungsbereich>
+      <xplan:refScan>
+        <xplan:XP_ExterneReferenz>
+          <xplan:georefURL>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_geo.tfw</xplan:georefURL>
+          <xplan:referenzName>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_geo.tif</xplan:referenzName>
+          <xplan:referenzURL>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_geo.tif</xplan:referenzURL>
+          <xplan:datum>2024-09-27</xplan:datum>
+        </xplan:XP_ExterneReferenz>
+      </xplan:refScan>
+      <xplan:verfahren>1000</xplan:verfahren>
+      <xplan:gehoertZuPlan xlink:href="#BP_Plan_BP_082375003027_4463_11_01"></xplan:gehoertZuPlan>
+    </xplan:BP_Bereich>
+  </gml:featureMember>
+  <gml:featureMember>
+    <xplan:BP_Plan gml:id="BP_Plan_BP_082375003027_4463_11_01">
+      <xplan:name>Sondergebiet Container- und Lagerplatz 1 Aenderung</xplan:name>
+      <xplan:beschreibung></xplan:beschreibung>
+      <xplan:genehmigungsDatum>2024-07-24</xplan:genehmigungsDatum>
+      <xplan:raeumlicherGeltungsbereich>
+        <gml:Polygon gml:id="_720a510c-5356-46c0-b261-49e0b9971706" srsName="http://www.opengis.net/def/crs/EPSG/0/25832" srsDimension="2">
+          <gml:exterior>
+            <gml:LinearRing>
+              <gml:posList>483430.27 5367746.71 483330.04 5367717.77 483355.94 5367692.64 483375.17 5367564.75 483375.77 5367560.73 483404.17000000004 5367570.285 483424.46551618463 5367475.59301327 483453.9 5367481.14 483438.06 5367554.64 483437.27 5367558.26 483436.63 5367561.19 483440.91 5367562.61 483478.03 5367574.98 483479.46 5367577.61 483450.97 5367682.63 483430.27 5367746.71</gml:posList>
+            </gml:LinearRing>
+          </gml:exterior>
+        </gml:Polygon>
+      </xplan:raeumlicherGeltungsbereich>
+      <xplan:externeReferenz>
+        <xplan:XP_SpezExterneReferenz>
+          <xplan:referenzName>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_Legende.pdf</xplan:referenzName>
+          <xplan:referenzURL>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_Legende.pdf</xplan:referenzURL>
+          <xplan:datum>2024-09-27</xplan:datum>
+          <xplan:typ>1020</xplan:typ>
+        </xplan:XP_SpezExterneReferenz>
+      </xplan:externeReferenz>
+      <xplan:externeReferenz>
+        <xplan:XP_SpezExterneReferenz>
+          <xplan:referenzName>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_Plan.pdf</xplan:referenzName>
+          <xplan:referenzURL>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_Plan.pdf</xplan:referenzURL>
+          <xplan:datum>2024-09-27</xplan:datum>
+          <xplan:typ>1030</xplan:typ>
+        </xplan:XP_SpezExterneReferenz>
+      </xplan:externeReferenz>
+      <xplan:texte xlink:href="#BP_TextAbschnitt_BP_082375003027_4463_11_01"></xplan:texte>
+      <xplan:gemeinde>
+        <xplan:XP_Gemeinde>
+          <xplan:rs>082375003027</xplan:rs>
+          <xplan:gemeindeName>Gemeinde Eutingen im Gäu</xplan:gemeindeName>
+          <xplan:ortsteilName>Weitingen</xplan:ortsteilName>
+        </xplan:XP_Gemeinde>
+      </xplan:gemeinde>
+      <xplan:planArt>10001</xplan:planArt>
+      <xplan:rechtsstand>4000</xplan:rechtsstand>
+      <xplan:aufstellungsbeschlussDatum>2024-07-23</xplan:aufstellungsbeschlussDatum>
+      <xplan:inkrafttretensDatum>2024-09-27</xplan:inkrafttretensDatum>
+      <xplan:bereich xlink:href="#BP_Bereich_BP_082375003027_4463_11_01"></xplan:bereich>
+    </xplan:BP_Plan>
+  </gml:featureMember>
+  <gml:featureMember>
+    <xplan:XP_TextAbschnitt gml:id="BP_TextAbschnitt_BP_082375003027_4463_11_01">
+      <xplan:refText>
+        <xplan:XP_ExterneReferenz>
+          <xplan:referenzName>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_Textteil.pdf</xplan:referenzName>
+          <xplan:referenzURL>082375003027_4463_11_01_Sondergebiet_Container_Lagerplatz_1A_Textteil.pdf</xplan:referenzURL>
+          <xplan:datum>2024-07-24</xplan:datum>
+        </xplan:XP_ExterneReferenz>
+      </xplan:refText>
+      <xplan:rechtscharakter>1000</xplan:rechtscharakter>
+    </xplan:XP_TextAbschnitt>
+  </gml:featureMember>
+</xplan:XPlanAuszug>
--- io/plugins/eu.esdihumboldt.hale.io.gml/src/eu/esdihumboldt/hale/io/gml/writer/XPlanGmlInstanceWriter.java
+++ io/plugins/eu.esdihumboldt.hale.io.gml/src/eu/esdihumboldt/hale/io/gml/writer/XPlanGmlInstanceWriter.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -31,18 +32,31 @@ import java.util.Set;
 
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.geom.Geometry;
+import org.opengis.referencing.operation.MathTransform;
 
 import eu.esdihumboldt.hale.common.core.io.IOProviderConfigurationException;
 import eu.esdihumboldt.hale.common.core.io.ProgressIndicator;
 import eu.esdihumboldt.hale.common.core.io.report.IOReport;
 import eu.esdihumboldt.hale.common.core.io.report.IOReporter;
+import eu.esdihumboldt.hale.common.instance.geometry.GeometryFinder;
 import eu.esdihumboldt.hale.common.instance.graph.reference.ReferenceGraph;
 import eu.esdihumboldt.hale.common.instance.graph.reference.impl.XMLInspector;
+import eu.esdihumboldt.hale.common.instance.helper.DepthFirstInstanceTraverser;
+import eu.esdihumboldt.hale.common.instance.helper.InstanceTraverser;
 import eu.esdihumboldt.hale.common.instance.model.Instance;
 import eu.esdihumboldt.hale.common.instance.model.InstanceCollection;
 import eu.esdihumboldt.hale.common.instance.model.ResourceIterator;
 import eu.esdihumboldt.hale.common.instance.model.impl.DefaultInstanceCollection;
 import eu.esdihumboldt.hale.common.instance.model.impl.MultiInstanceCollection;
+import eu.esdihumboldt.hale.common.schema.geometry.CRSDefinition;
+import eu.esdihumboldt.hale.common.schema.geometry.GeometryProperty;
+import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.AbstractFlag;
 import eu.esdihumboldt.hale.io.gml.writer.internal.DefaultMultipartHandler;
@@ -50,6 +64,7 @@ import eu.esdihumboldt.hale.io.gml.writer.internal.MultipartHandler;
 import eu.esdihumboldt.hale.io.gml.writer.internal.StreamGmlWriter;
 import eu.esdihumboldt.hale.io.xsd.model.XmlElement;
 import eu.esdihumboldt.util.Pair;
+import eu.esdihumboldt.util.format.DecimalFormatUtil;
 
 /**
  * Writes instances to a XPlanGML XPlanAuszug
@@ -100,6 +115,264 @@ public class XPlanGmlInstanceWriter extends StreamGmlWriter {
 
 	private boolean isPartitionByPlanConfigured() {
 		return getParameter(PARAM_PARTITION_BY_PLAN).as(Boolean.class, false);
+	}
+
+	/**
+	 * Writes a {@code gml:boundedBy} with a {@code gml:Envelope} for the
+	 * XPlanAuszug container. This defines the mandatory default CRS of the XPlanung
+	 * model through the envelope's {@code srsName} (XPlanung conformance rule
+	 * 2.1.3.1).
+	 *
+	 * @see StreamGmlWriter#writeAdditionalElements(XMLStreamWriter,
+	 *      InstanceCollection, TypeDefinition, IOReporter)
+	 */
+	@Override
+	protected void writeAdditionalElements(XMLStreamWriter writer, InstanceCollection instances,
+			TypeDefinition containerDefinition, IOReporter reporter) throws XMLStreamException {
+		// The gml:boundedBy must precede the feature members in the container, so
+		// the extent is computed in a separate pass over the instances here. This
+		// intentionally reads (and reprojects) the geometries a second time rather
+		// than buffering all of them in memory, which keeps the streaming writer's
+		// memory profile intact for large plans.
+		boolean written = supportsBoundedByEnvelope(containerDefinition)
+				&& writeBoundedBy(writer, instances, reporter);
+
+		if (!written) {
+			// Fall back to the default handling (e.g. GML 2 boundedBy) if the
+			// container's schema doesn't declare gml:boundedBy, the target GML
+			// namespace doesn't support the Envelope content model written by
+			// writeBoundedBy, or no extent could be determined from the instances.
+			super.writeAdditionalElements(writer, instances, containerDefinition, reporter);
+		}
+	}
+
+	/**
+	 * @param containerDefinition the container type definition
+	 * @return <code>true</code> if the container's schema declares a
+	 *         {@code gml:boundedBy} child and the target GML namespace is GML 3.2
+	 *         ({@link #NS_GML_32}). GML 2 and GML 3.0/3.1 all share the
+	 *         {@link #NS_GML} namespace returned by {@link #getGmlNs()} for those
+	 *         versions, but unlike GML 3.2 their {@code gml:Envelope} does not
+	 *         support {@code lowerCorner}/{@code upperCorner}, so
+	 *         {@link #writeBoundedBy(XMLStreamWriter, InstanceCollection, IOReporter)}
+	 *         must not be used for them
+	 */
+	private boolean supportsBoundedByEnvelope(TypeDefinition containerDefinition) {
+		if (!NS_GML_32.equals(getGmlNs())) {
+			return false;
+		}
+
+		// containerDefinition.getChild(QName) requires an exact (namespace
+		// qualified) match, but boundedBy may be declared/inherited in either the
+		// GML 3.2 or GML 2/3.0/3.1 namespace depending on which schema the
+		// container type derives from, so match on the local name instead; this
+		// includes children inherited from a super type (e.g. gml:AbstractFeatureType),
+		// see TypeDefinition#getChildren()
+		for (ChildDefinition<?> child : containerDefinition.getChildren()) {
+			if ("boundedBy".equals(child.getName().getLocalPart()) //$NON-NLS-1$
+					&& child.asProperty() != null) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Determine the overall extent of the given instances and write it as a
+	 * {@code gml:boundedBy} containing a {@code gml:Envelope}. The envelope's
+	 * {@code srsName} provides the default CRS required for a valid XPlanung model.
+	 *
+	 * @param writer the XML stream writer
+	 * @param instances the instances written to the container
+	 * @param reporter the reporter
+	 * @return <code>true</code> if a bounding envelope was written,
+	 *         <code>false</code> if no extent could be determined (e.g. because no
+	 *         geometries were present)
+	 * @throws XMLStreamException if writing the elements fails
+	 */
+	private boolean writeBoundedBy(XMLStreamWriter writer, InstanceCollection instances,
+			IOReporter reporter) throws XMLStreamException {
+		final Envelope envelope = new Envelope();
+		CRSDefinition crsDef = null;
+		String crsCode = null;
+		boolean mixedCrs = false;
+		boolean mixedCrsGeometrySkipped = false;
+		boolean conversionFailed = false;
+
+		// If a target CRS is configured, every geometry below is expected to end
+		// up in that CRS; used to detect a silently failed conversion (see below).
+		final CRSDefinition targetCrs = getTargetCRS();
+		final boolean hasUsableTargetCrs = targetCrs != null && targetCrs.getCRS() != null;
+		final String targetCrsCode = extractCode(targetCrs);
+
+		final InstanceTraverser traverser = new DepthFirstInstanceTraverser();
+		try (ResourceIterator<Instance> it = instances.iterator()) {
+			while (it.hasNext()) {
+				Instance inst = it.next();
+
+				GeometryFinder finder = new GeometryFinder(getTargetCRS());
+				traverser.traverse(inst, finder);
+
+				for (GeometryProperty<?> geomProperty : finder.getGeometries()) {
+					// Extract the geometry using the same conversion (e.g. to the
+					// target CRS) that is applied when the geometry is written, so
+					// that the envelope and its srsName are consistent with the
+					// feature geometries. The reporter is intentionally not passed
+					// on here to avoid duplicating the per-feature error that gets
+					// reported below when the feature geometry itself is written;
+					// instead, a failed conversion is detected explicitly below,
+					// since AbstractGeoInstanceWriter#convertGeometry otherwise
+					// falls back to returning the untransformed geometry silently.
+					Pair<Geometry, CRSDefinition> pair = extractGeometry(geomProperty, true, null);
+					if (pair == null) {
+						continue;
+					}
+
+					Geometry geom = pair.getFirst();
+					if (geom == null || geom.isEmpty()) {
+						continue;
+					}
+
+					CRSDefinition geomCrs = pair.getSecond();
+
+					if (hasUsableTargetCrs) {
+						String returnedCode = extractCode(geomCrs);
+						boolean convertedToTarget = targetCrsCode == null ? returnedCode == null
+								: targetCrsCode.equals(returnedCode);
+						if (!convertedToTarget) {
+							// The conversion to the target CRS silently failed for
+							// this geometry; its coordinates are not comparable to
+							// the other, correctly reprojected geometries, so it
+							// cannot be used to compute the envelope.
+							conversionFailed = true;
+							continue;
+						}
+					}
+
+					if (crsDef == null) {
+						// first geometry encountered defines the envelope's CRS
+						crsDef = geomCrs;
+						crsCode = extractCode(geomCrs);
+						envelope.expandToInclude(geom.getEnvelopeInternal());
+						continue;
+					}
+
+					String otherCode = extractCode(geomCrs);
+					boolean sameCrs = crsCode == null ? otherCode == null
+							: crsCode.equals(otherCode);
+					if (sameCrs) {
+						envelope.expandToInclude(geom.getEnvelopeInternal());
+						continue;
+					}
+
+					// Geometry in a different CRS than the envelope's reference
+					// CRS. This only happens when no target CRS is configured to
+					// unify them; with a target CRS configured, geometries either
+					// end up in that CRS above or are excluded as a conversion
+					// failure. Reproject it to the envelope's reference CRS so its
+					// extent stays numerically correct; if that is not possible,
+					// exclude the geometry from the envelope rather than mixing
+					// incompatible coordinates.
+					mixedCrs = true;
+					Geometry reprojected = reprojectGeometry(geom, geomCrs, crsDef, reporter);
+					if (reprojected != null) {
+						envelope.expandToInclude(reprojected.getEnvelopeInternal());
+					}
+					else {
+						mixedCrsGeometrySkipped = true;
+					}
+				}
+			}
+		}
+
+		if (conversionFailed) {
+			// A geometry could not be reprojected to the configured target CRS,
+			// so the envelope cannot be reliably computed: an incomplete or
+			// wrong extent would be worse than none. The per-feature error for
+			// the affected geometry is reported separately when it is written.
+			reporter.error(
+					"Could not determine the XPlanAuszug bounding envelope: at least one geometry could not be reprojected to the configured target CRS, so its extent cannot be reliably included. The mandatory default CRS (srsName) is omitted rather than risk an incorrect extent."); //$NON-NLS-1$
+			return false;
+		}
+
+		if (envelope.isNull()) {
+			return false;
+		}
+
+		final String gmlNs = getGmlNs();
+		final String srsName = extractCode(crsDef);
+		final DecimalFormat formatter = getCoordinateFormatter();
+
+		if (srsName == null) {
+			reporter.warn(
+					"No CRS could be determined for the XPlanAuszug bounding envelope; the mandatory default CRS (srsName) is missing and the output will not satisfy XPlanung conformance rule 2.1.3.1. Configure a target CRS or ensure the source geometries carry a CRS."); //$NON-NLS-1$
+		}
+		else if (mixedCrs) {
+			reporter.warn(String.format(
+					"The exported geometries use more than one CRS but no target CRS is configured to unify them. Geometries in a CRS other than the envelope's reference CRS (%s) were reprojected for the XPlanAuszug bounding envelope%s. Configure a target CRS to obtain consistent output.", //$NON-NLS-1$
+					srsName,
+					mixedCrsGeometrySkipped
+							? "; some of them could not be reprojected and were excluded from the extent" //$NON-NLS-1$
+							: ""));
+		}
+
+		writer.writeStartElement(gmlNs, "boundedBy"); //$NON-NLS-1$
+		writer.writeStartElement(gmlNs, "Envelope"); //$NON-NLS-1$
+		if (srsName != null) {
+			writer.writeAttribute("srsName", srsName); //$NON-NLS-1$
+		}
+		// The envelope is computed from a 2D JTS Envelope, so it always has two
+		// coordinate values per corner, regardless of whether the CRS itself is
+		// 3D; without srsDimension a 3D srsName would imply a dimension mismatch
+		// with the two-value lowerCorner/upperCorner written below.
+		writer.writeAttribute("srsDimension", "2"); //$NON-NLS-1$ //$NON-NLS-2$
+
+		writer.writeStartElement(gmlNs, "lowerCorner"); //$NON-NLS-1$
+		writer.writeCharacters(DecimalFormatUtil.applyFormatter(envelope.getMinX(), formatter) + " " //$NON-NLS-1$
+				+ DecimalFormatUtil.applyFormatter(envelope.getMinY(), formatter));
+		writer.writeEndElement();
+
+		writer.writeStartElement(gmlNs, "upperCorner"); //$NON-NLS-1$
+		writer.writeCharacters(DecimalFormatUtil.applyFormatter(envelope.getMaxX(), formatter) + " " //$NON-NLS-1$
+				+ DecimalFormatUtil.applyFormatter(envelope.getMaxY(), formatter));
+		writer.writeEndElement();
+
+		writer.writeEndElement(); // Envelope
+		writer.writeEndElement(); // boundedBy
+
+		return true;
+	}
+
+	/**
+	 * Reproject a geometry from its source CRS to the given target CRS. Used to
+	 * unify geometries in different CRS for the bounding envelope when no target
+	 * CRS is configured for the writer (in which case
+	 * {@link #extractGeometry(Object, boolean, IOReporter)} does not already
+	 * reproject them).
+	 *
+	 * @param geom the geometry to reproject
+	 * @param sourceCrs the CRS of the geometry
+	 * @param targetCrs the CRS to reproject to
+	 * @param reporter the reporter
+	 * @return the reprojected geometry, or <code>null</code> if it could not be
+	 *         reprojected (e.g. because a CRS is not resolvable or no transform
+	 *         could be found)
+	 */
+	private Geometry reprojectGeometry(Geometry geom, CRSDefinition sourceCrs,
+			CRSDefinition targetCrs, IOReporter reporter) {
+		if (sourceCrs == null || sourceCrs.getCRS() == null || targetCrs == null
+				|| targetCrs.getCRS() == null) {
+			return null;
+		}
+		try {
+			MathTransform transform = CRS.findMathTransform(sourceCrs.getCRS(), targetCrs.getCRS());
+			return JTS.transform(geom, transform);
+		} catch (Exception e) {
+			reporter.warn(
+					"Could not reproject a geometry to the XPlanAuszug bounding envelope's CRS; it was excluded from the extent computation", //$NON-NLS-1$
+					e);
+			return null;
+		}
 	}
 
 	/**
